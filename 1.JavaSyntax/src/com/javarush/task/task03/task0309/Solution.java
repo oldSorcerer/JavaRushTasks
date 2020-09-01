836c10accd7ae0a11c624b893779b0f193057730
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-01 20:40:47 +0300

    add solution task 3 l 4

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0309;
+
+/* 
+Сумма 5 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(1+2);
+        System.out.println(1+2+3);
+        System.out.println(1+2+3+4);
+        System.out.println(1+2+3+4+5);
+
+    }
+}
