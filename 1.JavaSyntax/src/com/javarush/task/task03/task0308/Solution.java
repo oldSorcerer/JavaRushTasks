e638a13817b8bbfeca52c451e90ffbf1d58b7a70
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-01 20:41:03 +0300

    add solution task 3 l 4

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(1*2*3*4*5*6*7*8*9*10);
+
+    }
+}
