4050909503ae0a58dccb5d6137d7b55424cc0b8e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-01 20:40:24 +0300

    add solution task 3 l 4

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0305;
+
+/* 
+Я так давно родился
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("MAY 1 2012");
+
+    }
+}
