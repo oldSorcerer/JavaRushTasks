d990634924ef132bba89f3b0e60d99c09cfb0d20
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-01 20:40:09 +0300

    add solution task 3 l 3

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0304;
+
+/* 
+Задача на проценты
+*/
+
+public class Solution {
+    public static double addTenPercent(int i) {
+        return i * 1.1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(9));
+    }
+}
