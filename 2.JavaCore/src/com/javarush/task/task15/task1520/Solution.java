5c3c0d6e92130e6744f1b4ca1a22fb8c0fd6e3ef
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-01 21:02:28 +0300

    add solution task

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1520/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task15.task1520;
+
+/* 
+Тренировка мозга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Duck duck = new Duck();
+        Util.fly(duck);
+        Util.move(duck);
+    }
+
+    public static class Duck implements CanFly, CanMove {
+        @Override
+        public void doAction() {
+            System.out.println("Flying");
+        }
+
+        @Override
+        public void doAnotherAction() {
+            System.out.println("Moving");
+        }
+    }
+
+    public static class Util {
+        static void fly(CanFly animal) {
+            animal.doAction();
+        }
+
+        static void move(CanMove animal) {
+            animal.doAnotherAction();
+        }
+    }
+
+    public static interface CanFly {
+        void doAction();
+    }
+
+    public static interface CanMove {
+        void doAnotherAction();
+    }
+}
