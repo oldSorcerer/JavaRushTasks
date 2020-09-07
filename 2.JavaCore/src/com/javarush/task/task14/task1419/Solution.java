bb06d435cf1dacb17fd745e56f22b379487d919c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-08 00:45:39 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task14.task1419;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Нашествие исключений
+*/
+
+public class Solution {
+    public static List<Exception> exceptions = new ArrayList<Exception>();
+
+    public static void main(String[] args) {
+        initExceptions();
+
+        for (Exception exception : exceptions) {
+            System.out.println(exception);
+        }
+    }
+
+    private static void initExceptions() {   //the first exception
+        try {
+            float i = 1 / 0;
+
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        //напишите тут ваш код
+
+    }
+}
