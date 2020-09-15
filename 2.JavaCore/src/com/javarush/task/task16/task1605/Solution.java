4bec0d334bfdced92ecee9ef9ef79f8a69bcf9b9
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-15 15:25:12 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1605/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task16.task1605;
+
+import java.util.Date;
+
+/* 
+Поговорим о музыке
+*/
+
+public class Solution {
+    public static int delay = 1000;
+
+    public static void main(String[] args) {
+        Thread violin = new Thread(new Violin("Player"));
+        violin.start();
+    }
+
+    public static void sleepNSeconds(int n) {
+        try {
+            Thread.sleep(n * delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public interface MusicalInstrument extends Runnable {
+        Date startPlaying();
+
+        Date stopPlaying();
+    }
+
+    public static class Violin implements MusicalInstrument {
+
+        private String owner;
+
+        public Violin(String owner) {
+            this.owner = owner;
+        }
+
+        public Date startPlaying() {
+            System.out.println(this.owner + " is starting to play");
+            return new Date();
+        }
+
+        public Date stopPlaying() {
+            System.out.println(this.owner + " is stopping playing");
+            return new Date();
+        }
+
+        @Override
+        public void run() {
+            Date start = startPlaying();
+            sleepNSeconds(1);
+            Date stop = stopPlaying();
+            System.out.format("Playing %s ms", stop.getTime() - start.getTime());
+        }
+    }
+}
