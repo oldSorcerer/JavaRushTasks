a2a7277f63046290f98afdbf711c072987bfec03
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-15 14:18:58 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<>(5);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            list.add(new Thread(new SpecialThread()));
+        }
+
+    }
+
+    public static class SpecialThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1609/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1609;
+
+/* 
+Справедливость
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Mouse alpha = new Mouse("#1");
+        //alpha.join();
+        Mouse mouse1 = new Mouse("#2");
+        Mouse mouse2 = new Mouse("#3");
+    }
+
+    private static void eat() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    public static class Mouse extends Thread {
+        public Mouse(String name) {
+            super(name);
+            start();
+        }
+
+        public void run() {
+            System.out.println(getName() + " is starting to eat");
+            eat();
+            System.out.println(getName() + " has finished eating");
+        }
+    }
+}
