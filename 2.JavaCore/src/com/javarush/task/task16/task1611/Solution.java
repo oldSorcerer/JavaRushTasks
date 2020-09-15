2775afd0a0a96c574d8f77e1bd6597d812668433
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-15 22:43:51 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1611/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task16.task1611;
+
+/* 
+Часы
+*/
+
+public class Solution {
+    public static volatile boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Clock clock = new Clock();
+        Thread.sleep(2000);
+        isStopped = true;
+        System.out.println("The clock has to be stopped");
+        Thread.sleep(2000);
+        System.out.println("Double-check");
+    }
+
+    public static class Clock extends Thread {
+        public Clock() {
+            setPriority(MAX_PRIORITY);
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isStopped) {
+                    printTikTak();
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void printTikTak() throws InterruptedException {
+            System.out.println("Tik");
+            Thread.sleep(500);
+            System.out.println("Tak");
+            Thread.sleep(500);
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+
+        TestThread testThread = new TestThread();
+        testThread.start();
+        testThread.interrupt();
+    }
+
+    public static class TestThread extends Thread {
+        public void run() {
+
+        }
+
+    }
+}
