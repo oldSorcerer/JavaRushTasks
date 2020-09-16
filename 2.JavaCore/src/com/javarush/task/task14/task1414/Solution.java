744a0d0ee01413081fb4e93a35a176f819ba1ebd
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-16 19:18:00 +0300

    add sol

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -9,14 +9,19 @@ MovieFactory
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //ввести с консоли несколько ключей (строк), пункт 7
 
-        /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
-        */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true) {
+            String key = reader.readLine();
+            if (key.equals("soapOpera") || key.equals("cartoon") || key.equals("thriller")) {
+                Movie movie = MovieFactory.getMovie(key);
+                System.out.println(movie.getClass().getSimpleName());
+            } else {
+                MovieFactory.getMovie(key);
+                break;
+            }
+        }
     }
 
     static class MovieFactory {
@@ -24,12 +29,12 @@ public class Solution {
         static Movie getMovie(String key) {
             Movie movie = null;
 
-            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
-            if ("soapOpera".equals(key)) {
+            if ("soapOpera".equals(key))
                 movie = new SoapOpera();
-            }
-
-            //напишите тут ваш код, пункты 5,6
+            if ("cartoon".equals(key))
+                movie = new Cartoon();
+            if ("thriller".equals(key))
+                movie = new Thriller();
 
             return movie;
         }
@@ -41,5 +46,10 @@ public class Solution {
     static class SoapOpera extends Movie {
     }
 
-    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
+
 }
