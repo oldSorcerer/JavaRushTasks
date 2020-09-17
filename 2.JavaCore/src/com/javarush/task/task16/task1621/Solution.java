54450b05a87e087515d0589801d71c1de8056fc7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-09-17 09:11:58 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task16.task1621;
+
+/* 
+Thread.currentThread - всегда возвращает текущую нить
+*/
+
+public class Solution {
+    static int count = 5;
+
+    public static void main(String[] args) {
+        ThreadNamePrinter tnp = new ThreadNamePrinter();
+        tnp.start();
+        for (int i = 0; i < count; i++) {
+            tnp.printMsg();
+        }
+    }
+
+    public static class ThreadNamePrinter extends Thread {
+        public void run() {
+            for (int i = 0; i < count; i++) {
+                printMsg();
+            }
+        }
+
+        public void printMsg() {
+            Thread t = Thread.currentThread();
+            String name = t.getName();
+            System.out.println("Name=" + name);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
