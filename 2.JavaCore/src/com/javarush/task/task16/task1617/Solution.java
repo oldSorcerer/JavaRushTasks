ae54b8b31717145e5206c8543d94634d46d12f21
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-05 20:38:59 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 2;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (numSeconds > 0) {
+                    System.out.print(numSeconds + " ");
+                    Thread.sleep(1000);
+                    numSeconds--;
+                }
+                System.out.print("Марш!");
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+            }
+        }
+    }
+}
