bb76b3bd9e040166171017348d524557681aab35
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-11 12:25:57 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        TestThread.isActive = false;
+
+    }
+
+    public static class TestThread implements Runnable {
+
+        public static boolean isActive = true;
+
+        public void run() {
+            while (isActive) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task17/task1718/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task17.task1718;
+
+/* 
+Глажка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person diana = new Person("Diana");
+        Person igor = new Person("Igor");
+        diana.start();
+        igor.start();
+    }
+
+    public static class Person extends Thread { //Человек
+
+        public Person(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            Iron iron = takeIron();
+            Clothes clothes = takeClothes();
+            iron(iron, clothes);
+            returnIron();
+        }
+
+        protected Iron takeIron() {
+            System.out.println("Taking the iron");
+            return new Iron();
+        }
+
+        protected Iron returnIron() {
+            System.out.println("Returning the iron");
+            return new Iron();
+        }
+
+        protected Clothes takeClothes() {
+            return new Clothes("T-shirt");
+        }
+
+        protected void iron(Iron iron, Clothes clothes) {
+            System.out.println(getName() + " is ironing a " + clothes.name);
+        }
+    }
+
+    public static class Iron {
+    } //Утюг
+
+    public static class Clothes {//Одежда
+        String name;
+
+        public Clothes(String name) {
+            this.name = name;
+        }
+    }
+}
