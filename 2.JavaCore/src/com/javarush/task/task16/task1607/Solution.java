31e704926e45ac732f348cbc3c9d660a18ade341
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-13 19:51:02 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1607/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task16.task1607;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Horse Racing
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = prepareHorsesAndStart(10);
+        while (calculateHorsesFinished(horses) != horses.size()) {
+        }
+
+    }
+
+    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
+        int finishedCount = 0;
+
+        for (Horse hors : horses) {
+            if (hors.isFinished()) {
+                finishedCount++;
+            } else{
+                System.out.println("Waiting for " + hors.getName());
+                hors.join();
+            }
+        }
+        return finishedCount;
+    }
+
+    public static List<Horse> prepareHorsesAndStart(int horseCount) {
+        List<Horse> horses = new ArrayList<>(horseCount);
+        String number;
+        for (int i = 1; i < horseCount + 1; i++) {
+            number = i < 10 ? ("0" + i) : "" + i;
+            horses.add(new Horse("Horse_" + number));
+        }
+
+        for (int i = 0; i < horseCount; i++) {
+            horses.get(i).start();
+        }
+        return horses;
+    }
+
+    public static class Horse extends Thread {
+
+        private boolean isFinished;
+
+        public Horse(String name) {
+            super(name);
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public void run() {
+            String s = "";
+            for (int i = 0; i < 1001; i++) {   // Delay
+                s += "" + i;
+                if (i == 1000) {
+                    s = " has finished the race!";
+                    System.out.println(getName() + s);
+                    isFinished = true;
+                }
+            }
+        }
+    }
+}
