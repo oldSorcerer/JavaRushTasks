1b8b5906d1a485cb7c4c18fb2220f6dcbf2cd85b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-18 14:33:29 +0300

    add sol

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task16.task1623;
+
+/* 
+Рекурсивное создание нитей
+*/
+
+public class Solution {
+    static int count = 15;
+    static volatile int createdThreadCount;
+
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
+    }
+
+    public static class GenerateThread extends Thread {
+
+        public GenerateThread() {
+            super(String.valueOf(++createdThreadCount));
+            start();
+        }
+
+        public void run() {
+            if (createdThreadCount < count) {
+                System.out.println(new GenerateThread());
+            }
+        }
+
+        @Override
+        public String toString() {
+            return getName() + " created";
+        }
+    }
+}
