2b599224fb903c67c35b018b1d73eb84e29a6430
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-18 20:03:38 +0300

    add sol

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -1,4 +1,52 @@
 package com.javarush.task.task21.task2113;
 
 public class Horse {
+
+    public String name;
+    public double speed;
+    public double distance;
+
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+
+    public void move() {
+        distance += (speed * Math.random());
+    }
+
+    public void print() {
+        for (int i = 0; i < (int) distance; i++) {
+            System.out.print(".");
+        }
+        System.out.println(name);
+
+    }
 }
