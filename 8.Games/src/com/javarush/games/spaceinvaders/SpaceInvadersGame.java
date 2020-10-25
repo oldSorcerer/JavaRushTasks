f8dfab1eef9693d8e3958c9f5c645d0deb3632ee
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-10-25 22:17:24 +0300

    add sol

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,13 +1,20 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game {
     
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
 
+    private List<Star> stars;
+
     private void createGame() {
+        createStars();
         drawScene();
     }
     
@@ -20,10 +27,20 @@ public class SpaceInvadersGame extends Game {
             for (int x = 0; x < HEIGHT; x++) {
                 setCellValueEx(x, y, Color.BLACK, "");
             }
-
+        }
+        for (Star star : stars) {
+            star.draw(this);
         }
     }
-    
+
+    private void createStars() {
+        stars = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            stars.add(new Star(i + 5.5, i + 4.5));
+        }
+
+    }
+
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
@@ -1,8 +1,16 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.*;
+
 public class Star extends GameObject {
+
+    private final static String STAR_SIGN = "\u2605";
 
     public Star(double x, double y) {
         super(x, y);
+    }
+
+    public void draw(Game game) {
+        game.setCellValueEx((int)x, (int)y, Color.NONE, STAR_SIGN, Color.WHEAT, 100);
     }
 }
