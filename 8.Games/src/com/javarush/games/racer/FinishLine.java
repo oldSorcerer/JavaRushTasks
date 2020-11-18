824b8cebf54157708a5c7535e259f707150ed902
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-11-19 00:23:32 +0300

    add solution Racer step 22 of 28

---
+++ 8.Games/src/com/javarush/games/racer/FinishLine.java
@@ -0,0 +1,20 @@
+package com.javarush.games.racer;
+
+public class FinishLine extends GameObject {
+
+    private boolean isVisible = false;
+
+    public FinishLine() {
+        super(RacerGame.ROADSIDE_WIDTH, -1 * ShapeMatrix.FINISH_LINE.length, ShapeMatrix.FINISH_LINE);
+    }
+
+    public void show() {
+        isVisible = true;
+    }
+
+    public void move(int boost) {
+        if (isVisible) {
+            y += boost;
+        }
+    }
+}
