0ca70b74180623d30f1a6a14ff4f9333f8fff45f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-11-24 18:22:08 +0300

    add solution for game Space Invaders step 5 of 34

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/GameObject.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/GameObject.java
@@ -1,11 +1,33 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.*;
+
 public class GameObject {
     public double x;
     public double y;
+    public int[][] matrix;
+    public int width;
+    public int height;
 
     public GameObject(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+        width = matrix[0].length;
+        height = matrix.length;
+    }
+
+    public void draw(Game game) {
+        for (int j = 0; j < width ; j++) {  // x
+            for (int i = 0; i < height; i++) { // y
+                game.setCellValueEx((int)x + j, (int)y + i, Color.values()[matrix[i][j]], "");
+            }
+        }
+        
+        
+    }
+
 }
