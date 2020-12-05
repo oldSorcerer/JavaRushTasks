f07753d199fc74db2d6153e135ea1728fd01f917
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-05 19:38:40 +0300

    add solution for game Space Invaders step 30 of 34

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
@@ -18,5 +19,27 @@ public class Boss extends EnemyShip {
         if (frameCount%10 == 0 || !isAlive) {
             super.nextFrame();
         }
+    }
+
+    @Override
+    public Bullet fire() {
+        if (!isAlive) {
+            return null;
+        }
+        if (matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) {
+            return new Bullet(x + 6, y + height, Direction.DOWN);
+        } else
+            return new Bullet(x, y + height, Direction.DOWN);
+    }
+
+    @Override
+    public void kill() {
+        if (!isAlive) {
+            return;
+        }
+        isAlive = false;
+        setAnimatedView(ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
     }
 }
