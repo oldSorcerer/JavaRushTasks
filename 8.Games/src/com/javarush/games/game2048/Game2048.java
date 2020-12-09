221092a163566bb6e835506d82c705fb0f498a46
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-09 15:45:21 +0300

    add solution for game 2048 step 7 of 18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -78,4 +78,20 @@ public class Game2048 extends Game{
         String strValue = value > 0 ? "" + value : "";
         setCellValueEx(x, y,newColor, strValue);
     }
+
+    private boolean compressRow(int[] row) {
+        int insertPosition = 0;
+        boolean result = false;
+        for (int x = 0; x < SIDE; x++) {
+            if (row[x] > 0 ) {
+                if (x  != insertPosition) {
+                    row[insertPosition] = row[x];
+                    row[x] = 0;
+                    result = true;
+                }
+                insertPosition++;
+            }
+        }
+        return result;
+    }
 }
