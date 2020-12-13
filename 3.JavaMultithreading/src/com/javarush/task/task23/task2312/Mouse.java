92c3fe17e0f3ec4e9619fdb6f9a51c0b62cdf025
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-13 18:31:09 +0300

    add solution for task 2312 step 4 of 18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -5,5 +5,18 @@ public class Mouse {
     private int x;
     private int y;
 
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
 
 }
