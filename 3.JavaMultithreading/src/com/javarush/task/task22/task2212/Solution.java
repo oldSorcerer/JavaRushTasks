a70b60906643bba6b792cfee9e557761c52176f4
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-13 18:14:26 +0300

    add task 2212

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task22.task2212;
+
+/* 
+Проверка номера телефона
+*/
+
+public class Solution {
+    public static boolean checkTelNumber(String telNumber) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
