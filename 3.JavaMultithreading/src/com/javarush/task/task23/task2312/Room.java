dc255a8018938c6f2b6d444ae4e6ceed104d2c26
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-13 18:15:08 +0300

    add solution for task 2312 step 1 of 18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task23.task2312;
+
+public class Room {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task23.task2312;
+
+public class Snake {
+}
