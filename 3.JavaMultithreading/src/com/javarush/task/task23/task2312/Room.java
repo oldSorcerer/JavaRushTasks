6f9b35ef46b782ffc5df8e760a6dcbc8abe351fc
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-14 11:02:29 +0300

    add solution for task 2312 step 9 of 18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -2,6 +2,43 @@ package com.javarush.task.task23.task2312;
 
 public class Room {
 
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public Snake getSnake() {
+        return snake;
+    }
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
     public static void main(String[] args) {
         
     }
