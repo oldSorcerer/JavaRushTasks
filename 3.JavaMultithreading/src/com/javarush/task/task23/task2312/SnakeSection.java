2e18b83fd672fa82971b844a030e297fb59b8613
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-12-20 16:15:37 +0300

    add solution for task 2312 step 16 of 18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -16,4 +16,14 @@ public class SnakeSection {
     public int getY() {
         return y;
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
