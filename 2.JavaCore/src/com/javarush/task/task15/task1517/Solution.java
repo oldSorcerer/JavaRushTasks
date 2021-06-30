d148d616825d4d27ea7478e0bfcf73c5d7976c47
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-06-30 16:11:28 +0300

    add solution task

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1517/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task15.task1517;
+
+/* 
+Статики и исключения
+*/
+
+public class Solution {
+    public static int A = 0;
+
+    static {
+        if (true)
+            throw new ExceptionInInitializerError();
+    }
+
+    public static int B = 5;
+
+    public static void main(String[] args) {
+        System.out.println(B);
+    }
+}
