9ff70eb05d5e9cdcf5678809a4f2f1dd4c5667b3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-07-07 16:57:13 +0300

    add solution task 1527

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = reader.readLine();
+
+        String [] words = string.substring(string.lastIndexOf("?") + 1).split("&");
+
+        for (String word : words) {
+
+            if (word.contains("=")) {
+                System.out.print(word.substring(0, word.indexOf("=")) + " ");
+            } else
+                System.out.print(word + " ");
+        }
+        System.out.println(" ");
+
+        for (String word : words) {
+            if (word.contains("obj")) {
+                try {
+                    alert(Double.parseDouble(word.substring(4)));
+                } catch (NumberFormatException e) {
+                    alert(word.substring(4));
+                }
+            }
+        }
+    }
+
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
+    }
+}
