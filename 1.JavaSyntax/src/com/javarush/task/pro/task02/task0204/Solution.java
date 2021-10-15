c3e463d6ac5f3f9390110b06fb20529f10ea6e6c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:36:24 +0300

    add solution task 0204

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0204/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0204;
+
+/* 
+9-ка это перевернутая 6-ка
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int six = 6;
+
+        six++;
+        six++;
+        six++;
+
+        System.out.println(six);
+    }
+}
