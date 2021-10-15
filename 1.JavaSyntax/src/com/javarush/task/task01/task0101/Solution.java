ca6d48f2ba72e8fc3668fb3b4b7f1d6e025cc73f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 18:12:45 +0300

    add solution task 0101

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -1,11 +1,11 @@
 package com.javarush.task.task01.task0101;
 
 /* 
-Я думаю, быть программистом - это круто
+Я думаю, быть программистом круто
 */
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом круто");
     }
 }
