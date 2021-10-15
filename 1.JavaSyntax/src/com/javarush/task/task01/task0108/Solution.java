705ec10c9d5d7afeaae9f15c7f61b90a1ecd9814
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:18:44 +0300

    add solution task 0108

--- 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
@@ -9,15 +9,15 @@ public class Solution {
         int a = 3;
         int b = 2;
 
-        System.out.print("два");
+        //System.out.print("два");
         System.out.print(b);
         System.out.print(" плюс ");
-        System.out.print(" минус ");
+        //System.out.print(" минус ");
         System.out.print(a);
-        System.out.print("три");
+        //System.out.print("три");
         System.out.print(" равно ");
-        System.out.print(" будет ");
-        System.out.print("пять");
+        //System.out.print(" будет ");
+        //System.out.print("пять");
         System.out.print(a + b);
     }
 }
