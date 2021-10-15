389d08cf1333644f14c3abebe1151dcd1337af59
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:38:58 +0300

    add solution task 0205

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task02.task0205;
+
+/* 
+Когда-то было 32 зуба
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int toothCounter = 32;
+
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+        System.out.println(toothCounter);
+    }
+}
