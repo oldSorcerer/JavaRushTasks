1a5128bc95d903e98b0dd89485d5e010dfbcc95a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 18:15:29 +0300

    add solution task 0109

--- 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -6,6 +6,10 @@ package com.javarush.task.task01.task0105;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+
+
     }
 }
