e887f184acc00ab87c08fcd80d4f979373220047
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 00:46:54 +0300

    add solution task 0211

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0211/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0211;
+
+/* 
+Получение длины строки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        //напишите тут ваш код
+        System.out.println(emptyString);
+        System.out.println("Gomu Gomu no Bazooka!");
+        System.out.println((emptyString + 2 + 2 + "22"));
+    }
+}
