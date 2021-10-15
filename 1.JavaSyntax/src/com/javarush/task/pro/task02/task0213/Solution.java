96616148feb7c687ad6a705422368b6138bd4dcb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 00:50:19 +0300

    add solution task 0213

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0213/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0213;
+
+/* 
+Причешем резюме
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String title = "Senior Lead Principal Software Engineer Data Architect";
+        String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
+        String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
+
+        //напишите тут ваш код
+        System.out.println("RESUME".toLowerCase());
+        System.out.println("TITLE: ".toLowerCase() + title);
+        System.out.println("DEGREE: ".toLowerCase() + degree);
+        System.out.println("CAREER: ".toLowerCase() + career);
+    }
+}
