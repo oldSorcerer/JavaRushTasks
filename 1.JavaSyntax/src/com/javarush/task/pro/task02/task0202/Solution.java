3fedf2a758e366d8d02479a696f22b69af603d20
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:33:14 +0300

    add solution task 0202

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100;
+
+        System.out.println(meters);
+    }
+}
