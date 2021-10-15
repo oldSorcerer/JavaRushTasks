457d02dc983e8ee7684f875616f1fc1adc1fe590
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:45:05 +0300

    add solution task 0208

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0208;
+
+/* 
+Пустота и пробелы
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String emptiness = "пустота";
+
+        String fullness = ""; //напишите тут ваш код
+
+        System.out.println(fullness);
+    }
+}
