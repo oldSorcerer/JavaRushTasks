c49814808a8c5a41d488396b2d30548d86121aab
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:41:38 +0300

    add solution task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "word", phrase = "phrase", line = "line", text = "text";
+
+    }
+}
