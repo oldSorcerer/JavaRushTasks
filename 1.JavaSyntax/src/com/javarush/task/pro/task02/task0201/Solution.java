3f65a549f4ae6de444df5397836f84cf0954e8c0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:25:30 +0300

    add solution task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 1, numericValue = 2, size = 3, number = 4;
+
+    }
+}
