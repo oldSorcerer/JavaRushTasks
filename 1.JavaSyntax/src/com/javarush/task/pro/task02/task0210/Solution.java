bc95569dd51fb66535fc802dcfb3388da49cdffe
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 00:18:38 +0300

    add solution task 0210

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0210/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0210;
+
+/* 
+С каждым разом огромнее
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String bigAmount = "500";
+        String greatAmount = "100000";
+
+        int hugeAmount = 0; //напишите тут ваш код
+
+        System.out.println(hugeAmount);
+    }
+}
