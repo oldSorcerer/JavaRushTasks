0aa0c21ac3bb4d822c9e1c30c203228f9582e4eb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-15 19:44:35 +0300

    add solution task 0207

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0207;
+
+/* 
+Логотип решает все
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = s3 + s1 + s2; //напишите тут ваш код
+
+        System.out.println(tagline);
+    }
+}
