14715500c433dce32fb6cdc469d8edd8c74af9de
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 13:15:57 +0300

    add solution task 0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
