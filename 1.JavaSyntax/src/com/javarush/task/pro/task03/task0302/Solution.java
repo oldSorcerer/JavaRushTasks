3104099f8119a53194d072742bd4fd2964b6107a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 18:10:53 +0300

    add solution task 0302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+
+        String name = scanner1.nextLine();
+        int age = scanner2.nextInt();
+
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + militaryCommissar);
+        }
+
+    }
+}
