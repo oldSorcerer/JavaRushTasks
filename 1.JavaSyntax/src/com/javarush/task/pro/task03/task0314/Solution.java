ea75afcffdc03361a1197714b8969f4994a717f5
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 19:37:06 +0300

    add solution task 0314

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        if (string.equalsIgnoreCase(secret) )
+            System.out.println("доступ разрешен");
+        else
+            System.out.println("доступ запрещен");
+    }
+}
