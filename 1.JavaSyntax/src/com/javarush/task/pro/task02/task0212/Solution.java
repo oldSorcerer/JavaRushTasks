5fb4044a92eeaea680caef3ae69c00e42220c5aa
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 11:12:48 +0300

    add solution task 0212

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0212/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0212;
+
+/* 
+Попрошу регистр на меня не повышать!
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        //напишите тут ваш код
+        System.out.println(usa);
+        System.out.println("Винни Пух");
+        System.out.println(caps + "they know I mean business");
+    }
+}
