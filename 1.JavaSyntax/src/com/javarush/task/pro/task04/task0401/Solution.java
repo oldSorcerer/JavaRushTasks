3883f1a2929ba53760f8ff0ebd9ccc577cf0faf3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 20:08:21 +0300

    add solution task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+
+        int i = 0;
+        while (i < 100) {
+            System.out.println(quote);
+            i++;
+        }
+
+    }
+}
