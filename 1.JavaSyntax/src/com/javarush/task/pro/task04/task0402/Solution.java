28803cdffec3d0e08aaf30ee9a4b323d5e67114b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 20:10:59 +0300

    add solution task 0402

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Все любят Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println(string + text);
+            i++;
+        }
+
+
+    }
+}
