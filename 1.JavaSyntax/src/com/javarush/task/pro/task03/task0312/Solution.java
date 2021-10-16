c9695c541d9c8e04f8a70707648edf109a03a79e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 19:34:07 +0300

    add solution task 0312

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task03.task0312;
+
+import java.util.Scanner;
+
+/* 
+Сравним строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+
+        String string1 = scanner1.nextLine();
+        String string2 = scanner1.nextLine();
+
+        if (string1.equals(string2))
+            System.out.println("строки одинаковые");
+        else 
+            System.out.println("строки разные");
+    }
+}
