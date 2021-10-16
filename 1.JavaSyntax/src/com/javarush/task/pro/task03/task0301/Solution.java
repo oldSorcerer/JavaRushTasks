0d613abd0afc7877832286df9998c283e6f5fe6d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 13:23:49 +0300

    add solution task 0301

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task03.task0301;
+
+import java.util.Scanner;
+
+/* 
+Тепло или холодно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+        //напишите тут ваш код
+    }
+}
