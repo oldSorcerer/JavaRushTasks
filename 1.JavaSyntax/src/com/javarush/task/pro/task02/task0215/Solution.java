44a7cb8739c22c9409c522a6157de89f0bed09fa
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-16 13:23:28 +0300

    add solution task 0215

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -9,7 +9,16 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+
+        int res = (n1 + n2 + n3) / 3;
+
+        System.out.println(res);
+
+
 
     }
 }
