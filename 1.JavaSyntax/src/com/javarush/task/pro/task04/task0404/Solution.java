d1c5e8042266843d97b98833a0e1775b84579a19
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-17 10:21:26 +0300

    add solution task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5) {
+            System.out.print("Q");
+            int j = 0;
+            while (j <9) {
+                System.out.print("Q");
+                j++;
+            }
+            System.out.println();
+            i++;
+
+        }
+
+
+    }
+}
