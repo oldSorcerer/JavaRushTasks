bff86be3bcfcdb433cebed1843af956a3a3b7f43
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-18 14:32:49 +0300

    add solution task 0313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task03.task0313;
+
+public class Solution {
+    public static String string1 = "Амиго";
+    public static String string2 = string1;
+    public static String string3 = new String(string1);
+
+    public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+
+        System.out.println(string1.equals(string2) ? same : different);
+        System.out.println(string1.equals(string3) ? same : different);
+        System.out.println(string2.equals(string3) ? same : different);
+
+    }
+}
