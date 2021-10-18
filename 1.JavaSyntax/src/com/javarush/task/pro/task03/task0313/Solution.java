8e011759f376d87cdc84ee960344c73a8d6177c3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-18 14:39:18 +0300

    add solution task 0313

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
         String same = "ссылки на строки одинаковые";
         String different = "ссылки на строки разные";
 
-        System.out.println(string1.equals(string2) ? same : different);
-        System.out.println(string1.equals(string3) ? same : different);
-        System.out.println(string2.equals(string3) ? same : different);
+        System.out.println((string1 == string2) ? same : different);
+        System.out.println((string1 == string3) ? same : different);
+        System.out.println((string2 == string3) ? same : different);
 
     }
 }
