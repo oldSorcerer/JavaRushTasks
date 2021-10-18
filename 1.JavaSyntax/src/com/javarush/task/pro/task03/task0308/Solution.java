0a8072efdcdee822d675bd15e68812fa4d2b9d6a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-18 19:17:08 +0300

    add solution task 0308

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0308;
+
+import java.util.Scanner;
+
+/* 
+Координатные четверти
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("4");
+        }
+    }
+}
