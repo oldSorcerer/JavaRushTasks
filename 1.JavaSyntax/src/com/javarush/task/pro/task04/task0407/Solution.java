fdcf9be21a307e1c8a3ce25c7e891fa51f6ddc91
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-21 23:55:58 +0300

    add solution task 0407

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task04.task0407;
+
+/* 
+Сумма чисел, не кратных 3
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        int sum = 0;
+
+        while (i < 100) {
+
+            i++;
+
+            if ((i % 3) == 0 ) {
+                continue;
+            }
+            sum = sum + i;
+
+        }
+
+        System.out.println(sum);
+
+    }
+}
