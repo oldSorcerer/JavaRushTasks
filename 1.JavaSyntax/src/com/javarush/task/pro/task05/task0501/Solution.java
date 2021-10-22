028012a84766d543c5f06dca73c7021dbe1a8f16
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-23 00:26:56 +0300

    add solution task 0501

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task05.task0501;
+
+/* 
+Создаем массивы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
