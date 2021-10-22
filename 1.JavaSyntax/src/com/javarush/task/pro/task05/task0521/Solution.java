562d4526adb6a1413c99d42d6e3f89871a36bd49
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-22 19:39:35 +0300

    add solution task 0521

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0521/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task05.task0521;
+
+/* 
+Выполнение фрагмента кода
+*/
+
+public class Solution {
+
+    public static int result = 45967;
+
+    public static void main(String[] args) {
+        String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
+        System.out.println(loop);
+    }
+}
+
