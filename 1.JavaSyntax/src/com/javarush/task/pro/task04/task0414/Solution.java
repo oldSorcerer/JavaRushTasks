95ae71dd97b3cdd41390086a9ebdac6e77df7a96
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-23 00:11:52 +0300

    add solution task 0414

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0414/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task04.task0414;
+
+import java.util.Scanner;
+
+/* 
+Хорошего не бывает много
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String string = sc.nextLine();
+        int number = sc.nextInt();
+
+        if (number > 0 && number < 5 ) {
+            int i = 0;
+            do {
+                System.out.println(string);
+                i++;
+            } while (i < number);
+        } else
+            System.out.println(string);
+    }
+}
