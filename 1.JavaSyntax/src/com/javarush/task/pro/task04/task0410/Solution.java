cce9fc09d98fb14b81dc2359cff33c524b348844
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-22 19:31:03 +0300

    add task 0410

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task04.task0410;
+
+import java.util.Scanner;
+
+/* 
+Второе минимальное число из введенных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
