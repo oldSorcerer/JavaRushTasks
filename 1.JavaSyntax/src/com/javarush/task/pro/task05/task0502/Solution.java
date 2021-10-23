dca9e1f524c12f0bcb1bf7a9cbe9efafb85ec43e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-23 20:09:18 +0300

    add solution task 0502

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task05.task0502;
+
+/* 
+Четные и нечетные ячейки массива
+*/
+
+public class Solution {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < strings.length; i++) {
+            if (i % 2 == 0) {
+                strings[i] = EVEN;
+            } else {
+                strings[i] = ODD;
+            }
+        }
+
+        System.out.print("index = 0");
+        System.out.println(" value = " + strings[0]);
+        System.out.print("index = 1");
+        System.out.println(" value = " + strings[1]);
+        System.out.print("index = 2");
+        System.out.println(" value = " + strings[2]);
+        System.out.print("index = 3");
+        System.out.println(" value = " + strings[3]);
+        System.out.print("index = 4");
+        System.out.println(" value = " + strings[4]);
+    }
+}
