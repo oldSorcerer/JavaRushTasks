b76b9e501a51c588a429b4a76c8dead732125121
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-27 23:43:33 +0300

    add solution task 0418

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0418/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task04.task0418;
+
+import java.util.Scanner;
+
+/* 
+Стакан наполовину пуст или наполовину полон?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double glass = 0.5;
+
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.nextBoolean()) {
+            System.out.println((int)Math.ceil(glass));
+        } else {
+            System.out.println((int)Math.floor(glass));
+        }
+
+
+    }
+}
