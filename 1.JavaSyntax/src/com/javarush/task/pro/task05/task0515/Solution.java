d4870960417b4045374cd616a152c43db93b45da
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:04:09 +0300

    add solution task 0515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0515/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0515;
+
+import java.util.Arrays;
+
+/* 
+Сравнение двумерных массивов
+*/
+
+public class Solution {
+
+    public static int[][] arrayFirst = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    public static int[][] arraySecond = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.deepEquals(arrayFirst, arraySecond));
+    }
+}
