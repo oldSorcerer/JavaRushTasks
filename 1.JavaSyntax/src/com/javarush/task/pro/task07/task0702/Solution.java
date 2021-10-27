98e70248207e56c04b5d1ba1150ae18e82a63c08
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 01:22:03 +0300

    add solution task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte aByte = Byte.MAX_VALUE;
+    short aShort = Short.MAX_VALUE;
+    int anInt = Integer.MAX_VALUE;
+
+    long aLong = Long.MAX_VALUE;
+}
