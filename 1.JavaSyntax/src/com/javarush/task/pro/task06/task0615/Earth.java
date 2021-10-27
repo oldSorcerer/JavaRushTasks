befec35f9431a63a7c20888c4272cf3d9d581982
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 01:02:10 +0300

    add solution task 0615

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0615;
+
+/* 
+Все что нужно знать о Земле
+*/
+
+public class Earth {
+    public static final String NAME = "Земля";
+    public static final double SQUARE = 510_100_000;
+    public static final long POPULATION = 7_594_000_000L;
+    public static final long EQUATOR_LENGTH = 40_075_696;
+}
