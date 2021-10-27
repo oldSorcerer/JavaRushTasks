1452746df8bddca20834a594d72c0bb648aaba1b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 01:03:21 +0300

    add solution task 0614

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0614/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task06.task0614;
+
+/* 
+Вспоминаем геометрию
+*/
+
+public class Solution {
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Плошадь круга равна: " + pi * radius * radius);
+        System.out.println("Периметр круга равен: " + pi * diameter);
+    }
+}
