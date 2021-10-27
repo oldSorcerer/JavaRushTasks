2104f20e46fbf8d8e360b20e724897c9deb6d96c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 01:16:13 +0300

    add solution task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    boolean aBoolean;
+    byte aByte;
+    short aShort;
+    int anInt;
+    char aChar;
+    long aLong;
+
+    float aFloat;
+    double aDouble;
+
+
+}
