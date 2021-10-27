9ec8558e37121df89679353f27d4eae87824ad5c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:24:56 +0300

    add solution task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(String name){
+
+    }
+    public static void universalMethod(String firstName, String secondName){
+
+    }
+    public static void universalMethod(int number){
+
+    }
+    public static void universalMethod(int number1, int number2){
+
+    }
+    public static void universalMethod(boolean flag){
+
+    }
+    public static void universalMethod(boolean flag1, boolean flag2){
+
+    }
+    public static void universalMethod(String name, int number){
+
+    }
+    public static void universalMethod(String name, int number, boolean flag){
+
+    }
+    public static void universalMethod(int number, String name, boolean flag){
+
+    }
+
+}
