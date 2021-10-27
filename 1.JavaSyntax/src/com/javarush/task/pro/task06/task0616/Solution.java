dc8f8dbb239d3531d402beff15fea645895157db
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:50:25 +0300

    add solution task 0616

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0616/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task06.task0616;
+
+/* 
+Учет работников
+*/
+
+public class Solution {
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
