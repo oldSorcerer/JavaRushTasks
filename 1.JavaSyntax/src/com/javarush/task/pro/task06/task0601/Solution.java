96715dcad5939e2e3f3e33d4a20935e21881ae5b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:07:32 +0300

    add solution task 0601

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0601/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task06.task0601;
+
+/* 
+Разделяй понятия
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printHydrogenInfo();
+        printJavaInfo();
+
+    }
+
+    public static void printHydrogenInfo() {
+        System.out.println("Водород:");
+        System.out.println("Это химический элемент периодической системы с обозначением H и атомным номером 1.");
+        System.out.println("Является самым легким элементом таблицы.");
+        System.out.println("Водород применяется в:");
+        System.out.println("-Химической промышленности;");
+        System.out.println("-Нефтеперерабатывающей промышленности;");
+        System.out.println("-Авиации;");
+        System.out.println("-Электроэнергетике.");
+    }
+
+    public static void printJavaInfo() {
+        System.out.println("Остров Ява:");
+        System.out.println("Входит в состав Индонезии.");
+        System.out.println("На острове зародились государства Матарам, Маджапахит, Демак.");
+        System.out.println("Ява является самым населенным островом в мире:");
+        System.out.println("Общее население — 140 млн. человек.");
+        System.out.println("Плотность населения — 1061 чел/км.кв.");
+        System.out.println("Здесь производят один из знаменитых сортов кофе — Копи Лювак.");
+    }
+
+}
