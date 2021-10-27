ea2185f4b6513d0b3da32695095d3d8baf48c4c8
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:15:45 +0300

    add task 0506

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task05.task0506;
+
+import java.util.Scanner;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+
+
+
+    }
+}
