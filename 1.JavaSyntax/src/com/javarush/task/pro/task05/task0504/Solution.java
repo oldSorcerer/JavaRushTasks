ba9893bd25e1ba67b1361448dfa769a83ee36fd5
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 00:15:58 +0300

    add task 0504

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0504;
+
+/* 
+Объединяем массивы
+*/
+
+public class Solution {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
