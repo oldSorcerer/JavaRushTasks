528e0dd29ba544395ecedc1bd12cfd6da617856c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 01:23:13 +0300

    add solution task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte aByte = Byte.MIN_VALUE;
+    short aShort = Short.MIN_VALUE;
+    int anInt = Integer.MIN_VALUE;
+
+    long aLong = Long.MIN_VALUE;
+}
