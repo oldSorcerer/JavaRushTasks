2dc644a501609a96534e4fd8ee2c6656fcf8c543
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-28 18:04:44 +0300

    add solution task 0706

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0706;
+
+/* 
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double a = 0.0;
+        double b = 0.0;
+        div(a, b);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
