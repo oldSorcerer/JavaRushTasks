bdf2115efdcd8b853a7363d1ce21b3bd31f2bb0b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 23:56:11 +0300

    add solution task 0713

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0713/Human.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task07.task0713;
+
+public class Human extends Terran {
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0713/Terran.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task07.task0713;
+
+/* 
+Простое наследование
+*/
+
+public class Terran {
+}
