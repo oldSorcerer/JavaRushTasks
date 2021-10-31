13a66a1104a6e11deea9498411a809aaffe1f7c3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:10:08 +0300

    add solution task 0901

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0901/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task09.task0901;
+
+/* 
+Целочисленные литералы
+*/
+
+public class Solution {
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
+}
