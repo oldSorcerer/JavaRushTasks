0c45aab9efe4bcec47cb25448f7f0bc352b27345
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:12:18 +0300

    add solution task 0902

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0902/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task09.task0902;
+
+/* 
+Литералы с плавающей точкой
+*/
+
+public class Solution {
+    public double a = 0.;
+    public double b = .0;
+    public double c = 100D;
+    public double d = 100.0;
+    public double e = 1.11E5;
+    public float f = 200F;
+    public float g = 0.F;
+}
