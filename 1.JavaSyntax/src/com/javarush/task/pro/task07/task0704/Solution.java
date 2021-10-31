ba3d978de5f1294a72868cdcbffaf7e462becff0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 00:10:11 +0300

    add solution task 0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 1.2742E+4;
+    double lightSpeed = 2.99792458E+8;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.5E-2;
+    double javaDeveloperSalary = 1.0E+4;
+}
