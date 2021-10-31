bf82b737f23eec5ab8535dddac7405d5612be6f2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:42:28 +0300

    add solution task 0710

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0710/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task07.task0710;
+
+/* 
+Массив значений
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+}
