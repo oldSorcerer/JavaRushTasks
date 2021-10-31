a3713a0d013279159f38cbc3d7dbf2f6c81f956f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 23:54:27 +0300

    add solution task 0710

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int[] c = new int[5];
+        int[] d = new int[5];
+        int[] f = new int[5];
+
+        new Scanner(System.in);
+        new Scanner(System.in);
+
+    }
+}
