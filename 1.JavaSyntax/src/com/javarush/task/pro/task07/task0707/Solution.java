1b448807af706e7273d6f25395d941b30dd89adf
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 00:07:21 +0300

    add solution task 0707

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0707/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0707;
+
+/* 
+5 оттенков char
+*/
+
+public class Solution {
+    public static char char1 = 101;
+    public static char char2 = 'H';
+    public static char char3 = 114;
+    public static char char4 = '\u006F';
+    public static char char5 = 108;
+    public static char char6 = 'h';
+    public static char char7 = 0x0063;
+    public static char char8 = ' ';
+    public static char char9 = 'l';
+    public static char char10 = 0x61;
+
+    public static void main(String[] args) {
+        System.out.print(char2);
+        System.out.print(char1);
+        System.out.print(char5);
+        System.out.print(char9);
+        System.out.print(char4);
+        System.out.print(char8);
+        System.out.print(char7);
+        System.out.print(char6);
+        System.out.print(char10);
+        System.out.print(char3);
+        System.out.println();
+    }
+}
