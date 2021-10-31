9ea230b23980e7c6e9ec549bb2464626d5fc91f8
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 00:16:16 +0300

    add solution task 0806

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0806/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task08.task0806;
+
+/* 
+Импорты: Part 1
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
