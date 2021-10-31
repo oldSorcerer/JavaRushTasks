ce17a31a234c5ee0c07d0c796d12f9375aa22ba7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:20:43 +0300

    add solution task 0904

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0904/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task09.task0904;
+
+/* 
+Символьные литералы
+*/
+
+public class Solution {
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
+
+    public static void main(String[] args) {
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
+        System.out.print(fourth);
+    }
+}
