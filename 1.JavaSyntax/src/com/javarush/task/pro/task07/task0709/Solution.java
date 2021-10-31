c484599f4c0837867baa9298e0612d18e5370a96
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:40:02 +0300

    add solution task 0709

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0709/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0709;
+
+/* 
+Очень правильные типы
+*/
+
+public class Solution {
+    long a = 109 + 15;
+    int b = (int) a * 2;
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+}
