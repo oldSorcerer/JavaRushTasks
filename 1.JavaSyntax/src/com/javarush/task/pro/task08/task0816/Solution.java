921085a6d6df078707ff72861a7bc9dcfd6d0e1d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 00:39:04 +0300

    add solution task 0816

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0816/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task08.task0816;
+
+/* 
+Приоритеты
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(number + number * (number + ++number));
+    }
+
+}
