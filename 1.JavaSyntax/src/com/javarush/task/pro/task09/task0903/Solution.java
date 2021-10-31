dfa0dc17a809df4d08a4fb852c08b46d07351a24
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:17:47 +0300

    add solution task 0903

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0903/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task09.task0903;
+
+/* 
+Строковые литералы
+*/
+
+public class Solution {
+    public String text = "Всегда пишите код так, "
+                        + "будто сопровождать его будет "
+                        + "склонный к насилию психопат, "
+                        + "который знает, где вы живете. "
+                        + "Martin Golding";
+}
