07eb771501a58a52c7c2e7300d2eaa2e562f89d9
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-10-31 20:35:19 +0300

    add solution task 0708

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0708/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task07.task0708;
+
+/* 
+Правильные типы
+*/
+
+public class Solution {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+        setValues(100);
+    }
+
+    public static void setValues(long value){
+        a = (byte) value;
+        b = (short) value;
+        c = (int) value;
+        d = value;
+
+    }
+}
