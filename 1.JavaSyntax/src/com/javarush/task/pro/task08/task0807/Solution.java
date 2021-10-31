5d3ac3eee5adbdc1b9e94b9458c1be4dc84d623b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 00:17:41 +0300

    add solution task 0806

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0807/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task08.task0807;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/* 
+Импорты: Part 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
