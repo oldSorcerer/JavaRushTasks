4a59efabb936d3d8b9d16b9900f76a623dc1683f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-02 00:30:45 +0300

    add solution task 0505

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task05.task0505;
+
+import java.util.Scanner;
+
+/* 
+Reverse
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = Integer.parseInt(sc.nextLine());
+
+        int[] array = new int[number];
+
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+                array[i] = Integer.parseInt(sc.nextLine());
+            }
+        }
+
+        if (number % 2 != 0) {
+            for (int i = 0; i < array.length ; i++) {
+                System.out.println(array[i] + " ");
+            }
+        } else {
+            for (int i = array.length - 1; i >= 0 ; i--) {
+                System.out.println(array[i] + " ");
+            }
+
+        }
+
+
+    }
+}
