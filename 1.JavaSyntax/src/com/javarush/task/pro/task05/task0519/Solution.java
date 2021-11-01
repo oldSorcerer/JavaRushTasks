98fb2f44b0d506637b33dc68d7b13c2fab9e815e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-02 00:07:29 +0300

    add solution task 0519

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task05.task0519;
+
+import java.util.Arrays;
+
+/* 
+Есть ли кто?
+*/
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+
+        int[] copyArray = Arrays.copyOf(array, array.length);
+
+        Arrays.sort(copyArray);
+
+        int k = Arrays.binarySearch(copyArray, element);
+
+        System.out.println(k >= 0 ? "true" : "false");
+        
+    }
+}
