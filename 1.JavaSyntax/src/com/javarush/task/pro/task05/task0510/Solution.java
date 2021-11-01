20ac2ad269c1b1ec15e024d9b2577825db8a3388
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-01 23:30:12 +0300

    add solution task 0510

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0510;
+
+/* 
+Треугольный массив
+*/
+
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
