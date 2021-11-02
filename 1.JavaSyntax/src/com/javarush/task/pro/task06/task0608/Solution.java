84b29687c4c3852a077121898b71cd0fd6047c8a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-03 00:57:56 +0300

    add solution task 0608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long number) {
+        return number * number * number;
+    }
+}
