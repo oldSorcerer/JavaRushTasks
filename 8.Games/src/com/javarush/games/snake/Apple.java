aa058e3bae9b364ca64b19802178ce98dff135c7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-04 21:44:24 +0300

    fix game Snake

--- 8.Games/src/com/javarush/games/snake/Apple.java
+++ 8.Games/src/com/javarush/games/snake/Apple.java
@@ -7,12 +7,12 @@ public class Apple extends GameObject {
     private static final String APPLE_SIGN = "\uD83C\uDF4E";
 
     public boolean isAlive = true;
-    
+
     public Apple(int x, int y) {
         super(x, y);
     }
-    
+
     public void draw(Game game) {
-       game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.RED, 75);
+        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.RED, 75);
     }
 }
