0c47c61916f98cf4ecdd0ed13ae90227f564b7c2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-06 23:39:26 +0300

    add solution task 0802

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task08.task0802;
+
+/* 
+Утильный класс: часть 2
+*/
+
+public class Solution {
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+        return Math.pow(number, power);
+    }
+}
