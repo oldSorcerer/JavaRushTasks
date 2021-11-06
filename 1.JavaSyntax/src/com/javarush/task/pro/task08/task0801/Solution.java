e016fcacc1a96a22fed7ef351f7b94002006f4aa
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-06 20:40:23 +0300

    add solution task 0801

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+/* 
+Утильный класс: часть 1
+*/
+
+public class Solution {
+
+    public static double sin(double a) {
+        return Math.sin(Math.toRadians(a));
+    }
+
+    public static double cos(double a) {
+        return Math.cos(Math.toRadians(a));
+    }
+
+    public static double tan(double a) {
+        return Math.tan(Math.toRadians(a));
+    }
+}
