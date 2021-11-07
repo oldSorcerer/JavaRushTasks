87183c61be86fd06e3359bae1f349301552db0c2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-07 18:16:00 +0300

    add solution task 1101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1101/SolarSystem.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task11.task1101;
+
+/* 
+Солнечная система — наш дом
+*/
+
+public class SolarSystem {
+
+    public static long age = 4_568_200_000L;
+
+    public static int planetsCount = 8;
+
+    public static int starsCount = 1;
+
+    public static String starName = "Солнце";
+
+    public static int galacticCenterDistance = 27170;
+
+
+}
