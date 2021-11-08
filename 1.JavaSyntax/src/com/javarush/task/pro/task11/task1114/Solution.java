d1c9002325041be410e64bbf9c91479c11d349dc
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:32:14 +0300

    add solution task 1114

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1114/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task11.task1114;
+
+/* 
+И снова скобки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
