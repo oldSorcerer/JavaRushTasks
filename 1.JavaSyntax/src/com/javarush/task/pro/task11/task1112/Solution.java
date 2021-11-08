ff6229578dfb9d6c9db7a6d5b44bab5041088bd0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:17:30 +0300

    add solution task 1110

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1112/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1112;
+
+/* 
+Кон, котэ и нация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
