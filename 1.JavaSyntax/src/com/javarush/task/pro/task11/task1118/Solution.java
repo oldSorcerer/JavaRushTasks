ccc30ad2ea5fc5e0473d58f5b9ee18fc4a490da1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:22:14 +0300

    add solution task 1118

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1118/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task11.task1118;
+
+/* 
+Импортный код
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
