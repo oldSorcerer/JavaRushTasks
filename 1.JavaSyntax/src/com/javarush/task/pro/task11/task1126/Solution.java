f7801ff72cac8b52599473537fab24ccf859da7f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:52:01 +0300

    add solution task 1126

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1126;
+
+/* 
+Студент
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Student student = new Student("Амиго");
+        System.out.println(student.name);
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1126;
+
+/* 
+Студент
+*/
+
+public class Student {
+    public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
