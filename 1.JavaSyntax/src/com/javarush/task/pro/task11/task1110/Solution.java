076822f42cee67e8249a5a20375a3895a2b637ff
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:16:14 +0300

    add solution task 1110

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1110/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1110;
+
+/* 
+Точка и запятая
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
