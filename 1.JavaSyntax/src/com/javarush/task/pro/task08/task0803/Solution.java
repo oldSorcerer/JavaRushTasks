24af5f422b319de13702d5904eaf4e5a61537e1b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:20:07 +0300

    add solution task 0803

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+/* 
+Минимальный элемент массива
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        return new int[10];
+    }
+}
