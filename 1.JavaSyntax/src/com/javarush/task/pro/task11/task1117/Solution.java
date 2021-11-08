ae579ac1542ae9cd3c5ba8d5be36678dae4c8bc6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:23:21 +0300

    add solution task 1117

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1117;
+
+/*
+Пакет нужен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test package");
+    }
+}
