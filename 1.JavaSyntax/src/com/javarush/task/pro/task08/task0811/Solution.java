8104d9edf05825719eed5f02d209e9cdcc0ee9f1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-08 15:41:25 +0300

    add solution task 0811

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0811/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task08.task0811;
+
+/* 
+Побитовые операции
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x = 7;
+        //x = x & 25;
+        x = x & 5;
+       // x = x & 3;
+        x = x & 12;
+        x = x | 1;
+        System.out.println(x);
+    }
+}
