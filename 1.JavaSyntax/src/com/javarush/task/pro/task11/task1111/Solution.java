a99be1fabfcd219a66df0b73c4302cf508ad37d2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:19:58 +0300

    add solution task 1111

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1111/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1111;
+
+/* 
+Ох уж эти кавычки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
