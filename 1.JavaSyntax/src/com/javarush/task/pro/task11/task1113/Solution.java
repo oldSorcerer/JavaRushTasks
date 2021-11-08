5d97ce2524165fa8e4cbd95deebc59d6e0b05f74
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:29:19 +0300

    add solution task 1113

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1113/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1113;
+
+/* 
+Следим за скобками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
