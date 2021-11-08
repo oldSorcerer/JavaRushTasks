4eb0faffee2d08d74606603cf7709bb74c94f2a9
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 01:21:09 +0300

    add solution task 1115

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1115/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1115;
+
+/* 
+Многострадальный метод main
+*/
+
+public class Solution {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
