69671b8fcd77f36718ba1a363398762375e65585
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 23:35:50 +0300

    add solution task 0911

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0911/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task09.task0911;
+
+/* 
+Поздравление
+*/
+
+public class Solution {
+    public static String partyFace = "\uD83E\uDD73";
+    public static String balloon = "\uD83C\uDF88";
+    public static String gift = "\uD83C\uDF81";
+    public static String partyPopper = "\uD83C\uDF89";
+    public static String cake = "\uD83C\uDF82";
+
+    public static void main(String[] args) {
+        printCongratulation();
+    }
+
+    public static void printCongratulation() {
+        String happyBirthday = "С днем рождения!";
+        String congratulation = partyFace + balloon + partyPopper + cake + gift;
+        System.out.println(happyBirthday + congratulation);
+    }
+}
