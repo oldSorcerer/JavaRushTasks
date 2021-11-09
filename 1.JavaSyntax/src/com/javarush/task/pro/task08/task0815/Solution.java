be3b68cdf75d19a6fb8d9f68c88bdfabf7b78fde
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 23:39:27 +0300

    add solution task 1001

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0815/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task08.task0815;
+
+/* 
+Логические операторы
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        taskForCondition();
+    }
+
+    public static void taskForCondition() {
+        int number = 15;
+        boolean conditionFirst = number < 15 && number > 25 && (number += 15) < 25;
+        boolean conditionSecond = (number | 5) > 12 || (number = number + 2) > 40;
+        System.out.println(number);
+    }
+}
