4288dcfe8b965a9b3d86436ac232937cb12a161f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 16:04:21 +0300

    add solution task 1107

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task11.task1107;
+
+/* 
+Двигатель — сердце автомобиля
+*/
+
+public class Car {
+
+    public Engine engine;
+
+    class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public  void stop() {
+            isRunning = false;
+        }
+
+    }
+}
+
+
