619a7645f3ed1f4449ae09207e632c5ffe5796d1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 19:55:56 +0300

    add solution task 0805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/MagicBall.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task08.task0805;
+
+import java.util.Random;
+
+public class MagicBall {
+    private static final String CERTAIN = "Бесспорно";
+    private static final String DEFINITELY = "Определённо да";
+    private static final String MOST_LIKELY = "Вероятнее всего";
+    private static final String OUTLOOK_GOOD = "Хорошие перспективы";
+    private static final String ASK_AGAIN_LATER = "Спроси позже";
+    private static final String TRY_AGAIN = "Попробуй снова";
+    private static final String NO = "Мой ответ — нет";
+    private static final String VERY_DOUBTFUL = "Весьма сомнительно";
+
+    public static String getPrediction() {
+
+        Random r = new Random();
+
+        int rand = r.nextInt(8);
+
+        switch (rand) {
+            case 0: return CERTAIN;
+            case 1: return DEFINITELY;
+            case 2: return MOST_LIKELY;
+            case 3: return OUTLOOK_GOOD;
+            case 4: return ASK_AGAIN_LATER;
+            case 5: return TRY_AGAIN;
+            case 6: return NO;
+            case 7: return VERY_DOUBTFUL;
+        }
+        return null;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task08.task0805;
+
+/* 
+Magic 8 ball
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Заработаю ли я себе на BMW?");
+        System.out.println(MagicBall.getPrediction());
+    }
+}
