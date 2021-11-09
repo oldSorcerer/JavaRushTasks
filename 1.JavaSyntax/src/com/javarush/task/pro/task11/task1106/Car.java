87695e259d02516b82443349b3342b812414c5f3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 15:59:12 +0300

    add solution task 1106

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1106/Car.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task11.task1106;
+
+/* 
+Одинаковые машины
+*/
+
+public class Car {
+    private String modelName;
+    private int speed;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
