e1fa44c220f595e766f51a807608ea1f16c53884
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 16:13:12 +0300

    add solution task 1125

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1125/University.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task11.task1125;
+
+/* 
+Составляем список студентов
+*/
+
+public class University {
+    private final String[] students = new String[30];
+
+    public void addStudent(String student) {
+        int i = 0;
+        for (; i < students.length; i++) {
+            if (students[i] == null) {
+                break;
+            }
+        }
+        students[i] = student;
+    }
+
+    public void printStudents() {
+        for (String student : students) {
+            if (student != null) {
+                System.out.println(student);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        University university = new University();
+        university.addStudent("Максим Федоренко");
+        university.addStudent("Олег Кесарчук");
+        university.printStudents();
+    }
+}
