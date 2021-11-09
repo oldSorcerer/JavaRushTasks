566d7c3106060bd8f843eec9659024e0f490ccee
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 22:32:19 +0300

    add solution task 0813

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0813/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0813;
+
+/* 
+Степень двойки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getPowerOfTwo(3));
+    }
+
+    public static int getPowerOfTwo(int power) {
+        return 2 << power - 1;
+    }
+}
