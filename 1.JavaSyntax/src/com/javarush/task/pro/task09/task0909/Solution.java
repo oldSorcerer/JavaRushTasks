2c98589f940e64ec58eeebde014749984f5d4b4f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 22:50:11 +0300

    add solution task 0909

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task09.task0909;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\" ");
+    }
+}
