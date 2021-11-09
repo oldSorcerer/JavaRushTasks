67a8a7c9d3184ce535370196720c5156fe17fa48
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 15:56:51 +0300

    add solution task 1104

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        System.out.println( Math.round(Double.parseDouble(string)));
+    }
+}
