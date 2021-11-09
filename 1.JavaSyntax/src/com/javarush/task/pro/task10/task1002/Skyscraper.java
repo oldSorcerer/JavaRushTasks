37c919e515ebb3e4a33fbcb7827d3b375aeadb5e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 23:44:14 +0300

    add solution task 1002

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1002/Skyscraper.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task10.task1002;
+
+/* 
+Построим новый жилой комплекс JavaRush Towers
+*/
+
+public class Skyscraper {
+    public static final String SKYSCRAPER_WAS_BUILD = "Небоскреб построен.";
+    public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Небоскреб построен. Количество этажей - ";
+    public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Небоскреб построен. Застройщик - ";
+
+    public Skyscraper() {
+        System.out.println(SKYSCRAPER_WAS_BUILD);
+    }
+
+    public Skyscraper(int floor) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + floor);
+    }
+
+    public Skyscraper(String developer) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperTower = new Skyscraper(50);
+        Skyscraper skyscraperSkyline = new Skyscraper("JavaRushDevelopment");
+    }
+}
+
