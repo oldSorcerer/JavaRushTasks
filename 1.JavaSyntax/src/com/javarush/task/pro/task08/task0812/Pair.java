5f4b387f29cea55fa4196d771ca43146eccb9c84
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 21:51:39 +0300

    add solution task 0812

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0812/Pair.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task08.task0812;
+
+public class Pair {
+    private int x;
+    private int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("x=%d, y=%d", x, y);
+    }
+
+    public void swap() {
+        int tmp = x ^ y;
+        x = y;
+        y = tmp ^ x;
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0812/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task08.task0812;
+
+/* 
+Swap по-новому
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        /* expected output
+        x=4, y=5
+        x=5, y=4
+         */
+
+        Pair pair = new Pair(4, 5);
+        System.out.println(pair);
+        pair.swap();
+        System.out.println(pair);
+    }
+}
