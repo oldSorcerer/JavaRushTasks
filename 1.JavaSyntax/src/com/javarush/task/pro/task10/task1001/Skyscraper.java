edb8d1a3b925a09086e8f0ab1e3ad8784e9fe8de
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-09 23:44:04 +0300

    add solution task 1001

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1001;
+
+/* 
+Создадим небоскреб
+*/
+
+public class Skyscraper {
+
+    public Skyscraper() {
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+    }
+}
