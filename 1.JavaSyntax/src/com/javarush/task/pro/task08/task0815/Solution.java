171405568044c8c509993d2835f7c9e691b05e5b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-11 18:23:43 +0300

    add solution task 0815

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0815/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0815/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 
     public static void taskForCondition() {
         int number = 15;
-        boolean conditionFirst = number < 15 && number > 25 && (number += 15) < 25;
+        boolean conditionFirst = number < 15 & number > 25 & (number += 15) < 25;
         boolean conditionSecond = (number | 5) > 12 || (number = number + 2) > 40;
         System.out.println(number);
     }
