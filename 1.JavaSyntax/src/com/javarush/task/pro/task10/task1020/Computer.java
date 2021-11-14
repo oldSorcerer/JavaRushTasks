a724af498a5ff4d0c5fed2aaef5fbc37a7bece77
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-14 23:16:25 +0300

    add solution task 1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+
+    public Computer() {
+        new SystemUnit();
+        new Monitor();
+        new Keyboard();
+        new Mouse();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Mouse.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Mouse {
+}
