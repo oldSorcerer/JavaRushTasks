0e29a818dabdaf6c6d37708447090c3800a55223
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-14 23:08:09 +0300

    add solution task 1013

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1013/Hotel.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task10.task1013;
+
+/* 
+Расставь правильно этажи
+*/
+
+public class Hotel {
+    public int thirdFloor = 3;
+    public int fourthFloor = thirdFloor + 1;
+    public int fifthFloor = fourthFloor + 1;
+    public int sixthFloor = fifthFloor + 1;
+}
