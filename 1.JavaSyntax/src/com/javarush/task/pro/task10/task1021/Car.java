d8461e94e5773b8bbd47c3b18423d7f93d8c3ea1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-14 23:20:18 +0300

    add solution task 1021

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task10.task1021;
+
+/* 
+Автопроизводитель
+*/
+
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+
+    public void initialize(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+}
