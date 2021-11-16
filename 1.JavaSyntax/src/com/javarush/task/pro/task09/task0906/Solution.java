ef4d70ad120e0fc0a37721d1f92e26cccabd6695
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-16 23:54:18 +0300

    add solution task 0906

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0906/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task09.task0906;
+
+/* 
+Двоичный конвертер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = Integer.MAX_VALUE;
+        System.out.println("Десятичное число " + decimalNumber + " равно двоичному числу " + toBinary(decimalNumber));
+        String binaryNumber = "1111111111111111111111111111111";
+        System.out.println("Двоичное число " + binaryNumber + " равно десятичному числу " + toDecimal(binaryNumber));
+    }
+
+    public static String toBinary(int decimalNumber) {
+        if (decimalNumber <= 0){
+            return "";
+        }
+        String binaryNumber = "";
+        while (decimalNumber != 0) {
+            binaryNumber = (decimalNumber % 2) + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+
+        return binaryNumber;
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        if (binaryNumber == null || binaryNumber.equals("")) {
+            return 0;
+        }
+        int decimalNumber = 0;
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            char ch = binaryNumber.charAt(binaryNumber.length() - 1 - i);
+            String str = String.valueOf(ch);
+            decimalNumber = decimalNumber + Integer.parseInt(str) * (int) Math.pow(2, i);
+        }
+        return decimalNumber;
+    }
+}
