26de7778c432cbec5e64f8435dc74e642ff7fee2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-19 17:58:33 +0300

    add solution task 0912

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0912/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task09.task0912;
+
+/* 
+Проверка URL-адреса
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
+
+    public static String checkDomain(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
+}
