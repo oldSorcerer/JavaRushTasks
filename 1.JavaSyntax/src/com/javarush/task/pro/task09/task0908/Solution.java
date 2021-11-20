f53c510e6eb92e9e0e8b83e8f846de5450a82813
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-20 19:24:55 +0300

    add comment for solution 0908

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0908/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0908/Solution.java
@@ -14,18 +14,18 @@ public class Solution {
     }
 
     public static String toHex(String binaryNumber) {
-
+        // проверяем если binaryNumber ноль или содержит пустую строку (isEmpty) или имеет символы отличные от 0 и 1
         if (binaryNumber == null || binaryNumber.isEmpty() || !binaryNumber.matches("[01]+")) {
             return "";
-        }
+        } // если количество символов не кратно 4 добовляем еще 1
         if (binaryNumber.length() % 4 != 0) {
             binaryNumber = "0" + binaryNumber;
         }
-        String hexNumber = "";
+        String hexNumber = ""; // в цыкле проверяем что дина строки кратна 4 и что строка больше 0
         while (binaryNumber.length() % 4 == 0 && binaryNumber.length() > 0) {
-            String fourBit = binaryNumber.substring(0, 4);
+            String fourBit = binaryNumber.substring(0, 4); // отделяем первые 4 символа
 
-            String oneHex  = switch (fourBit) {
+            String oneHex  = switch (fourBit) { // это многоуровнивый if else где мы сравниваем отделенные 4 байта
                 case "0000" -> "0";
                 case "0001" -> "1";
                 case "0010" -> "2";
@@ -45,22 +45,23 @@ public class Solution {
                 default -> null;
             };
 
-            hexNumber += oneHex;
+            hexNumber += oneHex; // если сть совпадения записываем их в новую переменную
 
-            int index = binaryNumber.length();
-            binaryNumber = binaryNumber.substring(4, index);
+            int index = binaryNumber.length(); // определяем дленну строки
+            binaryNumber = binaryNumber.substring(4, index); // перезапысываем нашу же строку новой но уже с 4 индекса и до конца
         }
         return hexNumber;
     }
 
     public static String toBinary(String hexNumber) {
+        // та же проверка что и выше только вхождени на другие сымволы
         if (hexNumber == null || hexNumber.isEmpty() || !hexNumber.matches("[0-9a-f]+")) {
             return "";
         }
         String binaryNumber = "";
         for (int i = 0; i < hexNumber.length(); i++) {
-            String oneHex = String.valueOf(hexNumber.charAt(i));
-            String oneBit = switch (oneHex) {
+            String oneHex = String.valueOf(hexNumber.charAt(i)); // берем первый символ переделываем его в стринг
+            String oneBit = switch (oneHex) { // находим символу выше соотведствие
                 case "0" -> "0000";
                 case "1" -> "0001";
                 case "2" -> "0010";
@@ -79,7 +80,7 @@ public class Solution {
                 case "f" -> "1111";
                 default -> null;
             };
-            binaryNumber += oneBit;
+            binaryNumber += oneBit; // собираем строку
         }
         return binaryNumber;
     }
