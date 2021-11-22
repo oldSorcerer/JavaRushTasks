5418b8aedcd201b15ee8563642e75192ab4d9340
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 00:45:01 +0300

    add solution for task 0914

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        return path.replaceAll("jdk.*?/", jdk + "/");
+    }
+}
