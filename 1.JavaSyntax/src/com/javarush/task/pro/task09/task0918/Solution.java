5b93a0f1fc3f12e936d03fad104bf2c164dc5cd0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 02:06:17 +0300

    add solution for task 0918

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0918/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task09.task0918;
+
+/* 
+Поработаем со StringBuilder
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        StringBuilder builder = new StringBuilder(string);
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        StringBuilder builder = new StringBuilder(string);
+        return builder.replace(start, end, str);
+    }
+}
