d863dfb290758ad3600a1cb834fc5d22dd29b021
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 03:07:10 +0300

    add solution for task 1018

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1018/Coordinate.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task10.task1018;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1018/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1018;
+
+/* 
+Геттер и сеттер в деле
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setX(15);
+        coordinate.setY(25);
+        System.out.println(coordinate.getX());
+        System.out.println(coordinate.getY());
+    }
+}
