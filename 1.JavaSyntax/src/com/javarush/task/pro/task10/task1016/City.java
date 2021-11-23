a0f6c487b44f663cfb082efd8f4c628dfa7b5a0f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 20:28:58 +0300

    add solution for task 1016

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1016/City.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1016;
+
+public class City {
+    private String name;
+    private int temperature;
+
+    public City(String name, int temperature) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1016/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1016;
+
+/* 
+Прогноз погоды
+*/
+
+public class Solution {
+
+    public static void showWeather(City city) {
+        System.out.println("В городе " + city.getName() + " сегодня температура воздуха " + city.getTemperature());
+    }
+
+    public static void main(String[] args) {
+        City dubai = new City("Дубай", 40);
+        showWeather(dubai);
+    }
+}
