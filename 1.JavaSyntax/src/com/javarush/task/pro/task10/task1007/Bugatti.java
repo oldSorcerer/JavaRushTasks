857036aaae94f73578b152d7e4f86f74590e4bd7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 03:09:47 +0300

    add solution for task 1007

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1007/Bugatti.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1007;
+
+/* 
+С крышей или без? Вот в чем вопрос
+*/
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
