bba15c3bb2ea33a910224ea8d1179087b842e033
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 20:20:33 +0300

    add solution for task 1009

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1009/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1009;
+
+/* 
+Сравнение строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s1 = new String("JavaRush");
+        s1 = "JavaRush";
+        String s2 = "JavaRush";
+//        s2 = new String("JavaRush");
+        String s3 = "JavaRush";
+//        s3 = new String("JavaRush");
+        System.out.println(s1 == s3);
+        System.out.println(s2.equals(s3));
+    }
+}
