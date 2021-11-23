97df7d4dfe7155a2f03a14dcdc340972511a70e6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 03:04:19 +0300

    add solution for task 1014

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1014/Market.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task10.task1014;
+
+/* 
+Купи 10 батонов
+*/
+
+public class Market {
+    private static boolean hasEggs = true;
+
+    public static void main(String[] args) {
+        boolean hasEggs = false;
+        makePurchases(hasEggs);
+    }
+
+    public static void makePurchases(boolean hasEggs) {
+        if (hasEggs) {
+            System.out.println("Купил 10 батонов");
+        } else {
+            System.out.println("Купил 1 батон");
+        }
+    }
+}
