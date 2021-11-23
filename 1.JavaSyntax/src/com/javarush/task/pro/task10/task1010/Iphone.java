ac44eefdaca4b53862897d0ef8edd35e6f748ddf
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-23 20:57:08 +0300

    add solution for task 1010

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Iphone.java
@@ -0,0 +1,47 @@
+package com.javarush.task.pro.task10.task1010;
+
+import java.util.Objects;
+
+/* 
+Два айфона
+*/
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+   /* @Override
+    public boolean equals(Objects obj) {
+        if (this == obj) {
+            return true;
+        }
+
+
+        return false;
+    }
+
+   /* @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Iphone iphone = (Iphone) o;
+        return price == iphone.price &&
+                Objects.equals(model, iphone.model) &&
+                Objects.equals(color, iphone.color);
+    }*/
+
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
