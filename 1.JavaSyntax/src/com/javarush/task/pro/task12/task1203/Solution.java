280d0c868462e6d238a6c981bbbf287fcb38180e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-24 23:47:11 +0300

    add solution for task 1203

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1203/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task12.task1203;
+
+/* 
+Сравнение чисел
+*/
+
+public class Solution {
+    static Integer first = 1000;
+    static Integer second = 1000;
+    static int third = 1000;
+
+    public static void main(String[] args) {
+        System.out.println(first.equals(second));
+        System.out.println(third == second);
+        System.out.println(third == first);
+    }
+}
