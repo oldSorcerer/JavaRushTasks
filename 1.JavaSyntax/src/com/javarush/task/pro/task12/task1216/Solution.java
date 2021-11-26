b4d8f4feca6d193433844bf1f17b0fe1ed15db12
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-11-26 21:32:42 +0300

    add solution for task 1216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1216/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task12.task1216;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вперед в будущее
+*/
+
+public class Solution {
+    private int number = 54;
+    private String string = "string";
+
+    public static void main(String[] args) {
+        var integer = 22;
+        var string = "string";
+        var array = new int[5];
+        var strings = new ArrayList<String>();
+        var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        var maxValue = Integer.valueOf(Integer.MAX_VALUE);
+    }
+}
