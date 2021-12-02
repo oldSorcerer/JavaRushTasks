3eae79249ed996d3f16bf73e49b2d0e763db3bbb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 02:16:11 +0300

    add solution from task 1217

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task12.task1217;
+
+/* 
+Назад в прошлое
+*/
+
+public class Solution {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
