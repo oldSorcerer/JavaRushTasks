703787763d1becff7157f99ae49b3b26d5ec1744
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 01:12:23 +0300

    add solution from task 1222

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1222/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task12.task1222;
+
+import java.util.ArrayList;
+
+/* 
+Выводим в консоли разные типы
+*/
+
+public class Solution {
+
+    public static void printAnything(ArrayList arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        var arrayList = new ArrayList<>();
+        arrayList.add(15);
+        arrayList.add("Hello");
+        arrayList.add(154);
+        arrayList.add("string");
+
+        printAnything(arrayList);
+    }
+}
