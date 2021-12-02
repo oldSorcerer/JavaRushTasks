0d1e3e4c1951a8286148f26d4a80d6600d2ab880
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 02:01:55 +0300

    add solution from task 1205

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1205/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task12.task1205;
+
+/* 
+Метод деления
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        System.out.println(a / b);
+    }
+}
