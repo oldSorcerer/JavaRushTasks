beb2b24eb021e46c40859ea75cb073a33aa4db8e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-02 18:51:28 +0300

    add solution from task 1108

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1108/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task11.task1108;
+
+/* 
+Утильный калькулятор
+*/
+
+public class Solution {
+    public static final String EQUAL = " = ";
+
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 15;
+        Calculator.add(a, b);
+        Calculator.subtract(a, b);
+        Calculator.multiply(a, b);
+        Calculator.divide(a, b);
+    }
+
+    static  class Calculator {
+
+        public static void add(int a, int b) {
+            System.out.println(a + " + " + b + EQUAL + (a + b));
+        }
+
+        public static void subtract(int a, int b) {
+            System.out.println(a + " - " + b + EQUAL + (a - b));
+        }
+
+        public static void multiply(int a, int b) {
+            System.out.println(a + " * " + b + EQUAL + (a * b));
+        }
+
+        public static void divide(int a, int b) {
+            System.out.println(a + " / " + b + EQUAL + (a / b));
+        }
+    }
+}
