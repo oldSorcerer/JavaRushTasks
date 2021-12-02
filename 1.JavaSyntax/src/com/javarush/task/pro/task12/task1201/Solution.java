7da857dd7b2b4835f343f4d65c4a53feb6bce365
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 01:08:37 +0300

    add solution from task 1201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1201/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task12.task1201;
+
+/* 
+Автоупаковка
+*/
+
+public class Solution {
+    byte byteValue;
+    short shortValue;
+    int intValue;
+    long longValue;
+
+    float floatValue;
+    double doubleValue;
+
+    char charValue;
+    boolean booleanValue;
+
+    Byte byteValueBox = Byte.valueOf(byteValue);
+    Short shortValueBox = Short.valueOf(shortValue);
+    Integer integerValueBox = Integer.valueOf(intValue);
+    Long longValueBox = Long.valueOf(longValue);
+
+    Float floatValueBox = Float.valueOf(floatValue);
+    Double doubleValueBox = Double.valueOf(doubleValue);
+
+    Character characterValueBox = Character.valueOf(charValue);
+    Boolean booleanValueBox = Boolean.valueOf(booleanValue);
+
+}
