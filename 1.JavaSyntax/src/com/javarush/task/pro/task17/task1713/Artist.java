7b35a8f54e850decd7b195cead988b693a4293af
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:10:49 +0300

    add solution from task 1713

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1713/Artist.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task17.task1713;
+
+/* 
+Да начнется представление!
+*/
+
+public interface Artist extends Dance, Sing {
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1713/Dance.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task17.task1713;
+
+public interface Dance {
+}
