9d98cacab83e69c5f96d61a212fec67f31a4a218
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 23:57:32 +0300

    add solution from task 1710

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1710/Person.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task17.task1710;
+
+/* 
+От абстракции к реальности
+*/
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
