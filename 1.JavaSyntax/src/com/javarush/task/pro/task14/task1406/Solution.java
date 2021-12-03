b812151f590461f16efb3c12f0a2e4b513d7601e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:55:31 +0300

    add solution from task 1406

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1406/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task14.task1406;
+
+import java.util.Scanner;
+
+/* 
+Купи слона
+*/
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        buyElephant(null, scanner);
+    }
+
+    static void buyElephant(String answer, Scanner scanner) {
+        if (answer == null) {
+            System.out.println("Купи слона");
+        } else if (answer.toLowerCase().equals("ок")) {
+            System.out.println("Так-то лучше :) Список твоих отговорок:");
+            throw new SecurityException();
+        } else {
+            System.out.println("Все говорят \"" + answer + "\", а ты купи слона");
+        }
+
+        answer = scanner.nextLine();
+
+        try {
+            buyElephant(answer, scanner);
+        } catch (Exception e) {
+            System.out.println(answer);
+        }
+    }
+}
