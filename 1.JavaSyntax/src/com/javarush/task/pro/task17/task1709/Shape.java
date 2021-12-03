13154d4eb1c96d60911284677ca7d0209bd48e03
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 23:54:16 +0300

    add solution from task 1709

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+public abstract class Shape {
+
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+}
