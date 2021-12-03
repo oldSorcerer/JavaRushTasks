6848b8c91e45b30ed0b14edf71b7a1e6b8864d85
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:02:35 +0300

    add solution from task 1712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1712/Carrot.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task17.task1712;
+
+public class Carrot implements Eatable {
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1712/Eatable.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task17.task1712;
+
+/* 
+Съедобное — несъедобное
+*/
+
+public interface Eatable {
+}
