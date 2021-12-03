a1f881d5fc05ab44623474e78624b0e97dc6813c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:46:09 +0300

    add solution from task 1219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+/* 
+Двойные фигурные скобки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>()
+        {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
