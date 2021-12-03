4adf8aa632b5dd44fbc82e9c48b391ccc1a91aad
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:51:18 +0300

    add solution from task 1221

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/Faculty.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task12.task1221;
+
+import java.util.ArrayList;
+
+/* 
+Дженерики и студенты
+*/
+
+public class Faculty {
+
+    public static void main(String[] args) {
+        var students = new ArrayList<Student>();
+        students.add(new Student("Акакий"));
+        students.add(new Student("Любослав"));
+
+        printStudentNames(students);
+    }
+
+    public static void printStudentNames(ArrayList<Student> students) {
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getName());
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/Student.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task12.task1221;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
