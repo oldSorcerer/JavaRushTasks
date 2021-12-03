25d306b40bd505c9f956ef1d9435140d8a1f3124
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 00:47:43 +0300

    add solution from task 1218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1218/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task12.task1218;
+
+import java.io.File;
+import java.util.*;
+
+/* 
+Shine bright like a diamond
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
