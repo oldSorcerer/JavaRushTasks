c621a5efb4737c1fe496a5d5adb520fc6b817cb6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-03 18:37:11 +0300

    add solution from task 1206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1206/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.pro.task12.task1206;
+
+/* 
+Анализ строк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Думаю, это будет новой фичей." +
+                "Только не говорите никому, что она возникла случайно.";
+
+        System.out.println("Количество цифр в строке : " + countDigits(string));
+        System.out.println("Количество букв в строке : " + countLetters(string));
+        System.out.println("Количество пробелов в строке : " + countSpaces(string));
+    }
+
+    public static int countDigits(String string) {
+        char[] chars = string.toCharArray();
+        int count = 0;
+        for (char aChar : chars) {
+            if (Character.isDigit(aChar)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countLetters(String string) {
+        char[] chars = string.toCharArray();
+        int count = 0;
+        for (char aChar : chars) {
+            if (Character.isLetter(aChar)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countSpaces(String string) {
+        char[] chars = string.toCharArray();
+        int count = 0;
+        for (char aChar : chars) {
+            if (Character.isSpaceChar(aChar)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
