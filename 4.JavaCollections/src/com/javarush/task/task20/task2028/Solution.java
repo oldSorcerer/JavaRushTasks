4cc6617452823f2e2a07f3c97fdb0ad1664ca9c2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 17:12:47 +0300

    add solution collections from task 2028

---
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task20.task2028;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+    }
+}
