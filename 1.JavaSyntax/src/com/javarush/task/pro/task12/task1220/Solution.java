79491ace03ddffc2c090e8183b160b41111993d6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 17:14:54 +0300

    add solution Syntax task 1220

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1220/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task12.task1220;
+
+import java.util.ArrayList;
+
+/* 
+Охотники за привидением: в погоне за типами
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var elements = new ArrayList<>();
+        elements.add("Привет");
+        elements.add(10);
+        elements.add(new Integer[15]);
+        elements.add(new LinkageError());
+
+        checkElementsType(elements);
+    }
+
+    public static void checkElementsType(ArrayList<Object> elements) {
+        for (Object element : elements) {
+            if (element instanceof String str) {
+                str.printString();
+            }
+        }
+    }
+
+    public static void printString() {
+        System.out.println("Строка");
+    }
+
+    public static void printInteger() {
+        System.out.println("Целое число");
+    }
+
+    public static void printIntegerArray() {
+        System.out.println("Массив целых чисел");
+    }
+
+    public static void printUnknown() {
+        System.out.println("Другой тип данных");
+    }
+}
