7ec0c393af5de65cd1942c26d3d580f05d913da2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-04 17:56:57 +0300

    add solution Syntax task 1210

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1210/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1210/Solution.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static int[] numbers = new int[10];
+    public static ArrayList<Integer> numbers = new ArrayList<>();
 
     public static void main(String[] args) {
         init();
@@ -19,16 +19,16 @@ public class Solution {
 
     public static void init() {
         for (int i = 0; i < 10; i++) {
-            numbers[i] = i;
+            numbers.add(i);
         }
     }
 
     public static void reverse() {
-        int n = numbers.length - 1;
-        for (int i = 0; i < numbers.length / 2; i++) {
-            int temp = numbers[i];
-            numbers[i] = numbers[n - i];
-            numbers[n - i] = temp;
+        int n = numbers.size() - 1;
+        for (int i = 0; i < numbers.size() / 2; i++) {
+            int temp = numbers.get(i);
+            numbers.set(i, numbers.get(n - i));
+            numbers.set(n - i, temp);
         }
     }
 
