95c9f3ea2bc27532191430e18a59c7d0c2c3be53
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-06 16:03:35 +0300

    add solution Syntax task 1302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1302/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task13.task1302;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
+
+/* 
+Проверка присутствия
+*/
+
+public class Solution {
+    public static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
+
+    public static void checkWords(String word) {
+        if (words.contains(word)) {
+            System.out.println("Слово " + word + " есть в множестве");
+        } else {
+            System.out.println("Слова " + word + " нет в множестве");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkWords("JavaScript");
+        checkWords("Java");
+    }
+}
