42909cceb9527df88060c894f277b20516fba438
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-06 16:28:33 +0300

    add solution Game mini task 01

---
+++ 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
@@ -0,0 +1,11 @@
+package com.javarush.games.minigames.mini01;
+
+import com.javarush.engine.cell.Game;
+
+public class FirstGame extends Game {
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+}
