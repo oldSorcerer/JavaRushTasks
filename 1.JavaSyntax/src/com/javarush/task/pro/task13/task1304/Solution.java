10bb9b836f29fee749005154e4b67f2da6c329b6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-06 18:29:08 +0300

    add solution Syntax task 1304

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1304/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task13.task1304;
+
+import java.util.*;
+
+/* 
+Переводим итератор в цикл for-each
+*/
+
+public class Solution {
+
+    public static void printList(ArrayList<String> words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void printHashSet(HashSet<String> words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] wordsArray = "Думаю, это будет новой фичей. Только не говорите никому, что она возникла случайно.".split(" ");
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(wordsArray));
+        HashSet<String> wordsHashSet = new HashSet<>(wordsList);
+        printList(wordsList);
+        System.out.println("__________________________________");
+        printHashSet(wordsHashSet);
+    }
+}
