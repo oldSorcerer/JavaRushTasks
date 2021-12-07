39e23127ba6f4c0cdcd5583a5c2ce649c59bcf45
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:32:43 +0300

    add solution Core task 1115

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1115/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task11.task1115;
+
+/* 
+От школьника до пенсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student{
+
+    }
+
+    public class Retiree extends Worker{
+
+    }
+
+}
