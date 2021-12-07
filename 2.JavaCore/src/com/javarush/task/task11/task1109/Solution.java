f4e79014a0212a378f7d270ddb78c455cad802ce
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:19:17 +0300

    add solution Core task 1109

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1109/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task11.task1109;
+
+/* 
+Как кошка с собакой
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Vaska", 5);
+        Dog dog = new Dog("Sharik", 4);
+
+        cat.isDogNear(dog);
+        dog.isCatNear(cat);
+    }
+}
+
+class Cat {
+    private String name;
+    private int speed;
+
+    public Cat(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isDogNear(Dog dog) {
+        return this.speed > dog.getSpeed();
+    }
+}
+
+class Dog {
+    private String name;
+    private int speed;
+
+    public Dog(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isCatNear(Cat cat) {
+        return this.speed > cat.getSpeed();
+    }
+}
+
+
+
