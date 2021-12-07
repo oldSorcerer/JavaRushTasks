2130803ae4b0032c52f302a77ef31103785deca0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:48:27 +0300

    add solution Core task 1121

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1121/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task11.task1121;
+
+/* 
+Чужой код, незнакомый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
