11a372afe9aa9ab503a15f46eb9db72d37dd25de
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:29:21 +0300

    add solution Core task 1112

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1112/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task11.task1112;
+
+/* 
+iPhone и Apple
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple {
+
+    }
+}
