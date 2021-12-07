d64b191a5a0e6532bca2d3bf0227ea7becbbb97f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:40:04 +0300

    add solution Core task 1118

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1118/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task11.task1118;
+
+/* 
+Городское хозяйство
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
