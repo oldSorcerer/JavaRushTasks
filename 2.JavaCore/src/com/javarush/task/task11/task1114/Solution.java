4568bf01f03742475d341deece7b6fcf8c43e978
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-07 11:31:30 +0300

    add solution Core task 1114

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1114/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task11.task1114;
+
+/* 
+Происхождение видов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish {
+
+    }
+
+    public class Dinosaur extends Lizard{
+
+    }
+
+}
