a71e19e88b706225d629a8f184588e2257ee1117
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:44:09 +0300

    add solution Core task 1211

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1211/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1211;
+
+/* 
+Абстрактный класс Pet
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+    }
+
+}
