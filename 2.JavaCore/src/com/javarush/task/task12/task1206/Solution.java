cf1c5f4b50c5bbfefb52a6734dde3e90d3bc442d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:25:05 +0300

    add solution Core task 1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i) {
+    }
+
+    public static void print(String str) {
+    }
+}
