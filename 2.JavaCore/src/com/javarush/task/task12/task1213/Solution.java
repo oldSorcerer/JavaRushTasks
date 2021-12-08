0ef73042f9066f6f421a8e049bbcb49d8e78015c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:46:01 +0300

    add solution Core task 1213

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1213/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1213;
+
+/* 
+«Исправь код», часть 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+
+}
