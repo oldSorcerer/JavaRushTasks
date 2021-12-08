fa3a96861a65d8134b5c662f38bb65dd67d0a38f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:28:32 +0300

    add solution Core task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print(new Integer(1));
+    }
+
+    public static void print(int i) {
+    }
+
+    public static void print(Integer i) {
+    }
+}
