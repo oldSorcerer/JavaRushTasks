70999f20f539bf318e7f79cdce9fc11ab11691a9
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:54:29 +0300

    add solution Core task 1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+    }
+
+    public interface CanRun {
+        void run();
+    }
+
+    public interface CanSwim {
+        void swim();
+    }
+}
