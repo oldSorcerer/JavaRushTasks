a56a10498bf77d9bbe4c4d629ce349af7e39d6f8
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:36:32 +0300

    add solution Core task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String str) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(Integer i) {
+
+    }
+
+    public static void print(String str, int i) {
+
+    }
+
+    public static void print(String str, int i, Integer x) {
+
+    }
+}
