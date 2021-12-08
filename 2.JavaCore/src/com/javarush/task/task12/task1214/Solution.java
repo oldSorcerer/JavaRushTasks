74b14b08b4935938cc26bf975fb0db026f88e847
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:47:07 +0300

    add solution Core task 1214

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1214/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1214;
+
+/* 
+Корова — тоже животное
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Animal {
+        public abstract String getName();
+    }
+
+    public static class Cow extends Animal {
+        @Override
+        public String getName() {
+            return "Корова";
+        }
+    }
+
+}
