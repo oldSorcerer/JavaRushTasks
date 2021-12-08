b262806bfb1422ea12d48b0fe21d98af23d1e25e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 02:48:26 +0300

    add solution Core task 1215

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task12.task1215;
+
+/* 
+Кошки не должны быть абстрактными!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet {
+
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+
+    public static class Dog extends Pet {
+
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+
+}
