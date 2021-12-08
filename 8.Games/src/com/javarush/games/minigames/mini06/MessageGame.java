07a65de07107caab3bde5f20268820b32ee8d474
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-08 15:21:20 +0300

    add solution Game mini task 06

---
+++ 8.Games/src/com/javarush/games/minigames/mini06/MessageGame.java
@@ -0,0 +1,26 @@
+package com.javarush.games.minigames.mini06;
+
+import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.Color;
+
+/* 
+Диалоговые окна
+*/
+
+public class MessageGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(3, 3);
+    }
+
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        //напишите тут ваш код
+    }
+
+    @Override
+    public void onMouseRightClick(int x, int y) {
+        //напишите тут ваш код
+    }
+}
