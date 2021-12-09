7b61964a50c437e3a4add700f3dd4d74c9223420
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-09 12:26:50 +0300

    add solution Core task 1219

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1219/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task12.task1219;
+
+/* 
+Создаем человека
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+
+    public class Human implements CanRun, CanSwim {
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Duck implements CanFly, CanSwim, CanRun {
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Penguin implements CanSwim, CanRun{
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Airplane implements CanFly {
+
+        @Override
+        public void fly() {
+
+        }
+    }
+}
