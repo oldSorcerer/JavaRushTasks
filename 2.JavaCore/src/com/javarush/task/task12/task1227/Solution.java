f2017e8237b408deebe42b1c41fb9008cce716ff
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-10 20:20:00 +0300

    add solution Core task 1227

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1227/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task12.task1227;
+
+/* 
+CanFly, CanRun, CanSwim для классов Duck, Penguin, Toad
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+    public class Duck {
+
+    }
+
+    public class Penguin {
+
+    }
+
+    public class Toad {
+
+    }
+}
