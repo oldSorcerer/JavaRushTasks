453c1bbade7522eeadbdf69af9ee1a0c93e712d1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-10 20:04:18 +0300

    add solution Core task 1226

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1226/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1226;
+
+/* 
+Лазать, летать и бегать
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Cat {
+    }
+
+    public class Dog {
+    }
+
+    public class Tiger extends Cat {
+    }
+
+    public class Duck {
+    }
+}
