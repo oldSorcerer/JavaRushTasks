e8e92f460ac72ec5408d7c86c34c44aed638d655
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-10 21:32:51 +0300

    add solution Core task 1232

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1232/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1232;
+
+/* 
+Нужно добавить в программу новую функциональность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Pegasus horse = new Pegasus();
+    }
+
+    public static interface CanFly {
+        public void fly();
+    }
+
+    public static class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus {
+    }
+}
