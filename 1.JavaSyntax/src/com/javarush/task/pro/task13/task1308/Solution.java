40fa07a60b6526d3b71c59d223d3fa7e2ae96c4e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-11 15:30:53 +0300

    add solution Syntax task 1308

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1308/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task13.task1308;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Изучаем методы класса Collections, часть 3
+*/
+
+public class Solution {
+
+    public static Integer min(ArrayList<Integer> list) {
+        return Collections.min(list);
+    }
+
+    public static Integer max(ArrayList<Integer> list) {
+        return Collections.max(list);
+    }
+
+    public static int frequency(ArrayList<Integer> list, Integer element) {
+        return Collections.frequency(list, element);
+    }
+
+    public static int binarySearch(ArrayList<Integer> list, Integer key) {
+        Collections.sort(list);
+        return Collections.binarySearch(list, key);
+    }
+}
