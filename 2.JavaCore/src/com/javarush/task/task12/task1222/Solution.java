96af96e405b280349040bf14ed9daba7c40e577a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-13 01:34:46 +0300

    add solution Core task 1222

--- 2.JavaCore/src/com/javarush/task/task12/task1222/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1222/Solution.java
@@ -30,5 +30,9 @@ public class Solution {
 
     public static class Cat extends Pet {
 
+        @Override
+        public void setName(String name) {
+            this.name = "Я - кот";
+        }
     }
 }
