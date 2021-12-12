8b6fa038087c38f9829f6f29cc01b9b0f94dd1ce
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-13 02:14:51 +0300

    add solution Core task 1232

--- 2.JavaCore/src/com/javarush/task/task12/task1232/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1232/Solution.java
@@ -19,6 +19,10 @@ public class Solution {
         }
     }
 
-    public static class Pegasus {
+    public static class Pegasus extends Horse implements CanFly {
+        @Override
+        public void fly() {
+
+        }
     }
 }
