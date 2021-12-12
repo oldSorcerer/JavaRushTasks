b8b83af0a78aca49ead2096cfb8310737f02e75b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-13 00:50:05 +0300

    add solution Syntax task 1317

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1317/Month.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task13.task1317;
+
+public class Month {
+
+    public static final Month JANUARY = new Month(0);
+    public static final Month FEBRUARY = new Month(1);
+    public static final Month MARCH = new Month(2);
+    public static final Month APRIL = new Month(3);
+    public static final Month MAY = new Month(4);
+    public static final Month JUNE = new Month(5);
+    public static final Month JULY = new Month(6);
+    public static final Month AUGUST = new Month(7);
+    public static final Month SEPTEMBER = new Month(8);
+    public static final Month OCTOBER = new Month(9);
+    public static final Month NOVEMBER = new Month(10);
+    public static final Month DECEMBER = new Month(11);
+
+    private static final Month[] array = {JANUARY, FEBRUARY, MARCH,
+            APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER,
+            NOVEMBER, DECEMBER };
+
+    private final int value;
+
+    private Month(int value) {
+        this.value = value;
+    }
+
+    public static Month[] values() {
+        return array;
+    }
+
+    public int ordinal() {
+        return this.value;
+    }
+}
