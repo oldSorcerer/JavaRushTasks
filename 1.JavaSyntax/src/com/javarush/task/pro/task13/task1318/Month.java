77b949aeb3aaf1a7377a2810dfd020185203d14e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-13 01:04:46 +0300

    add solution Syntax task 1318

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Month.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task13.task1318;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
