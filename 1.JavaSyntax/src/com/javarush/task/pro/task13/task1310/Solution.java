9aa7ac605ed5ac5c9fd7c1d77dcc3ab78fb87097
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-12 16:08:47 +0300

    add solution Syntax task 1310

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1310/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task13.task1310;
+
+import java.util.HashMap;
+
+/* 
+Успеваемость студентов-2
+*/
+
+public class Solution {
+    public static HashMap<String, Double> grades = new HashMap<>();
+
+    public static void main(String[] args) {
+        addStudents();
+        System.out.println("Список студентов группы: ");
+        printStudents();
+        System.out.print("Средний балл группы: " + getAverageMark());
+    }
+
+    public static void addStudents() {
+        grades.put("Давыдов Олег", 4.3d);
+        grades.put("Шульга Николай", 4.1d);
+        grades.put("Колос Василий", 4.9d);
+        grades.put("Шевченко Тарас", 3.7d);
+        grades.put("Марчук Любослав", 3.2d);
+    }
+
+    public static void printStudents() {
+        for (String key : grades.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    public static Double getAverageMark() {
+        double sum = 0.0;
+        for (String key : grades.keySet()) {
+            sum += grades.get(key);
+        }
+        return sum / grades.size();
+    }
+}
