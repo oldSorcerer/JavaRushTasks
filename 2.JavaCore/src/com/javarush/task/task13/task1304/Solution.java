dda12bd8a16bfa3cb58db9e7c8b13084d1dcd1ef
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-13 02:28:07 +0300

    add solution Core task 1304

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task13.task1304;
+
+/* 
+Selectable и Updatable
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    public class Screen implements Selectable, Updatable{
+
+        @Override
+        public void onSelect() {
+
+        }
+
+        @Override
+        public void refresh() {
+
+        }
+    }
+}
