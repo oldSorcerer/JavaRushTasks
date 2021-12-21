c826668f631b766b5f628879a696c77987576bc6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-21 23:44:50 +0300

    add solution Syntax task 1409

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1409/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task14.task1409;
+
+/* 
+Пропускаем сквозь себя
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        generateLuckyNumber();
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception("не повезло");
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
