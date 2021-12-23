73dc9082939e329872e968efb2cd430fdd5b9f44
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-23 20:01:34 +0300

    add solution Syntax task 1415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task14.task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/* 
+Стек в домашних условиях
+*/
+
+public class MyStack {
+
+    private final List<String> storage = new ArrayList<>();
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        String str =  storage.get(0);
+        storage.remove(0);
+        return str;
+    }
+
+    public String peek() {
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        int index = -1;
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).equals(s)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
