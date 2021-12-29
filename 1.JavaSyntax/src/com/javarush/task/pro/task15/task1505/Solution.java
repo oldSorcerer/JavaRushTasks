ef079a9794e1062f5b955623fffebe1189759a01
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-29 23:07:38 +0300

    add solution Syntax task 1505

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1505/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task15.task1505;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/* 
+Что-то не копируется...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in);
+             var inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
+             var outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))
+        ) {
+            int size = 1024;
+            byte[] buffer = new byte[size];
+            while (inputStream.available() > 0) {
+                int read = inputStream.read(buffer);
+                outputStream.write(buffer, 0, read);
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
