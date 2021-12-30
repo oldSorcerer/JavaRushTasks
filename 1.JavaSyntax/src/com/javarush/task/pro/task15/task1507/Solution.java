6f39ba72bb6dc929bedff57332f3e45453971101
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-31 00:01:27 +0300

    add solution Syntax task 1507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1507/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1507;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Пропускаем не всех
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+    }
+}
+
