db888c41613c8ea3b4fd284080a2a9091526a29d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-31 23:54:54 +0300

    add solution Syntax task 1518

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1518/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task15.task1518;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+А что же внутри папки?
+*/
+
+public class Solution {
+
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path directory = Path.of(scanner.nextLine());
+
+        DirectoryStream<Path> files = Files.newDirectoryStream(directory);
+
+        for (Path path : files) {
+            if (Files.isRegularFile(path)) {
+                System.out.println(path + THIS_IS_FILE);
+            } else if (Files.isDirectory(path)) {
+                System.out.println(path + THIS_IS_DIR);
+            }
+        }
+
+    }
+}
+
