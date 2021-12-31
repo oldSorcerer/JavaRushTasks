d6a8196d2a6884f31b38e09a10a25c28853c1b12
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-31 14:01:22 +0300

    add solution Syntax task 1515

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -11,7 +11,14 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        //напишите тут ваш код
+
+        Path path = Path.of(str);
+        if (path.isAbsolute()) {
+            System.out.println(str);
+        } else {
+            System.out.println(path.toAbsolutePath());
+        }
+
     }
 }
 
