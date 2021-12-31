3e85aecb58f68e383e244594085c212664368d67
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-01 01:02:40 +0300

    add solution Syntax task 1519

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1519/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task15.task1519;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Поверхностное копирование
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+
+
+
+    }
+}
+
