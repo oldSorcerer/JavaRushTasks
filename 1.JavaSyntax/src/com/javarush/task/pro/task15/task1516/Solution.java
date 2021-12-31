8d52452989cfa858a9ec04e409dbe6234308cb22
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2021-12-31 23:25:26 +0300

    add solution Syntax task 1516

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.isEmpty()) {
+                break;
+            }
+
+            if (Files.isRegularFile(Path.of(str))) {
+                System.out.println(str + THIS_IS_FILE);
+            } else if (Files.isDirectory(Path.of(str))) {
+                System.out.println(str + THIS_IS_DIR);
+            } else {
+                break;
+            }
+
+        }
+
+    }
+}
+
