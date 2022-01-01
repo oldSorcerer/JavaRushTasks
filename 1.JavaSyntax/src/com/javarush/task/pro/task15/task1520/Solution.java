ea173ea67c798909ff6388fbb49516469c49b421
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-01 17:05:16 +0300

    add solution Syntax task 1520

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1520/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task15.task1520;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Перемещение файлов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+
+        try(DirectoryStream<Path> files = Files.newDirectoryStream(sourceDirectory)) {
+            for (Path path : files) {
+                if (Files.isRegularFile(path)) {
+                    Files.move(path, targetDirectory.resolve(path.getFileName()));
+                }
+            }
+        }
+
+
+    }
+}
+
