4afc3a98a8c758fee6800cbdb26137b176da4863
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-01 21:44:33 +0300

    add solution Syntax task 1509

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1509/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task15.task1509;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
+
+/* 
+Еще раз читаем из консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (InputStream stream = System.in;
+             Scanner scanner = new Scanner(stream)) {
+            String line = scanner.nextLine();
+            char[] chars = line.toCharArray();
+            Set<Character> characters = new HashSet<>();
+            for (char aChar : chars) {
+                if (Character.isAlphabetic(aChar)) {
+                    characters.add(Character.toLowerCase(aChar));
+                }
+            }
+            System.out.println(characters);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
