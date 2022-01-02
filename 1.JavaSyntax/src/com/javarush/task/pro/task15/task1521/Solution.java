53938dc1d2850eeff671a345e6997bebe29fa17f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-02 14:01:20 +0300

    add solution Syntax task 1521

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1521/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task15.task1521;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Временное сохранение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        //напишите тут ваш код
+    }
+}
