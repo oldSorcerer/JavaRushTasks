e22ed4213b5bbe6a07490ed22a4f39b488cd9914
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-02 12:15:53 +0300

    add solution Syntax task 1511

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1511/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task15.task1511;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Пишем символы в файл
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] chars = args[0].toCharArray();
+        try (Scanner scanner = new Scanner(System.in)) {
+           BufferedWriter writer = Files.newBufferedWriter(Path.of(scanner.nextLine()));
+           writer.write(chars);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
+
