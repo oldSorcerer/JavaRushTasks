6b8c2d6b83ea4e5d8978ec63f13e263b42457b7c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-10 17:22:33 +0300

    add solution Core task 1815

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1815/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task18.task1815;
+
+import java.util.List;
+
+/* 
+Таблица
+*/
+
+public class Solution {
+    public class TableInterfaceWrapper implements TableInterface {
+
+        private TableInterface tableInterface;
+
+        public TableInterfaceWrapper(TableInterface tableInterface) {
+            this.tableInterface = tableInterface;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            tableInterface.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return tableInterface.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            tableInterface.setHeaderText(newHeaderText);
+        }
+    }
+
+    public interface TableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
