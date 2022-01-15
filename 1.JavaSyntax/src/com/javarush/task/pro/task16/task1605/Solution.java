3f5e2b19f92bffd71b6568c1a90c7288e717cd6b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-15 13:05:16 +0300

    add solution Syntax task 1605

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1605/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task16.task1605;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+Карманный телепорт
+*/
+
+public class Solution {
+
+    public static final String INVENTED = "Пожалуйста, можете забрать ваш карманный телепорт. Спасибо за ожидание!";
+    public static final String NOT_INVENTED = "Извините, телепорт еще не изобрели, приходите через 10 лет.";
+
+    public static final Calendar INVENTION_DAY = new GregorianCalendar(2192, Calendar.JUNE, 12);
+
+    public static void main(String[] args) {
+        Calendar currentDay = Calendar.getInstance();
+        while (!isTeleportInvented(currentDay)) {
+            currentDay.add(Calendar.YEAR, 10);
+        }
+    }
+
+    static boolean isTeleportInvented(Calendar currentDay) {
+        if (currentDay.after(INVENTION_DAY)) {
+            System.out.println(INVENTED);
+            return true;
+        } else {
+            System.out.println(NOT_INVENTED);
+            return false;
+        }
+
+    }
+}
