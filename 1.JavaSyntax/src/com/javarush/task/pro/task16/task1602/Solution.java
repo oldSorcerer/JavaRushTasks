5d1c7c7a62b1560c26327b7917733944110ad2d2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-15 14:11:50 +0300

    add solution Syntax task 1602

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1602/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task16.task1602;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/* 
+Подчищаем хвосты
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<Date> dateList = new ArrayList<>();
+        dateList.add(new Date(2015, 12, 25, 20, 40));
+        dateList.add(new Date("July 20, 1969"));
+        dateList.add(new Date(1989, 11, 9));
+        dateList.add(new Date("January 1, 2000"));
+
+        System.out.println("before fixes:");
+        dateList.forEach(System.out::println);
+
+        fixDate(dateList);
+
+        System.out.println("after fixes:");
+        dateList.forEach(System.out::println);
+    }
+
+    static void fixDate(List<Date> brokenDates) {
+        Date date = new Date(122, 0, 15);
+        for (Date brokenDate : brokenDates) {
+            if (brokenDate.after(date)) {
+                brokenDate.setMonth(brokenDate.getMonth() - 1);
+                int year = brokenDate.getYear() - 1900;
+                brokenDate.setYear(year);
+            }
+        }
+
+    }
+}
