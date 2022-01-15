2f8b63d5153b2c9d72dd1a685eb86dc4d5e62a42
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-15 18:22:48 +0300

    add solution Syntax task 1609

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1609/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task16.task1609;
+
+import java.time.LocalDate;
+
+/* 
+С годами моложе
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        LocalDate jubilee40Years = LocalDate.of(2010, 3, 13);
+
+        LocalDate jubilee30Years = jubilee40Years.minusYears(10);
+
+        System.out.println(Math.abs(jubilee30Years.getYear()));
+    }
+}
