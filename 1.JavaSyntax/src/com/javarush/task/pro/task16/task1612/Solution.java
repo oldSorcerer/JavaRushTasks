7bb4312a952b983aef66c42783243b5b5fc7cd56
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-15 23:53:28 +0300

    add solution Syntax task 1612

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1612/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task16.task1612;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+
+/* 
+Синтезируем LocalDateTime
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<LocalDate, List<LocalTime>> dateMap = DateTimeGenerator.generateDateMap();
+        printCollection(dateMap.entrySet());
+
+        Set<LocalDateTime> dateSet = convert(dateMap);
+        printCollection(dateSet);
+    }
+
+    static Set<LocalDateTime> convert(Map<LocalDate, List<LocalTime>> sourceMap) {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static void printCollection(Collection<?> collection) {
+        System.out.println("-----------------------------------------------------");
+        collection.forEach(System.out::println);
+    }
+}
