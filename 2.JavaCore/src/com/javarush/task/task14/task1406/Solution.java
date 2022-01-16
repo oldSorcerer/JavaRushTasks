f6a2bbcdc9a40e41399f1599488e63366a20987b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-16 14:21:38 +0300

    add solution Core task 1406

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1406/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task14.task1406;
+
+/* 
+Без ошибок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Object obj = new Jerry();
+
+        Mouse mouse = (Mouse) obj;
+        GrayMouse grayMouse = (GrayMouse) mouse;
+        Jerry jerry = (Jerry) grayMouse;
+
+        printClasses(obj, mouse, grayMouse, jerry);
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GrayMouse grayMouse, Jerry jerry) {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(grayMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse {
+    }
+
+    static class GrayMouse extends Mouse {
+    }
+
+    static class Jerry extends GrayMouse {
+    }
+}
