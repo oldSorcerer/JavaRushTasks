8d21b2c27ab0e9f6aa7973f5985a7a9b56146c3f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-16 14:40:55 +0300

    add solution Core task 1407

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1407/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task14.task1407;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Player and Dancer
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Person person = null;
+        String key;
+        while (!(key = reader.readLine()).equals("exit")) {
+            if ("player".equals(key)) {
+                person = new Player();
+            } else if ("dancer".equals(key)) {
+                person = new Dancer();
+            }
+            haveFun(person);
+        }
+    }
+
+    public static void haveFun(Person person) {
+        if (person instanceof Player) {
+            ((Player) person).play();
+        }
+        if (person instanceof Dancer) {
+            ((Dancer) person).dance();
+        }
+    }
+
+    interface Person {
+    }
+
+    static class Player implements Person {
+        void play() {
+            System.out.println("playing");
+        }
+    }
+
+    static class Dancer implements Person {
+        void dance() {
+            System.out.println("dancing");
+        }
+    }
+}
