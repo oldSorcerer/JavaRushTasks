8b7bf47bb9789ff8c4fa47a8473172ea42bfe976
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-16 13:58:47 +0300

    add solution Core task 1403

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1403/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task14.task1403;
+
+/* 
+Building и School
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Building school = getSchool();
+        Building shop = getBuilding();
+
+        System.out.println(school);
+        System.out.println(shop);
+    }
+
+    public static Building getSchool() {
+        return new School();
+    }
+
+    public static Building getBuilding() {
+        return new Building();
+    }
+
+    static class School extends Building {
+        @Override
+        public String toString() {
+            return "School";
+        }
+    }
+
+    static class Building {
+        @Override
+        public String toString() {
+            return "Building";
+        }
+    }
+}
