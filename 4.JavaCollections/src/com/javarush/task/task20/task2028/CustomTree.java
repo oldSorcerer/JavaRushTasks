43fc9a995c5a7d1e45fc815896a88d80a433008c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-18 19:32:08 +0300

    add solution Collections task 2028_1

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,8 +2,6 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
 
 /* 
 Построй дерево(1)
@@ -11,73 +9,13 @@ import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
 
-    Entry<String> root;
-
-    public CustomTree() {
-
-    }
-
     @Override
     public String get(int index) {
-        throw  new UnsupportedOperationException();
-    }
-
-    @Override
-    public String set(int index, String element) {
-        throw  new UnsupportedOperationException();
-    }
-
-    @Override
-    public void add(int index, String element) {
-        throw  new UnsupportedOperationException();
-    }
-
-    @Override
-    public String remove(int index) {
-        throw  new UnsupportedOperationException();
-    }
-
-    @Override
-    public List<String> subList(int fromIndex, int toIndex) {
-        throw  new UnsupportedOperationException();
-    }
-    @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-        throw  new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends String> c) {
-        throw  new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public int size() {
         return 0;
-    }
-
-    public String getParent(String s) {
-        return null;
-
-    }
-
-    static class Entry<T> implements Serializable {
-        String elementName;
-        boolean availableToAddLeftChildren;
-        boolean availableToAddRightChildren;
-        Entry<T> parent;
-        Entry<T> leftChild;
-        Entry<T> rightChild;
-
-        public Entry (String elementName) {
-            this.elementName = elementName;
-            availableToAddLeftChildren = true;
-            availableToAddRightChildren = true;
-        }
-
-        public boolean isAvailableToAddChildren() {
-            return this.availableToAddLeftChildren || this.availableToAddRightChildren;
-        }
-
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -5,13 +5,5 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
-
-        for (int i = 1; i < 16; i++) {
-            list.add(String.valueOf(i));
-        }
-
-        System.out.println("The list size is " + list.size());
-        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
-        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
