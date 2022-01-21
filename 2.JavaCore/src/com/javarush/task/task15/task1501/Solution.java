ee469020660202193f5a6f82b0f32137f34c357a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-21 17:11:42 +0300

    add solution Core task 1501

--- 2.JavaCore/src/com/javarush/task/task15/task1501/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1501/Solution.java
@@ -6,7 +6,6 @@ package com.javarush.task.task15.task1501;
 
 public class Solution {
     public static void main(String[] args) {
-
     }
 
     public interface Movable {
@@ -26,12 +25,10 @@ public class Solution {
         public boolean isMovable() {
             return false;
         }
-
         @Override
         public Object getAllowedAction(String name) {
             return null;
         }
-
         @Override
         public Object getAllowedAction() {
             return null;
