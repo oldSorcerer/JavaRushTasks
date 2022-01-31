56f7de5fab4f83793a06fa07bd7009e9b15d7349
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-01-31 18:24:15 +0300

    add solution Core task 1511

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1511/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task15.task1511;
+
+import java.io.Serializable;
+
+/* 
+Максимально простой код-1
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        JuniorJavaDev me = new JuniorJavaDev();
+        System.out.println(me.askHubert("What do you think about this task?"));
+        System.out.println(me.askZapp("When will the next update happen?"));
+    }
+
+    public interface SpecificSerializable extends Serializable {
+    }
+
+    public static class JavaDev implements SpecificSerializable {
+        String answerQuestion(String question) {
+            return String.format("I'll be thinking of [%s]", question);
+        }
+    }
+
+    public static class JuniorJavaDev extends JavaDev {
+        JavaDev zapp = new JavaDev();
+        JavaDev hubert = new JavaDev();
+
+        String askZapp(String question) {
+            return zapp.answerQuestion(question);
+        }
+
+        String askHubert(String question) {
+            return hubert.answerQuestion(question);
+        }
+    }
+}
