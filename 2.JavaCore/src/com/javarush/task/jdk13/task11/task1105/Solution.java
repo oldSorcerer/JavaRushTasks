f7def4a97b42a671d536b4bec7f4a14f13a37e44
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-06 19:50:16 +0300

    add solution Core task 1105

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1105/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task11.task1105;
+
+/* 
+Неприступный кот
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        private void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
