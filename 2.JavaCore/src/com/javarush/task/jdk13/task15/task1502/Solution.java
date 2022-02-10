2b1ebd246ba2af650bdc4574dcfc5437beb28b61
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-10 15:53:18 +0300

    add solution Core task 1502

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task15/task1502/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task15.task1502;
+
+/* 
+ООП - Перегрузка - убираем лишнее
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером!");
+    }
+}
