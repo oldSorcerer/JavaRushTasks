580b56942069b8f2eb854c0d3bab8d75873fdb8d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-10 15:28:44 +0300

    add solution Core task 1210

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task12/task1210/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task12.task1210;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int number) {
+
+    }
+
+    public static void print(String string) {
+
+    }
+}
