be2a65b78ad0eb74702d04f1138bb10e2452b9f9
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-11 21:33:14 +0300

    add solution Core task 1501

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task15/task1501/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.jdk13.task15.task1501;
+
+/* 
+Что-то лишнее
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print((short) 1);
+        print((Number) 1);
+        print(1);
+        print((Integer) 1);
+        print((int) 1);
+    }
+
+    public static void print(Integer i) {
+        System.out.println("Это Integer");
+    }
+
+    public static void print(Short i) {
+        System.out.println("Это Object");
+    }
+
+    public static void print(Object i) {
+        System.out.println("Это Object");
+    }
+
+    public static void print(double i) {
+        System.out.println("Это double");
+    }
+
+    public static void print(Double i) {
+        System.out.println("Это double");
+    }
+}
