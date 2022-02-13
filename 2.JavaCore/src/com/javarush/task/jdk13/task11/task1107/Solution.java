704326592403d94fbd1c7cf81a47bb7d409d2472
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-13 10:48:01 +0300

    add solution Core task 1107

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1107/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task11.task1107;
+
+/* 
+«Из ребра его»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
