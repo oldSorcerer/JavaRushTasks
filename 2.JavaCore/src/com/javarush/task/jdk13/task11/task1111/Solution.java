77f76533c34548bb2f821b04c1ea86536a2a042f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-13 10:47:02 +0300

    add solution Core task 1111

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1111/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.jdk13.task11.task1111;
+
+/* 
+Помощь шахматной школе
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class ChessPieces {
+    }
+
+    public class King extends ChessPieces {
+    }
+
+    public class Queen extends ChessPieces {
+    }
+
+    public class Rook extends ChessPieces {
+    }
+
+    public class Knight extends ChessPieces {
+    }
+
+    public class Bishop extends ChessPieces {
+    }
+
+    public class Pawn extends ChessPieces {
+    }
+}
