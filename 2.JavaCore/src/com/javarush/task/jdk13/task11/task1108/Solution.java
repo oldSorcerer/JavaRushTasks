e6423ebd7be0ff42393a16d14c1a296bf9ae0b0d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-13 10:48:13 +0300

    add solution Core task 1108

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1108/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.jdk13.task11.task1108;
+
+/* 
+Играем в Дарвина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
