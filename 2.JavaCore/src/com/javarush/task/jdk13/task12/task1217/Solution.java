08a2229e00f1e45479ded0d885e95c02f7c97450
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-14 11:17:19 +0300

    add solution Core task 1217

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task12/task1217/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.jdk13.task12.task1217;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+        void fly(int a);
+    }
+
+}
