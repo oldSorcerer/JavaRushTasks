d4a22ca3cac367de48e8733ebe092e7ad1c1317e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-14 11:58:18 +0300

    add solution Core task 1304

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task13/task1304/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.jdk13.task13.task1304;
+
+/* 
+Эй, ты там живой?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Person {
+        boolean isAlive();
+    }
+
+    interface Presentable extends Person {
+
+    }
+}
