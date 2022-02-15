5f6b37111b0b9cc6df3f2795c9756b80448ddfe2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-15 11:48:02 +0300

    add solution Core task 1306

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task13/task1306/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.jdk13.task13.task1306;
+
+/* 
+Будущий управленец
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Person {
+        void use(Person person);
+
+        void startToWork();
+    }
+
+    interface HasManagementPotential {
+        boolean inspiresOthersToWork();
+    }
+
+    interface Secretary extends Person {
+    }
+
+    interface Boss extends HasManagementPotential, Person {
+    }
+
+    class Manager implements Boss {
+        public void use(Person person) {
+            person.startToWork();
+        }
+
+        public void startToWork() {
+        }
+
+        public boolean inspiresOthersToWork() {
+            return true;
+        }
+    }
+
+    class Subordinate implements Secretary {
+        public void use(Person person) {
+        }
+
+        public void startToWork() {
+        }
+    }
+}
