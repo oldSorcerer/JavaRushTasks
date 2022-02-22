44bf7ca2e94e0078e232841ac7f01fd84224ff66
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-22 13:12:52 +0300

    add solution Syntax task 1804

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1804/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task18.task1804;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/* 
+Знакомство с лямбда-выражением
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var numbers = new ArrayList<Integer>();
+
+        Collections.addAll(numbers, 123, -6, 12, 0, 44, 5678, -350);
+
+        sortNumbers(numbers);
+
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public static void sortNumbers(ArrayList<Integer> numbers) {
+        Collections.sort(numbers, (i1, i2) -> i1 - i2);
+    }
+}
