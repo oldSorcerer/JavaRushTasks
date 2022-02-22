5c7111d3610f9f7431785d9996165bc242ce74a8
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-22 13:20:40 +0300

    add solution Syntax task 1805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task18.task1805;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/* 
+Прощание с лямбда-выражением
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+
+        Collections.addAll(strings, "JavaRush", "Amigo", "Java Developer", "CodeGym");
+
+        sortStringsByLength(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static void sortStringsByLength(ArrayList<String> strings) {
+
+        StringComparator stringComparator = new StringComparator();
+
+        Collections.sort(strings, stringComparator);
+    }
+}
