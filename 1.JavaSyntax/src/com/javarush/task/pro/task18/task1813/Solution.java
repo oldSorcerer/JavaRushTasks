530a7b2971092025736bd02b0ac83f81f44e4951
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-24 00:12:14 +0300

    add solution Syntax task 1813

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1813/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task18.task1813;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+/* 
+Будущее за электрокарами
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var cars = new ArrayList<Car>();
+        Collections.addAll(cars,
+                new Car("Range Rover", false),
+                new Car("Model S", true),
+                new Car("Navigator", false),
+                new Car("Model 3", true),
+                new Car("Camaro", false),
+                new Car("Escalade", false),
+                new Car("Mustang", false),
+                new Car("Model X", true),
+                new Car("X5", false),
+                new Car("Model Y", true));
+
+        var carStream = onlyElectricCars(cars);
+        carStream.forEach(System.out::println);
+    }
+
+    public static Stream<Car> onlyElectricCars(ArrayList<Car> cars) {
+        //напишите тут ваш код
+        return Stream.empty();
+    }
+}
