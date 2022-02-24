06852586df65f926cb9c6ddc759984d22638655e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-25 01:58:47 +0300

    add solution Syntax task 1815

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1815/Account.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task18.task1815;
+
+public class Account {
+    private String name;
+    private String email;
+
+    public Account(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1815/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task18.task1815;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+/* 
+Преобразование данных
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var accounts = new ArrayList<Account>();
+        Collections.addAll(accounts,
+                new Account("Elly", "elly@yandex.ru"),
+                new Account("Jocker", "jocker@gmail.com"),
+                new Account("Neo", "neo@yahoo.com"));
+
+        Stream<String> emails = getEmails(accounts);
+        emails.forEach(System.out::println);
+    }
+
+    public static Stream<String> getEmails(ArrayList<Account> accounts) {
+        //напишите тут ваш код
+        return Stream.empty();
+    }
+}
