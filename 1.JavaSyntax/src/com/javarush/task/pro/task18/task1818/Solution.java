75ea74885ecd4b63b45c13dfb42c11e39b1d51e1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-02-28 13:49:55 +0300

    add solution Syntax task 1818

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1818/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.pro.task18.task1818;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.stream.Stream;
+
+/* 
+Просрочен ли товар?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var productStream = Stream.of(
+                new Product("Milk", LocalDate.of(2020, Month.MARCH, 12)),
+                new Product("Cheese", LocalDate.of(2020, Month.MARCH, 31)),
+                new Product("Buckwheat", LocalDate.of(2021, Month.MARCH, 1)),
+                new Product("Rice", LocalDate.of(2022, Month.MAY, 11)),
+                new Product("Wine", LocalDate.of(2023, Month.JANUARY, 10)),
+                new Product("Tea", LocalDate.of(2024, Month.AUGUST, 27)),
+                new Product("Salt", LocalDate.of(2025, Month.DECEMBER, 15))
+        );
+
+        String answer = hasExpiredProduct(productStream) ? "Yes" : "No";
+        System.out.println("Has stream expired products? " + answer);
+    }
+
+    public static boolean hasExpiredProduct(Stream<Product> productStream) {
+        //напишите тут ваш код
+        return false;
+    }
+}
+
+class Product {
+    private String name;
+    private LocalDate expirationDate;
+
+    public Product(String name, LocalDate expirationDate) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+}
