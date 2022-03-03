bcf3bdeedb9a85e90b7aeee852f37ca94d21371d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-03 10:31:18 +0300

    add solution Syntax task 1817

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1817/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task18.task1817;
+
+import java.util.stream.Stream;
+
+/* 
+Анализ потока чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String answerYes = "Yes";
+        String answerNo = "No";
+
+        Stream<Integer> stream1 = Stream.of(10, -22, 3, 12, -85, 0, 142);
+
+        String answerNegativeEvenNumbers = hasNegativeEvenNumbers(stream1) ? answerYes : answerNo;
+        System.out.println("Any negative even numbers in the stream? " + answerNegativeEvenNumbers);
+
+        Stream<Integer> stream2 = Stream.of(10, 22, 3, 12, 85, 142);
+
+        String answerOnlyPositiveNumbers = hasOnlyPositiveNumbers(stream2) ? answerYes : answerNo;
+        System.out.println("Only positive numbers in the stream? " + answerOnlyPositiveNumbers);
+
+        Stream<Integer> stream3 = Stream.of(-10, -22, -3, -12, -85, -142);
+
+        String answerOnlyNegativeNumbers = hasOnlyNegativeNumbers(stream3) ? answerYes : answerNo;
+        System.out.println("Only negative numbers in the stream? " + answerOnlyNegativeNumbers);
+    }
+
+    public static boolean hasNegativeEvenNumbers(Stream<Integer> stream) {
+        //напишите тут ваш код
+        return false;
+    }
+
+    public static boolean hasOnlyPositiveNumbers(Stream<Integer> stream) {
+        //напишите тут ваш код
+        return false;
+    }
+
+    public static boolean hasOnlyNegativeNumbers(Stream<Integer> stream) {
+        //напишите тут ваш код
+        return false;
+    }
+}
