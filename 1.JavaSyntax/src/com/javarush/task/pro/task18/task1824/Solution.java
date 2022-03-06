84fe723cd059136abeaa4ed6db215203f744a8dd
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-06 18:00:31 +0300

    add solution Syntax task 1824

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1824/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task18.task1824;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+/* 
+Из потока данных — в map
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getMap(stringStream).forEach((s, i) -> System.out.println(s + " - " + i));
+    }
+
+    public static Map<String, Integer> getMap(Stream<String> stringStream) {
+        return stringStream.collect(Collectors.toMap(e -> e, e -> e.length()));
+    }
+}
