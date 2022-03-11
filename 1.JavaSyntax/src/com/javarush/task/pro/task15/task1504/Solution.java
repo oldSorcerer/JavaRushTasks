e388a532175b1e8a2daa04663c6367f43ee8b60d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-11 12:37:17 +0300

    add solution Syntax task 1504

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -12,8 +12,24 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        //напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+
+        try (Scanner scanner = new Scanner(System.in);
+             InputStream inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
+             OutputStream outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))
+        ) {
+            byte[] bytesIn = inputStream.readAllBytes();
+            byte[] bytesOut = new byte[bytesIn.length];
+            for (int i = 0; i < bytesIn.length; i += 2 ) {
+                if (i < bytesIn.length - 1) {
+                    bytesOut[i] = bytesIn[i + 1];
+                    bytesOut[i + 1] = bytesIn[i];
+                } else {
+                    bytesOut[i] = bytesIn[i];
+                }
+            }
+            outputStream.write(bytesOut);
+        }
     }
 }
 
