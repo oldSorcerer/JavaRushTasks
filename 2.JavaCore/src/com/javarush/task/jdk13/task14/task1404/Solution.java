da30fccc2f4d42eb8db05fb9fdf27d1f2081e8f3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-11 12:52:58 +0300

    add solution Core task 1404

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task14/task1404/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.jdk13.task14.task1404;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Коты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat;
+            switch (key) {
+                case "vaska":
+                    cat = new MaleCat("Василий");
+                    break;
+                case "murka":
+                    cat = new FemaleCat("Мурочка");
+                    break;
+                case "kiska":
+                    cat = new FemaleCat("Кисюлька");
+                    break;
+                default:
+                    cat = new Cat(key);
+                    break;
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "Я уличный кот " + getName();
+        }
+    }
+
+    static class MaleCat extends Cat {
+        MaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - солидный кошак по имени " + getName();
+        }
+    }
+
+    static class FemaleCat extends Cat {
+        FemaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - милая кошечка по имени " + getName();
+        }
+    }
+}
