fef1f426bdb4c3737e3c80ed2655ecdbb976b7fe
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-13 14:21:39 +0300

    add solution Core task 3502

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3502/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.jdk13.task35.task3502;
+
+import java.util.List;
+
+/* 
+Знакомство с дженериками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class SomeClass {
+    }
+}
