df525013edf2b07996f93599e63c6d446336b26d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-18 18:56:08 +0300

    add solution Core task 1505

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task15/task1505/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.jdk13.task15.task1505;
+
+import java.io.Serializable;
+
+/* 
+Максимально простой код-1
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        JuniorJavaDev me = new JuniorJavaDev();
+        System.out.println(me.askHubert("What do you think about ru.rush.task.jdk13.task15.task1511?"));
+        System.out.println(me.askZapp("When will the next update happen?"));
+    }
+
+    public interface SpecificSerializable extends Serializable {
+    }
+
+    public static class JavaDev extends Object implements SpecificSerializable {
+        String answerQuestion(String question) {
+            return String.format("I'll be thinking of [%s]", question);
+        }
+    }
+
+    public static class JuniorJavaDev extends Object, JavaDev implements SpecificSerializable {
+        JavaDev zapp = new JavaDev();
+        JavaDev hubert = new JavaDev();
+
+        String askZapp(String question) {
+            return zapp.answerQuestion(question);
+        }
+
+        String askHubert(String question) {
+            return hubert.answerQuestion(question);
+        }
+    }
+}
