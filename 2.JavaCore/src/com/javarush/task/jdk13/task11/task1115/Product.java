262e8e0b35555c380a3a2ce0b55280280d39c4e2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-20 17:53:30 +0300

    add solution Core task 1115

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1115/Product.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task11.task1115;
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
