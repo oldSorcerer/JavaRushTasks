06c7548a1d9be3f44c3564665c1c8432ec5d8a83
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-21 12:27:25 +0300

    add solution Core task 1401

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task14/task1401/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.jdk13.task14.task1401;
+
+/* 
+Bingo!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Object animal = new Tiger();
+        boolean isCat = animal instanceof Cat;
+        boolean isTiger = animal instanceof Tiger;
+        boolean isPet = animal instanceof Pet;
+
+        printResults(isCat, isTiger, isPet);
+    }
+
+    private static void printResults(boolean cat, boolean tiger, boolean pet) {
+        if (cat && tiger && pet) {
+            System.out.println("Bingo!");
+        }
+    }
+
+    static class Pet {
+    }
+
+    static class Cat extends Pet {
+    }
+
+    static class Tiger extends Cat {
+    }
+}
