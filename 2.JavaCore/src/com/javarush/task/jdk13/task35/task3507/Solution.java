1a042f8109b2e87503ff1d36c17311f9ada0425e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-21 12:20:37 +0300

    add solution Core task 3507

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3507/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.jdk13.task35.task3507;
+
+import java.util.*;
+
+/* 
+Collections & Generics
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static ArrayList newArrayList(Object... elements) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static HashSet newHashSet(Object... elements) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static HashMap newHashMap(List keys, List values) {
+        //напишите тут ваш код
+        return null;
+    }
+}
