6d2a3f194d712b101e72378f5485f2fa20c468f4
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-22 16:05:12 +0300

    add solution Core task 1510

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task15/task1510/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task15.task1510;
+
+/* 
+Статики и исключения
+*/
+
+public class Solution {
+    public static int A = 0;
+
+    static {
+        //throw an exception here - выбросьте эксепшн тут
+    }
+
+    public static int B = 5;
+
+    public static void main(String[] args) {
+        System.out.println(B);
+    }
+}
