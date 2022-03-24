2cbd5c2ba0bb9dad29fb4099c72e68bb54859d4f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-24 11:40:16 +0300

    add solution Core task 2101

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task21/task2101/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.jdk13.task21.task2101;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+
+public class Solution {
+    private final String first;
+    private final String last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Solution n) {
+        return n.first.equals(first) && n.last.equals(last);
+    }
+
+    public int hashCode() {
+        return 31 * first.hashCode() + last.hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
