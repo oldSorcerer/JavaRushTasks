06325a622be6349dad760ef1b5effd0b0881f86f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-27 23:47:19 +0300

    add solution Core task 3402

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3402/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.jdk13.task34.task3402;
+
+/* 
+Факториал с помощью рекурсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
