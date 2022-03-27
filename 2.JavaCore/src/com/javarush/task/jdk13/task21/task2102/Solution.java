cea869fc0560518044a49f643e083d3fdcb02d13
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-27 23:35:32 +0300

    add solution Core task 2102

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task21/task2102/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.jdk13.task21.task2102;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+
+public class Solution {
+    private final String first;
+    private final String last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof Solution)) {
+            return false;
+        }
+        Solution n = (Solution) o;
+        return n.first.equals(first) && n.last.equals(last);
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
