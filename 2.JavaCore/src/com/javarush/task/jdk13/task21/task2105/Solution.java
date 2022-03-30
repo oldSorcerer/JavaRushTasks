2345d8cd8214b195f31cadfb36c67b2fb6976d51
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-30 16:35:09 +0300

    add solution Core task 2105

--- 2.JavaCore/src/com/javarush/task/jdk13/task21/task2105/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task21/task2105/Solution.java
@@ -4,8 +4,21 @@ package com.javarush.task.jdk13.task21.task2105;
 Запретить клонирование
 */
 
+import java.util.Objects;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        A objectA = new A(2, 7);
+        Object cloneA = objectA.clone();
+        System.out.println(objectA.equals(cloneA));
+
+        B objectB = new B(2, 7 , "Name");
+        Object cloneB = objectB.clone();
+        System.out.println(objectB.equals(cloneB));
+
+        C objectC = new C(2, 7, "Name2");
+        Object cloneС = objectC.clone();
+        System.out.println(objectC.equals(cloneС));
 
     }
 
@@ -25,6 +38,25 @@ public class Solution {
         public int getJ() {
             return j;
         }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            A copyA = new A(getI(), getJ());
+            return copyA;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            A a = (A) o;
+            return i == a.i && j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
+        }
     }
 
     public static class B extends A {
@@ -38,11 +70,46 @@ public class Solution {
         public String getName() {
             return name;
         }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException  {
+            throw new CloneNotSupportedException();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+            B b = (B) o;
+            return name.equals(b.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), name);
+        }
     }
 
     public static class C extends B {
         public C(int i, int j, String name) {
             super(i, j, name);
+        }
+
+        @Override
+        protected Object clone() {
+            C copyC = new C(getI(), getJ(), getName());
+            return copyC;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return super.equals(o);
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
         }
     }
 }
