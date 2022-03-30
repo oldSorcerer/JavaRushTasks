7496f9f5995719ea73aee011632fdb0ae6b30046
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-03-30 19:50:49 +0300

    add solution Core task 2104

--- 2.JavaCore/src/com/javarush/task/jdk13/task21/task2104/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task21/task2104/Solution.java
@@ -1,39 +1,86 @@
 package com.javarush.task.jdk13.task21.task2104;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /* 
 Глубокое клонирование карты
 */
 
-public class Solution {
+public class Solution implements Cloneable {
+
+    protected Map<String, User> users = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.users.put("Hubert", new User(172, "Hubert"));
         solution.users.put("Zapp", new User(41, "Zapp"));
         try {
-            Solution clone = (Solution) solution.clone(); // add (Solution)
+            Solution clone = solution.clone();
             System.out.println(solution);
             System.out.println(clone);
+            System.out.println(solution == clone);
 
             System.out.println(solution.users);
             System.out.println(clone.users);
+            System.out.println(solution.users == clone.users);
         } catch (CloneNotSupportedException e) {
             e.printStackTrace(System.err);
         }
     }
 
-    protected Map<String, User> users = new LinkedHashMap();
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution solution = new Solution();
+        Map<String, User> map = new LinkedHashMap<>();
 
-    public static class User {
+        for (Map.Entry<String, User> entry : users.entrySet()) {
+            map.put(entry.getKey(), entry.getValue().clone());
+        }
+        solution.users = map;
+        return solution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Solution solution = (Solution) o;
+        return Objects.equals(users, solution.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(users);
+    }
+
+    public static class User implements Cloneable {
         int age;
         String name;
 
         public User(int age, String name) {
             this.age = age;
             this.name = name;
+        }
+
+        @Override
+        public User clone() throws CloneNotSupportedException {
+            return new User(age, name);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(age, user.age) && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
         }
     }
 }
