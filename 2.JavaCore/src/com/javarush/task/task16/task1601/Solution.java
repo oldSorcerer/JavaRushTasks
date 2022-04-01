958fd855dae912eb6508b11082f1e6e210437a65
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-01 15:44:09 +0300

    add solution Core task 1601

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task16.task1601;
+
+/* 
+My first thread
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        TestThread task = new TestThread();
+        new Thread(task).start();
+    }
+
+    private static class TestThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("My first thread");
+        }
+    }
+}
