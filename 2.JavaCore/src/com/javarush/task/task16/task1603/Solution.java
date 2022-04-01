73dce636c5efbb7283ddba975f0ec3f28c05ef45
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-01 15:47:58 +0300

    add solution Core task 1603

--- 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -8,18 +8,29 @@ import java.util.List;
 */
 
 public class Solution {
-    public static volatile List<Thread> list = new ArrayList<>(5);
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
 
     public static void main(String[] args) {
+        SpecialThread specialThread1 = new SpecialThread();
+        SpecialThread specialThread2 = new SpecialThread();
+        SpecialThread specialThread3 = new SpecialThread();
+        SpecialThread specialThread4 = new SpecialThread();
+        SpecialThread specialThread5 = new SpecialThread();
 
-        for (int i = 0; i < 5; i++) {
-            list.add(new Thread(new SpecialThread()));
-        }
+        Thread thread1 = new Thread(specialThread1);
+        Thread thread2 = new Thread(specialThread2);
+        Thread thread3 = new Thread(specialThread3);
+        Thread thread4 = new Thread(specialThread4);
+        Thread thread5 = new Thread(specialThread5);
 
+        list.add(thread1);
+        list.add(thread2);
+        list.add(thread3);
+        list.add(thread4);
+        list.add(thread5);
     }
 
     public static class SpecialThread implements Runnable {
-        @Override
         public void run() {
             System.out.println("it's a run method inside SpecialThread");
         }
