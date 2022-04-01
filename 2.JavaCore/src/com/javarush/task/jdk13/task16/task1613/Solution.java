4d5b24fd34500c1def246e7cb30bd58a27761296
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-01 14:38:59 +0300

    add solution Core task 1613

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task16/task1613/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.jdk13.task16.task1613;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<>(5);
+
+    public static void main(String[] args) {
+        SpecialThread specialThread1 = new SpecialThread();
+        SpecialThread specialThread2 = new SpecialThread();
+        SpecialThread specialThread3 = new SpecialThread();
+        SpecialThread specialThread4 = new SpecialThread();
+        SpecialThread specialThread5 = new SpecialThread();
+
+        Thread thread1 = new Thread(specialThread1);
+        Thread thread2 = new Thread(specialThread2);
+        Thread thread3 = new Thread(specialThread3);
+        Thread thread4 = new Thread(specialThread4);
+        Thread thread5 = new Thread(specialThread5);
+
+       list.add(thread1);
+       list.add(thread2);
+       list.add(thread3);
+       list.add(thread4);
+       list.add(thread5);
+
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
