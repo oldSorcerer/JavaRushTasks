8c7dc6d9387bf1a8310ab41da4d5812dc4848b0a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-02 00:44:16 +0300

    add solution Core task 1328

--- 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -1,36 +1,36 @@
 package com.javarush.task.task13.task1328;
 
-public abstract class AbstractRobot implements Attackable, Defensable{
+public abstract class AbstractRobot implements Attackable, Defensable {
 
-    private static int hitCount = (int) (Math.random() * 4) + 1;
+    private static int hitCount = (int)(Math.random() * 4) + 1;
 
     public abstract String getName();
 
     public BodyPart attack() {
-        BodyPart attackedBodyPart = null;
+        BodyPart attackedBodyPart;
         hitCount = hitCount + 1;
 
         if (hitCount == 1) {
-            attackedBodyPart = BodyPart.ARM;
-        } else if (hitCount == 2) {
-            attackedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 3) {
-            attackedBodyPart = BodyPart.LEG;
-        } else {
-            hitCount = 0;
             attackedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else  {
+            hitCount = 0;
+            attackedBodyPart = BodyPart.LEG;
         }
         return attackedBodyPart;
     }
 
     public BodyPart defense() {
-        BodyPart defendedBodyPart = null;
+        BodyPart defendedBodyPart;
         hitCount = hitCount + 2;
 
         if (hitCount == 1) {
-            defendedBodyPart = BodyPart.HEAD;
-        } else if (hitCount == 2) {
             defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 3) {
             defendedBodyPart = BodyPart.ARM;
         } else {
