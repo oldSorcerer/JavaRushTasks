bee91899f68176bddccb00d657c994d65efc0157
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-03 00:35:27 +0300

    add solution Collections task 3402

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task34.task3402;
+
+/* 
+Факториал с помощью рекурсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+}
