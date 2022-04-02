be1ff1e2c2f8662c6826a0de2590d734312edd8f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-02 15:10:30 +0300

    add solution Core task 1415

--- 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        List<Apartment> apartments = new ArrayList<Apartment>();
+        List<Apartment> apartments = new ArrayList<>();
         apartments.add(new OneRoomApt());
         apartments.add(new TwoRoomApt());
         apartments.add(new ThreeRoomApt());
@@ -18,18 +18,18 @@ public class Solution {
     }
 
     public static void cleanAllApartments(List<Apartment> apartments) {
-
         for (Apartment apartment : apartments) {
-            if (apartment instanceof OneRoomApt)
+            if (apartment instanceof OneRoomApt) {
                 ((OneRoomApt) apartment).clean1Room();
-            if (apartment instanceof TwoRoomApt)
+            } else if (apartment instanceof TwoRoomApt) {
                 ((TwoRoomApt) apartment).clean2Rooms();
-            if (apartment instanceof ThreeRoomApt)
+            } else if (apartment instanceof ThreeRoomApt) {
                 ((ThreeRoomApt) apartment).clean3Rooms();
+            }
         }
     }
 
-    static interface Apartment {
+    interface Apartment {
     }
 
     static class OneRoomApt implements Apartment {
