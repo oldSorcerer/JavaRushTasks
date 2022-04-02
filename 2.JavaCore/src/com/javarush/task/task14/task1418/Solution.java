36f7fdf3f7e46b8064fc0ff1617503184c014ca6
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-02 13:18:37 +0300

    add solution Core task 1418

--- 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
@@ -8,41 +8,35 @@ import java.util.List;
 */
 
 public class Solution {
+    public static void main(String[] args) {
+        List<Number> list = new LinkedList<>();
+        initList(list);
+        printListValues(list);
+        processCastedObjects(list);
+    }
 
-    static void initList(List<Number> list) {
+    public static void initList(List<Number> list) {
         list.add(new Double(1000f));
         list.add(new Double("123e-445632"));
         list.add(new Float(-90 / -3));
         list.remove(new Double("123e-445632"));
     }
 
-    static void printListValues(List<Number> list) {
+    public static void printListValues(List<Number> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
+
     }
-
-    static void processCastedObjects(List<Number> list) {
+    public static void processCastedObjects(List<Number> list) {
         for (Number object : list) {
-
-            if (object instanceof Float) {
-                Float a = (Float) object;
-                System.out.println("Is float value defined? " + !(a.isNaN()));
-            } else if (object instanceof Double) {
+            if (object instanceof Double) {
                 Double a = (Double) object;
                 System.out.println("Is double value infinite? " + a.isInfinite());
+            } else if (object instanceof Float) {
+                Float a = (Float) object;
+                System.out.println("Is float value defined? " + !(a.isNaN()));
             }
         }
-    }
-
-    public static void main(String[] args) {
-        List<Number> list = new LinkedList<Number>();
-
-        initList(list);
-        printListValues(list);
-        processCastedObjects(list);
-
-
-
     }
 }
