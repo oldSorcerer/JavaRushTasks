742a325f912cdd2960acc41da03423b2883666a1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-03 22:48:25 +0300

    add solution Core task 1417

--- 2.JavaCore/src/com/javarush/task/task14/task1417/Money.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Money.java
@@ -1,20 +1,11 @@
 package com.javarush.task.task14.task1417;
 
 public abstract class Money {
-
-    private double amount;
-
     public Money(double amount) {
-        this.amount = amount;
     }
 
-    public double getAmount() {
-        return amount;
-    }
+    public abstract double getAmount();
 
     public abstract String getCurrencyName();
-
-
-
 }
 
