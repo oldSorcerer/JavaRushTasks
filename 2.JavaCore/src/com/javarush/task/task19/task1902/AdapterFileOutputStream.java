4cf944c7c1e81e5b98847dbd6b4f0b7e8fee0644
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-03 22:47:55 +0300

    add solution Core task 1902

--- 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -13,6 +13,5 @@ public class AdapterFileOutputStream {
 
     }
 
-
 }
 
--- 2.JavaCore/src/com/javarush/task/task19/task1902/AmigoStringWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AmigoStringWriter.java
@@ -3,6 +3,7 @@ package com.javarush.task.task19.task1902;
 import java.io.IOException;
 
 public interface AmigoStringWriter {
+
     void flush() throws IOException;
 
     void writeString(String s) throws IOException;
