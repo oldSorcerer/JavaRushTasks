4ece3728e9a6937bbd4231691df3fbbf2628816b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-05 22:32:11 +0300

    add solution Core task 1620

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task16/task1620/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task16.task1620;
+
+/* 
+Часы
+*/
+
+public class Solution {
+    public static volatile boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Clock clock = new Clock();
+        Thread.sleep(2000);
+        isStopped = true;
+        System.out.println("The clock has to be stopped");
+        Thread.sleep(2000);
+        System.out.println("Double-check");
+    }
+
+    public static class Clock extends Thread {
+        public Clock() {
+            setPriority(MAX_PRIORITY);
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isStopped) {
+                    printTikTak();
+                }
+            } catch (InterruptedException ignore) {
+            }
+        }
+
+        private void printTikTak() throws InterruptedException {
+            //add your code here - добавь код тут
+        }
+    }
+}
