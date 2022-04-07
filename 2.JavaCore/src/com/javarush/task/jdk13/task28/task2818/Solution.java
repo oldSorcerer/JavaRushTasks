332a1a26deb3af653e9ce302a19d80721f5d6f7b
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-07 16:58:13 +0300

    add solution Core task 2818

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2818/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.jdk13.task28.task2818;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/* 
+С ScheduledExecutor по галактике
+*/
+
+public class Solution {
+
+    public static ScheduledExecutorService scheduledPool = Executors.newScheduledThreadPool(3);
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+
+        scheduledPool.shutdown();
+    }
+}
