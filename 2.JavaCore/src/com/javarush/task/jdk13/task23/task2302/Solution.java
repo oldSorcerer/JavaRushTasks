ce14c37d9d1c9e07f4973282f5ab103019a93a63
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-08 15:53:47 +0300

    add solution Core task 2302

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task23/task2302/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.jdk13.task23.task2302;
+
+/* 
+Как выбрать нужное?
+*/
+
+public class Solution {
+    public static final String TEST = "test";
+    static Object obj;
+
+    public static void main(String[] args) {
+        obj = Solution.TEST;
+        System.out.println(obj);
+    }
+
+    public static class TEST {
+        @Override
+        public String toString() {
+            return "test class";
+        }
+    }
+}
