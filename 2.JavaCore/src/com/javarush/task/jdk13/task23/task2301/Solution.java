f609e4341a802a664f8122e6099a96f15948ce0f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-09 21:56:46 +0300

    add solution Core task 2301

--- 2.JavaCore/src/com/javarush/task/jdk13/task23/task2301/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task23/task2301/Solution.java
@@ -13,7 +13,17 @@ public class Solution {
 
     public static Solution[] getTwoSolutions() {
 
-        return null;
+        Solution solution0 = new Solution();
+        Solution solution1 = new Solution();
+        Solution [] solutions = {solution0, solution1};
+
+        solution0.innerClasses[0] = new InnerClass();
+        solution0.innerClasses[1] = new InnerClass();
+
+        solution1.innerClasses[0] = new InnerClass();
+        solution1.innerClasses[1] = new InnerClass();
+
+        return solutions;
     }
 
     public static class InnerClass {
