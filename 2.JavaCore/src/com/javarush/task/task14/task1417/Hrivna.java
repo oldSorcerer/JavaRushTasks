5b4184a49c426c8cc7d7084ae7970aa7d8d185df
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-10 22:12:18 +0300

    add solution Core task 1417

--- 2.JavaCore/src/com/javarush/task/task14/task1417/Hrivna.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Hrivna.java
@@ -1,13 +1,13 @@
-package com.javarush.task.task14.task1417;
-
-public class Hrivna extends  Money {
-
-    public Hrivna(double amount) {
-        super(amount);
-    }
-
-    @Override
-    public String getCurrencyName() {
-        return "UAH";
-    }
-}
+//package com.javarush.task.task14.task1417;
+//
+//public class Hrivna extends  Money {
+//
+//    public Hrivna(double amount) {
+//        super(amount);
+//    }
+//
+//    @Override
+//    public String getCurrencyName() {
+//        return "UAH";
+//    }
+//}
--- 2.JavaCore/src/com/javarush/task/task14/task1417/USD.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/USD.java
@@ -1,13 +1,13 @@
-package com.javarush.task.task14.task1417;
-
-public class USD extends Money {
-
-    public USD(double amount) {
-        super(amount);
-    }
-
-    @Override
-    public String getCurrencyName() {
-        return "USD";
-    }
-}
+//package com.javarush.task.task14.task1417;
+//
+//public class USD extends Money {
+//
+//    public USD(double amount) {
+//        super(amount);
+//    }
+//
+//    @Override
+//    public String getCurrencyName() {
+//        return "USD";
+//    }
+//}
