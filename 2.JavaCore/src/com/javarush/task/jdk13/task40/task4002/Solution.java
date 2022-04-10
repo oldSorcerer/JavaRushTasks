0f8f17aa1c8e1b8c7b8f1f94bf31d8eff3ab0cb5
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-10 22:09:53 +0300

    add solution Core task 4002

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task40/task4002/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.jdk13.task40.task4002;
+
+import java.io.IOException;
+
+public class Solution {
+
+
+    public static void main(String[] args) throws IOException {
+        new MessageServer().start();
+        UdpClient client1 = new UdpClient(4446);
+        UdpClient client2 = new UdpClient(4447);
+
+        System.out.println(client1.sendReceive("111111111"));
+        System.out.println(client1.sendReceive("222222222"));
+        System.out.println(client1.sendReceive("333333333"));
+        System.out.println(client1.sendReceive("444444444"));
+
+        System.out.println(client2.sendReceive("qqqqqqq"));
+
+        System.out.println(client1.sendReceive("55555555"));
+        System.out.println(client1.sendReceive("end"));
+
+        client1.close();
+        client2.close();
+    }
+}
