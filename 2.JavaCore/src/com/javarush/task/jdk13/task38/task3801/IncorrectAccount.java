40780d366cf3fcc4404dfead889b7286db3d15f0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-10 22:11:59 +0300

    add solution Core task 3801

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3801/IncorrectAccount.java
@@ -0,0 +1,6 @@
+package com.javarush.task.jdk13.task38.task3801;
+
+public class IncorrectAccount {
+    @LongPositive
+    private String amountString;
+}
---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3801/LongPositive.java
@@ -0,0 +1,10 @@
+package com.javarush.task.jdk13.task38.task3801;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+public @interface LongPositive {
+}
