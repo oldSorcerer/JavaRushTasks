8e6bbc4e9384d908ebc766f0b3cd6853c30d6dc2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-10 23:22:54 +0300

    add solution Core task 3509

--- 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
@@ -13,25 +13,23 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public static Double sum(List list) {
-        Double result = 0.0;
-        for (int i = 0; i < list.size(); i++) {
-            Number numb = (Number) list.get(i);
+    public static Double sum(List<? extends Number> list) {
+        double result = 0.0;
+        for (Number numb : list) {
             result += numb.doubleValue();
         }
         return result;
     }
 
-    public static Double multiply(List list) {
-        Double result = 1.0;
-        for (int i = 0; i < list.size(); i++) {
-            Number numb = (Number) list.get(i);
+    public static Double multiply(List<? extends Number> list) {
+        double result = 1.0;
+        for (Number numb : list) {
             result *= numb.doubleValue();
         }
         return result;
     }
 
-    public static String concat(List list) {
+    public static String concat(List<?> list) {
         StringBuilder builder = new StringBuilder();
         for (Object obj : list) {
             builder.append(obj);
@@ -39,10 +37,9 @@ public class Solution {
         return builder.toString();
     }
 
-    public static List combine(List list) {
-        List result = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            Collection collection = (Collection) list.get(i);
+    public static <T> List<T> combine(List<? extends Collection> list) {
+        List<T> result = new ArrayList<>();
+        for (Collection collection : list) {
             result.addAll(collection);
         }
         return result;
