53cd12251043f58298f9e84ba862b13e9fcabfa7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-10 22:45:46 +0300

    add solution Core task 3508

--- 2.JavaCore/src/com/javarush/task/jdk13/task35/task3508/House.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3508/House.java
@@ -1,7 +1,6 @@
 package com.javarush.task.jdk13.task35.task3508;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /* 
@@ -10,35 +9,15 @@ import java.util.List;
 
 public class House<T> {
 
-    private List<T> residents = new ArrayList<>();
+    private final List<T> residents = new ArrayList<>();
 
     public void enter(T resident) {
         residents.add(resident);
-        //checkConflicts();
     }
 
     public void leave(T resident) {
         residents.remove(resident);
     }
-
-    /*private void checkConflicts() {
-        boolean conflict = false;
-        for (Object resident : residents) {
-            if (resident instanceof Dog) {
-                conflict = true;
-            }
-        }
-
-        if (conflict) {
-            Iterator iterator = residents.iterator();
-            while (iterator.hasNext()) {
-                Object resident = iterator.next();
-                if (resident instanceof Cat) {
-                    iterator.remove();
-                }
-            }
-        }
-    }*/
 
     @Override
     public String toString() {
@@ -59,13 +38,11 @@ public class House<T> {
         House<Dog> dogHouse = new House<>();
         dogHouse.enter(bruno);
         dogHouse.enter(larsik);
-        //dogHouse.enter(barsik);
-        System.out.println(dogHouse.toString());
+        System.out.println(dogHouse);
 
         House<Cat> catHouse = new House<>();
         catHouse.enter(barsik);
         catHouse.enter(keksik);
-        //catHouse.enter(bruno);
-        System.out.println(catHouse.toString());
+        System.out.println(catHouse);
     }
 }
