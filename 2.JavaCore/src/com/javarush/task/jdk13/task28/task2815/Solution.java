e5f39d5fd2b67d18bdc2a3373813ceb6e842fb89
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-12 15:58:33 +0300

    add solution Core task 2815

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2815/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.jdk13.task28.task2815;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/* 
+Пакуем лампочки
+*/
+
+public class Solution {
+
+    private static final BlockingQueue<String> QUEUE = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+
+    public static void runProcess(ExecutorService consumerService) {
+        System.out.println("--------------------------------------");
+        simulatingProducers();
+        startConsumers(consumerService);
+    }
+
+    private static void simulatingProducers() {
+        int maxProdBulbs = 5_000_000;
+        Random rnd = new Random();
+        System.out.println("Эмулируем работу производителей...");
+        System.out.println("Производители произвели " + maxProdBulbs + " лампочек...");
+
+        //напишите тут ваш код
+
+    }
+
+    private static void startConsumers(ExecutorService consumerService) {
+        System.out.println("Для упаковки используется: " + consumerService.getClass().getSimpleName());
+
+        int queueSize = QUEUE.size();
+        Consumer consumer = new Consumer();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < queueSize; i++) {
+            consumerService.execute(consumer);
+        }
+
+        consumerService.shutdown();
+        try {
+            consumerService.awaitTermination(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
+            System.out.println("Упаковщики упаковали все лампочки за " + (System.currentTimeMillis() - startTime) + " мс");
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            String bulb = QUEUE.poll();
+//            if (bulb != null) {
+//                System.out.println("Упакована: " + bulb + " упаковщиком: " + Thread.currentThread().getName());
+//            }
+        }
+    }
+}
