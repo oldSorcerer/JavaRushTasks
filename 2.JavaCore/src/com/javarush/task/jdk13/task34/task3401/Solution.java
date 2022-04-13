bb3c61bec27341e61b0ed7a4275f59568db3b8d3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-13 23:15:10 +0300

    add solution Core task 3401

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3401/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3401/Solution.java
@@ -15,6 +15,12 @@ public class Solution {
     }
 
     public int fibonacci(int n) {
-        return 0;
+        if (n < 0 ) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 }
