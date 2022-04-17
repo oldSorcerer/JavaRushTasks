be0ec3ba3e5db46933e580ca6c5f97170e286ddc
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-17 23:46:31 +0300

    add solution Collections task 3509

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.*;
+
+
+/* 
+Collections & Generics
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        return new ArrayList<>(Arrays.asList(elements));
+    }
+
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        return new HashSet<>(Arrays.asList(elements));
+    }
+
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        HashMap<K, V> map = new HashMap<>();
+        if (keys.size() == values.size()) {
+            for (int i = 0; i < keys.size(); i++) {
+                map.put(keys.get(i), values.get(i));
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+        return map;
+    }
+}
