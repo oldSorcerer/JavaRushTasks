f29655412f26397c3dad9d7c039b492f547c9564
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-17 23:20:44 +0300

    add solution Collections task 3510

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3510/House.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task35.task3510;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Вход воспрещен!
+*/
+
+public class House<T> {
+
+    private List<T> residents = new ArrayList<>();
+
+    public void enter(T resident) {
+        residents.add(resident);
+    }
+
+    public void leave(T resident) {
+        residents.remove(resident);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("В доме находятся:\n");
+        for (Object resident : residents) {
+            builder.append(resident.toString()).append("\n");
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Dog bruno = new Dog("Bruno");
+        Puppy larsik = new Puppy("Larsik");
+        Cat barsik = new Cat("Barsik");
+        Kitten keksik = new Kitten("Keksik");
+
+        House<Dog> dogHouse = new House<>();
+        dogHouse.enter(bruno);
+        dogHouse.enter(larsik);
+        System.out.println(dogHouse);
+
+        House<Cat> catHouse = new House<>();
+        catHouse.enter(barsik);
+        catHouse.enter(keksik);
+        System.out.println(catHouse);
+    }
+}
