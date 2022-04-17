5787f3e00211bc3b8f1ec7e4b6698c95c066cfef
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-18 00:47:48 +0300

    add solution Collections task 3508

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3508/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task35.task3508;
+
+import java.util.List;
+
+/* 
+extends vs super
+*/
+
+public abstract class Solution {
+    public abstract <T> void one(List<T> destination, List<T> source);
+
+    public abstract <T> void two(List<T> destination, List<? extends T> source);
+
+    public abstract <T> void three(List<? super T> destination, List<T> source);
+
+    public abstract <T> void four(List<? super T> destination, List<? extends T> source);
+}
