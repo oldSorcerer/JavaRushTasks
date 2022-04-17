466b431afb33e6dba825c282f799a475fc5dd9e1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-17 17:50:28 +0300

    add solution Core task 1615

--- 2.JavaCore/src/com/javarush/task/task16/task1615/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1615/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.task16.task1615;
 */
 
 public class Solution {
-    public static volatile Runway RUNWAY = new Runway();   //1 взлетная полоса
+    public static volatile Runway RUNWAY = new Runway();   // взлетная полоса
 
     public static void main(String[] args) throws InterruptedException {
         Plane plane1 = new Plane("Самолет #1");
@@ -16,16 +16,16 @@ public class Solution {
     private static void waiting() {
         try {
             Thread.sleep(100);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException ignored) {
 
         }
+
     }
 
     private static void takingOff() {
-        //fix this method - исправь этот метод
         try {
             Thread.sleep(100);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException ignored) {
         }
     }
 
@@ -38,21 +38,21 @@ public class Solution {
         public void run() {
             boolean isAlreadyTakenOff = false;
             while (!isAlreadyTakenOff) {
-                if (RUNWAY.trySetTakingOffPlane(this)) {    //если взлетная полоса свободна, занимаем ее
+                if (RUNWAY.trySetTakingOffPlane(this)) {    // если взлетная полоса свободна, занимаем ее
                     System.out.println(getName() + " взлетает");
-                    takingOff();//взлетает
+                    takingOff();// взлетает
                     System.out.println(getName() + " уже в небе");
                     isAlreadyTakenOff = true;
                     RUNWAY.setTakingOffPlane(null);
-                } else if (!this.equals(RUNWAY.getTakingOffPlane())) {  //если взлетная полоса занята самолетом
+                } else if (!this.equals(RUNWAY.getTakingOffPlane())) {  // если взлетная полоса занята самолетом
                     System.out.println(getName() + " ожидает");
-                    waiting(); //ожидает
+                    waiting(); // ожидает
                 }
             }
         }
     }
 
-    public static class Runway { //взлетная полоса
+    public static class Runway { // взлетная полоса
         private Thread t;
 
         public Thread getTakingOffPlane() {
