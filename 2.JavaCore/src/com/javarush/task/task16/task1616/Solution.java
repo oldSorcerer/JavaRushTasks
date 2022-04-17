b832f10dd069a2be1c5d2084e270db509ad73a61
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-17 17:26:27 +0300

    add solution Core task 1616

--- 2.JavaCore/src/com/javarush/task/task16/task1616/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1616/Solution.java
@@ -10,17 +10,13 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(in);
-        //create and run thread
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.start();
-        //read a string
-        reader.readLine();
-        stopwatch.interrupt();
-        //close streams
-        reader.close();
-        in.close();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.start();
+            reader.readLine();
+            stopwatch.interrupt();
+        }
     }
 
     public static class Stopwatch extends Thread {
@@ -32,7 +28,6 @@ public class Solution {
                     Thread.sleep(1000);
                     seconds++;
                 }
-
             } catch (InterruptedException e) {
                 System.out.println(seconds);
             }
