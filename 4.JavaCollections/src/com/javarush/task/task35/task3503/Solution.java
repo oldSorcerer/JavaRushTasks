476611556300322f1bd2298cd751a8fc15b1b3b7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-18 00:32:16 +0300

    add solution Collections task 3503

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3503/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task35.task3503;
+
+/* 
+Несколько суперклассов для дженерика
+*/
+
+public class Solution<T extends ClassForGenerics & InterfaceForGenerics> {
+    public static void main(String[] args) {
+        Solution<TestClassGood> testClassSolution = new Solution<>();
+        testClassSolution.check();
+    }
+
+    public void check() {
+        System.out.println("Works!");
+    }
+
+    public static class TestClassGood extends ClassForGenerics implements InterfaceForGenerics {
+
+    }
+
+    public static class TestClassWrong1 extends ClassForGenerics {
+
+    }
+
+    public static class TestClassWrong2 implements InterfaceForGenerics {
+
+    }
+
+}
