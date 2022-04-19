7bd5acdfbfcc2ac1ce955c449deed270897d02e3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-19 12:02:39 +0300

    add solution Core task 1626

--- 2.JavaCore/src/com/javarush/task/jdk13/task16/task1626/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task16/task1626/Solution.java
@@ -5,20 +5,25 @@ package com.javarush.task.jdk13.task16.task1626;
 */
 
 public class Solution {
+    public static volatile boolean flag = false;
+
     public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread(new TestThread());
+        TestThread testThread = new TestThread();
+        Thread t = new Thread(testThread);
         t.start();
         Thread.sleep(3000);
         ourInterruptMethod();
+
     }
 
     public static void ourInterruptMethod() {
-
+        flag = true;
     }
 
     public static class TestThread implements Runnable {
+
         public void run() {
-            while (true) {
+            while (!flag) {
                 try {
                     System.out.println("he-he");
                     Thread.sleep(500);
