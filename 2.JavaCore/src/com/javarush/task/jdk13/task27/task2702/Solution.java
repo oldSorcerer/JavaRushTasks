5c8dbbea3c041da6398bd93004e95e9869c31671
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-26 21:41:21 +0300

    add solution Core task 2702

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task27/task2702/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.jdk13.task27.task2702;
+
+/* 
+Модификаторы и deadlock
+*/
+
+public class Solution {
+    private final String field;
+
+    public Solution(String field) {
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void sout(Solution solution) {
+        System.out.format("111:  %s: %s %n", this.field, solution.getField());
+        solution.sout2(this);
+    }
+
+    public void sout2(Solution solution) {
+        System.out.format("222:  %s: %s %n", this.field, solution.getField());
+        solution.sout(this);
+    }
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution("first");
+        final Solution solution2 = new Solution("second");
+        new Thread(new Runnable() {
+            public void run() {
+                solution.sout(solution2);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                solution2.sout(solution);
+            }
+        }).start();
+
+    }
+}
