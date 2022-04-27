931bbeb6d1813cd7dcca58d344458287face2122
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-27 23:42:07 +0300

    add solution Core task 1704

--- 2.JavaCore/src/com/javarush/task/jdk13/task17/task1704/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task17/task1704/Solution.java
@@ -17,13 +17,13 @@ public class Solution {
 
         public final List<String> notes = new ArrayList<>();
 
-        public void addNote(int index, String note) {
+        public synchronized void addNote(int index, String note) {
             System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
             notes.add(index, note);
             System.out.println("Уже добавлена заметка [" + note + "]");
         }
 
-        public void removeNote(int index) {
+        public synchronized void removeNote(int index) {
             System.out.println("Сейчас будет удалена заметка с позиции " + index);
             String note = notes.remove(index);
             System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
