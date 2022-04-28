bda9e2b43d8cbec73b98cbfed1708dc8592093eb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-28 15:02:20 +0300

    add solution Multithreading task 2705

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2705/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task27.task2705;
+
+/* 
+Второй вариант deadlock
+*/
+
+public class Solution {
+    private final Object lock = new Object();
+
+    public synchronized void firstMethod() {
+        synchronized (lock) {
+            doSomething();
+        }
+    }
+
+    public void secondMethod() {
+        synchronized (lock) {
+            synchronized (this) {
+                doSomething();
+            }
+        }
+    }
+
+    private void doSomething() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
