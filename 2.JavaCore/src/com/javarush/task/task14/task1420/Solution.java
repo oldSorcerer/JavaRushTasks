1a118c113d5fdb0e05ae9bb592ffbf0ce008b0e8
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-28 15:35:40 +0300

    add solution Core task 1420

--- 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task14.task1420;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 /* 
 НОД
@@ -11,19 +10,16 @@ import java.math.BigInteger;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
-        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int num1 = Integer.parseInt(reader.readLine());
+            int num2 = Integer.parseInt(reader.readLine());
 
-        int[] number = new int[2];
-
-        for (int i = 0; i < 2; i++) {
-            number[i] = Integer.parseInt(rd.readLine());
+            for (int i = Math.min(num1, num2); i > 0; i--) {
+                if (num1 % i == 0 && num2 % i == 0 ) {
+                    System.out.println(i);
+                    break;
+                }
+            }
         }
-        if (number[0] <= 0 || number[1] <= 0)
-            throw new Exception("test");
-
-        BigInteger bi1 = BigInteger.valueOf(number[0]);
-        BigInteger bi2 = BigInteger.valueOf(number[1]);
-
-        System.out.println(bi1.gcd(bi2));
     }
 }
