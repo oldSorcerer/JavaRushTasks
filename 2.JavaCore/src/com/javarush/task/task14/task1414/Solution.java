a5b85e969d6a44478841094188935afd7f35e675
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-29 00:24:07 +0300

    add solution Core task 1414

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -9,29 +9,27 @@ MovieFactory
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //ввести с консоли несколько ключей (строк), пункт 7
-
-        /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
-        */
-
+       try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+           String string;
+           while ((string = reader.readLine()) != null) {
+               Movie movie = MovieFactory.getMovie(string);
+               if (movie == null) {
+                   return;
+               }
+               System.out.println(movie.getClass().getSimpleName());
+           }
+       }
     }
 
     static class MovieFactory {
 
         static Movie getMovie(String key) {
-            Movie movie = null;
-
-            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
-            if ("soapOpera".equals(key)) {
-                movie = new SoapOpera();
-            }
-
-            //напишите тут ваш код, пункты 5,6
-
-            return movie;
+            return switch (key) {
+                case "soapOpera" -> new SoapOpera();
+                case "cartoon" -> new Cartoon();
+                case "thriller" -> new Thriller();
+                default -> null;
+            };
         }
     }
 
@@ -41,5 +39,9 @@ public class Solution {
     static class SoapOpera extends Movie {
     }
 
-    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
 }
