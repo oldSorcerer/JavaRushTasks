ebada0add8d5c7e1c302354925a3011530c3067f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-29 22:28:58 +0300

    add solution Multithreading task 2113 Hippodrome 5/16

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -6,4 +6,33 @@ public class Horse {
     private double speed;
     private double distance;
 
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
