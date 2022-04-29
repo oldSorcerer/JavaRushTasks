b1dd2704a844ba66c80397ebdc287202e6d1f17f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-29 22:45:24 +0300

    add solution Multithreading task 2113 Hippodrome 5/16

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
 
     private List<Horse> horses;
+    public static Hippodrome game;
 
     public List<Horse> getHorses() {
         return horses;
@@ -15,6 +17,14 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
+        List<Horse> horses = new ArrayList<>();
+
+        horses.add(new Horse("Lucky", 3, 0));
+        horses.add(new Horse("Slevin", 3, 0));
+        horses.add(new Horse("Homer", 3, 0));
+
+        game = new Hippodrome(horses);
+
 
     }
 }
