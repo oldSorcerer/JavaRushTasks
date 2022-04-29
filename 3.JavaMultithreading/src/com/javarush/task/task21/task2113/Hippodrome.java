2c62b0c910bb3ab7621734ea652e2bcfbcf97db7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-04-29 22:17:48 +0300

    add solution Multithreading task 2113 Hippodrome 1/16

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,73 +1,8 @@
 package com.javarush.task.task21.task2113;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Hippodrome {
-    public static Hippodrome game;
-    private List<Horse> horses;
-
-    public Hippodrome(List<Horse> horses) {
-        this.horses = horses;
-    }
-
-    public List<Horse> getHorses() {
-        return horses;
-    }
-
-
-    public void run() throws InterruptedException {
-        for (int i = 1; i <= 100; i++) {
-            move();
-            print();
-            Thread.sleep(200);
-        }
-
-    }
-
-    public void move() {
-        for (Horse hors : horses) {
-            hors.move();
-        }
-    }
-
-    public void print() {
-        for (Horse hors : horses) {
-            hors.print();
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println();
-        }
-    }
-
-    public Horse getWinner() {
-
-        Horse winner = horses.get(0);
-        for (Horse hors : horses) {
-            if (winner.getDistance() < hors.getDistance())
-                winner = hors;
-        }
-
-        return winner;
-    }
-
-    public void printWinner() {
-        System.out.println("Winner is " + getWinner().getName() + "!");
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        game = new Hippodrome(new ArrayList<>());
-
-        game.getHorses().add(new Horse("лошадь_1", 3, 0));
-        game.getHorses().add(new Horse("лошадь_2", 3, 0));
-        game.getHorses().add(new Horse("лошадь_3", 3, 0));
-
-        game.run();
-
-        game.printWinner();
-
-
+    public static void main(String[] args) {
 
     }
 }
