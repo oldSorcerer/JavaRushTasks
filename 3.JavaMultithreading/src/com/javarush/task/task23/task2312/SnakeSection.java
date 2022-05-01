78d5735b19280c3628dc20f4d2ebfc75b77c7efd
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-02 01:10:50 +0300

    add solution Multithreading task 2312 Snake 2/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,29 +1,4 @@
 package com.javarush.task.task23.task2312;
 
 public class SnakeSection {
-    private int x;
-    private int y;
-
-    public SnakeSection(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 }
