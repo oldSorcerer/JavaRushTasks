509dbe35b81b8fb6715d439336f9c6344a1a35bb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-02 01:14:48 +0300

    add solution Multithreading task 2312 Snake 4/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -3,4 +3,17 @@ package com.javarush.task.task23.task2312;
 public class Mouse {
     private int x;
     private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
