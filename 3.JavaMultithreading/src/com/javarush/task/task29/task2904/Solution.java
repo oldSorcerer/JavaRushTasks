55d255aaed53aec9217db2811c37a5f8d898fe31
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-02 22:55:04 +0300

    add solution Multithreading task 2904

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2904/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task29.task2904;
+
+/* 
+Особенности автобоксинга
+*/
+
+public class Solution {
+    private Integer[] array = new Integer[]{1, 2, 3, 4};
+
+    public static void main(String[] args) {
+        Number value1 = new Solution().getValueByIndex(5); //-1.0, class java.lang.Double expected
+        Number value2 = new Solution().getValueByIndex(2); //3, class java.lang.Integer expected
+
+        System.out.println(value1 + ", " + value1.getClass().toString());
+        System.out.println(value2 + ", " + value2.getClass().toString());
+    }
+
+    Number getValueByIndex(int index) {
+        return (index >= 0 && index < array.length) ? array[index] : new Double(-1);
+    }
+}
