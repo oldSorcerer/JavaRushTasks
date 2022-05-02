5ced95b33a37fdb211239e927be3dbca58ca98bd
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-02 23:08:19 +0300

    add solution Multithreading task 2413 Arkanoid 2/20

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,26 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public static void main(String[] args) {
 
     }
