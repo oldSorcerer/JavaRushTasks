dde683d69b1194a8095a3439bd3452811d729f91
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-02 23:01:06 +0300

    add solution Multithreading task 2301

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2301/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task23.task2301;
+
+/* 
+Запрети наследование
+*/
+
+public class Solution {
+
+    public static final class Listener {
+        public void onMouseDown(int x, int y) {
+            // Do something when the mouse down event occurs
+        }
+
+        public void onMouseUp(int x, int y) {
+            // Do something when the mouse up event occurs
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
