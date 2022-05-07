3681ee84272133a9176f0e0996960d26256ca9d7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-07 15:22:33 +0300

    add solution Syntax task 0720

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/GalavirXIII.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class GalavirXIII {
+    public static int superWeapon = Integer.MAX_VALUE;
+    public double health = Double.MAX_VALUE;
+
+    public void defend (double def){
+        health -=  superWeapon/def;
+    }
+
+    public int attack(){
+        return superWeapon;
+    }
+
+}
