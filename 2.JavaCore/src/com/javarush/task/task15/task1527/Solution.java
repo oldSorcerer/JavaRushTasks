3e86f9354fcabb471f435a3a01b34baaecb182f0
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-07 15:24:14 +0300

    add solution Core task 1527

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task15.task1527;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Парсер реквестов
@@ -11,8 +12,22 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String url = reader.readLine();
-        //напишите тут ваш код
+        //String url = reader.readLine();
+        String url = "http://javarush.ru/alpha/index.html?lvl=15&view&name=Amigo";
+
+        int index = url.indexOf("?");
+        String[] split = url.substring(url.indexOf("?") + 1).split("&");
+        /*for (String str : split) {
+            if (str.contains("obj")) {
+                String obj = str.substring(4);
+                double aDouble = Double.parseDouble(obj);
+                alert(aDouble);
+            }
+        }*/
+        for (String str : split) {
+            System.out.print(str + " ");
+        }
+
     }
 
     public static void alert(double value) {
