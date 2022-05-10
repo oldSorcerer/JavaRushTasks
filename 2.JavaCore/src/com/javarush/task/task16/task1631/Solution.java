b397a3e374e8268c5c51dd03c9d18589d4d8b135
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-10 22:38:52 +0300

    add solution Core task 1631

--- 2.JavaCore/src/com/javarush/task/task16/task1631/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/Solution.java
@@ -10,6 +10,5 @@ Factory method pattern
 public class Solution {
     public static void main(String[] args) {
         ImageReader reader = ImageReaderFactory.getImageReader(ImageTypes.JPG);
-
     }
 }
