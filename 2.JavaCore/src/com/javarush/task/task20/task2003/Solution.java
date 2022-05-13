e577245d54202d43a6f6693b43e034d0d69bc73d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-13 22:56:03 +0300

    add solution Core task 2003

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        //напишите тут ваш код
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
