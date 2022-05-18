c6f01e02e4363ff8d57b28a79bf5bab46ee92aaa
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-18 21:14:36 +0300

    add solution Core task 2026

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task20.task2026;
+
+/* 
+Алгоритмы-прямоугольники
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a1 = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        byte[][] a2 = new byte[][]{
+                {1, 0, 0, 1},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 0, 0, 1}
+        };
+
+        int count1 = getRectangleCount(a1);
+        System.out.println("count = " + count1 + ". Должно быть 2");
+        int count2 = getRectangleCount(a2);
+        System.out.println("count = " + count2 + ". Должно быть 4");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        return 0;
+    }
+}
