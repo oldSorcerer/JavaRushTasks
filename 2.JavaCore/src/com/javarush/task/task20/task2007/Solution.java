1ebe0bc92f8300ded50fa6c75b790a79dec70bbc
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-05-27 12:31:27 +0300

    add solution Core task 2007

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task20.task2007;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Как сериализовать JavaRush?
+*/
+
+public class Solution {
+    public static class JavaRush {
+        public List<User> users = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
