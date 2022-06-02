e56b8ca846a07f77ee97f96c52ea3fbc43b64176
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-02 12:27:27 +0300

    add solution Core task 2009

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task20.task2009;
+
+import java.io.Serializable;
+
+/* 
+Как сериализовать static?
+*/
+
+public class Solution {
+    public static class ClassWithStatic {
+        public static String staticString = "This is a static test string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
