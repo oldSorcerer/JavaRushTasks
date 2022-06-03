eb02ffcfa80a0f7d74401750da3f0f3955c59511
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-03 20:03:08 +0300

    add solution from JavaCollections task 4001

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task40.task4001;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* 
+POST, а не GET
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost(new URL("http://requestb.in/1cse9qt1"), "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(URL url, String urlParameters) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            response.append(responseLine);
+        }
+        bufferedReader.close();
+
+        System.out.println("Response: " + response.toString());
+    }
+}
