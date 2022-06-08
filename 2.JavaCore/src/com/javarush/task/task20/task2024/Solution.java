964cc1045301427806eb34f8921bc877c652aec2
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-08 10:02:47 +0300

    add solution Core task 2024

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2024/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task20.task2024;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Знакомство с графами
+*/
+
+public class Solution {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
