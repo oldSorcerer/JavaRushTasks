33eca1913a5f7a19b21f1f53e79e33d2eec04178
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-11 13:36:03 +0300

    add solution Core task 2006

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2006/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task20.task2006;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Как сериализовать?
+*/
+
+public class Solution {
+    public static class Human {
+        public String name;
+        public List<String> assets = new ArrayList<>();
+
+        public Human() {
+        }
+
+        public Human(String name, String... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
