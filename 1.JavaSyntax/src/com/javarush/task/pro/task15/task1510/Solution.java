6e8c0b35be101c07107114ecf5808347b16d580f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-13 10:28:16 +0300

    add solution Syntax task 1510

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         byte[] bytes = args[0].getBytes();
         try (Scanner scanner = new Scanner(System.in)) {
-            Files.write(Path.of(scanner.nextLine()),bytes);
+            Files.write(Path.of(scanner.nextLine()), bytes);
         } catch (IOException e) {
             System.out.println("Something went wrong : " + e);
         }
