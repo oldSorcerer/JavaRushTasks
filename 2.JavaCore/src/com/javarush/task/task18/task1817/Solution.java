a6bb2defe92df9f2075a036b940c9ed5925f03d7
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-18 13:09:04 +0300

    add solution Core task 1817

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task18.task1817;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Пробелы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
