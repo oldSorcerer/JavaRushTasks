4f0f74f53a609646cbed8c03f59b628f3e85d28a
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-19 15:05:22 +0300

    add solution Core task 1828

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task18.task1828;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Прайсы 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
