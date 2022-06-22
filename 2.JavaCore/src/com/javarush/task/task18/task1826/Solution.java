f5ea5ff309a735f187d61aa1fd2496169ec2639c
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-23 00:54:40 +0300

    add solution Core task 1826

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task18.task1826;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+Шифровка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+}
