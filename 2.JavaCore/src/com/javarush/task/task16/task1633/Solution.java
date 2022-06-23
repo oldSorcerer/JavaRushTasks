fe765bc68fb79526ffdbd5357d0f4e716b1d1e2f
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-23 21:26:52 +0300

    add solution Core task 1633

--- 2.JavaCore/src/com/javarush/task/task16/task1633/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1633/Solution.java
@@ -13,6 +13,9 @@ public class Solution {
         Thread threadA = new Thread(commonThread, "Нить 1");
         Thread threadB = new Thread(commonThread, "Нить 2");
 
+        threadA.setUncaughtExceptionHandler(handler);
+        threadB.setUncaughtExceptionHandler(handler);
+
         threadA.start();
         threadB.start();
 
