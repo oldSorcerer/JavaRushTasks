8868b310f299a7435c71c023cf6e38132618a9c3
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-26 20:56:03 +0300

    add solution Core task 1721

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -2,8 +2,10 @@ package com.javarush.task.task17.task1721;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /* 
@@ -14,11 +16,26 @@ public class Solution {
     public static List<String> allLines = new ArrayList<>();
     public static List<String> forRemoveLines = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader readerPathOne = new BufferedReader(new FileReader(reader.readLine()));
+             BufferedReader readerPathTwo = new BufferedReader(new FileReader(reader.readLine()));) {
+            while (readerPathOne.ready()) {
+                allLines.add(readerPathOne.readLine());
+            }
+            while (readerPathTwo.ready()) {
+                forRemoveLines.add(readerPathTwo.readLine());
+            }
+            new Solution().joinData();
+        }
     }
 
-    public void joinData() throws CorruptedDataException {
-
-
+    public  void joinData() throws CorruptedDataException {
+        if (new HashSet<>(allLines).containsAll(forRemoveLines)) {
+            allLines.removeAll(forRemoveLines);
+        } else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
     }
 }
