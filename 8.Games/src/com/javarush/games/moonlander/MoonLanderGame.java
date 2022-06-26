5f0f891f46922af12b24da2b0c1014e5cc9c5d4d
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-27 00:29:52 +0300

    add solution for game Moon Lander step 13 of 23

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -3,24 +3,59 @@ package com.javarush.games.moonlander;
 import com.javarush.engine.cell.*;
 
 public class MoonLanderGame extends Game {
-
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
 
     private Rocket rocket;
     private GameObject landscape;
-    private GameObject platform;
     private boolean isUpPressed;
     private boolean isLeftPressed;
     private boolean isRightPressed;
-    private boolean isGameStopped;
-    private int score;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
         showGrid(false);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        rocket.move(isUpPressed, isLeftPressed, isRightPressed);
+        check();
+        drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x > WIDTH - 1 || x < 0 || y < 0 || y > HEIGHT - 1) {
+            return;
+        }
+        super.setCellColor(x, y, color);
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (Key.RIGHT == key) {
+            isRightPressed = true;
+            isLeftPressed = false;
+        } else if (Key.LEFT == key) {
+            isLeftPressed = true;
+            isRightPressed = false;
+        } else if (Key.UP == key) {
+            isUpPressed = true;
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        if (Key.RIGHT == key) {
+            isRightPressed = false;
+        } else if (Key.LEFT == key) {
+            isLeftPressed = false;
+        } else if (Key.UP == key) {
+            isUpPressed = false;
+        }
     }
 
     private void createGame() {
@@ -30,91 +65,29 @@ public class MoonLanderGame extends Game {
         isUpPressed = false;
         isLeftPressed = false;
         isRightPressed = false;
-        isGameStopped = false;
-        score = 1000;
     }
 
     private void drawScene() {
-        for(int x = 0; x < WIDTH; x++) {
-            for(int y = 0; y < HEIGHT; y++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
                 setCellColor(x, y, Color.BLACK);
             }
         }
+
         rocket.draw(this);
         landscape.draw(this);
     }
 
     private void createGameObjects() {
-        rocket = new Rocket(WIDTH/2.0, 0);
-        landscape = new GameObject(0,25, ShapeMatrix.LANDSCAPE);
-        platform = new GameObject(23, MoonLanderGame.HEIGHT - 1, ShapeMatrix.PLATFORM);
+        rocket = new Rocket(WIDTH / 2.0, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
     }
-
-    @Override
-    public void onTurn(int step) {
-        rocket.move(isUpPressed, isLeftPressed, isRightPressed);
-        check();
-        drawScene();
-        if (score > 0) {
-            score--;
-        }
-        setScore(score);
-    }
-
-    @Override
-    public void setCellColor(int x, int y, Color color) {
-        if ( x < WIDTH && y < HEIGHT && x >= 0 && y >= 0) {
-            super.setCellColor(x, y, color);
-        }
-    }
-
-    @Override
-    public void onKeyPress(Key key) {
-        if (key.equals(Key.UP)) {
-            isUpPressed = true;
-        } else if (key.equals(Key.LEFT)) {
-            isLeftPressed = true;
-            isRightPressed = false;
-        } else if (key.equals(Key.RIGHT)) {
-            isRightPressed = true;
-            isLeftPressed = false;
-        } else if (key.equals(Key.SPACE) && isGameStopped) {
-            createGame();
-        }
-    }
-
-    @Override
-    public void onKeyReleased(Key key) {
-        if (key.equals(Key.UP)) {
-            isUpPressed = false;
-        } else if (key.equals(Key.LEFT)) {
-            isLeftPressed = false;
-        } else if (key.equals(Key.RIGHT)) {
-            isRightPressed = false;
-        }
-    }
-
     private void check() {
-        if (rocket.isCollision(platform) && rocket.isStopped()) {
-            win();
-        } else if (rocket.isCollision(landscape)) {
-            gameOver();
-        }
     }
-
     private void win() {
-        rocket.land();
-        isGameStopped = true;
-        showMessageDialog(Color.NONE, "WIN", Color.GREEN, 75);
-        stopTurnTimer();
-    }
 
+    }
     private void gameOver() {
-        rocket.crash();
-        isGameStopped = true;
-        showMessageDialog(Color.NONE, "GAME OVER", Color.RED, 75);
-        stopTurnTimer();
-        score = 0;
-    }
 
+    }
 }
--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,23 +1,13 @@
 package com.javarush.games.moonlander;
 
-import com.javarush.engine.cell.*;
-import java.util.Arrays;
-
 public class Rocket extends GameObject {
     private double speedY = 0;
     private double speedX = 0;
     private double boost = 0.05;
     private double slowdown = boost / 10;
 
-    private RocketFire downFire;
-    private RocketFire leftFire;
-    private RocketFire rightFire;
-
     public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
-        downFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_DOWN_1, ShapeMatrix.FIRE_DOWN_2, ShapeMatrix.FIRE_DOWN_3));
-        leftFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_SIDE_1, ShapeMatrix.FIRE_SIDE_2));
-        rightFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_SIDE_1, ShapeMatrix.FIRE_SIDE_2));
     }
 
     public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
@@ -27,12 +17,11 @@ public class Rocket extends GameObject {
             speedY += boost;
         }
         y += speedY;
+
         if (isLeftPressed) {
             speedX -= boost;
-            x += speedX;
         } else if (isRightPressed) {
             speedX += boost;
-            x += speedX;
         } else if (speedX > slowdown) {
             speedX -= slowdown;
         } else if (speedX < -slowdown) {
@@ -42,7 +31,6 @@ public class Rocket extends GameObject {
         }
         x += speedX;
         checkBorders();
-        switchFire(isUpPressed, isLeftPressed, isRightPressed);
     }
 
     private void checkBorders() {
@@ -58,71 +46,7 @@ public class Rocket extends GameObject {
             speedY = 0;
         }
     }
-
     public boolean isStopped() {
         return speedY < 10 * boost;
     }
-
-    public boolean isCollision(GameObject object) {
-        int transparent = Color.NONE.ordinal();
-
-        for (int matrixX = 0; matrixX < width; matrixX++) {
-            for (int matrixY = 0; matrixY < height; matrixY++) {
-                int objectX = matrixX + (int) x - (int) object.x;
-                int objectY = matrixY + (int) y - (int) object.y;
-
-                if (objectX < 0 || objectX >= object.width || objectY < 0 || objectY >= object.height) {
-                    continue;
-                }
-
-                if (matrix[matrixY][matrixX] != transparent && object.matrix[objectY][objectX] != transparent) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public void land() {
-        y--;
-    }
-
-    public void crash() {
-        matrix = ShapeMatrix.ROCKET_CRASH;
-    }
-
-    private void switchFire(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
-        if (isUpPressed) {
-            downFire.x = x + (width / 2);
-            downFire.y = y + height;
-            downFire.show();
-        } else {
-            downFire.hide();
-        }
-
-        if (isLeftPressed) {
-            leftFire.x = x + width;
-            leftFire.y = y + height;
-            leftFire.show();
-        } else {
-            leftFire.hide();
-        }
-
-        if (isRightPressed) {
-            rightFire.x = x - ShapeMatrix.FIRE_SIDE_1[0].length;
-            rightFire.y = y + height;
-            rightFire.show();
-        } else {
-            rightFire.hide();
-        }
-    }
-
-    @Override
-    public void draw(Game game) {
-        super.draw(game);
-        downFire.draw(game);
-        leftFire.draw(game);
-        rightFire.draw(game);
-    }
-
 }
