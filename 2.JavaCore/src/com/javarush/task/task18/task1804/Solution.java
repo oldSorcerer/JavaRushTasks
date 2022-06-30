e5774e7163b9d32bc654450ad70fc28ada6f0001
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-06-30 18:05:35 +0300

    add solution Core task 1804

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task18.task1804;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Самые редкие байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] byteCountArray = new int[256];
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream inputStream = new FileInputStream(reader.readLine()); ) {
+            while (inputStream.available() > 0) {
+                byteCountArray[inputStream.read()] += 1;
+            }
+        }
+
+        int minCount = Integer.MAX_VALUE;
+        for (int byteCount : byteCountArray) {
+            if (byteCount > 0 && byteCount < minCount) {
+                minCount = byteCount;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < byteCountArray.length; i++) {
+            if (byteCountArray[i] == minCount) {
+                list.add(i);
+            }
+        }
+
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+
+    }
+}
