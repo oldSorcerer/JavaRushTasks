46393086961bc2030e7e467db8853e233c35bd85
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-10 12:22:36 +0300

    add solution Core task 1825

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task18.task1825;
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Собираем файл
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
