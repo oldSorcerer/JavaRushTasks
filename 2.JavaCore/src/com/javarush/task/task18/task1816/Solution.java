35e86269aedec8df0a1f8a03dd289ac9dbbd4618
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-12 14:23:50 +0300

    add solution Core task 1816

--- 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -2,15 +2,24 @@ package com.javarush.task.task18.task1816;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /* 
 Английские буквы
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        int count = 0;
+
+        try (FileReader fileReader = new FileReader(args[0])) {
+            while (fileReader.ready()) {
+                int read = fileReader.read();
+                if (read >= 65 && read <= 122 && Character.isAlphabetic(read)) {
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
     }
 }
