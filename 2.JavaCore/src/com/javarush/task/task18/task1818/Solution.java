39e658834cc687cedfb8ec58a202f5e3822bec0e
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-12 22:46:32 +0300

    add solution Core task 1818

--- 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -7,7 +7,19 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        OutputStream outputStream = new FileOutputStream(console.readLine(), true);
+        BufferedReader readerFileTwo = new BufferedReader(new FileReader(console.readLine()));
+        BufferedReader readerFileThree = new BufferedReader(new FileReader(console.readLine()))) {
 
+            while (readerFileTwo.ready()) {
+                outputStream.write(readerFileTwo.readLine().getBytes());
+            }
+            while (readerFileThree.ready()) {
+                outputStream.write(readerFileThree.readLine().getBytes());
+            }
+        }
     }
 }
