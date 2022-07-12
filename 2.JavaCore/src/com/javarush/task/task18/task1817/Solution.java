dbb0689b1b75837c94304929b1a317de2ac732c1
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-12 21:27:46 +0300

    add solution Core task 1817

--- 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task18.task1817;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,7 +9,27 @@ import java.io.IOException;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        StringBuilder stringBuilder = new StringBuilder();
+        int countSpace = 0;
+
+        try (FileReader fileReader = new FileReader(args[0]);
+             BufferedReader reader = new BufferedReader(fileReader)) {
+            while (reader.ready()) {
+                String string = reader.readLine();
+                stringBuilder.append(string);
+            }
+            String bigString = stringBuilder.toString();
+            int countAll = bigString.length();
+            for (char aChar : bigString.toCharArray()) {
+                if (Character.isSpaceChar(aChar)) {
+                    countSpace++;
+                }
+            }
+            if (countAll != 0) {
+                System.out.printf("%.2f", (double) countSpace / countAll * 100);
+            }
+        }
     }
 }
