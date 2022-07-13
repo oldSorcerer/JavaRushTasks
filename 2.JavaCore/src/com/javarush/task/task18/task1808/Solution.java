b0665ae8382f03ee8187677a5fb558a2be696a92
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-14 00:04:07 +0300

    add solution Core task 1808

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -7,7 +7,23 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream readerFileOne = new FileInputStream(console.readLine());
+             FileOutputStream writerFileTwo = new FileOutputStream(console.readLine());
+             FileOutputStream writerFileThree = new FileOutputStream(console.readLine())) {
+
+            int halfFile = (readerFileOne.available() + 1) / 2;
+            int count = 0;
+            while (readerFileOne.available() > 0) {
+                if (count < halfFile) {
+                    writerFileTwo.write(readerFileOne.read());
+                    count ++;
+                } else {
+                    writerFileThree.write(readerFileOne.read());
+                }
+            }
+        }
     }
 }
