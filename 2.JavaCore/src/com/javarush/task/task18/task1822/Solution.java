3396c3e9682a5417a045550fef39d46d74166adb
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2022-07-14 12:42:02 +0300

    add solution Core task 1822

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task18.task1822;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Поиск данных внутри файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
