9885207e22aaea1c930f6c28385fdde7339115ee
oldSorcerer <Echorupta0habibi0viki>
2022-07-16 16:20:08 +0300

    add solution Core task 1820

--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -7,7 +7,18 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))) {
+            while (reader.ready()) {
+                String[] strings = reader.readLine().split(" ");
+                StringBuilder stringBuilder = new StringBuilder();
+                for (String string : strings) {
+                    stringBuilder.append((int) Math.round(Double.parseDouble(string))).append(" ");
+                }
+                writer.write(stringBuilder + System.lineSeparator());
+            }
+        }
     }
 }
