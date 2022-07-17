929d8ce1c966d77b9d671242e00db42bef9cf728
oldSorcerer <Echorupta0habibi0viki>
2022-07-17 11:39:18 +0300

    add solution Core task 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,14 +1,30 @@
 package com.javarush.task.task18.task1821;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 /* 
 Встречаемость символов
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Map<Character, Integer> map = new TreeMap<>();
 
+        try (FileInputStream inputStream = new FileInputStream(args[0])) {
+            while (inputStream.available() > 0) {
+                char aChar = (char) inputStream.read();
+                if (!map.containsKey(aChar)) {
+                    map.put(aChar, 1);
+                } else {
+                    map.put(aChar, map.get(aChar) + 1);
+                }
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
