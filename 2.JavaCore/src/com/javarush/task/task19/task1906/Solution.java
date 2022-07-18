5bf1a8a9c179f4f72cb4bc5772633c7d533b44b8
oldSorcerer <Echorupta0habibi0viki>
2022-07-18 10:14:24 +0300

    add solution Core task 1906

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task19.task1906;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Четные символы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = reader.readLine();
+        String name2 = reader.readLine();
+
+
+    }
+}
