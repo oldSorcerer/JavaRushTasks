36dddbd75ccfc28f94d2bce2f2a79caad4d89651
oldSorcerer <Echorupta0habibi0viki>
2022-07-18 10:11:37 +0300

    add solution Core task 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -13,5 +13,15 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        try (FileReader fileReader = new FileReader(name)) {
+            while (fileReader.ready()) {
+                String string = reader.readLine();
+
+            }
+        }
+
     }
 }
