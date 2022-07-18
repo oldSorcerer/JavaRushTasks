51e5d763d5155584e163073034e60d90b734f65e
oldSorcerer <Echorupta0habibi0viki>
2022-07-18 18:43:38 +0300

    add solution Core task 1907

--- 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -10,6 +10,21 @@ import java.io.InputStreamReader;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            int count = 0;
+            while (reader.ready()) {
+                String string = reader.readLine();
+                String replacedString = string.replaceAll("\\p{P}", " ").replaceAll("\\s", " ");
+                for (String str: replacedString.split(" ")) {
+                    if (str.equals("world")) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
     }
 }
