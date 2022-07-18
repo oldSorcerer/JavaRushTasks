ef7788158c3c512670e1d2df9d668cbbfba32323
oldSorcerer <Echorupta0habibi0viki>
2022-07-19 01:54:32 +0300

    add solution Core task 1917

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task19.task1917;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/* 
+Свой FileWriter
+*/
+
+public class FileConsoleWriter {
+
+    private final FileWriter fileWriter;
+
+    public FileConsoleWriter(String fileName) throws IOException {
+        this.fileWriter = new FileWriter(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        this.fileWriter = new FileWriter(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        this.fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        this.fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        this.fileWriter = new FileWriter(fd);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        System.out.println(new String(cbuf, off, len));
+    }
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println((char) c);
+    }
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.println(str);
+    }
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(new String(str.getBytes(), off, len));
+    }
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(new String(cbuf));
+    }
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+
+
+}
