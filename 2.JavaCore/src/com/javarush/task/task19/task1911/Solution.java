d36db913fcd3d56b8710dd577870c4785307b6af
oldSorcerer <Echorupta0habibi0viki>
2022-07-19 18:18:00 +0300

    add solution Core task 1911

--- 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task19.task1911;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 
 /* 
@@ -10,11 +11,26 @@ import java.io.PrintStream;
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        try (PrintStream consoleStream = System.out;
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             PrintStream stream = new PrintStream(outputStream)) {
+
+            System.setOut(stream);
+
+            testString.printSomething();
+
+            String string = outputStream.toString();
+
+            System.setOut(consoleStream);
+
+            System.out.println(string.toUpperCase());
+        }
     }
 
     public static class TestString {
+
         public void printSomething() {
             System.out.println("it's a text for testing");
         }
