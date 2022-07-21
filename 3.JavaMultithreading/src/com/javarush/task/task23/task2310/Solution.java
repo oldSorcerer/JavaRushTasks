9fb508795707bae812f4adfe4679c036cca2160e
oldSorcerer <Echorupta0habibi0viki>
2022-07-21 23:42:31 +0300

    add solution Java Multithreading task 2310

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2310/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task23.task2310;
+
+/* 
+Напряги извилины!
+*/
+
+public class Solution {
+    private String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("The Darkside Hacker") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
+    public static void main(String[] args) {
+        new Solution("Риша").sout();
+    }
+}
