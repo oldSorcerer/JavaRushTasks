904ed58231203ddd2b1d376d95e313dc4dd37225
oldSorcerer <Echorupta0habibi0viki>
2022-07-28 21:31:07 +0300

    add solution Core task 1802

--- 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -14,9 +14,9 @@ public class Solution {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              FileInputStream inputStream = new FileInputStream(reader.readLine())) {
             while (inputStream.available() > 0) {
-                int date = inputStream.read();
-                if (minByte > date) {
-                    minByte = date;
+                int data = inputStream.read();
+                if (minByte > data) {
+                    minByte = data;
                 }
             }
         }
