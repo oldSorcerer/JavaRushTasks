be6210db810d3f767d1054a6688afa67d87728e9
oldSorcerer <Echorupta0habibi0viki>
2022-07-28 10:51:17 +0300

    add solution Core task 1916

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -12,13 +12,33 @@ import java.util.List;
 */
 
 public class Solution {
-    public static List<LineItem> lines = new ArrayList<LineItem>();
+    public static List<LineItem> lines = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<String> stringListFileOne = new ArrayList<>();
+        List<String> stringListFileTwo = new ArrayList<>();
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader readerFileOne = new BufferedReader(new FileReader(console.readLine()));
+            BufferedReader readerFileTwo = new BufferedReader(new FileReader(console.readLine()))) {
+
+            while (readerFileOne.ready()) {
+                stringListFileOne.add(readerFileOne.readLine());
+            }
+            while (readerFileTwo.ready()) {
+                stringListFileTwo.add(readerFileOne.readLine());
+            }
+        }
+
+        for (int i = 0; i < stringListFileOne.size(); i++) {
+            if (stringListFileOne.get(i).equals(stringListFileTwo.get(i))) {
+                lines.add(new LineItem(Type.SAME, stringListFileOne.get(i)));
+            }
+        }
     }
 
 
-    public static enum Type {
+    public enum Type {
         ADDED,        //добавлена новая строка
         REMOVED,      //удалена строка
         SAME          //без изменений
