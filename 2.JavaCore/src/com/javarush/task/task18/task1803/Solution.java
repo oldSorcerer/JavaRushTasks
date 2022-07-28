92e888cf755051e634ffedfc3d68f3020547d137
oldSorcerer <Echorupta0habibi0viki>
2022-07-28 21:30:38 +0300

    add solution Core task 1803

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
         int[] byteCountArray = new int[256];
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            FileInputStream inputStream = new FileInputStream(reader.readLine()); ) {
+            FileInputStream inputStream = new FileInputStream(reader.readLine())) {
             while (inputStream.available() > 0) {
                 byteCountArray[inputStream.read()] += 1;
             }
