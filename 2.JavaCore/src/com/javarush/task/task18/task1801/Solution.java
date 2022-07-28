9c95e7cd7da34b934ce414e91876dbcfc127f7b4
oldSorcerer <Echorupta0habibi0viki>
2022-07-28 21:19:35 +0300

    add solution Core task 1801

--- 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -11,16 +11,16 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
-        int maxByte = 0;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             FileInputStream inputStream = new FileInputStream(reader.readLine())) {
+            int maxByte = 0;
             while (inputStream.available() > 0) {
                 int date = inputStream.read();
                 if (maxByte < date) {
                     maxByte = date;
                 }
             }
+            System.out.println(maxByte);
         }
-        System.out.println(maxByte);
     }
 }
