e07d1a10c8e3f928516933d6844ed04d6a6a3438
oldSorcerer <Echorupta0habibi0viki>
2022-08-01 23:08:09 +0300

    add solution Core task 1825

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -1,51 +1,43 @@
 package com.javarush.task.task18.task1825;
 
-import java.io.*;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
 /* 
 Собираем файл
 */
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
     public static void main(String[] args) throws IOException {
 
-        TreeSet<String> set = new TreeSet<>();
+        Map<Integer, String> map = new HashMap<>();
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             while (true) {
                 String string = reader.readLine();
                 if (string.equals("end")) {
                     break;
                 }
-                set.add(string);
+                String substring = string.substring(string.lastIndexOf(".part") + 5);
+                int index = Integer.parseInt(substring);
+                if (!map.containsKey(index)) {
+                    map.put(index, string);
+                }
             }
         }
-        set.stream().sorted().forEach(i -> new ReadThread(i).start());
-    }
 
-    static class ReadThread extends Thread {
-        String fileName;
-
-        public ReadThread(String fileName) {
-            this.fileName = fileName;
-        }
-
-        @Override
-        public void run() {
+        for (Map.Entry<Integer, String> pair : map.entrySet()) {
+            String fileName = pair.getValue();
             String shortName = fileName.substring(0, fileName.lastIndexOf("."));
             try (InputStream inputStream = new FileInputStream(fileName);
-                OutputStream outputStream = new FileOutputStream(shortName, true)
-            ) {
+                 OutputStream outputStream = new FileOutputStream(shortName)) {
                 byte[] buffer = new byte[inputStream.available()];
                 while (inputStream.available() > 0) {
                     int read = inputStream.read(buffer);
                     outputStream.write(buffer, 0, read);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
         }
     }
