4051312f186690783aa7e5fc20e0f6cc10a9e287
oldSorcerer <Echorupta0habibi0viki>
2022-08-02 19:39:24 +0300

    add solution Core task 1819

--- 2.JavaCore/src/com/javarush/task/task18/task1819/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1819/Solution.java
@@ -8,20 +8,21 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String pathFileOne = console.readLine();
-        StringBuilder contentFileOne = new StringBuilder();
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            String pathFileOne = console.readLine();
+            StringBuilder contentFileOne = new StringBuilder();
 
-        try (BufferedReader readerFileOne = new BufferedReader(new FileReader(pathFileOne));
-             OutputStream outputStream = new FileOutputStream(pathFileOne);
-             BufferedReader readerFileTwo = new BufferedReader(new FileReader(console.readLine()))) {
-            while (readerFileOne.ready()) {
-                contentFileOne.append(readerFileOne.readLine());
+            try (BufferedReader readerFileOne = new BufferedReader(new FileReader(pathFileOne));
+                 OutputStream outputStream = new FileOutputStream(pathFileOne);
+                 BufferedReader readerFileTwo = new BufferedReader(new FileReader(console.readLine()))) {
+                while (readerFileOne.ready()) {
+                    contentFileOne.append(readerFileOne.readLine());
+                }
+                while (readerFileTwo.ready()) {
+                    outputStream.write(readerFileTwo.readLine().getBytes());
+                }
+                outputStream.write(contentFileOne.toString().getBytes());
             }
-            while (readerFileTwo.ready()) {
-                outputStream.write(readerFileTwo.readLine().getBytes());
-            }
-            outputStream.write(contentFileOne.toString().getBytes());
         }
     }
 }
