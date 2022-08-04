0f57337eb4fe288cd59f075c8dfd19b7345ef7a7
oldSorcerer <Echorupta0habibi0viki>
2022-08-04 14:01:46 +0300

    add solution Core task 1828

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -1,18 +1,63 @@
 package com.javarush.task.task18.task1828;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /* 
 Прайсы 2
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Map<String, String> map = new LinkedHashMap<>();
+
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            String fileName = console.readLine();
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+                while (reader.ready()) {
+                    String string = reader.readLine();
+                    String index = string.substring(0, 8).trim();
+                    String substring = string.substring(8);
+                    if (!map.containsKey(index)) {
+                        map.put(index, substring);
+                    }
+                }
+                if(args.length != 0) {
+                    if (args[0].equals("-u")) {
+                        String id = args[1].trim();
+                        for (Map.Entry<String, String> pair : map.entrySet()) {
+                            if (id.equals(pair.getKey())) {
+                                map.put(id, checkLength(args[2], 30) +
+                                        checkLength(args[3], 8) +
+                                        checkLength(args[4], 4));
+                            }
+                        }
+                    } else if (args[0].equals("-d")) {
+                        String id = args[1].trim();
+                        map.entrySet().removeIf(key -> key.getKey().equals(id));
+                    }
+                }
+            }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    writer.write(checkLength(entry.getKey(), 8) + entry.getValue());
+                    writer.newLine();
+                }
+            }
+        }
+    }
+
+    public static String checkLength(String string, int limit) {
+        StringBuilder builder = new StringBuilder(string);
+        if (string.length() > limit) {
+            return string.substring(0, limit);
+        } else {
+            while (builder.length() != limit) {
+                builder.append(" ");
+            }
+            return builder.toString();
+        }
     }
 }
