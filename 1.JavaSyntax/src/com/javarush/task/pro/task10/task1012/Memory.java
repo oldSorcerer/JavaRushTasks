1f4cb41f55454d6361193280834890ca6680e45f
oldSorcerer <Echorupta0habibi0viki>
2022-08-05 17:06:03 +0300

    add solution Syntax task 1012

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1012/Memory.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1012/Memory.java
@@ -16,7 +16,7 @@ public class Memory {
 
     public static void executeDefragmentation(String[] array) {
         for (int i = array.length; i >= 1; i--) {
-            for (int j = 1;  j < i; j++) {
+            for (int j = 1; j < i; j++) {
                 if (array[j - 1] == null) {
                     array[j - 1] = array[j];
                     array[j] = null;
