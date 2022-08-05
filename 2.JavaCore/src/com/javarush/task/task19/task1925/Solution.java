9476873deecaaaed74302cbeb47b007623445a70
oldSorcerer <Echorupta0habibi0viki>
2022-08-05 17:39:28 +0300

    add solution Core task 1925

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task19.task1925;
 
 import java.io.*;
-import java.util.ArrayList;
 
 /* 
 Длинные слова
@@ -10,18 +9,20 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws IOException {
 
-        String fileNameOne = args[0];
-        String fileNameTwo = args[1];
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileNameOne));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(fileNameTwo))
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))
             ) {
+            StringBuilder builder = new StringBuilder();
             while (reader.ready()) {
                 String string = reader.readLine();
                 String[] strings = string.split(" ");
-
+                for (String str : strings) {
+                    if (str.length() > 6) {
+                        builder.append(str).append(",");
+                    }
+                }
             }
+            writer.write(builder.toString().replaceAll(",$", ""));
         }
-
     }
 }
