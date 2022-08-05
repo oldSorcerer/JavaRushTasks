d86efdb1bcb5bec4f4e0b3b33e089b5b39560ebe
oldSorcerer <Echorupta0habibi0viki>
2022-08-05 17:41:55 +0300

    add solution Core task 1918

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task19.task1918;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Знакомство с тегами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
