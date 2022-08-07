51e2a09cdb4482bd87c6745dfa87662405af7983
oldSorcerer <Echorupta0habibi0viki>
2022-08-07 21:22:33 +0300

    add solution Core task 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -7,7 +7,7 @@ import java.io.*;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
 
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
             String fileName = console.readLine();
