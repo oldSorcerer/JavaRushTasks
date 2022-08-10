0a7f1ee8f1f18441a5f053cf753127843169262c
oldSorcerer <Echorupta0habibi0viki>
2022-08-10 11:23:54 +0300

    add solution Core task 2004

--- 2.JavaCore/src/com/javarush/task/task20/task2004/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2004/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task20.task2004;
 
 import java.io.*;
+import java.util.Objects;
 
 /* 
 Читаем и пишем в файл статики
@@ -8,10 +9,8 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
-        //you can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
-        //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
-        try {
 
+         try {
             File yourFile = File.createTempFile("your_file_name", null);
             OutputStream outputStream = new FileOutputStream(yourFile);
             InputStream inputStream = new FileInputStream(yourFile);
@@ -28,16 +27,13 @@ public class Solution {
             loadedObject.j = 7;
 
             loadedObject.load(inputStream);
-            //here check that the classWithStatic object is equal to the loadedObject object - проверьте тут, что classWithStatic и loadedObject равны
 
             outputStream.close();
             inputStream.close();
 
         } catch (IOException e) {
-            //e.printStackTrace();
             System.out.println("Oops, something is wrong with my file");
         } catch (Exception e) {
-            //e.printStackTrace();
             System.out.println("Oops, something is wrong with the save/load method");
         }
     }
@@ -48,30 +44,32 @@ public class Solution {
         public int j;
 
         public void save(OutputStream outputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            try (PrintWriter printWriter = new PrintWriter(outputStream)) {
+                printWriter.println(staticString);
+                printWriter.println(this.i);
+                printWriter.println(this.j);
+            }
         }
 
         public void load(InputStream inputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                staticString = reader.readLine();
+                this.i = Integer.parseInt(reader.readLine());
+                this.j = Integer.parseInt(reader.readLine());
+            }
         }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-
             ClassWithStatic that = (ClassWithStatic) o;
-
-            if (i != that.i) return false;
-            return j == that.j;
-
+            return i == that.i && j == that.j;
         }
 
         @Override
         public int hashCode() {
-            int result = i;
-            result = 31 * result + j;
-            return result;
+            return Objects.hash(i, j);
         }
     }
 }
