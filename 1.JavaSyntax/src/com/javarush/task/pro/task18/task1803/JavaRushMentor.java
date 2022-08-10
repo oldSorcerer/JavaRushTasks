b35542ceb916f1025f949ce8a9dacf54e4304730
oldSorcerer <Echorupta0habibi0viki>
2022-08-10 14:48:50 +0300

    add solution Syntax task 1803

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1803/JavaRushMentor.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1803/JavaRushMentor.java
@@ -1,7 +1,7 @@
 package com.javarush.task.pro.task18.task1803;
 
 public class JavaRushMentor {
-    private String name;
+    private final String name;
 
     public JavaRushMentor(String name) {
         this.name = name;
