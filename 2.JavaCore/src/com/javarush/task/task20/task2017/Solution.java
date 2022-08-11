79cba3ed1474ef2642396ccd5b9b57b9ce47bce3
oldSorcerer <Echorupta0habibi0viki>
2022-08-11 20:34:23 +0300

    add solution Core task 2017

--- 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task20.task2017;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
@@ -7,12 +8,18 @@ import java.io.Serializable;
 Десериализация
 */
 
-public class Solution {
+public class Solution implements Serializable {
     public A getOriginalObject(ObjectInputStream objectStream) {
-        return null;
+        A a;
+        try {
+            a = (A) objectStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return a;
     }
 
-    public class A {
+    public class A implements Serializable {
     }
 
     public class B extends A {
