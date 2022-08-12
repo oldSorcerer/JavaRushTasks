c4bfb36194a654f39bc4163d3a5ba46242d6dd9a
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 13:41:02 +0300

    add solution University Professional task 4106

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4106/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task41.task4106;
+
+/* 
+Вселенная под ключ
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Universe universe = new Universe();
+        Universe anotherUniverse = new Universe();
+        System.out.println(universe);
+        System.out.println(anotherUniverse);
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4106/Universe.java
@@ -0,0 +1,36 @@
+package com.javarush.task.jdk13.task41.task4106;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public final class Universe {
+
+    private final Set<String> laws = new HashSet<>();
+
+    public Universe() {
+        System.out.println("Fine-tuning the laws of the universe begins.");
+        tune("скорость света");
+        tune("гравитационная постоянная");
+        tune("постоянная Планка");
+        tune("масса электрона");
+        tune("масса протона");
+        tune("заряд электрона");
+        System.out.println("Fine-tuning the laws of the universe is complete.");
+    }
+
+    public Set<String> getLaws() {
+        return Collections.unmodifiableSet(laws);
+    }
+
+    private void tune(String low) {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("устанавливается " + low);
+        laws.add(low);
+    }
+}
