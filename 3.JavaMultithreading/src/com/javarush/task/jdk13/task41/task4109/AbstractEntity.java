166c01c405018c9897fd4e1eb38daf5a765c5ac5
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 13:42:16 +0300

    add solution University Professional task 4109

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4109/AbstractEntity.java
@@ -0,0 +1,9 @@
+package com.javarush.task.jdk13.task41.task4109;
+
+import java.util.Date;
+
+public abstract class AbstractEntity {
+    public int id;
+    public Date createdTime;
+    public Date updatedTime;
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4109/CguRequisition.java
@@ -0,0 +1,6 @@
+package com.javarush.task.jdk13.task41.task4109;
+
+public class CguRequisition extends RequisitionWithPhone {
+    public String requestedSubscriptionType;
+    public String language;
+}
