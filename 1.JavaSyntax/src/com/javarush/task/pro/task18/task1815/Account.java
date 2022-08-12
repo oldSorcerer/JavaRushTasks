59e5d21f75e832514ab7fe94037ddc1e9c39a0fe
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 14:30:22 +0300

    add solution Syntax task 1815

--- 1.JavaSyntax/src/com/javarush/task/pro/task18/task1815/Account.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1815/Account.java
@@ -1,8 +1,8 @@
 package com.javarush.task.pro.task18.task1815;
 
 public class Account {
-    private String name;
-    private String email;
+    private final String name;
+    private final String email;
 
     public Account(String name, String email) {
         this.name = name;
