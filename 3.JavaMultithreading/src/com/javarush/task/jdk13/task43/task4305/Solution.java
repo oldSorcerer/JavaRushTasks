da05085682b004267833774585748f96ad3fcfd4
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 14:04:31 +0300

    add solution University Professional task 4305

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4305/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.jdk13.task43.task4305;
+
+/* 
+Вернуться к истокам
+*/
+
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+        boolean result = helloWorldChecker(choice);
+        System.out.println("The result is: " + result);
+    }
+
+    public static boolean helloWorldChecker(String s) {
+        //напишите тут ваш код
+
+        return false;
+    }
+}
