df22bf5b01895dc5f45a8af644a980f2a2f91bf2
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 14:07:48 +0300

    add solution University Professional task 4312

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4312/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.jdk13.task43.task4312;
+
+import java.util.Set;
+
+/* 
+Антихакер
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        hackTheUniverse();
+        Universe.getInstance().getLaws().forEach(System.out::println);
+    }
+
+    public static void hackTheUniverse() {
+        Set<String> laws = Universe.getInstance().getLaws();
+        try {
+            laws.removeAll(laws);
+            laws.add("Всё, что может пойти не так, пойдёт не так.");
+            laws.add("Соседняя очередь всегда движется быстрее.");
+            laws.add("Работа заполняет время, отведённое на неё.");
+            laws.add("90% чего угодно — ерунда.");
+            laws.add("Где работает закон подлости, там отдыхает теория вероятности.");
+        } catch (Exception e) {
+            System.out.println("Вселенная защищена лучше, чем мы думали.");
+        }
+    }
+}
