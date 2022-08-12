0ca145a45e7059eb3a43c03dd5c03a51f1c96fe1
oldSorcerer <Echorupta0habibi0viki>
2022-08-12 13:39:18 +0300

    add solution University Professional task 4102

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4102/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.jdk13.task41.task4102;
+
+/* 
+Фирменный рецепт
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Pizza pizza = new SimplePizza();
+        printInfo(pizza);
+
+//        pizza = new WithChicken(pizza);
+//        printInfo(pizza);
+//
+//        pizza = new WithSausage(pizza);
+//        printInfo(pizza);
+//
+//        pizza = new WithArugula(pizza);
+//        printInfo(pizza);
+    }
+
+    public static void printInfo(Pizza c) {
+        System.out.println("Итого: " + c.getCost() + "; Состав: " + c.getIngredients());
+    }
+}
