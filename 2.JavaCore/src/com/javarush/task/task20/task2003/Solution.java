22231a829c6b9bc0431aee25d14789b8eda857e0
oldSorcerer <Echorupta0habibi0viki>
2022-08-14 13:09:19 +0300

    add solution Core task 2003

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -14,11 +14,19 @@ public class Solution {
     public static Map<String, String> runtimeStorage = new HashMap<>();
 
     public static void save(OutputStream outputStream) throws Exception {
-        //напишите тут ваш код
+        Properties properties = new Properties();
+        for (Map.Entry<String, String> entry : runtimeStorage.entrySet()) {
+            properties.setProperty(entry.getKey(), entry.getValue());
+        } //properties.forEach((k,v) ->properties.setProperty(k.toString(), v.toString()));
+        properties.store(outputStream, null);
     }
 
     public static void load(InputStream inputStream) throws IOException {
-        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            runtimeStorage.put(entry.getKey().toString(), entry.getValue().toString());
+        } // properties.forEach((k,v) -> properties.put(k.toString(), v.toString()));
     }
 
     public static void main(String[] args) {
