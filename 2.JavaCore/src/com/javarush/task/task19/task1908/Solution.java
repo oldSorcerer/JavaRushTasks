2795469ea28cab8b871e149b5e5e4cd380bd1763
oldSorcerer <Echorupta0habibi0viki>
2022-08-14 22:38:52 +0300

    add solution Core task 1908

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task19.task1908;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /* 
 Выделяем числа
@@ -10,7 +8,6 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        List<Integer> list = new ArrayList<>();
 
         try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
              BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
@@ -20,13 +17,10 @@ public class Solution {
                 for (String str : string.split(" ")) {
                     try {
                         int number = Integer.parseInt(str);
-                        list.add(number);
+                        writer.write(number + " ");
                     } catch (Exception ignore) {
                     }
                 }
-            }
-            for (Integer integer: list) {
-                writer.write(integer + " ");
             }
         }
     }
