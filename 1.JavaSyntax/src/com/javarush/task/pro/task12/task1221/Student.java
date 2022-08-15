75624a40087252a8ba995e9c2abd8a1324c07289
oldSorcerer <Echorupta0habibi0viki>
2022-08-15 16:54:13 +0300

    add solution Syntax task 1221

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/Student.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1221/Student.java
@@ -1,7 +1,7 @@
 package com.javarush.task.pro.task12.task1221;
 
 public class Student {
-    private String name;
+    private final String name;
 
     public Student(String name) {
         this.name = name;
