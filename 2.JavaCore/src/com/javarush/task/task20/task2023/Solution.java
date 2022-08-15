8f6d094a78dfce859dafeaab53fd4207398fc47f
oldSorcerer <Echorupta0habibi0viki>
2022-08-15 23:17:58 +0300

    add solution Core task 2023

--- 2.JavaCore/src/com/javarush/task/task20/task2023/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2023/Solution.java
@@ -11,17 +11,19 @@ public class Solution {
     }
 
     public static class A {
-        public void method1() {
+        private void method1() {
             System.out.println("A class, method1");
         }
 
         public void method2() {
             System.out.println("A class, method2");
+            method1();
         }
     }
 
     public static class B extends A {
-        public void method1() {
+        public void method1(B b) {
+            super.method2();
             System.out.println("B class, method1");
         }
 
@@ -37,6 +39,7 @@ public class Solution {
 
         public void method2() {
             System.out.println("C class, method2");
+            super.method1(new B());
         }
     }
 }
