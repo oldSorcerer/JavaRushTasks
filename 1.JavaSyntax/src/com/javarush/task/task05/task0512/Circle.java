9dd768db8d623165c76833118305bde48556bde5
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:36:16 +0300

    add solution Syntax task 0512

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0512/Circle.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task05.task0512;
+
+/* 
+Создать класс Circle
+*/
+
+public class Circle {
+    //напишите тут ваш код
+int centerX, centerY, radius, width, color;
+    public void initialize(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+
+    }
+    public void initialize(int centerX, int centerY, int radius, int width) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.width = width;
+
+    }
+public void initialize(int centerX, int centerY, int radius, int width, int color) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.radius = radius;
+    this.width = width;
+    this.color = color;
+}
+    public static void main(String[] args) {
+
+    }
+}
+/*centerX, centerY, radius
+— centerX, centerY, radius, width
+— centerX, centerY, radius, width, color*/
