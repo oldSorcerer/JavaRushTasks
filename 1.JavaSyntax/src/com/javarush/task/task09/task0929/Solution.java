06864137df3a1d9ba62d3580f7b9a0544a6fa1bc
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 12:56:56 +0300

    add solution Syntax task 0929

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task09.task0929;
+
+import java.io.*;
+
+/* 
+Обогатим код функциональностью!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sourceFileName = reader.readLine();
+        String destinationFileName = reader.readLine();
+        String sourceFileName2 = reader.readLine();
+        InputStream fileInputStream;
+        try {
+            fileInputStream = getInputStream(sourceFileName);
+        } catch(IOException e){
+            System.out.println("Файл не существует.");
+            try{
+                fileInputStream = getInputStream(sourceFileName);
+            }catch(IOException e1){
+                fileInputStream = getInputStream(sourceFileName2);
+            }
+}
+        OutputStream fileOutputStream = getOutputStream(destinationFileName);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    public static InputStream getInputStream(String fileName) throws IOException {
+        return new FileInputStream(fileName);
+    }
+
+    public static OutputStream getOutputStream(String fileName) throws IOException {
+        return new FileOutputStream(fileName);
+    }
+}
+
+/*Обогатим код функциональностью!
+Задача: Программа вводит два имени файла. И копирует первый файл на место, заданное вторым именем.
+Новая задача: Программа вводит два имени файла. И копирует первый файл на место, заданное вторым именем.
+Если файла (который нужно копировать) с указанным именем не существует, то программа должна вывести надпись «Файл не существует.» и еще раз прочитать имя файла с консоли, а уже потом считывать файл для записи.
+
+
+Требования:
+1. Программа должна считывать имена файлов.
+2. Метод main должен обрабатывать исключения кидаемые методом getInputStream. Если возникло исключение, нужно вывести сообщение «Файл не существует.» и еще раз прочитать имя файла с консоли
+3. Программа должна копировать содержимое первого файла во второй.
+4. В методе main должен вызываться метод getInputStream.
+5. Метод getInputStream изменять нельзя.
+6. В методе main должен вызывать метод getOutputStream.
+7. Метод getOutputStream изменять нельзя.*/
