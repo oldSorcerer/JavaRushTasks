42381a902086321e7cee1d918ff7cf7e2a2e7e37
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:01:54 +0300

    add solution Syntax task 0912

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0912/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task09.task0912;
+
+/* 
+Исключение при работе с числами
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+try {
+    int num = Integer.parseInt("XYZ");
+    System.out.println(num);
+}catch(NumberFormatException r){
+    System.out.print("NumberFormatException");
+}
+        //напишите тут ваш код
+    }
+}
+/*Исключение при работе с числами
+Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+int num=Integer.parseInt("XYZ");
+System.out.println(num);
+
+
+Требования:
+1. Программа должна выводить сообщение на экран.
+2. В программе должен быть блок try-catch.
+3. Программа должна отлавливать исключения конкретного типа, а не все возможные (Exception).
+4. Выведенное сообщение должно содержать тип возникшего исключения.
+5. Имеющийся код в методе main не удалять.*/
