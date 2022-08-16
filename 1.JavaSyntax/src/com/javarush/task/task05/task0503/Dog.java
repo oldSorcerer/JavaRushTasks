4e30bdb1cd667f668bee5694881066b2525622b0
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:32:57 +0300

    add solution Syntax task 0503

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0503;
+
+
+/* 
+Геттеры и сеттеры для класса Dog
+*/
+
+public class Dog {
+    //напишите тут ваш код
+private  String name;
+    private  int age;
+    public static void main(String[] args) {
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getAge() {
+        return this.age;
+    }
+}
+/*Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создайте геттеры и сеттеры для всех переменных класса Dog.*/
