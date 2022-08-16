f6303260b142837308b4c89032984a7f50955004
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:13:47 +0300

    add solution Syntax task 0106

--- 1.JavaSyntax/src/com/javarush/task/task01/task0106/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0106/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0106;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("Это не баг, это фича.");
     }
 }
