16cb9def50044ef1f59c7b188b69564c77325ec0
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 14:23:20 +0300

    add solution Syntax task 0605

--- 1.JavaSyntax/src/com/javarush/task/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0605/Solution.java
@@ -10,16 +10,16 @@ import java.io.*;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
-        double weight = Double.parseDouble(bis.readLine());
-        double height = Double.parseDouble(bis.readLine());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        double weight = Double.parseDouble(reader.readLine());
+        double height = Double.parseDouble(reader.readLine());
 
         Body.massIndex(weight, height);
     }
 
     public static class Body {
         public static void massIndex(double weight, double height) {
-            //напишите тут ваш код
             double a = weight / (height * height);
             if (a < 18.5)
                 System.out.println("Недовес: меньше чем 18.5");
@@ -29,29 +29,6 @@ public class Solution {
                 System.out.println("Избыточный вес: между 25 и 29.9");
             else if (a >= 30)
                 System.out.println("Ожирение: 30 или больше");
-
         }
     }
 }
-
-/*Контролируем массу тела
-Программа должна считывать введенные пользователем вес в килограммах и рост в метрах и выводить на экран сообщение о индексе массы тела.
-Реализуй статический метод класса Body. Метод должен определить индекс массы тела, и вывести на экран сообщение:
-«Недовес: меньше чем 18.5» — если индекс массы тела меньше 18.5,
-«Нормальный: между 18.5 и 24.9» — если индекс массы тела между 18.5 и 24.9,
-«Избыточный вес: между 25 и 29.9» — если индекс массы тела между 25 и 29.9,
-«Ожирение: 30 или больше» — если индекс массы тела 30 или больше.
-
-Подсказка:
-Индекс массы тела = вес в кг / (рост в метрах * рост в метрах)
-
-Пример вывода для 68.4 и 1.77:
-Нормальный: между 18.5 и 24.9
-
-
-Требования:
-1. Метод massIndex должен выводить текст на экран.
-2. Метод massIndex должен выводить "Недовес: меньше чем 18.5" на экран, если индекс массы тела меньше 18.5.
-3. Метод massIndex должен выводить "Нормальный: между 18.5 и 24.9" на экран, если индекс массы тела между 18.5 и 24.9.
-4. Метод massIndex должен выводить "Избыточный вес: между 25 и 29.9" на экран, если индекс массы тела между 25 и 29.9.
-5. Метод massIndex должен выводить "Ожирение: 30 или больше" на экран, если индекс массы тела 30 или больше.*/
