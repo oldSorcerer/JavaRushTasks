3d53b22a327f22ef299d0aca1bf1e0882b620af1
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:04:11 +0300

    add solution Syntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task01.task0101;
+
+/* 
+Первая программа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Диего + Амиго = Друзья");
+    }
+}
