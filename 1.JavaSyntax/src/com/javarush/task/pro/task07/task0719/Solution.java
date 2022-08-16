d8feab903d5b1772b50112b5ab1a0d0b3915efb4
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:33:49 +0300

    add solution Syntax task 0719

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        AutomaticKitchen automaticKitchen = new AutomaticKitchen();
+        Gun gun = new Gun();
+        ProtectiveFieldGenerator fieldGenerator = new ProtectiveFieldGenerator();
+        RepairDrone repairDrone = new RepairDrone();
+        RobotCleaner robotCleaner = new RobotCleaner();
+    }
+}
