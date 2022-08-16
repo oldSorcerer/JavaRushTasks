281447277c1801fbded61248f2cbd2a4534dd849
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:41:31 +0300

    add solution Syntax task 0406

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0406/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0406;
+
+/* 
+Реализовать метод setName
+*/
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        this.fullName = firstName + " " + lastName;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
