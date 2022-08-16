d26dcb9d78539bb051d6ec703b1f5e4ce0b85e87
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:31:51 +0300

    add solution Syntax task 0308

--- 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -6,7 +6,6 @@ package com.javarush.task.task03.task0308;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(1*2*3*4*5*6*7*8*9*10);
-
+        System.out.println(2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
     }
 }
