e09533ec0e5476437fc0d22e385c00dd24a4b8a0
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:57:39 +0300

    add solution Syntax task 0524

--- 1.JavaSyntax/src/com/javarush/task/task05/task0524/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0524/Circle.java
@@ -8,13 +8,13 @@ public class Circle {
     public double x;
     public double y;
     public double r;
-    public Circle(double x,double y,double r){
-      this.x = x;
+
+    public Circle(double x, double y, double r) {
+        this.x = x;
         this.y = y;
         this.r = r;
     }
 
-    //напишите тут ваш код
     public static void main(String[] args) {
 
     }
