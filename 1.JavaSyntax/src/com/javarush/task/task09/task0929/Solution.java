e1bcdd7600ab5d8f292d381c7806f64fb881177c
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:16:56 +0300

    add solution Syntax task 0929

--- 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
@@ -16,14 +16,14 @@ public class Solution {
         InputStream fileInputStream;
         try {
             fileInputStream = getInputStream(sourceFileName);
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Файл не существует.");
-            try{
+            try {
                 fileInputStream = getInputStream(sourceFileName);
-            }catch(IOException e1){
+            } catch (IOException e1) {
                 fileInputStream = getInputStream(sourceFileName2);
             }
-}
+        }
         OutputStream fileOutputStream = getOutputStream(destinationFileName);
 
         while (fileInputStream.available() > 0) {
@@ -43,18 +43,3 @@ public class Solution {
         return new FileOutputStream(fileName);
     }
 }
-
-/*Обогатим код функциональностью!
-Задача: Программа вводит два имени файла. И копирует первый файл на место, заданное вторым именем.
-Новая задача: Программа вводит два имени файла. И копирует первый файл на место, заданное вторым именем.
-Если файла (который нужно копировать) с указанным именем не существует, то программа должна вывести надпись «Файл не существует.» и еще раз прочитать имя файла с консоли, а уже потом считывать файл для записи.
-
-
-Требования:
-1. Программа должна считывать имена файлов.
-2. Метод main должен обрабатывать исключения кидаемые методом getInputStream. Если возникло исключение, нужно вывести сообщение «Файл не существует.» и еще раз прочитать имя файла с консоли
-3. Программа должна копировать содержимое первого файла во второй.
-4. В методе main должен вызываться метод getInputStream.
-5. Метод getInputStream изменять нельзя.
-6. В методе main должен вызывать метод getOutputStream.
-7. Метод getOutputStream изменять нельзя.*/
