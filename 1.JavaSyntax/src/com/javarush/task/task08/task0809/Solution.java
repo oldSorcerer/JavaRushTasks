2eae663e3c6d7e7cab168c4fe65009649e30eb07
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:00:37 +0300

    add solution Syntax task 0809

--- 1.JavaSyntax/src/com/javarush/task/task08/task0809/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0809/Solution.java
@@ -16,14 +16,11 @@ public class Solution {
     }
 
     public static long getTimeMsOfInsert(List list) {
-        //напишите тут ваш код
-Date d1 = new Date();
-long s1 = d1.getTime();
+
+        Date start = new Date();
         insert10000(list);
-        Date d2 = new Date();
-        long s2 = d2.getTime();
-        return s2-s1;
-        //напишите тут ваш код
+        Date end = new Date();
+        return end.getTime() - start.getTime();
 
     }
 
@@ -33,14 +30,3 @@ long s1 = d1.getTime();
         }
     }
 }
-/*Время для 10 тысяч вставок
-Измерить, сколько времени занимает 10 тысяч вставок для каждого списка.
-Метод getTimeMsOfInsert должен вернуть время своего исполнения в миллисекундах.
-
-
-Требования:
-1. Программа должна выводить числа на экран.
-2. Метод main должен вызывать метод getTimeMsOfInsert только два раза.
-3. Метод insert10000(List list) должен вставлять 10 тысяч элементов в список.
-4. Метод getTimeMsOfInsert должен вызывать метод insert10000 только один раз.
-5. Метод getTimeMsOfInsert должен вернуть время в миллисекундах, которое занимает 10 тысяч вставок в список.*/
