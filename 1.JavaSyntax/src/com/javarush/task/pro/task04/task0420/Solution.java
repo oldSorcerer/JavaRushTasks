45b4fe7fda6560263358584b281127bc0f61b9c0
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:16:17 +0300

    add solution Syntax task 0420

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0420/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task04.task0420;
+
+import java.util.Scanner;
+
+/* 
+Страшное число
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Введите предполагаемое число");
+
+        checkNumber(3);
+    }
+
+    public static void checkNumber(int number) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+
+        while (true) {
+            inputNumber = scanner.nextInt();
+            if (number == inputNumber) {
+                System.out.println("Загадочное число равно " + number);
+                break;
+            }
+            System.out.println("Попробуй еще раз");
+        }
+
+    }
+}
