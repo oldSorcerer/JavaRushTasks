1ceeb53ef485346434897623c317c74874f5053e
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 14:24:23 +0300

    add solution Syntax task 0620

--- 1.JavaSyntax/src/com/javarush/task/task06/task0620/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0620/Solution.java
@@ -14,21 +14,12 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String max = "Max is ";
+        String string = "Max is ";
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
-        int maxx = a > b ? a : b;
+        int max = Math.max(a, b);
 
-        System.out.println(max + maxx);
+        System.out.println(string + max);
     }
 
 }
-/*Исправляем ошибки юности
-Задача: Программа вводит два числа с клавиатуры и выводит их максимум в виде «Max is 25».
-
-
-Требования:
-1. Программа должна считывать данные с клавиатуры.
-2. Программа должна выводить результат на экран.
-3. Текст выведенный на экран должен начинаться с «Max is».
-4. Текст выведенный на экран должен заканчиваться максимумом из 2х введенных чисел.*/
