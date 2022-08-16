3cccae9bbf7b32503eaa6c14c16e7944ad72b818
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:13:30 +0300

    add solution Syntax task 0102

--- 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.task01.task0102;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Элли самая красивая");
+        }
     }
 }
