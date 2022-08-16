3cab7feb83e88dcbe6824e7f863e793c95397747
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:06:53 +0300

    add solution Syntax task 0917

--- 1.JavaSyntax/src/com/javarush/task/task09/task0917/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0917/Solution.java
@@ -10,18 +10,18 @@ public class Solution {
     }
 
     public static void processExceptions(Solution obj) {
-      try {
-          obj.method1();
+        try {
+            obj.method1();
 
-          obj.method2();
-          obj.method3();
-      } catch(NullPointerException q){
-          printStack(q);
-      } catch(IndexOutOfBoundsException q){
-          printStack(q);
-      } catch(NumberFormatException q){
-          printStack(q);
-      }
+            obj.method2();
+            obj.method3();
+        } catch (NullPointerException q) {
+            printStack(q);
+        } catch (IndexOutOfBoundsException q) {
+            printStack(q);
+        } catch (NumberFormatException q) {
+            printStack(q);
+        }
     }
 
     public static void printStack(Throwable throwable) {
@@ -43,17 +43,3 @@ public class Solution {
         throw new NumberFormatException();
     }
 }
-
-/*Перехват unchecked-исключений
-В методе processExceptions обработайте все unchecked исключения.
-Нужно вывести стек-трейс каждого возникшего исключения используя метод printStack.
-Можно использовать только один блок try..catch
-
-
-Требования:
-1. Метод processExceptions должен вызывать метод method1.
-2. Метод processExceptions должен вызывать метод method2.
-3. Метод processExceptions должен вызывать метод method3.
-4. Метод processExceptions должен использовать только один блок try..catch.
-5. Метод processExceptions должен отлавливать все unchecked исключения и выводить стек-трейс каждого из них, используя метод printStack.
-6. Программа должна выводить на экран текст.*/
