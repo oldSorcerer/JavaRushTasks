cfbe42b71bbf64ed9095fb94675edcad611e7e86
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 12:58:39 +0300

    add solution Syntax task 0923

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Гласные и согласные
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String g = r.readLine();
+        char[] q = g.toCharArray();
+        String gl="";
+        String sogl="";
+        for(char t:q){
+            if (isVowel(t)){
+                gl=gl+t+" ";
+            }
+            else if (t!=' ')
+                sogl=sogl+t+" ";
+        }
+        System.out.println(gl);
+        System.out.println(sogl);
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   // ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
+/*Гласные и согласные
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа должна вывести на экран две строки:
+1. первая строка содержит только гласные буквы из введённой строки.
+2. вторая — только согласные буквы и знаки препинания из введённой строки.
+Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.
+Пример ввода:
+Мама мыла раму.
+
+Пример вывода:
+а а ы а а у
+М м м л р м .
+
+
+Требования:
+1. Программа должна считывать данные с клавиатуры.
+2. Программа должна выводить две строки.
+3. Первая строка должна содержать только гласные буквы из введенной строки, разделенные пробелом.
+4. Вторая строка должна содержать только согласные и знаки препинания из введенной строки, разделенные пробелом.
+5. Каждая строка должна заканчиваться пробелом.*/
