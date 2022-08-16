be2f3f56030d1da6f7b6bd9e45dd57aaaba690ba
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:15:41 +0300

    add solution Syntax task 0719

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0;i<10;i++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        for(int i = 9;i>=0;i--){
+           System.out.println(list.get(i));
+        }
+        //напишите тут ваш код
+    }
+}
+/*Вывести числа в обратном порядке
+Ввести с клавиатуры 10 чисел и заполнить ими список.
+Вывести их в обратном порядке.
+Использовать только цикл for.
+
+
+Требования:
+1. Объяви переменную типа список целых чисел и сразу проинициализируй ee.
+2. Считай 10 целых чисел с клавиатуры и добавь их в список.
+3. Выведи числа в обратном порядке.
+4. Используй цикл for.*/
