b663eb7305c0384db160090716ad1499ac12f662
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:11:00 +0300

    add solution Syntax task 0807

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+return new ArrayList<>();
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+        return new LinkedList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+/*LinkedList и ArrayList
+Нужно создать два списка – LinkedList и ArrayList.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Программа должна содержать только три метода.
+4. Метод createArrayList() должен создавать и возвращать список ArrayList.
+5. Метод createLinkedList() должен создавать и возвращать список LinkedList.*/
