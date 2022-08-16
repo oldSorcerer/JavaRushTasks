f9fb1032bdae22afcc38852cbd1c558607751a76
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:06:14 +0300

    add solution Syntax task 0915

--- 1.JavaSyntax/src/com/javarush/task/task09/task0915/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0915/Solution.java
@@ -14,22 +14,20 @@ public class Solution {
     public static void main(String[] args) {
         try {
             processExceptions();
-        } catch (FileSystemException q){
+        } catch (FileSystemException q) {
             BEAN.log(q);
         }
     }
 
-    public static void processExceptions() throws FileSystemException{
+    public static void processExceptions() throws FileSystemException {
         try {
             BEAN.methodThrowExceptions();
-        } catch (CharConversionException q){
+        } catch (CharConversionException q) {
             BEAN.log(q);
-        }
-        catch (FileSystemException q){
+        } catch (FileSystemException q) {
             BEAN.log(q);
             throw q;
-        }
-        catch (IOException q){
+        } catch (IOException q) {
             BEAN.log(q);
         }
     }
@@ -50,26 +48,3 @@ public class Solution {
         }
     }
 }
-/*Перехват выборочных исключений
-1. Разберись, какие исключения бросает метод BEAN.methodThrowExceptions.
-2. Метод processExceptions должен вызывать метод BEAN.methodThrowExceptions и обрабатывать исключения:
-2.1. если возникло исключение FileSystemException, то логировать его (вызвать метод BEAN.log) и пробросить дальше
-2.2. если возникло исключение CharConversionException или любое другое IOException, то только логировать его (вызвать метод BEAN.log)
-3. Добавь в объявление метода processExceptions класс исключения, которое ты пробрасываешь в п.2.1.
-4. В методе main обработай оставшееся исключение — логируй его. Используй try..catch
-
-Подсказка:
-Если ты захватил исключение MyException, которое не хотел захватывать, его можно пробросить дальше кодом вида:
-catch (MyException e) {
- throw e;
-}
-
-
-Требования:
-1. Метод processExceptions должен вызывать метод BEAN.methodThrowExceptions.
-2. Метод processExceptions должен логировать исключение FileSystemException (вызвать метод BEAN.log), а затем пробросить его дальше.
-3. Метод processExceptions должен только логировать (вызвать метод BEAN.log) исключение CharConversionException.
-4. Метод processExceptions должен только логировать любое исключение IOException.
-5. Добавь в объявление метода processExceptions класс исключения FileSystemException.
-6. Метод main должен использовать try..catch.
-7. Метод main должен логировать исключения, которые кидает метод processExceptions.*/
