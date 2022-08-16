8e4db2c09d93cffd2a337fda3131d6b801a88dde
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:28:55 +0300

    add solution Syntax task 0602

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Mrakobes.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task06.task0620;
+
+public class Mrakobes {
+    private static boolean isAlive = true;
+
+    public static void setIsAlive(boolean isAli) {
+        isAlive = isAli;
+    }
+
+    public static void printState() {
+        System.out.println("Амиго" + (isAlive ? " НЕ " : " ") + "победил Мракобеса Безумного");
+    }
+}
