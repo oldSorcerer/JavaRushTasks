417ce25aa0412da58c1391679e3105d99f56d152
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:11:29 +0300

    add solution Syntax task 0315

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0315/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task03.task0315;
+
+/* 
+Ворота фабрики
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        boolean isFabricOpen = true;
+        if (isFabricOpen) {
+            System.out.println("Дверь открыта");
+        } else {
+            System.out.println("Дверь закрыта");
+        }
+    }
+}
