3f937cb6f6c5e38974365f6ecbdea7f496de2375
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 12:36:00 +0300

    add solution Syntax task 0430

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0430;
+
+/* 
+От 1 до 10
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
