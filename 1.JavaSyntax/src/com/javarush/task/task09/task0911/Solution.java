edd7f546bb6292b5dbc1e96f739e1df3bab1be56
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 22:56:25 +0300

    add solution Syntax task 0911

--- 1.JavaSyntax/src/com/javarush/task/task09/task0911/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0911/Solution.java
@@ -8,27 +8,12 @@ import java.util.HashMap;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-try {
-    HashMap<String, String> map = new HashMap<String, String>(null);
-    map.put(null, null);
-    map.remove(null);
-}catch(NullPointerException p){
-    System.out.print("NullPointerException");
-}
-        //напишите тут ваш код
+        try {
+            HashMap<String, String> map = new HashMap<>(null);
+            map.put(null, null);
+            map.remove(null);
+        } catch (NullPointerException p) {
+            System.out.print("NullPointerException");
+        }
     }
 }
-/*Исключение при работе с коллекциями Map
-Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
-HashMap<String, String> map = new HashMap<String, String>(null);
-map.put(null, null);
-map.remove(null);
-
-
-Требования:
-1. Программа должна выводить сообщение на экран.
-2. В программе должен быть блок try-catch.
-3. Программа должна отлавливать исключения конкретного типа, а не все возможные (Exception).
-4. Выведенное сообщение должно содержать тип возникшего исключения.
-5. Имеющийся код в методе main не удалять.*/
