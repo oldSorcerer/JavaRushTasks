b36b43970470e2122100230bee2edc255b59e490
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:56:44 +0300

    add solution Syntax task 0830

--- 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -17,8 +17,8 @@ public class Solution {
 
         sort(array);
 
-        for (String x : array) {
-            System.out.println(x);
+        for (String string : array) {
+            System.out.println(string);
         }
     }
 
@@ -32,33 +32,9 @@ public class Solution {
                 }
             }
         }
-   /*     String g;
-        for(int i=0;i<array.length;i++){
-
-            for(int j=0;j<array.length;j++){
-                 if (isGreaterThan(array[i].toLowerCase(),array[j].toLowerCase())){
-                     g=array[j];
-                     array[j]=array[i];
-                     array[i]=g;
-                }
-            }
-        }*/
-        //напишите тут ваш код
     }
 
-    //Метод для сравнения строк: 'а' больше чем 'b'
     public static boolean isGreaterThan(String a, String b) {
         return a.compareTo(b) > 0;
     }
 }
-/*Задача по алгоритмам
-        Задача: Введи с клавиатуры 20 слов и выведи их в алфавитном порядке. Каждое слово - с новой строки.
-
-
-        Требования:
-        1. Программа должна выводить текст на экран.
-        2. Программа должна считывать значения с клавиатуры.
-        3. Класс Solution должен содержать три метода.
-        4. Метод main() должен вызывать метод sort().
-        5. Метод sort() должен вызывать метод isGreaterThan().
-        6. Выведенные слова должны быть отсортированы в алфавитном порядке.*/
