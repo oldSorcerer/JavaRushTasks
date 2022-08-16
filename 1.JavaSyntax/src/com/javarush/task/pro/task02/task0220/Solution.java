2e03e33ce170247fab9eedeec89d1fbc8073c38a
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:05:29 +0300

    add solution Syntax task 0220

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0220/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0220;
+
+public class Solution {
+    public static void main(String[] args) {
+        int stockQuote = 0;
+        stockQuote = stockQuote - 25 - 25;
+        stockQuote = stockQuote + 20;
+        //stockQuote = stockQuote - 300;
+        stockQuote = stockQuote + 250;
+        stockQuote = stockQuote + 30;
+        System.out.println("Котировки пошли вверх - " + stockQuote);
+    }
+}
