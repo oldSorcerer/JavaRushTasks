c50666de705cd47253260a2969d3b1ce9bf26bd9
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:30:40 +0300

    add solution Syntax task 0620

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0620;
+
+/* 
+Мракобес Безумный
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Mrakobes.setIsAlive(false);
+        Mrakobes.printState();
+    }
+}
