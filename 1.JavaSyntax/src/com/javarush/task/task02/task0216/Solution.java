0e887edd8e360be17dde356e126a1d661e781ea3
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:27:18 +0300

    add solution Syntax task 0216

--- 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
@@ -5,13 +5,10 @@ package com.javarush.task.task02.task0216;
 */
 public class Solution {
     public static int min(int a, int b, int c) {
-        int m;
-        if (a < b)
-            m = a;
-        else
-            m = b;
-        if (m > c)
+        int m = Math.min(a, b);
+        if (m > c) {
             m = c;
+        }
         return m;
     }
 
