ee7544ef7cc6cc8b1ef53a3a49d6bd0d8e75c2c2
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:32:13 +0300

    add solution Syntax task 0316

--- 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -7,7 +7,7 @@ package com.javarush.task.task03.task0316;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
-        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+        System.out.println("It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
 
     }
 }
