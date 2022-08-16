7d97fc56c42b3c630eecbc58cf416191c138e285
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:57:10 +0300

    add solution Syntax task 0519

--- 1.JavaSyntax/src/com/javarush/task/task05/task0519/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0519/Circle.java
@@ -6,32 +6,30 @@ package com.javarush.task.task05.task0519;
 
 
 public class Circle {
-    //напишите тут ваш код
-int centerX, centerY, radius, width, color;
+    int centerX, centerY, radius, width, color;
 
-public Circle(int centerX, int centerY,int  radius){
-    this.centerX = centerX;
-    this.centerY = centerY;
-    this.radius = radius;
-}
-    public Circle(int centerX,int  centerY, int radius,int width){
+    public Circle(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public Circle(int centerX, int centerY, int radius, int width) {
         this.centerX = centerX;
         this.centerY = centerY;
         this.radius = radius;
         this.width = width;
     }
-    public Circle(int centerX,int  centerY,int  radius, int width, int color){
+
+    public Circle(int centerX, int centerY, int radius, int width, int color) {
         this.centerX = centerX;
         this.centerY = centerY;
         this.radius = radius;
         this.width = width;
         this.color = color;
     }
+
     public static void main(String[] args) {
 
     }
 }
-/*Создать класс (Circle) круг, с тремя конструкторами:
-— centerX, centerY, radius
-— centerX, centerY, radius, width
-— centerX, centerY, radius, width, color*/
