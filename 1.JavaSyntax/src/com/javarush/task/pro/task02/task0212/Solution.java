fe9f97db6c2d764f28c45d5ab73e15dc471e78e7
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 09:59:54 +0300

    add solution Syntax task 0212

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0212/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0212/Solution.java
@@ -10,7 +10,6 @@ public class Solution {
         String caps = "if I type in caps ";
         String usa = "сша";
 
-        //напишите тут ваш код
         System.out.println(usa.toUpperCase());
         System.out.println("Винни Пух".toUpperCase());
         System.out.println((caps + "they know I mean business").toUpperCase());
