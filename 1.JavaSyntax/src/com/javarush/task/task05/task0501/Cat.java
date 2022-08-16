3d92a21dc4ad985addb6ad1fe0ac669f91579977
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:34:29 +0300

    add solution Syntax task 0501

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    int strength;
+    //напишите тут ваш код
+/* Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).*/
+    public static void main(String[] args) {
+
+    }
+}
