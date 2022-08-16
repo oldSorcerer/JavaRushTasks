37706c13d0c264308144a22f7827bf37f1f9aea6
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:37:55 +0300

    add solution Syntax task 0505

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task05.task0505;
+
+/* 
+Кошачья бойня
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat c = new Cat("A",10,1,2);
+        Cat c1 = new Cat("A1",1090,190,28);
+        Cat c2 = new Cat("A2",100,10,29);
+        System.out.println(c.fight(c1));
+        System.out.println(c2.fight(c1));
+        System.out.println(c.fight(c2));
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
