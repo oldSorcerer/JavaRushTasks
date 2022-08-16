0740b68f6c52ceffe713d81fc34b7212fcca83bb
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:16:46 +0300

    add solution Syntax task 0422

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0422/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task04.task0422;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/* 
+Баг в системе!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+        if (inputNumber <= 0) {
+            System.out.println("Программа завершена. Введенное число должно быть больше 0.");
+            return;
+        }
+
+        int count = 1;
+        for (; true; count++) {
+            int randomNumber = new Random().nextInt(inputNumber + 1);
+            if (inputNumber == randomNumber) {
+                System.out.println("Попытка №" + count);
+                break;
+            }
+
+        }
+    }
+}
