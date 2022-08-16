8e956aca0f7e23769aa72478024be14f6bb8cd1f
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:39:13 +0300

    add solution Syntax task 0531

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Совершенствуем функциональность
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b,c,d,e);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+    public static int min(int a, int b,int c, int d,int e )
+    {
+
+
+        return min(min(min(a,b), min(c,d)),e);
+    }
+    public static int min(int a, int b)
+    {
+        return a < b ? a : b;
+    }
+}
+/*Совершенствуем функциональность
+Текущая реализация: Программа вводит два числа с клавиатуры и выводит минимальное из них на экран.
+Новая задача: Программа вводит пять чисел с клавиатуры и выводит минимальное из них на экран.
+
+
+Требования:
+1. Программа должна считывать числа с клавиатуры.
+2. В классе должен быть метод public static void main.
+3. В классе должен быть метод public static min, принимающий 5 параметров типа int.
+4. Метод min должен возвращать минимум из 5 переданных чисел.
+5. Программа должна выводить строку, которая начинается на "Minimum = ".
+6. Программа должна выводить строку, которая заканчивается минимальным из 5 введенных чисел.*/
