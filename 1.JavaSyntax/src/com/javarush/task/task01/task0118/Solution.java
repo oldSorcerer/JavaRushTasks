c426eadb35a79be209578f794f689077f2af5f1e
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:14:31 +0300

    add solution Syntax task 0118

--- 1.JavaSyntax/src/com/javarush/task/task01/task0118/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0118/Solution.java
@@ -6,15 +6,15 @@ package com.javarush.task.task01.task0118;
 
 public class Solution {
     public static void main(String[] args) {
-        /*int a = 3;
-        int a = 10;
+        int a = 3;
+//        int a = 10;
         int b = 6;
-        int b = 12;
-        int sum = 1 + a + b;
-        int sum = 2 + a + b;
+//        int b = 12;
+//        int sum = 1 + a + b;
+//        int sum = 2 + a + b;
         int sum = 3 + a + b;
-        int sum = 4 + a + b;
+//        int sum = 4 + a + b;
 
-        System.out.println("sum = " + sum);*/
+        System.out.println("sum = " + sum);
     }
 }
