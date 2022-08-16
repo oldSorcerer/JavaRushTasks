2c4002ce191874d9bc2046b7bf71d12dfb79716e
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 16:00:42 +0300

    add solution Syntax task 0714

--- 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /* 
 Слова в обратном порядке
@@ -11,27 +12,16 @@ import java.util.Collections;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> k = new ArrayList<>();
-        for(int i = 0;i<5;i++){
-            k.add(r.readLine());
-        }
-        k.remove(2);
+        List<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = 3;i>=0;i--){
-           System.out.println(k.get(i));
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+        list.remove(2);
+
+        for (int i = 3; i >= 0; i--) {
+            System.out.println(list.get(i));
         }
     }
 }
-
-
-/*Введи с клавиатуры 5 слов в список строк. Удали 3 — ий элемент списка, и выведи оставшиеся элементы в обратном порядке.
-
-
-Требования:
-1. Объяви переменную типа список строк и сразу проинициализируй ee.
-2. Считай 5 строк с клавиатуры и добавь их в список.
-3. Удали третий элемент массива.
-4. Выведи элементы на экран.
-5. Порядок вывода должен быть обратный.*/
