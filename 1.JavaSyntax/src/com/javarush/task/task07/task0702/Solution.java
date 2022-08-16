14cce4b292ef7c91aab2cb5ee0535d4efb2b3b53
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 15:58:38 +0300

    add solution Syntax task 0702

--- 1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
@@ -9,27 +9,14 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-        String[] s = new String[10];
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0;i<8;i++){
-            s[i]=r.readLine();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = new String[10];
+        for (int i = 0; i < 8; i++) {
+            strings[i] = reader.readLine();
         }
 
-        for(int i=9;i>=0;i--){
-           System.out.println(s[i]);
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(strings[i]);
         }
     }
 }
-
-/*Массив из строчек в обратном порядке
-1. Создать массив на 10 строк.
-2. Ввести с клавиатуры 8 строк и сохранить их в массив.
-3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент — с новой строки.
-
-
-Требования:
-1. Программа должна создавать массив на 10 строк.
-2. Программа должна считывать строки для массива с клавиатуры.
-3. Программа должна выводить 10 строк, каждую с новой строки.
-4. Все строки массива (10 элементов) должны быть выведены в обратном порядке.*/
