58f0cef3f9f1f10fafbe54f7a493aff4853dd12a
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:07:05 +0300

    add solution Syntax task 0918

--- 1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 public class Solution {
     public static void main(String[] args) {
     }
-    
+
     static class MyException extends NullPointerException {
     }
 
-    static class MyException2 extends ArithmeticException{
+    static class MyException2 extends ArithmeticException {
     }
 
     static class MyException3 extends IOException {
@@ -23,17 +23,3 @@ public class Solution {
     static class MyException4 extends FileNotFoundException {
     }
 }
-
-/*Все свои, даже исключения
-Есть четыре класса MyException, MyException2, MyException3, MyException4.
-Унаследуй классы так, чтобы у тебя появилось любые два checked исключения и любые два unchecked исключения.
-
-Подсказка:
-Изучи внимательно классы Exception1, Exception2, Exception3 из второй задачи этого блока.
-
-
-Требования:
-1. В классе Solution должно быть 4 вложенных класса: MyException, MyException2, MyException3 и MyException4.
-2. Все вложенные классы должны быть унаследованы от классов исключений.
-3. Два класса из четырех должны быть checked исключениями.
-4. Два класса из четырех должны быть unchecked исключениями.*/
