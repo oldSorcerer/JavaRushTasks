367d215d1ac166f5695a7373f54b3d00d1c8ad49
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:31:32 +0300

    add solution Syntax task 0303

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -6,9 +6,8 @@ package com.javarush.task.task03.task0303;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(convertEurToUsd(5,2));
-        System.out.println(convertEurToUsd(50,0.2));
-
+        System.out.println(convertEurToUsd(5, 2));
+        System.out.println(convertEurToUsd(50, 0.2));
     }
 
     public static double convertEurToUsd(int eur, double course) {
