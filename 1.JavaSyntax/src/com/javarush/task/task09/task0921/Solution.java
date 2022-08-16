4f459fb7df02aaa8a15ee14d3815260c81f53016
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:07:38 +0300

    add solution Syntax task 0921

--- 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
@@ -15,32 +15,17 @@ public class Solution {
     }
 
     public static void readData() {
-        //напишите тут ваш код
-        ArrayList<Integer> a = new ArrayList<>();
-        try{
-            BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-while(true) {
-    a.add(Integer.parseInt(r.readLine()));
-}
-        } catch(Exception e){
-            for(Integer s:a){
-                System.out.println(s);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                list.add(Integer.parseInt(reader.readLine()));
+            }
+        } catch (Exception e) {
+            for (Integer integer : list) {
+                System.out.println(integer);
             }
         }
-
     }
 }
-/*Метод в try..catch
-Написать программу, которая будет вводить числа с клавиатуры.
-Код по чтению чисел с клавиатуры должен быть в методе readData.
-Обернуть все тело (код внутри readData) этого метода в try..catch.
-Если пользователь ввёл какой-то текст, вместо ввода числа, то метод должен перехватить исключение и вывести на экран все ранее введенные числа в качестве результата.
-Числа выводить с новой строки сохраняя порядок ввода.
-
-
-Требования:
-1. Программа должна считывать данные с клавиатуры.
-2. Метод main не изменять.
-3. Метод main не должен кидать исключений.
-4. Метод readData должен содержать блок try..catch.
-5. Если пользователь ввел текст, а не число, программа должна вывести все ранее введенные числа.*/
