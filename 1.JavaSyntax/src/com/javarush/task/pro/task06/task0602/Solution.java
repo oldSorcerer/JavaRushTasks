3e29875c0908c4c5ede4d0064192d1856f208a3f
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 10:29:06 +0300

    add solution Syntax task 0603

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0602;
+
+/* 
+Упорядоченная информация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.println("От меня вам всем приветик.");
+    }
+}
