7a9058e9873dac053ff5b26d80290a144001aff3
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 23:07:17 +0300

    add solution Syntax task 0919

--- 1.JavaSyntax/src/com/javarush/task/task09/task0919/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0919/Solution.java
@@ -10,28 +10,12 @@ public class Solution {
 
         try {
             divisionByZero();
-        } catch(Exception e){
-          //  StackTraceElement[] el =Thread.currentThread().getStackTrace();
-           // for(StackTraceElement w:el) {
-               // System.out.println(w.getMethodName());
-           e.printStackTrace();
-          //  }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static void divisionByZero(){
-        System.out.println(9/0);
+    public static void divisionByZero() {
+        System.out.println(9 / 0);
     }
 }
-/*Деление на ноль
-Создай метод public static void divisionByZero, в котором подели любое число на ноль и выведи на экран результат деления.
-Оберни вызов метода divisionByZero в try..catch. Выведи стек-трейс исключения используя метод exception.printStackTrace()
-
-
-Требования:
-1. В классе должен быть метод public static void divisionByZero().
-2. Метод divisionByZero должен содержать операцию деления любого числа на ноль.
-3. Метод divisionByZero должен вызывать System.out.println или System.out.print.
-4. Метод main должен содержать блок try..catch.
-5. Метод main должен отлавливать любые исключения метода divisionByZero.
-6. Программа должна выводить стек-трейс пойманого исключения.*/
