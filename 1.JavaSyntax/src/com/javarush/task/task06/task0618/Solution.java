a1349fcb5f082ad9bb6b573acaabb2be557cf9ba
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:40:32 +0300

    add solution Syntax task 0618

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task06.task0618;
+
+/* 
+KissMyShinyMetalAss
+*/
+
+public class Solution {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код   KissMyShinyMetalAss d = new KissMyShinyMetalAss();
+        System.out.println(new KissMyShinyMetalAss());
+    }
+
+
+}
+/*
+KissMyShinyMetalAss
+Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+
+
+Требования:
+1. Создай класс KissMyShinyMetalAss внутри класса Solution.
+2. Класс KissMyShinyMetalAss должен быть статическим.
+3. Класс KissMyShinyMetalAss должен быть public.
+4. В методе main создай объект класса KissMyShinyMetalAss.
+5. В методе main выведи объект класса KissMyShinyMetalAss на экран.
+ */
