a1c374eb2f3d05c108a43b50aead369ab6ed757c
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 14:00:00 +0300

    add solution Syntax task 0525

--- 1.JavaSyntax/src/com/javarush/task/task05/task0525/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0525/Solution.java
@@ -19,7 +19,6 @@ public class Solution {
         Dog duck22 = new Dog();
         System.out.println(duck12);
         System.out.println(duck22);
-        //напишите тут ваш код
     }
 
     public static class Duck {
@@ -27,31 +26,16 @@ public class Solution {
             return "Duck";
         }
     }
+
     public static class Cat {
         public String toString() {
             return "Cat";
         }
     }
+
     public static class Dog {
         public String toString() {
             return "Dog";
         }
     }
-
-    //напишите тут ваш код
 }
-/*И целой утки мало
-По аналогии с классом Duck (утка) создай классы Cat (кошка) и Dog (собака).
-Подумай, что должен возвращать метод toString в классах Cat и Dog?
-В методе main создай по два объекта каждого класса и выведи их на экран.
-Объекты класса Duck уже созданы и выводятся на экран.
-
-
-Требования:
-1. Создай класс Cat(кошка).
-2. Создай класс Dog(собака).
-3. У класса Cat должен быть верно реализован метод toString.
-4. У класса Dog должен быть верно реализован метод toString.
-5. В методе main создай два объекта типа Cat.
-6. В методе main создай два объекта типа Dog.
-7. Выведи все созданные объекты на экран.*/
