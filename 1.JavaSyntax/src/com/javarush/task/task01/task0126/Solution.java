677d5e00d9ce46a9c82dd92451b203d73352978d
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:14:56 +0300

    add solution Syntax task 0126

--- 1.JavaSyntax/src/com/javarush/task/task01/task0126/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0126/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.task01.task0126;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < 16; i++) {
+            System.out.println("Хочешь - решай задачи, не хочешь - решай нехотя");
+        }
     }
 }
