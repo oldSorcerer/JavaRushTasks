771f915cca2e99d29c79d8ce706c60dc35c13ac9
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 14:23:02 +0300

    add solution Syntax task 0610

--- 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -9,45 +9,22 @@ import java.io.InputStreamReader;
 
 public class ConsoleReader {
     public static String readString() throws Exception {
-        //напишите тут ваш код
-BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-return r.readLine();
+        return new BufferedReader(new InputStreamReader(System.in)).readLine();
     }
 
     public static int readInt() throws Exception {
-        //напишите тут ваш код
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        return Integer.parseInt(r.readLine());
+        return Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
     }
 
     public static double readDouble() throws Exception {
-        //напишите тут ваш код
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        return Double.parseDouble(r.readLine());
+        return Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine());
     }
 
     public static boolean readBoolean() throws Exception {
-        //напишите тут ваш код
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        return Boolean.parseBoolean(r.readLine());
+        return Boolean.parseBoolean(new BufferedReader(new InputStreamReader(System.in)).readLine());
     }
 
     public static void main(String[] args) {
 
     }
 }
-/*Класс ConsoleReader
-Сделать класс ConsoleReader, у которого будут 4 статических метода:
-String readString() – читает с клавиатуры строку
-int readInt() – читает с клавиатуры число
-double readDouble() – читает с клавиатуры дробное число
-boolean readBoolean() – читает с клавиатуры строку «true» или «false» и возвращает соответствующую логическую переменную true или false
-
-Внимание: создавайте переменную для чтения данных с консоли (BufferedReader или Scanner) внутри каждого метода.
-
-
-Требования:
-1. Метод readString должен считывать и возвращать строку(тип String).
-2. Метод readInt должен считывать и возвращать число(тип int).
-3. Метод readDouble должен считывать и возвращать дробное число(тип double).
-4. Метод readBoolean должен считывать и возвращать логическую переменную(тип boolean).*/
