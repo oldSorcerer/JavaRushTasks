79c239942eddfdc448d68e73096edeb670faf39f
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:33:05 +0300

    add solution Syntax task 0324

--- 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -6,8 +6,7 @@ package com.javarush.task.task03.task0324;
 
 public class Solution {
     public static void main(String[] args) {
-        for (int i=0; i < 10; i++)
-        System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
-
+        for (int i = 0; i < 10; i++)
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
     }
 }
