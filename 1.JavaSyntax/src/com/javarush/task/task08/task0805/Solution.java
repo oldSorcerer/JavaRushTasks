0468b4118c364057373212c8063a177161f10a8e
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 16:58:24 +0300

    add solution Syntax task 0805

--- 1.JavaSyntax/src/com/javarush/task/task08/task0805/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0805/Solution.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> map = new HashMap<>();
         map.put("Sim", "Sim");
         map.put("Tom", "Tom");
         map.put("Arbus", "Arbus");
@@ -25,19 +25,8 @@ public class Solution {
     }
 
     public static void printValues(Map<String, String> map) {
-        //напишите тут ваш код
-        for(Map.Entry<String,String> o:map.entrySet()){
+        for (Map.Entry<String, String> o : map.entrySet()) {
             System.out.println(o.getValue());
         }
     }
 }
-/*На экране — значения!
-Есть коллекция HashMap<String, String>, туда занесли 10 различных строк.
-Вывести на экран список значений, каждый элемент с новой строки.
-
-
-Требования:
-1. Программа должна создавать переменную коллекции HashMap с типом элементов String, String. Переменная должна быть сразу проинициализирована.
-2. Программа не должна считывать значения с клавиатуры.
-3. Программа должна добавлять в коллекцию 10 различных строк, согласно условию.
-4. Метод printValues() должен выводить на экран список значений коллекции, каждый элемент с новой строки.*/
