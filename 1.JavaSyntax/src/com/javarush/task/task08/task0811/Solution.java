2a64fca1f69664fc99c2804e509038761b3783f9
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:01:25 +0300

    add solution Syntax task 0811

--- 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -10,22 +10,18 @@ import java.util.List;
 
 public class Solution {
     public static List getListForGet() {
-        //напишите тут ваш код
-return new ArrayList();
+        return new ArrayList();
     }
 
     public static List getListForSet() {
-        //напишите тут ваш код
-return new ArrayList();
+        return new ArrayList();
     }
 
     public static List getListForAddOrInsert() {
-        //напишите тут ваш код
-return new LinkedList();
+        return new LinkedList();
     }
 
     public static List getListForRemove() {
-        //напишите тут ваш код
         return new LinkedList();
     }
 
@@ -33,18 +29,3 @@ return new LinkedList();
 
     }
 }
-
-/*
-Реализовать 4 метода.
-Каждый из них должен возвращать список, который лучше всего подходит для выполнения данных операций (быстрее всего справится с большим количеством операций).
-Ничего измерять не нужно.
-
-
-Требования:
-1. Программа не должна выводить текст на экран.
-2. Класс Solution должен содержать только 5 методов.
-3. Метод getListForGet должен возвращать список, который лучше всего подходит для операции взятия элемента.
-4. Метод getListForSet должен возвращать список, который лучше всего подходит для установки значения элемента.
-5. Метод getListForAddOrInsert должен возвращать список, который лучше всего подходит для операций добавления или вставки элемента.
-6. Метод getListForRemove должен возвращать список, который лучше всего подходит для операции удаления элемента.
- */
