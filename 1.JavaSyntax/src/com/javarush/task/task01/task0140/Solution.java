639c9ae6b4f420272709e6c10678551aeb3ab6e5
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:16:13 +0300

    add solution Syntax task 0140

--- 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -8,6 +8,6 @@ public class Solution {
     public static int number = 25;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println((int) Math.pow(number, 2));
     }
 }
