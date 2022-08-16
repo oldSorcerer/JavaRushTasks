403dbbc34893fe4ea198039719b13fe7f25540b6
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:28:03 +0300

    add solution Syntax task 0822

--- 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -18,38 +18,24 @@ public class Solution {
     }
 
     public static int getMinimum(List<Integer> array) {
-        // find minimum here — найти минимум тут
-        int min=array.get(0);
-        for(int q:array){
-            if (min>q){
-                min=q;
+
+        int min = array.get(0);
+        for (int q : array) {
+            if (min > q) {
+                min = q;
             }
         }
         return min;
     }
 
     public static List<Integer> getIntegerList() throws IOException {
-        //create and initialize a list here - создать и заполнить список тут
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(r.readLine());
-        ArrayList<Integer> t = new ArrayList<>();
-        for(int i =0;i<n;i++){
-            t.add(Integer.parseInt(r.readLine()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
         }
-        return t;
+        return list;
     }
 }
-/*Минимальное из N чисел
-1. Ввести с клавиатуры число N.
-2. Считать N целых чисел и заполнить ими список — метод getIntegerList.
-3. Найти минимальное число среди элементов списка — метод getMinimum.
-
-
-Требования:
-1. Программа должна выводить текст на экран.
-2. Программа должна считывать значения с клавиатуры.
-3. Класс Solution должен содержать только три метода.
-4. Метод getIntegerList() должен считать с клавиатуры число N, потом вернуть список размером N элементов, заполненный числами с клавиатуры.
-5. Метод getMinimum() должен вернуть минимальное число среди элементов списка.
-6. Метод main() должен вызывать метод getIntegerList().
-7. Метод main() должен вызывать метод getMinimum().*/
