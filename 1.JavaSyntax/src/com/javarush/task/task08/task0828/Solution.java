a72934c16b4a27dd9ab2c5c2f4caf2dad9f851f3
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:56:21 +0300

    add solution Syntax task 0828

--- 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
@@ -11,47 +11,24 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
-        HashMap<String,Integer> q = new HashMap<>();
 
-   /*     January
-                February
-        March
-                April
-        May
-                June
-        July
-                August
-        September
-                October
-        November
-                December*/
+        HashMap<String, Integer> map = new HashMap<>();
 
-        q.put("January",1);
-        q.put("February",2);
-        q.put("March",3);
-        q.put("April",4);
-        q.put("May",5);
-        q.put("June",6);
-        q.put("July",7);
-        q.put("August",8);
-        q.put("September",9);
-        q.put("October",10);
-        q.put("November",11);
-        q.put("December",12);
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        String s =  r.readLine();
-     System.out.println(s+" is "+q.get(s)+" month");
+        map.put("January", 1);
+        map.put("February", 2);
+        map.put("March", 3);
+        map.put("April", 4);
+        map.put("May", 5);
+        map.put("June", 6);
+        map.put("July", 7);
+        map.put("August", 8);
+        map.put("September", 9);
+        map.put("October", 10);
+        map.put("November", 11);
+        map.put("December", 12);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println(s + " is " + map.get(s) + " month");
     }
 }
-/*Номер месяца
-Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: «May is 5 month».
-Используйте коллекции.
-
-
-Требования:
-1. Программа должна выводить текст на экран.
-2. Программа должна считывать значения с клавиатуры.
-3. Класс Solution должен содержать один метод.
-4. Программа должна использовать коллекции.
-5. Программа должна считывать с клавиатуры имя месяца и выводить его номер на экран по заданному шаблону.*/
