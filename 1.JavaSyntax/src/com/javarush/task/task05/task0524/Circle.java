1edd2ccef721a24e2e2c579b0294b30bb408b5a5
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 13:36:59 +0300

    add solution Syntax task 0524

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0524/Circle.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task05.task0524;
+
+/* 
+Основа колеса
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double r;
+    public Circle(double x,double y,double r){
+      this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    //напишите тут ваш код
+    public static void main(String[] args) {
+
+    }
+}
