0c27786d4b34d00386e992391e3468f19c6995ec
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 17:35:29 +0300

    add solution Syntax task 0823

--- 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -11,33 +11,15 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
-        String newString=s.substring(0,1).toUpperCase();
-        for(int i=1;i<s.length();i++){
-            if (s.charAt(i-1)==' ')
-                newString=newString+s.substring(i,i+1).toUpperCase();
+        String string = reader.readLine();
+        String newString = string.substring(0, 1).toUpperCase();
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i - 1) == ' ')
+                newString = newString + string.substring(i, i + 1).toUpperCase();
             else
-                newString=newString+s.substring(i,i+1) ;
+                newString = newString + string.substring(i, i + 1);
         }
 
-System.out.println(newString);
-        //напишите тут ваш код
+        System.out.println(newString);
     }
 }
-/*Омовение Рамы
-Написать программу, которая вводит с клавиатуры строку текста.
-Программа заменяет в тексте первые буквы всех слов на заглавные.
-Вывести результат на экран.
-
-Пример ввода:
-мама мыла раму.
-
-Пример вывода:
-Мама Мыла Раму.
-
-
-Требования:
-1. Программа должна выводить текст на экран.
-2. Программа должна считывать строку с клавиатуры.
-3. Класс Solution должен содержать один метод.
-4. Программа должна заменять в тексте первые буквы всех слов на заглавные.*/
