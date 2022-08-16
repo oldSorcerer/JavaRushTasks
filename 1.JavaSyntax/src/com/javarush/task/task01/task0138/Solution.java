4c898eb21315aab0ab19e1d61ff9666fac8959b9
oldSorcerer <Echorupta0habibi0viki>
2022-08-16 11:15:56 +0300

    add solution Syntax task 0138

--- 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -6,7 +6,8 @@ package com.javarush.task.task01.task0138;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Мое любимое стихотворение:");
-        //напишите тут ваш код
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Мое любимое стихотворение:");
+        }
     }
 }
