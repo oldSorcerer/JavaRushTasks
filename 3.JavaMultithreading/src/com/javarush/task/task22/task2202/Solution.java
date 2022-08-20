a6324667c3bfc20d80d6df78652d7e3d5898a2b3
oldSorcerer <Echorupta0habibi0viki>
2022-08-20 16:55:34 +0300

    add solution Java Multithreading task 2202

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task22.task2202;
+
+/* 
+Найти подстроку
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+    public static String getPartOfString(String string) {
+        return null;
+    }
+
+    public static class TooShortStringException {
+    }
+}
