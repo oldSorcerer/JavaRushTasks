36ed5e0565b72da28fee7fa231dccd1e9822a87d
oldSorcerer <Echorupta0habibi0viki>
2022-08-21 22:08:41 +0300

    add solution Core task 2019

--- 2.JavaCore/src/com/javarush/task/task20/task2019/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2019/Solution.java
@@ -34,13 +34,13 @@ public class Solution implements Serializable {
 
     private final Map<String, String> m = new HashMap<>();
 
-    public Map<String, String> getMap() {
-        return m;
-    }
-
     public Solution() {
         m.put("Mickey", "Mouse");
         m.put("Mickey", "Mantle");
+    }
+
+    public Map<String, String> getMap() {
+        return m;
     }
 
     public int size() {
