cc496f6d936f1a7e5440729ae65e0ba719a783f4
oldSorcerer <Echorupta0habibi0viki>
2022-08-23 15:04:25 +0300

    add solution University Core task 2007

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task20/task2007/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task20.task2007;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+
+/* 
+Сериализация в JSON
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Cat cat = new Cat();
+        cat.name = "Murka";
+        cat.age = 5;
+        cat.weight = 3;
+
+        System.out.println(convertToJSON(cat));
+    }
+
+    public static String convertToJSON(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(object);
+    }
+
+    public static class Cat {
+        public String name;
+        public int age;
+        public int weight;
+
+        Cat() {
+        }
+    }
+}
