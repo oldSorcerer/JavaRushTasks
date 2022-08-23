564a3707bedb9d0a87391a5154cd202dee1c03dc
oldSorcerer <Echorupta0habibi0viki>
2022-08-23 15:04:52 +0300

    add solution University Core task 2009

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task20/task2009/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.jdk13.task20.task2009;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/* 
+Десериализация котов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Cat cat = getFromXml("<Cat><name>Матроскин</name><cats><cats><name>кот1</name><cats/></cats><cats><name>кот2</name><cats/></cats><cats><name>кот3</name><cats/></cats></cats></Cat>", Cat.class);
+        System.out.println(cat);
+    }
+
+    public static <T> T getFromXml(String xmlString, Class<T> clazz) throws IOException {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static class Cat {
+        public String name;
+        public ArrayList<Cat> cats = new ArrayList<>();
+
+        public Cat() {
+        }
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Cat{" +
+                    "name='" + name + '\'' +
+                    ", cats=" + cats +
+                    '}';
+        }
+    }
+}
+
