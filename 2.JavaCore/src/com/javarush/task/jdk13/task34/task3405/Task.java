049a24a4a7087a55f66aeb48e2bc9a84954c78a4
oldSorcerer <Echorupta0habibi0viki>
2022-08-24 11:48:40 +0300

    add solution University Core task 3405

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Task.java
@@ -0,0 +1,12 @@
+package com.javarush.task.jdk13.task34.task3405;
+
+public class Task {
+    private static final String COMPLEXITY = "Medium";
+    private String number;
+    private boolean isSolved;
+
+    public Task(String number, boolean isSolved) {
+        this.number = number;
+        this.isSolved = isSolved;
+    }
+}
