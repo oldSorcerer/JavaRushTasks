ffc3a6161584cbe0446e37e488e501683d3cc441
oldSorcerer <Echorupta0habibi0viki>
2022-08-26 17:29:49 +0300

    add solution University Core task 3803

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Fox.java
@@ -0,0 +1,5 @@
+package com.javarush.task.jdk13.task38.task3803;
+
+public class Fox {
+    String name = "fox";
+}
---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/SomeTest.java
@@ -0,0 +1,5 @@
+package com.javarush.task.jdk13.task38.task3803;
+
+@PrepareMyTest(value = {Solution.class, SomeTest.class}, fullyQualifiedNames = {"com.javarush.task.jdk13.task38.task3803.Fox"})
+public class SomeTest {
+}
