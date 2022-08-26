616ecf7474e07d3ddb007cba9b0acc9e78d84230
oldSorcerer <Echorupta0habibi0viki>
2022-08-26 16:17:46 +0300

    add solution University Core task 3410

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3410/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.jdk13.task34.task3410;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/* 
+Обнуление объекта
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User(28219, "Paulo", 40, 9999, "paulo@javarush.com");
+        System.out.println(user);
+        reset(user);
+        System.out.println(user);
+    }
+
+    public static void reset(Object object) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
