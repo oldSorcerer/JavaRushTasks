8524108ade5bdf332e81f4659da84ea2961db0bf
oldSorcerer <Echorupta0habibi0viki>
2022-08-27 13:07:37 +0300

    add solution Java Multithreading task 2207

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task22.task2207;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Обращенные слова
+*/
+
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null ? second :
+                            second == null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+
+        }
+    }
+
+}
