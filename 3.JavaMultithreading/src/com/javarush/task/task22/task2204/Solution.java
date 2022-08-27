659d49115ef6529f078d7d9477b0e65ec6986d89
oldSorcerer <Echorupta0habibi0viki>
2022-08-27 13:05:58 +0300

    add solution Java Multithreading task 2204

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2204/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task22.task2204;
+
+/* 
+Форматирование строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(String.format(getFormattedString(), 20.0 / 7.0, 10.0 / 3.0));
+        //должен быть вывод
+        //20 / 7 = 2,86
+        //Exp = 3,33e+00
+    }
+
+    public static String getFormattedString() {
+        return "20 / 7 = % %Exp = %";
+    }
+}
