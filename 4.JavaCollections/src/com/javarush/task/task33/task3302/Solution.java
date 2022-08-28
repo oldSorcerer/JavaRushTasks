7e3587445fbbf9d95780f103fd940578665a0c1c
oldSorcerer <Echorupta0habibi0viki>
2022-08-28 10:22:39 +0300

    add solution Collections task 3302

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3302/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task33.task3302;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+/* 
+Вторая сериализация в JSON
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Cat cat = new Cat();
+        cat.name = "Murka";
+        cat.age = 5;
+        cat.weight = 3;
+
+        StringWriter writer = new StringWriter();
+        convertToJSON(writer, cat);
+        System.out.println(writer.toString());
+    }
+
+    public static void convertToJSON(StringWriter writer, Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(writer, object);
+    }
+
+    @JsonAutoDetect
+    public static class Cat {
+        public String name;
+
+        public int age;
+
+        public int weight;
+
+        Cat() {
+        }
+    }
+}
