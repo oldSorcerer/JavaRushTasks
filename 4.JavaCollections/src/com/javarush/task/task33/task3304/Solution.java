a2b8f459dbaf8126e8695797084e1e3b9ae1b7af
oldSorcerer <Echorupta0habibi0viki>
2022-08-28 12:21:49 +0300

    add solution Collections task 3304

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3304/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task33.task3304;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/* 
+Конвертация из одного класса в другой используя JSON Ӏ 3304
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.disable(MapperFeature.USE_ANNOTATIONS);
+        return mapper.convertValue(one, resultClassObject);
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = First.class, name = "first"))
+    public static class First {
+        public int i;
+        public String name;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = Second.class, name = "second"))
+    public static class Second {
+        public int i;
+        public String name;
+    }
+}
