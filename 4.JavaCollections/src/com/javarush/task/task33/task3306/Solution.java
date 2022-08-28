c4ecbe8828c65426905a5791a761998ff9e9ba55
oldSorcerer <Echorupta0habibi0viki>
2022-08-28 17:14:32 +0300

    add solution Collections task 3306

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3306/Solution.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task33.task3306;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.IOException;
+import java.io.StringWriter;
+
+/* 
+Первая сериализация в XML
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException, JAXBException {
+        Cat cat = new Cat();
+        cat.name = "Murka";
+        cat.age = 5;
+        cat.weight = 3;
+
+        Dog dog = new Dog();
+        dog.name = "Killer";
+        dog.age = 8;
+        dog.owner = "Bill Jeferson";
+
+        StringWriter writer = new StringWriter();
+        convertToXml(writer, cat);
+        convertToXml(writer, dog);
+        System.out.println(writer);
+    }
+
+    public static void convertToXml(StringWriter writer, Object obj) throws IOException, JAXBException {
+        JAXBContext context = JAXBContext.newInstance(obj.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(obj, writer);
+    }
+
+    public static class Pet {
+        public String name;
+    }
+
+    @XmlRootElement
+    @XmlType(name = "cat")
+    public static class Cat extends Pet {
+        public int age;
+        public int weight;
+    }
+
+    @XmlRootElement
+    @XmlType(name = "dog")
+    public static class Dog extends Pet {
+        public int age;
+        public String owner;
+    }
+}
+
+/* expected output
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cat>
+    <name>Murka</name>
+    <age>5</age>
+    <weight>3</weight>
+</cat>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<dog>
+    <name>Killer</name>
+    <age>8</age>
+    <owner>Bill Jeferson</owner>
+</dog>
+        */
