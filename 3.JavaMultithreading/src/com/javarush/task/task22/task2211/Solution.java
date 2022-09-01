9703a3b52514265a6ff947f40d409b8b382a058a
oldSorcerer <Echorupta0habibi0viki>
2022-09-01 11:02:03 +0300

    add solution Multithreading task 2211

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/* 
+Смена кодировки
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    }
+}
