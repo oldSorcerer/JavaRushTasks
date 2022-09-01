fa13c439026ff4f5e52060333ac79269e40a690c
oldSorcerer <Echorupta0habibi0viki>
2022-09-01 11:04:32 +0300

    add solution Multithreading task 2210

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        return null;
+    }
+}
