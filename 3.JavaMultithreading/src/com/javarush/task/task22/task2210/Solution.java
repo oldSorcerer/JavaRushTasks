be9ef3c6f9bf0e26503862c275e2cec9a745eec0
oldSorcerer <Echorupta0habibi0viki>
2022-09-01 12:24:16 +0300

    add solution Multithreading task 2210

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -12,6 +12,13 @@ public class Solution {
     }
 
     public static String[] getTokens(String query, String delimiter) {
-        return null;
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] array = new String[tokenizer.countTokens()];
+        int i = 0;
+        while (tokenizer.hasMoreTokens()) {
+            array[i] = tokenizer.nextToken();
+            i++;
+        }
+        return array;
     }
 }
