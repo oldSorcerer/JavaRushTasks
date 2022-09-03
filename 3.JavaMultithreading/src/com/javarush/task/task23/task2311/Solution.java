1c296eff805a7ec205a6d8a02e156b342eabd9f9
oldSorcerer <Echorupta0habibi0viki>
2022-09-03 21:41:55 +0300

    add solution Multithreading task 2311

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2311/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task23.task2311;
+
+/* 
+Повторяем threads
+*/
+
+public class Solution {
+    public final String name;
+    public final String food;
+    public final String sound;
+
+    public Solution(String name, String food, String sound) {
+        this.name = name;
+        this.food = food;
+        this.sound = sound;
+    }
+
+    public void eat() {
+        System.out.println(name + ": Mmmmm, " + food);
+    }
+
+    public void play() {
+        System.out.println(name + ": " + sound + " " + sound);
+    }
+
+    public void sleep(long milliseconds) {
+        System.out.println(name + ": Zzzzzzz..." + (milliseconds / 1000) + " s");
+    }
+
+    public void live() throws InterruptedException {
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    someActions();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private void someActions() throws InterruptedException {
+                eat();
+                play();
+                sleep(1000);
+            }
+        };
+        thread.start();
+        thread.join();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Solution("Amigo", "beef", "knock").live();
+    }
+}
