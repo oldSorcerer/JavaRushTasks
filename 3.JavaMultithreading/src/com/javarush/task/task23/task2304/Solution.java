7266e0d91639dd5af917aa294c3bdfd9238b6fad
oldSorcerer <Echorupta0habibi0viki>
2022-09-03 21:37:40 +0300

    add solution Multithreading task 2304

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.List;
+import java.util.Map;
+
+/* 
+Inner 3
+*/
+
+public class Solution {
+
+    private List<Task> tasks;
+    private List<String> names;
+
+    private DbDataProvider taskDataProvider = new TaskDataProvider();
+    private DbDataProvider nameDataProvider = new NameDataProvider();
+
+    public void refresh() {
+        Map taskCriteria = MockView.getFakeTaskCriteria();
+        taskDataProvider.refreshAllData(taskCriteria);
+
+        Map nameCriteria = MockView.getFakeNameCriteria();
+        nameDataProvider.refreshAllData(nameCriteria);
+    }
+
+    private interface DbDataProvider<T> {
+        void refreshAllData(Map criteria);
+    }
+
+    class Task {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
