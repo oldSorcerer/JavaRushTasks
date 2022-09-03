858e6f473a81d424588e70368ad892ba7cf3b3a6
oldSorcerer <Echorupta0habibi0viki>
2022-09-03 21:43:17 +0300

    add solution Multithreading task 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/Util.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2401;
+
+import java.lang.reflect.Method;
+
+public class Util {
+    // Пример того, как можно использовать интерфейс-маркер
+    // Этот метод подходит только для классов, реализующих SelfInterfaceMarker
+    public static void testClass(SelfInterfaceMarker interfaceMarker) throws UnsupportedInterfaceMarkerException {
+        for (Method method : interfaceMarker.getClass().getDeclaredMethods()) {
+            System.out.println(method);
+        }
+    }
+}
