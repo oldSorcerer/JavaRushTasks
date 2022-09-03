8f34311487d4967fa7182fe604f3e082f2ec5450
oldSorcerer <Echorupta0habibi0viki>
2022-09-03 21:40:51 +0300

    add solution Multithreading task 2309

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task23.task2309;
+
+import com.javarush.task.task23.task2309.vo.*;
+
+import java.util.List;
+
+/* 
+Анонимность иногда так приятна!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        print(solution.getUsers());
+        print(solution.getLocations());
+    }
+
+    public static void print(List list) {
+        String format = "Id=%d, name='%s', description=%s";
+        for (Object obj : list) {
+            NamedItem item = (NamedItem) obj;
+            System.out.println(String.format(format, item.getId(), item.getName(), item.getDescription()));
+        }
+    }
+}
