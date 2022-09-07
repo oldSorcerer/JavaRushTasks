df61c95005e9f186c07cee75b1174c04fcbca95e
oldSorcerer <Echorupta0habibi0viki>
2022-09-07 13:53:44 +0300

    add solution Syntax task 0921

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0921/NimrodAi.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task09.task0921;
+
+/* 
+Пиратский разворот
+*/
+
+import java.util.Arrays;
+
+public class NimrodAi {
+    
+    public static void main(String[] args) {
+        PastryLoader[] pastryLoaders = new PastryLoader[]{new PastryLoader(), new PastryLoader(), new PastryLoader()};
+
+        scanMemory(pastryLoaders);
+        replaceTarget(pastryLoaders);
+        scanMemory(pastryLoaders);
+    }
+
+    public static void scanMemory(PastryLoader[] pastryLoaders) {
+        System.out.println();
+        for (PastryLoader pastryLoader : pastryLoaders) {
+            System.out.println(Arrays.deepToString(pastryLoader.memory));
+        }
+    }
+
+    public static void replaceTarget(PastryLoader[] pastryLoaders) {
+
+    }
+}
