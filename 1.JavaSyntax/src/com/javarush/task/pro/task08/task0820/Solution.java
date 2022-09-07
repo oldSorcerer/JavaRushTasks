848a2a0c182f8d978dcc50283dd12633dd9250b2
oldSorcerer <Echorupta0habibi0viki>
2022-09-07 13:51:19 +0300

    add solution Syntax task 0820

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task08.task0820;
+
+/* 
+Дубликаты на корабле
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Diego diego = new Diego();
+
+    }
+}
