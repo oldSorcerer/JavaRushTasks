c1dee21674ab3f9b9513d83716c7e903369641da
oldSorcerer <Echorupta0habibi0viki>
2022-09-07 15:16:27 +0300

    add solution Multithreading task 2203

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -5,8 +5,14 @@ package com.javarush.task.task22.task2203;
 */
 
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) throw new TooShortStringException();
+
+        String[] strings = string.split("\t");
+
+        if (strings.length < 3) throw new TooShortStringException();
+
+        return strings[1];
     }
 
     public static class TooShortStringException extends Exception {
