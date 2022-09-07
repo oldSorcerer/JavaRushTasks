335ad57da7386ac1f1ce48d30498ab143adb294e
oldSorcerer <Echorupta0habibi0viki>
2022-09-07 15:28:56 +0300

    add solution Multithreading task 2205

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2205/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2205/Solution.java
@@ -7,11 +7,9 @@ package com.javarush.task.task22.task2205;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(String.format(getFormattedString(), "JavaRush", "курс", "мне", "нравится"));
-        //должен быть вывод
-        //"МНЕ нравится курс JavaRush"
     }
 
     public static String getFormattedString() {
-        return "";
+        return "%3$S %4$s %2$s %1$s";
     }
 }
