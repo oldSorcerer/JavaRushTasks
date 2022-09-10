65849884ee8e72a755e41993deb80a13e2d878ec
oldSorcerer <Echorupta0habibi0viki>
2022-09-10 13:14:36 +0300

    add solution Multithreading task 2212

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -6,6 +6,16 @@ package com.javarush.task.task22.task2212;
 
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
+
+//        if (telNumber.charAt(0))
+
+
+        if (telNumber.contains("+()"))
+
+        if (!Character.isDigit(telNumber.charAt(telNumber.length() - 1))) {
+            return false;
+        }
+
         return false;
     }
 
