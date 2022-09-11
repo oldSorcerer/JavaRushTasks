212dde311a0badf821965e36414bbbf45f5b555f
oldSorcerer <Echorupta0habibi0viki>
2022-09-11 12:09:19 +0300

    add solution Multithreading task 2211

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task22.task2211;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /* 
@@ -9,5 +10,21 @@ import java.nio.charset.StandardCharsets;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(args[0]));
+             BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(args[1]))
+        ) {
+            Charset windows1251 = Charset.forName("Windows-1251");
+
+            while (inputStream.available() > 0) {
+
+                int read = inputStream.read(new byte[inputStream.available()]);
+
+                new String(new byte[inputStream.available()]);
+
+
+//                outputStream.write(reader.readLine().getBytes(StandardCharsets.UTF_8));
+            }
+        }
     }
 }
