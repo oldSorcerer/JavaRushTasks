36bc15ce2ca2e2a569881d2ba022bb366b375e1e
oldSorcerer <Echorupta0habibi0viki>
2022-09-12 14:08:01 +0300

    add solution Multithreading task 2801

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2801/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task28.task2801;
+
+/* 
+Осваиваем switch
+*/
+
+public class Solution {
+    public static enum E1 {A, B, C, Y}
+
+    public static enum E2 {D, E, F}
+
+    public static enum E3 {D, E, F}
+
+    public static void main(String[] args) {
+        Solution.switchTest(E1.C);
+        Solution.switchTest(E3.D);
+        Solution.switchTest(E2.D);
+        /* output
+        it's E1.C
+        undefined
+        it's E2.D
+         */
+    }
+
+    public static void switchTest(Enum obj) {
+        //add your code here
+    }
+}
