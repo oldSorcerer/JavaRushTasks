01d8f1dea533c635bff3ed43c4d7ae234c6b598e
oldSorcerer <Echorupta0habibi0viki>
2022-09-14 15:23:54 +0300

    add solution Multithreading task 2805

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task28.task2805;
+
+/* 
+Приоритеты в Threads
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.print(new MyThread().getPriority() + " ");
+        }
+        //output
+        //1 2 3 4 5 6 7 8 9 10 1 2
+
+        System.out.println();
+        ThreadGroup group = new ThreadGroup("someName");
+        group.setMaxPriority(7);
+        for (int i = 0; i < 12; i++) {
+            System.out.print(new MyThread(group, "name" + i).getPriority() + " ");
+        }
+        //output
+        //3 4 5 6 7 7 7 7 1 2 3 4
+    }
+
+}
