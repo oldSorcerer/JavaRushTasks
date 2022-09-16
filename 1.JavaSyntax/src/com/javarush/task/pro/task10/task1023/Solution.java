ea22ffe34dc6b7691e44ee2db4030c2f92db8927
oldSorcerer <Echorupta0habibi0viki>
2022-09-16 22:56:09 +0300

    add solution Syntax task 1023

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1023/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1023/Solution.java
@@ -26,6 +26,5 @@ public class Solution {
         };
 
         projector = new HologramProjector(spareParts[9], spareParts[11], spareParts[6], spareParts[1], spareParts[3], spareParts[7]);
-
     }
 }
