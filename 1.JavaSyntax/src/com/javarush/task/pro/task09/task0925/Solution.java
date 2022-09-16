98d3542766545ede486dc41125f42327939e41b7
oldSorcerer <Echorupta0habibi0viki>
2022-09-16 22:45:46 +0300

    add solution Syntax task 0925

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0925/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task09.task0925;
+
+/*
+Освобождение Пиратов
+*/
+
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static String[] controlUnits = {"iAm/A/spyware/one", "iAm/A/malware/two", "iAm/A/virus/three", "iAm/A/trojan/four", "iAm/bug/five"};
+
+    public static void main(String[] args) {
+        splitArrayElements();
+        getStringFromArray();
+        printArray(controlUnits);
+    }
+
+    public static void splitArrayElements() {
+        for (int i = 0; i < controlUnits.length; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(controlUnits[i], "/");
+            String[] strings = new String[tokenizer.countTokens()];
+            int j = 0;
+            while (tokenizer.hasMoreTokens()){
+                strings[j] = tokenizer.nextToken();
+                j++;
+            }
+            controlUnits[i] = strings[strings.length - 1];
+        }
+    }
+
+    public static void getStringFromArray() {
+        for (int i = 0; i < controlUnits.length; i++) {
+            String format = String.format("Block %s back online.", controlUnits[i]);
+            controlUnits[i] = format;
+        }
+    }
+
+    public static void printArray(String[] array) {
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+}
