f3a7872eb76301ee769841ace4ffbe518e06d4c0
oldSorcerer <Echorupta0habibi0viki>
2022-09-16 22:51:44 +0300

    add solution Syntax task 1023

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1023/HologramProjector.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1023;
+
+public class HologramProjector {
+    private String lens;
+    private String matrix;
+    private String condenser;
+    private String lamp;
+    private String prism;
+    private String mirror;
+
+    public HologramProjector(String lens, String matrix, String condenser, String lamp, String prism, String mirror) {
+        this.lens = lens;
+        this.matrix = matrix;
+        this.condenser = condenser;
+        this.lamp = lamp;
+        this.prism = prism;
+        this.mirror = mirror;
+    }
+}
