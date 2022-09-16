89c0542f53fd08c55e0b4db0b2160ecb7b6ce4b3
oldSorcerer <Echorupta0habibi0viki>
2022-09-16 23:03:34 +0300

    add solution Syntax task 1025

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1025/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1025/Solution.java
@@ -8,11 +8,14 @@ public class Solution {
 
     public static void main(String[] args) {
         Repeater repeater = new Repeater();
-        if (repeater.getFrequency() < 100){
-         repeater.setFrequency(repeater.getFrequency() * 10);
+
+        if (repeater.getFrequency() < 100) {
+            repeater.setFrequency(repeater.getFrequency() * 10);
         }
-        if (!(repeater.getTrajectory().equals("elliptic"))){
+        if (!repeater.getTrajectory().equals("elliptic")) {
             repeater.setTrajectory("elliptic");
         }
+
+        System.out.println(repeater);
     }
 }
