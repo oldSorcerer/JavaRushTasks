d43b23494d6b85c57cddd6097ae6840e92123f6c
oldSorcerer <Echorupta0habibi0viki>
2022-09-18 11:32:16 +0300

    add solution Multithreading task 2308

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -7,48 +7,49 @@ package com.javarush.task.task23.task2308;
 public class Solution {
 
     public final class Constants {
-        final static String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
-
+        public final static String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
+        public final static String USER_IS_NOT_AUTHORIZED = "The user is not authorized.";
+        public final static String USER_IS_BANNED = "The user is banned.";
+        public final static String ACCESS_IS_DENIED = "Access is denied.";
     }
-
 
     public class ServerNotAccessibleException extends Exception {
         public ServerNotAccessibleException() {
-            super("The server is currently not accessible.");
+            super(Constants.SERVER_IS_CURRENTLY_NOT_ACCESSIBLE);
         }
 
         public ServerNotAccessibleException(Throwable cause) {
-            super("The server is currently not accessible.", cause);
+            super(Constants.SERVER_IS_CURRENTLY_NOT_ACCESSIBLE, cause);
         }
     }
 
     public class UnauthorizedUserException extends Exception {
         public UnauthorizedUserException() {
-            super("The user is not authorized.");
+            super(Constants.USER_IS_NOT_AUTHORIZED);
         }
 
         public UnauthorizedUserException(Throwable cause) {
-            super("The user is not authorized.", cause);
+            super(Constants.USER_IS_NOT_AUTHORIZED, cause);
         }
     }
 
     public class BannedUserException extends Exception {
         public BannedUserException() {
-            super("The user is banned.");
+            super(Constants.USER_IS_BANNED);
         }
 
         public BannedUserException(Throwable cause) {
-            super("The user is banned.", cause);
+            super(Constants.USER_IS_BANNED, cause);
         }
     }
 
     public class RestrictionException extends Exception {
         public RestrictionException() {
-            super("Access is denied.");
+            super(Constants.ACCESS_IS_DENIED);
         }
 
         public RestrictionException(Throwable cause) {
-            super("Access is denied.", cause);
+            super(Constants.ACCESS_IS_DENIED, cause);
         }
     }
 
