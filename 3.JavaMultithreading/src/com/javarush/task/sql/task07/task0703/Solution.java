253f71ba53db84c7e8a13278cc3c6eaad20beefb
oldSorcerer <Echorupta0habibi0viki>
2022-09-22 22:01:27 +0300

    add solution SQL and Hibernate task 0703

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.sql.task07.task0703;
+
+import java.sql.*;
+
+/* 
+task0703
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
