00ca84189ee73352c2f30d4826f4365db3d4cb67
oldSorcerer <Echorupta0habibi0viki>
2022-09-23 18:45:26 +0300

    add solution Core task 1413

--- 2.JavaCore/src/com/javarush/task/task14/task1413/Computer.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Computer.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task14.task1413;
 
 public class Computer {
-    private Keyboard keyboard;
-    private Mouse mouse;
-    private Monitor monitor;
+    private final Keyboard keyboard;
+    private final Mouse mouse;
+    private final Monitor monitor;
 
     public Computer (Keyboard keyboard, Mouse mouse, Monitor monitor) {
         this.keyboard = keyboard;
