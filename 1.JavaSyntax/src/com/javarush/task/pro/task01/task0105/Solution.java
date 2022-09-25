2a6e587d931e43310a31bc028db4a763bf02d82f
oldSorcerer <Echorupta0habibi0viki>
2022-09-25 19:43:37 +0300

    add solution Syntax task 0105

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0105/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task01.task0105;
+
+/* 
+Антивирус
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String integrate = "Интегрируем ";
+        String utils = "антивирусные утилиты - ";
+        String soft = "JavaRush Monitor, JavaRush Guard, JavaRush Security ";
+        String system = "в систему, ";
+        String scan = "сканируем канал, ";
+        String remove = "очищаем вирусы и баги.";
+
+        System.out.print(integrate);
+        System.out.print(utils);
+        System.out.print(soft);
+        System.out.print(system);
+        System.out.print(scan);
+        System.out.println(remove);
+    }
+}
