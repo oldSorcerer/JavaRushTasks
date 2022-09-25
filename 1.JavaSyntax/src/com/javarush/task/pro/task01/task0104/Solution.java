a42b554ade89cf71c831316e468f21ca62b2f878
oldSorcerer <Echorupta0habibi0viki>
2022-09-25 18:55:21 +0300

    add solution Syntax task 0104

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0104/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task01.task0104;
+
+/* 
+Починить систему уборки города
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Уборка города началась");
+       // System.out.println("убираем людей");
+       // System.out.println("убираем животных");
+        System.out.println("убираем мусор");
+        System.out.println("убираем листья");
+        //  System.out.println("убираем птиц");
+        System.out.println("уборка города закончилась");
+    }
+}
