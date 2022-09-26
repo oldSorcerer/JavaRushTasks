7dd20f9316938a82a469c9250ccd40c360fbfb8d
oldSorcerer <Echorupta0habibi0viki>
2022-09-26 14:09:45 +0300

    add solution Multithreading task 2601

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task26.task2601;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/* 
+Почитать в инете про медиану выборки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer[] sort(Integer[] array) {
+        //implement logic here
+        return array;
+    }
+}
