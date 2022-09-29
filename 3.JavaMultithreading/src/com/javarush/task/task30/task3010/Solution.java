af51f37cb0d7638eb896c8b49b9133d8613e4931
oldSorcerer <Echorupta0habibi0viki>
2022-09-30 01:02:18 +0300

    add solution Multithreading task 3010

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3010;
+
+import java.util.regex.Pattern;
+
+/* 
+Минимальное допустимое основание системы счисления
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
