1172608616eb868a6ce1005d95bc36f72301b0eb
oldSorcerer <Echorupta0habibi0viki>
2022-09-29 22:56:20 +0300

    add solution Multithreading task 3009

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task30.task3009;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Палиндром?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
+        System.out.println(getRadix("123"));        //expected output: [6]
+        System.out.println(getRadix("5321"));       //expected output: []
+        System.out.println(getRadix("1A"));         //expected output: []
+    }
+}
