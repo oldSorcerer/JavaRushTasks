04578db852cce0b916fa3ee053607a4c8fe81077
oldSorcerer <Echorupta0habibi0viki>
2022-09-30 01:01:57 +0300

    add solution Multithreading task 3001

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3001/NumberSystem.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task30.task3001;
+
+public interface NumberSystem {
+    int getNumberSystemIntValue();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3001/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3001;
+
+import java.math.BigInteger;
+
+/* 
+Конвертер систем счислений
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Number number = new Number(NumberSystemType._10, "6");
+        Number result = convertNumberToOtherNumberSystem(number, NumberSystemType._2);
+        System.out.println(result);    //expected 110
+
+        number = new Number(NumberSystemType._16, "6df");
+        result = convertNumberToOtherNumberSystem(number, NumberSystemType._8);
+        System.out.println(result);    //expected 3337
+
+        number = new Number(NumberSystemType._16, "abcdefabcdef");
+        result = convertNumberToOtherNumberSystem(number, NumberSystemType._16);
+        System.out.println(result);    //expected abcdefabcdef
+    }
+
+    public static Number convertNumberToOtherNumberSystem(Number number, NumberSystem expectedNumberSystem) {
+        //напишите тут ваш код
+        return null;
+    }
+}
