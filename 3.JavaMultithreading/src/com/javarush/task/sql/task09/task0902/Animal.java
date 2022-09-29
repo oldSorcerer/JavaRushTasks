abe92bd982778be8bac5a3d1853ecd5d994650f5
oldSorcerer <Echorupta0habibi0viki>
2022-09-30 01:15:01 +0300

    add solution SQL and Hibernate task 0902

--- 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0902/Animal.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0902/Animal.java
@@ -1,10 +1,10 @@
 package com.javarush.task.sql.task09.task0902;
-
+/*
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+*/
 /* 
 task0902
 */
