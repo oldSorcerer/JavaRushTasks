fa0b01ac6d380c8997336bd2ba7658e6aedb77d3
oldSorcerer <Echorupta0habibi0viki>
2022-10-01 16:27:47 +0300

    add solution Core task 1333

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1333/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.pro.task13.task1333;
+
+/* 
+Спасти рядового Компиляена
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static List<Integer> ratingsList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        setRatings();
+        printList(); // before
+        improveRatings();
+        printList(); // after
+    }
+
+    public static void changeNegativeNumbers() {
+        for (int i = 0; i < ratingsList.size(); i++) {
+            if  (ratingsList.get(i) < 0){
+                ratingsList.set(i, ratingsList.get(i) * (-1));
+            }
+        }
+    }
+
+    public static void changeZerosToMaximumNumber() {
+        for (int i = 0; i < ratingsList.size(); i++) {
+            if (ratingsList.get(i) == 0){
+                ratingsList.set(i, Collections.max(ratingsList));
+            }
+        }
+    }
+
+    public static void reverseList() {
+        Collections.sort(ratingsList);
+        Collections.reverse(ratingsList);
+    }
+
+    public static void improveRatings() {
+        changeNegativeNumbers();
+        changeZerosToMaximumNumber();
+        reverseList();
+    }
+
+    private static void setRatings() {
+        int rating;
+        Random random = new Random();
+        int min = -5;
+        int max = 11;
+
+        for (int i = 0; i < 20; i++) {
+            rating = random.nextInt(max - min) + min;
+            ratingsList.add(rating);
+        }
+    }
+
+    private static void printList() {
+        ratingsList.forEach((value) -> System.out.print(value + " "));
+        System.out.println();
+    }
+}
