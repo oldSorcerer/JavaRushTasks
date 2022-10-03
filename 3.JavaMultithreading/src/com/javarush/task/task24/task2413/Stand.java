d1d39b94f11334fabb26d0d52f280960ee2e0556
oldSorcerer <Echorupta0habibi0viki>
2022-10-04 01:46:11 +0300

    add solution Multithreading task 2413 Arkanoid 16/20

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,14 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
@@ -13,5 +19,13 @@ public class Stand extends BaseObject {
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
