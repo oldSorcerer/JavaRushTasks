d77296ce477684c617f735af1ba86401e025c710
oldSorcerer <Echorupta0habibi0viki>
2022-10-04 01:35:43 +0300

    add solution Multithreading task 2413 Arkanoid 14/20

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -7,21 +7,28 @@ public class Ball extends BaseObject {
     private double dy;
     private boolean isFrozen;
 
-        public Ball(double x, double y, double speed, double direction) {
+    public Ball(double x, double y, double speed, double direction) {
         super(x, y, 1);
         this.speed = speed;
         this.direction = direction;
         isFrozen = true;
     }
 
+    public void start() {
+        isFrozen = false;
+    }
+
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     public void move() {
-
+        if (!isFrozen) {
+            x += dx;
+            y += dy;
+        }
     }
 
     public double getSpeed() {
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -2,9 +2,9 @@ package com.javarush.task.task24.task2413;
 
 public abstract class BaseObject {
 
-    private double x;
-    private double y;
-    private double radius;
+    protected double x;
+    protected double y;
+    protected double radius;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
