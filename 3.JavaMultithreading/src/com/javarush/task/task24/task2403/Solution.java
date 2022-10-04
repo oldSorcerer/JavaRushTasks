5308d4f2c8658a4f03c8fd0c3ba92c51eb9f2e8e
oldSorcerer <Echorupta0habibi0viki>
2022-10-04 20:48:41 +0300

    add solution Multithreading task 2403

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2403/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2403/Solution.java
@@ -11,39 +11,27 @@ import java.util.EventListener;
 
 public class Solution {
     public static class A implements Serializable {
+        protected A clone() throws CloneNotSupportedException {
+            return (A) super.clone();
+        }
     }
 
     public static class B implements Remote {
+        protected final B clone() throws CloneNotSupportedException {
+            return (B) super.clone();
+        }
     }
 
     public static class C extends ArrayDeque {
+        public C clone() {
+            return (C) super.clone();
+        }
     }
 
     public static class D implements EventListener {
-    }
-
-//    protected A clone() throws CloneNotSupportedException {
-//        return (A) super.clone();
-//    }
-
-//    protected Thread clone() throws CloneNotSupportedException {
-//        return (Thread) super.clone();
-//    }
-
-//    protected final B clone() throws CloneNotSupportedException {
-//        return (B) super.clone();
-//    }
-
-//    protected ArrayDeque clone() throws CloneNotSupportedException {
-//        return super.clone();
-//    }
-
-//    public C clone() {
-//        return (C) super.clone();
-//    }
-
-    protected D clone() throws CloneNotSupportedException {
-        return (D) super.clone();
+        protected D clone() throws CloneNotSupportedException {
+            return (D) super.clone();
+        }
     }
 
     public static void main(String[] args) {
