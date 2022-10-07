7df9e2a45a4f2a8a4e22ba0062ef93d799815b2d
oldSorcerer <Echorupta0habibi0viki>
2022-10-07 11:54:20 +0300

    add solution Multithreading task 2503

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Solution.java
@@ -19,15 +19,15 @@ public class Solution {
 
         Column.configureColumns(Column.Amount, Column.AccountNumber, Column.BankName);
 
-        for (Columnable columnable : Column.getVisibleColumns()) {
-            System.out.println(columnable.getColumnName());
-        }
-
-        System.out.println("--------------------");
-        Column.AccountNumber.hide();
-
-        for (Columnable columnable : Column.getVisibleColumns()) {
-            System.out.println(columnable.getColumnName());
-        }
+//        for (Columnable columnable : Column.getVisibleColumns()) {
+//            System.out.println(columnable.getColumnName());
+//        }
+//
+//        System.out.println("--------------------");
+//        Column.AccountNumber.hide();
+//
+//        for (Columnable columnable : Column.getVisibleColumns()) {
+//            System.out.println(columnable.getColumnName());
+//        }
     }
 }
