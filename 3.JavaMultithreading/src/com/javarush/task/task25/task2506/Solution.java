ba05c977ccdd0ff317f98af85fc3926f4cec9df3
oldSorcerer <Echorupta0habibi0viki>
2022-10-08 12:42:00 +0300

    add solution Multithreading task 2506

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2506/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/Solution.java
@@ -7,9 +7,9 @@ package com.javarush.task.task25.task2506;
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
         Thread target = new Thread();
-        LoggingStateThread loggingStateThread = new LoggingStateThread(target);
+//        LoggingStateThread loggingStateThread = new LoggingStateThread(target);
 
-        loggingStateThread.start(); //NEW
+//        loggingStateThread.start(); //NEW
         Thread.sleep(100);
         target.start();  //RUNNABLE
         Thread.sleep(100);
