159be48650199e82f955d155fe488aa97a1bd251
oldSorcerer <Echorupta0habibi0viki>
2022-10-08 12:42:19 +0300

    add solution Multithreading task 2504

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task25.task2504;
+
+/* 
+Switch для нитей
+*/
+
+public class Solution {
+    public static void processThreads(Thread... threads) {
+        //implement this method - реализуйте этот метод
+    }
+
+    public static void main(String[] args) {
+    }
+}
