3d92b64c662d35d507dfa7afc79d9fc98abe6f1e
oldSorcerer <Echorupta0habibi0viki>
2022-10-08 12:45:17 +0300

    add solution Multithreading task 2507

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2507/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task25.task2507;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/* 
+Работать в поте лица!
+*/
+
+public class Solution extends Thread {
+    private static final int BUFFER_SIZE = 2000;    //2000 bytes
+    private final Socket socket;
+    private final InputStream in;
+
+    public Solution(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+    }
+
+    public void interrupt() {
+        //implement logic here
+    }
+
+    public void run() {
+        try {
+            byte[] buf = new byte[BUFFER_SIZE];
+            while (true) {
+                int count = in.read(buf);
+                if (count < 0) {
+                    break;
+                } else {
+                    if (count > 0) {
+                        //process buffer here
+                    }
+                }
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
