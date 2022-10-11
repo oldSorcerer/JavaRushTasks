8afdd1ed33325926cfaee7d2be924de1de1b1f4c
oldSorcerer <Echorupta0habibi0viki>
2022-10-11 12:21:52 +0300

    add solution Multithreading task 3004

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3004/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task30.task3004;
+
+import java.util.concurrent.ForkJoinPool;
+
+/* 
+Fork/Join
+*/
+
+public class Solution {
+    private String binaryRepresentationMethod(int x) {
+        int a = x % 2;
+        int b = x / 2;
+        String result = String.valueOf(a);
+        if (b > 0) {
+            return binaryRepresentationMethod(b) + result;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result1 = solution.binaryRepresentationMethod(6);
+        System.out.println(result1);
+
+        System.out.println();
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+//        String result2 = forkJoinPool.invoke(new BinaryRepresentationTask(6));
+//        System.out.println(result2);
+    }
+
+}
