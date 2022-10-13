b9299c594436eb87e82ff3c2db5e50d6bb9a2ecd
oldSorcerer <Echorupta0habibi0viki>
2022-10-13 10:48:22 +0300

    add solution Collections task 3111

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3111;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchFileVisitor extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        byte[] content = Files.readAllBytes(file); // размер файла: content.length
+
+        return super.visitFile(file, attrs);
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task31.task3111;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/* 
+Продвинутый поиск файлов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        SearchFileVisitor searchFileVisitor = new SearchFileVisitor();
+
+//        searchFileVisitor.setPartOfName("amigo");
+//        searchFileVisitor.setPartOfContent("programmer");
+//        searchFileVisitor.setMinSize(500);
+//        searchFileVisitor.setMaxSize(10000);
+
+        Files.walkFileTree(Paths.get("D:/SecretFolder"), searchFileVisitor);
+
+//        List<Path> foundFiles = searchFileVisitor.getFoundFiles();
+//        for (Path file : foundFiles) {
+//            System.out.println(file);
+//        }
+    }
+
+}
