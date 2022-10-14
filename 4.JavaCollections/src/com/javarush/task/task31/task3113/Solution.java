0f391bb694a8b9aae4a18424c36957d87829aec1
oldSorcerer <Echorupta0habibi0viki>
2022-10-14 11:42:05 +0300

    add solution Collections task 3113

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task31.task3113;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/* 
+Что внутри папки?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
