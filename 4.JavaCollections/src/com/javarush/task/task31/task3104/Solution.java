90bcc0a7504e14ed1e76cb33d4287a105b0e6c1e
oldSorcerer <Echorupta0habibi0viki>
2022-10-14 11:40:47 +0300

    add solution Collections task 3104

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3104/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task31.task3104;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+/* 
+Поиск скрытых файлов
+*/
+
+public class Solution extends SimpleFileVisitor<Path> {
+    public static void main(String[] args) throws IOException {
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        final Solution solution = new Solution();
+        Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
+
+        List<String> result = solution.getArchived();
+        System.out.println("All archived files:");
+        for (String path : result) {
+            System.out.println("\t" + path);
+        }
+
+        List<String> failed = solution.getFailed();
+        System.out.println("All failed files:");
+        for (String path : failed) {
+            System.out.println("\t" + path);
+        }
+    }
+
+    private List<String> archived = new ArrayList<>();
+    private List<String> failed = new ArrayList<>();
+
+    public List<String> getArchived() {
+        return archived;
+    }
+
+    public List<String> getFailed() {
+        return failed;
+    }
+}
