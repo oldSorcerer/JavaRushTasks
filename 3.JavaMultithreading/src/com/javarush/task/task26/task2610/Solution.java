a2d43338d7b95343b45109c5f37a642e476bf631
oldSorcerer <Echorupta0habibi0viki>
2022-10-15 13:35:01 +0300

    add solution Multithreading task 2610

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2610/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task26.task2610;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/* 
+Мир скучен для скучных людей
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        BlockingQueue queue = new ArrayBlockingQueue(32);
+
+        Producer producer = new Producer(queue);
+//        Consumer consumer = new Consumer(queue);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(producer);
+//        executorService.submit(consumer);
+
+        Thread.sleep(2000);
+
+        executorService.shutdownNow();
+
+    }
+}
