03610fe9c974e88b24112b3010638ab85fce055a
oldSorcerer <Echorupta0habibi0viki>
2022-10-17 22:15:57 +0300

    add solution Multithreading task 2509

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2509/CancellableTask.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task25.task2509;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.RunnableFuture;
+
+public interface CancellableTask<T> extends Callable<T> {
+    void cancel();
+
+    RunnableFuture<T> newTask();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2509/SocketTask.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task25.task2509;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
+
+public abstract class SocketTask<T> implements CancellableTask<T> {
+    private Socket socket;
+
+    protected synchronized void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public synchronized void cancel() {
+        //close all resources here
+    }
+
+    public RunnableFuture<T> newTask() {
+        return new FutureTask<T>(this) {
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                //close all resources here by using proper SocketTask method
+                //call super-class method in finally block
+                return false;
+            }
+        };
+    }
+}
