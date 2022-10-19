e9cd37665a1007d7d648e409a37d20dab1a621cd
oldSorcerer <Echorupta0habibi0viki>
2022-10-19 11:08:21 +0300

    add solution Multithreading task 2909 refactoring 11/15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -23,21 +23,29 @@ public class Car {
         this.numberOfPassengers = numberOfPassengers;
     }
 
-    public int fill(double numberOfLiters) {
-        if (numberOfLiters < 0)
-            return -1;
+    public void fill(double numberOfLiters) throws Exception {
+        if (numberOfLiters < 0) {
+            throw new Exception();
+        }
         fuel += numberOfLiters;
-        return 0;
+    }
+    public boolean isSummer(Date date, Date summerStart, Date summerEnd) {
+        return date.after(summerStart) && date.before(summerEnd);
     }
 
-    public double getTripConsumption(Date date, int length, Date SummerStart, Date SummerEnd) {
-        double consumption;
-        if (date.before(SummerStart) || date.after(SummerEnd)) {
-            consumption = length * winterFuelConsumption + winterWarmingUp;
-        } else {
-            consumption = length * summerFuelConsumption;
-        }
-        return consumption;
+    public double getWinterConsumption(int length) {
+        return length * winterFuelConsumption + winterWarmingUp;
+    }
+
+    public double getSummerConsumption(int length) {
+        return length * summerFuelConsumption;
+    }
+
+
+    public double getTripConsumption(Date date, int length, Date summerStart, Date summerEnd) {
+        return isSummer(date, summerStart, summerEnd) ?
+                getSummerConsumption(length) :
+                getWinterConsumption(length);
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
@@ -81,11 +89,15 @@ public class Car {
     }
 
     public static Car create(int type, int numberOfPassengers) {
-        return switch (type) {
-            case TRUCK -> new Truck(numberOfPassengers);
-            case SEDAN -> new Sedan(numberOfPassengers);
-            case CABRIOLET -> new Cabriolet(numberOfPassengers);
-            default -> null;
-        };
+        switch (type) {
+            case TRUCK:
+                return new Truck(numberOfPassengers);
+            case SEDAN:
+                return new Sedan(numberOfPassengers);
+            case CABRIOLET:
+                return new Cabriolet(numberOfPassengers);
+            default:
+                return null;
+        }
     }
 }
