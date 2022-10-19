5c95c7627d93f0b62f1e8875320567890d5f02e5
oldSorcerer <Echorupta0habibi0viki>
2022-10-19 18:35:07 +0300

    add solution Syntax task 1130

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1130/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1130/Solution.java
@@ -9,8 +9,6 @@ public class Solution {
     public static final String[] lyrics = {"Du...", "Du hast...", "Du hast ultra metal"};
 
     public static void main(String[] args) {
-
-
         UltraMetal ultraMetal = new UltraMetal();
         ultraMetal.playUltraMetal(lyrics[0], lyrics[1], lyrics[2]);
     }
