c0ecbf910a883df6cb27254f3d9ca78b7dbe1c3e
oldSorcerer <Echorupta0habibi0viki>
2022-10-20 21:11:42 +0300

    add solution Multithreading task 2607

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2607/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task26.task2607;
+
+/* 
+Вежливость - это искусственно созданное хорошее настроение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
