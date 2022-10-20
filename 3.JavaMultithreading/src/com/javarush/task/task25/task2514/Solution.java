09a42f4412fcce1e382a0f4b652d97238c8d09ed
oldSorcerer <Echorupta0habibi0viki>
2022-10-20 21:02:50 +0300

    add solution Multithreading task 2514

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2514/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task25.task2514;
+
+/* 
+Первый закон Финэйгла: если эксперимент удался, что-то здесь не так...
+*/
+
+public class Solution {
+    public static class YieldRunnable implements Runnable {
+        private int index;
+
+        public YieldRunnable(int index) {
+            this.index = index;
+        }
+
+        public void run() {
+            System.out.println("begin-" + index);
+            System.out.println("end-" + index);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
