7d78b581f820f7a8196ee26134e5abf02b1f2c3b
oldSorcerer <Echorupta0habibi0viki>
2022-10-20 21:06:51 +0300

    add solution Multithreading task 2510

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+public class Solution extends Thread {
+
+    public Solution() {
+    }
+
+    public static void main(String[] args) {
+    }
+}
