a0f25f73fcaeffd232f29263fb5679ecdc6f6706
oldSorcerer <Echorupta0habibi0viki>
2022-10-26 15:44:58 +0300

    add solution Core task 1706

--- 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
@@ -2,7 +2,7 @@ package com.javarush.task.task17.task1706;
 
 public class OurPresident {
 
-    private static OurPresident president;
+    private static final OurPresident president;
 
     static {
         synchronized (OurPresident.class) {
