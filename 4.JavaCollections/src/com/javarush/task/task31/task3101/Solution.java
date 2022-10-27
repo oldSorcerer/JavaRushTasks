f897524a237b5860e901e07de6de401453bc1e08
oldSorcerer <Echorupta0habibi0viki>
2022-10-27 17:13:34 +0300

    add solution Collections task 3101

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3101;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+/* 
+Проход по дереву файлов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
