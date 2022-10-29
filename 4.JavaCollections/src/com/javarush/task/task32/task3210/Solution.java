26894e5ab33f9776b5618d71fccc734638a87e2f
oldSorcerer <Echorupta0habibi0viki>
2022-10-29 18:06:33 +0300

    add solution Collections task 3210

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task32.task3210;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/* 
+Используем RandomAccessFile
+*/
+
+public class Solution {
+    public static void main(String... args) {
+
+    }
+}
