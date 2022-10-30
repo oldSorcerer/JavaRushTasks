61df9a8a57d9bfd9f58089b6d73874c655379aac
oldSorcerer <Echorupta0habibi0viki>
2022-10-30 22:56:24 +0300

    add solution Collections task 3203

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3203/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task32.task3203;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/* 
+Пишем стек-трейс
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = getStackTrace(new IndexOutOfBoundsException("fff"));
+        System.out.println(text);
+    }
+
+    public static String getStackTrace(Throwable throwable) {
+        return null;
+    }
+}
