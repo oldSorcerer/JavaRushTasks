2e0fa22b3b4e19b95907c490abb575c8c27d2319
oldSorcerer <Echorupta0habibi0viki>
2022-11-01 21:17:43 +0300

    add solution Multithreading task 2405

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2405/SecondClass.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/SecondClass.java
@@ -3,7 +3,7 @@ package com.javarush.task.task24.task2405;
 public class SecondClass implements Action {   //second implementation
     public static String SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM = "\nSpecific action for anonymous SecondClass, param = ";
 
-    private static final String SECOND_CLASS_METHOD_STRING = "class SecondClass, method someAction";
+    private static String SECOND_CLASS_METHOD_STRING = "class SecondClass, method someAction";
 
     public SecondClass() {
         Solution.countActionObjects++;
