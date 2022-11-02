7ca9413673d59af56bbb7da15af63d4ab0473fa4
oldSorcerer <Echorupta0habibi0viki>
2022-11-02 12:05:23 +0300

    add solution Collections task 3512

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3512;
+
+public class Generator<T> {
+    T newInstance() {
+        return new T();
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task35.task3512;
+
+/* 
+Генератор объектов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        Generator<Event> eventGenerator = new Generator<>(Event.class);
+
+        System.out.println(eventGenerator.newInstance().getId());
+        System.out.println(eventGenerator.newInstance().getId());
+        System.out.println(eventGenerator.newInstance().getId());
+    }
+
+}
