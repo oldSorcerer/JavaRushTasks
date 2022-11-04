e823e7eb24cfba2816141c4eadd4764291770f22
oldSorcerer <Echorupta0habibi0viki>
2022-11-04 13:04:40 +0300

    add solution Collections task 3605

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task36.task3605;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+
+/* 
+Использование TreeSet
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    }
+}
