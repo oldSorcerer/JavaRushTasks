01511769ef4b02eafb578c1a97db5af5f4a324ff
oldSorcerer <Echorupta0habibi0viki>
2022-11-04 18:56:10 +0300

    add solution Multithreading task 2510

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -7,6 +7,15 @@ package com.javarush.task.task25.task2510;
 public class Solution extends Thread {
 
     public Solution() {
+        this.setUncaughtExceptionHandler((t, e) -> {
+            if (e instanceof Error) {
+                System.out.println("Нельзя дальше работать");
+            } else if (e instanceof Exception) {
+                System.out.println("Надо обработать");
+            } else if (e instanceof Throwable) {
+                System.out.println("Поживем - увидим");
+            }
+        });
     }
 
     public static void main(String[] args) {
