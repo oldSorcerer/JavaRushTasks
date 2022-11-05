9ba8760dacf71322a057a887b4d956fdb92c3692
oldSorcerer <Echorupta0habibi0viki>
2022-11-05 21:49:44 +0300

    add solution Multithreading task 2512

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -1,8 +1,5 @@
 package com.javarush.task.task25.task2512;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /* 
 Живем своим умом
 */
@@ -11,8 +8,14 @@ public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        t.interrupt();
+        if (e.getCause() != null) {
+            uncaughtException(t, e.getCause());
+        }
+        System.out.println(e);
     }
 
     public static void main(String[] args) {
+        new Solution().uncaughtException(new Thread(), new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI"))));
     }
 }
