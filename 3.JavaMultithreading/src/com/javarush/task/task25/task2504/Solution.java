afca177e8726b36d8fce69edab29d8f613647611
oldSorcerer <Echorupta0habibi0viki>
2022-11-06 00:15:23 +0300

    add solution Multithreading task 2504

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -6,7 +6,14 @@ Switch для нитей
 
 public class Solution {
     public static void processThreads(Thread... threads) {
-        //implement this method - реализуйте этот метод
+        for (Thread thread : threads) {
+            switch (thread.getState()) {
+                case NEW -> thread.start();
+                case BLOCKED, WAITING, TIMED_WAITING -> thread.interrupt();
+                case RUNNABLE -> thread.isInterrupted();
+                case TERMINATED -> System.out.println(thread.getPriority());
+            }
+        }
     }
 
     public static void main(String[] args) {
