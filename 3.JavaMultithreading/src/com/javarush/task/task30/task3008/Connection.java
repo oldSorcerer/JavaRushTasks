727c2c4f3f9a57b0a4588016504c3e145b7a197c
oldSorcerer <Echorupta0habibi0viki>
2022-11-12 00:48:50 +0300

    add solution Multithreading task 3008 Chat 5/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,4 +1,43 @@
 package com.javarush.task.task30.task3008;
 
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in) {
+            return (Message) in.readObject();
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+    @Override
+    public void close() throws IOException {
+        socket.close();
+        out.close();
+        in.close();
+    }
 }
