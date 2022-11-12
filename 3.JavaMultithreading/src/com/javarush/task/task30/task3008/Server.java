44da73530d6f4e75634e9202e3571614f15e7aa5
oldSorcerer <Echorupta0habibi0viki>
2022-11-12 14:44:34 +0300

    add solution Multithreading task 3008 Chat 7/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,37 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+            for (Connection connection : connectionMap.values()) {
+                try {
+                    connection.send(message);
+                } catch (IOException e) {
+                    ConsoleHelper.writeMessage("Не смогли отправить сообщение " + connection.getRemoteSocketAddress());
+                }
+            }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        private Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
@@ -20,19 +49,6 @@ public class Server {
             ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
         }
 
-    }
-
-    private static class Handler extends Thread {
-        private Socket socket;
-
-        private Handler(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-
-        }
     }
 
 }
