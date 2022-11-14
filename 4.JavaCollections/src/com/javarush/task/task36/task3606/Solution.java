453be680bbde77fa689f75b497878662f2a8527f
oldSorcerer <Echorupta0habibi0viki>
2022-11-14 12:05:55 +0300

    add solution Collections task 3606

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task36.task3606;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Осваиваем ClassLoader и Reflection
+*/
+
+public class Solution {
+    private List<Class> hiddenClasses = new ArrayList<>();
+    private String packageName;
+
+    public Solution(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Solution solution = new Solution(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "com/javarush/task/task36/task3606/data/second");
+        solution.scanFileSystem();
+        System.out.println(solution.getHiddenClassObjectByKey("secondhiddenclassimpl"));
+        System.out.println(solution.getHiddenClassObjectByKey("firsthiddenclassimpl"));
+        System.out.println(solution.getHiddenClassObjectByKey("packa"));
+    }
+
+    public void scanFileSystem() throws ClassNotFoundException {
+    }
+
+    public HiddenClass getHiddenClassObjectByKey(String key) {
+        return null;
+    }
+}
+
