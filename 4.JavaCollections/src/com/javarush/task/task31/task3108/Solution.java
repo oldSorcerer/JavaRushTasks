5e9dadf14587a222226c35cf8b9a17cd36db6fe9
oldSorcerer <Echorupta0habibi0viki>
2022-11-14 12:03:49 +0300

    add solution Collections task 3108

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3108/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task31.task3108;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/* 
+Исследуем Path
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Path path1 = Paths.get("D:/test/data/firstDir");
+        Path path2 = Paths.get("D:/test/data/secondDir/third");
+        Path resultPath = getDiffBetweenTwoPaths(path1, path2);
+        System.out.println(resultPath);   //expected output '../secondDir/third' or '..\secondDir\third'
+    }
+
+    public static Path getDiffBetweenTwoPaths(Path path1, Path path2) {
+        return null;
+    }
+}
