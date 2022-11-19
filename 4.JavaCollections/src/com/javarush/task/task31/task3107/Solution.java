9658318f4d524669e947022e92b73bb4c1eccd4e
oldSorcerer <Echorupta0habibi0viki>
2022-11-19 14:21:01 +0300

    add solution Collections task 3107

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3107;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/* 
+Null Object Pattern
+*/
+
+public class Solution {
+    private FileData fileData;
+
+    public Solution(String pathToFile) {
+    }
+
+    public FileData getFileData() {
+        return fileData;
+    }
+}
