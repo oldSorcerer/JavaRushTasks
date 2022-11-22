646b894596df691db95310fb6b72a085a50b6351
oldSorcerer <Echorupta0habibi0viki>
2022-11-22 13:30:42 +0300

    add solution Multithreading task 2706

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2706/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task27.task2706;
+
+/* 
+Убираем deadlock
+*/
+
+public class Solution {
+    public void safeMethod(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            longTimeMethod();
+            synchronized (obj2) {
+                unsafeMethod(obj1, obj2);
+            }
+        }
+    }
+
+    public void longTimeMethod() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    protected void unsafeMethod(Object obj1, Object obj2) {
+        System.out.println(obj1 + " " + obj2);
+    }
+
+    public static void main(String[] args) {
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+        final Solution solution = new Solution();
+
+        new Thread() {
+            @Override
+            public void run() {
+                solution.safeMethod(o1, o2);
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                solution.safeMethod(o2, o1);
+            }
+        }.start();
+    }
+}
