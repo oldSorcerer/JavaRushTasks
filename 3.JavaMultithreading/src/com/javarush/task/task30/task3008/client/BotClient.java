d167e6a1018255d643ef47a7b68cf1d861ada886
oldSorcerer <Echorupta0habibi0viki>
2022-11-23 23:15:02 +0300

    add solution Multithreading task 3008 Chat 18/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,4 +1,32 @@
 package com.javarush.task.task30.task3008.client;
 
-public class BotClient {
+import java.util.Random;
+
+public class BotClient extends Client {
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + (int)(Math.random() * 100);
+    }
+
+    public static void main(String[] args) {
+        new BotClient().run();
+    }
+
+    public class BotSocketThread extends SocketThread {
+
+
+    }
+
+
 }
