d11cd2b8923ff6fcdc7193221d629ddd6385fecf
oldSorcerer <Echorupta0habibi0viki>
2022-11-25 19:20:33 +0300

    add solution Syntax task 1223

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1223/TransportShip.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1223/TransportShip.java
@@ -8,13 +8,13 @@ public class TransportShip {
 
     public class OnBoardSystem {
         public Integer calculateJumpTime(Integer distance) {
-            int t = 0;
-            int S = 0;
-            while (S < distance){
-                t++;
-                S = (speed * t) + (acceleration * t * t / 2);
+            int time = 0;
+            int way = 0;
+            while (way < distance){
+                time++;
+                way = (speed * time) + (acceleration * time * time / 2);
             }
-            return t - 1;
+            return time - 1;
         }
     }
 }
