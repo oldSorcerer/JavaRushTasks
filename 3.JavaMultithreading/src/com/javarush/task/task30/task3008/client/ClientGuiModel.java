62f6f3624e298677ee93e0bb018ee58221aba865
oldSorcerer <Echorupta0habibi0viki>
2022-11-27 00:19:56 +0300

    add solution Multithreading task 3008 Chat 20/22

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiModel.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task30.task3008.client;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClientGuiModel {
+    private final Set<String> allUserNames = new HashSet<>();
+    private String newMessage;
+
+    public void addUser(String newUserName) {
+        allUserNames.add(newUserName);
+    }
+
+    public void deleteUser(String userName) {
+        allUserNames.remove(userName);
+    }
+
+
+    public Set<String> getAllUserNames() {
+        return Collections.unmodifiableSet(allUserNames);
+    }
+
+    public String getNewMessage() {
+        return newMessage;
+    }
+
+    public void setNewMessage(String newMessage) {
+        this.newMessage = newMessage;
+    }
+}
