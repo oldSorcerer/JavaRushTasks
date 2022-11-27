d23be9d894506bf44bee79cef1d60dcbee7434ba
oldSorcerer <Echorupta0habibi0viki>
2022-11-27 18:02:42 +0300

    add solution Multithreading task 3008 Chat 21/22

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1227/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1227/Solution.java
@@ -10,27 +10,26 @@ import java.util.List;
 
 public class Solution {
 
-    private static List<Robot> rescuedRobots = new ArrayList<>();
+    private static final List<Robot> rescuedRobots = new ArrayList<>();
 
     public static List<Robot> cableContents = new ArrayList<>(Arrays.asList(new Robot("T1000"), new Robot("Amigo"),
             new Robot("B1"), new Robot("Diego"), new Robot("Zed")));
 
 
     public static void main(String[] args) {
-    rescueRobots();
-    emptyGarbageBin();
-    printList(rescuedRobots);
+        rescueRobots();
+        emptyGarbageBin();
+        printList(rescuedRobots);
     }
 
-    public static void rescueRobots(){
+    public static void rescueRobots() {
         for (int i = 0; i < cableContents.size(); i++) {
             Robot robot = cableContents.get(i);
-            if (robot.getName().equals("Amigo")){
+            if (robot.getName().equals("Amigo")) {
                 rescuedRobots.add(robot);
                 cableContents.remove(robot);
                 i--;
-            }
-            else if (robot.getName().equals("Diego")){
+            } else if (robot.getName().equals("Diego")) {
                 rescuedRobots.add(robot);
                 cableContents.remove(robot);
                 i--;
