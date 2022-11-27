420c587f230bb12dba410a455bb1c679763b1860
oldSorcerer <Echorupta0habibi0viki>
2022-11-27 21:01:29 +0300

    add solution Multithreading task 3008

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiModel.java
@@ -8,15 +8,6 @@ public class ClientGuiModel {
     private final Set<String> allUserNames = new HashSet<>();
     private String newMessage;
 
-    public void addUser(String newUserName) {
-        allUserNames.add(newUserName);
-    }
-
-    public void deleteUser(String userName) {
-        allUserNames.remove(userName);
-    }
-
-
     public Set<String> getAllUserNames() {
         return Collections.unmodifiableSet(allUserNames);
     }
@@ -27,5 +18,13 @@ public class ClientGuiModel {
 
     public void setNewMessage(String newMessage) {
         this.newMessage = newMessage;
+    }
+
+    public void addUser(String newUserName) {
+        allUserNames.add(newUserName);
+    }
+
+    public void deleteUser(String userName) {
+        allUserNames.remove(userName);
     }
 }
