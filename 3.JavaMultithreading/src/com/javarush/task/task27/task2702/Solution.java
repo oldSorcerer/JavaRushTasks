2e727a31e8695e0c7dd98c1a5f4f5813d59255af
oldSorcerer <Echorupta0habibi0viki>
2022-11-30 22:24:13 +0300

    add solution Multithreading task 2612

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
@@ -5,12 +5,13 @@ package com.javarush.task.task27.task2702;
 */
 
 public class Solution {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         label:
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j > i) {
                     System.out.println();
+                    continue label;
                 }
                 System.out.print("S");
             }
