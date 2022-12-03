f7152f0243869a8a3f34dd2ddb40dae05a0759c4
oldSorcerer <Echorupta0habibi0viki>
2022-12-03 14:10:31 +0300

    add solution Multithreading task 3110 Archiver 5/18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task31.task3110.command;
+
+public abstract class ZipCommand implements Command {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task31.task3110.command;
+
+public class ZipRemoveCommand extends ZipCommand{
+
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
