e130c90d2c03fa35ad0bdd542fdbdb2f3507c373
oldSorcerer <Echorupta0habibi0viki>
2022-12-08 00:06:57 +0300

    add solution Multithreading task 3110 Archiver 11/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -1,9 +1,23 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand{
 
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            zipFileManager.createZip(Paths.get(ConsoleHelper.readString()));
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException exception) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
