22fc0447373fbe97a4f3a20dfa80de2e6d37f17b
oldSorcerer <Echorupta0habibi0viki>
2022-12-10 16:29:24 +0300

    add solution Multithreading task 3110 Archiver 16/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -1,9 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
-public class ZipRemoveCommand extends ZipCommand{
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
+public class ZipRemoveCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Удаление файла из архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
+        zipFileManager.removeFile(Paths.get(ConsoleHelper.readString()));
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
