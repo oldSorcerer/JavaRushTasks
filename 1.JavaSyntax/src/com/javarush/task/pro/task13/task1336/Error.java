6a5e95a3aa5180097bf7e4b444ca80dc1492cf5f
oldSorcerer <Echorupta0habibi0viki>
2022-12-10 22:56:05 +0300

    add solution Syntax task 1336

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1336/Error.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task13.task1336;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Error {
+    NETWORK_ERROR(100), SYSTEM_ERROR(200), COMPILATION_ERROR(300);
+
+    private static final Map<Integer, Error> errorMap = new HashMap<>();
+
+    private final int errorCode;
+
+    static {
+        for (Error error : Error.values()) {
+            errorMap.put(error.getErrorCode(), error);
+        }
+    }
+
+    Error(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public static Error getErrorByID(Integer errorNumber) {
+        return errorMap.get(errorNumber);
+    }
+
+    public Map<Integer, Error> getErrorMap() { return errorMap; }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1336/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task13.task1336;
+
+/* 
+Ошибка выхода из анабиоза
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Error error = Error.getErrorByID(200);
+        System.out.println(error);
+    }
+}
