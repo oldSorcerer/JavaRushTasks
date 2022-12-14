d337ce6e218c944ad8e5398ea332bfba89b0a0b1
oldSorcerer <Echorupta0habibi0viki>
2022-12-14 14:02:56 +0300

    add solution Collections task 3803

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task38.task3803;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Runtime исключения (unchecked exception)
+*/
+
+public class VeryComplexClass {
+    public void methodThrowsClassCastException() {
+
+    }
+
+    public void methodThrowsNullPointerException() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
