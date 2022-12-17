2d08f401485379a30644fc61ae13b552faf31d55
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 19:09:34 +0300

    add solution Collections task 2213 Tetris 7/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -23,4 +23,21 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
+
+
+    public void print() {
+
+    }
+
+    public void removeFullLines() {
+
+    }
+
+    public Integer getValue(int x, int y) {
+        return null;
+    }
+
+    public void setValue(int x, int y, int value) {
+
+    }
 }
