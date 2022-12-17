62f3649b43b0840750353a5872bca473e0b1df86
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 23:13:08 +0300

    add solution Collections task 2515 Space 7/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,18 +1,22 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-
-    private double x;
-    private double y;
-    private double radius;
-
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        isAlive = true;
+        this.isAlive = true;
     }
 
     public double getX() {
@@ -39,26 +43,47 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
+    }
+
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
+    }
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
     public boolean isAlive() {
         return isAlive;
     }
 
-    public void draw() {
-
-    }
-
-    public void move() {
-
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
     public void die() {
         isAlive = false;
     }
 
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
     public boolean isIntersect(BaseObject o) {
         double distance = Math.sqrt(Math.pow((o.x - x), 2) + Math.pow((o.y - y), 2));
         return distance < Math.max(this.radius, o.radius);
     }
-
-
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,4 +1,26 @@
 package com.javarush.task.task25.task2515;
 
 public class Canvas {
+
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
