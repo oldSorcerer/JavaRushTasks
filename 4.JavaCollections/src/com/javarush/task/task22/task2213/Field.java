f72ae1c4ad1a17f985f676922674abe19a56bfe5
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 18:52:33 +0300

    add solution Collections task 2213 Tetris 1/17

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task22.task2213;
+
+public class Field {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
