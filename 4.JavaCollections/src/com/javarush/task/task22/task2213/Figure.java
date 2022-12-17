17bbfc00741707053e16d816a3dd6c3e7ea6de36
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 19:11:35 +0300

    add solution Collections task 2213 Tetris 8/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -1,4 +1,26 @@
 package com.javarush.task.task22.task2213;
 
 public class Figure {
+
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
