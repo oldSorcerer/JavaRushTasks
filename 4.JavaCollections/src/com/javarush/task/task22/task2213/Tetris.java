47b30a0b402717d2a7ebc04a2cb931dc1aa95033
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 18:56:49 +0300

    add solution Collections task 2213 Tetris 3/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -4,6 +4,15 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
         
     }
