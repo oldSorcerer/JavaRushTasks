3f24dfef0b81289ec8aa5e4276ea785d6e5b009a
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 23:38:06 +0300

    add solution Collections task 2515 Space 10/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -2,8 +2,15 @@ package com.javarush.task.task25.task2515;
 
 public class Bomb extends BaseObject {
 
-    public Bomb(double x, double y, double radius) {
-        super(x, y, radius);
+    public Bomb(double x, double y) {
+        super(x, y, 1);
     }
 
+    public void move() {
+        y++;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -12,6 +12,8 @@ public class Space {
     private List<Rocket> rockets = new ArrayList<>();
     private List<Bomb> bombs = new ArrayList<>();
 
+    public static Space game;
+
     public Space(int width, int height) {
         this.width = width;
         this.height = height;
