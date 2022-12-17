c5ef64adf58d245bb389c0c9284d27efb2884494
oldSorcerer <Echorupta0habibi0viki>
2022-12-17 19:04:50 +0300

    add solution Collections task 2213 Tetris 6/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,4 +1,26 @@
 package com.javarush.task.task22.task2213;
 
 public class Field {
+
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new int[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
