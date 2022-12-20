1d1417379e9f22ae6b659726ea7bdb3c65dd54de
oldSorcerer <Echorupta0habibi0viki>
2022-12-21 00:49:35 +0300

    add solution Multithreading task 3209 HTML 2/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -13,7 +13,20 @@ public class Controller {
         this.view = view;
     }
 
+    public void init() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
 
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -20,4 +20,13 @@ public class View extends JFrame implements ActionListener {
     public void setController(Controller controller) {
         this.controller = controller;
     }
+
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        controller.exit();
+    }
 }
