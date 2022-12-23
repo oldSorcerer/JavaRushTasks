a78ec57a948c49f88216af47ba80220e336905c3
oldSorcerer <Echorupta0habibi0viki>
2022-12-23 13:45:40 +0300

    add solution Collections task 4012

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task40.task4012;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+/* 
+Полезные методы DateTime API
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLeap(LocalDate date) {
+        return false;
+    }
+
+    public static boolean isBefore(LocalDateTime dateTime) {
+        return false;
+    }
+
+    public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+        return null;
+    }
+
+    public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
+        return null;
+    }
+}
