ca2675683d728c41ab72ee07bb28f1cb4a25955c
oldSorcerer <Echorupta0habibi0viki>
2022-12-24 13:48:32 +0300

    add solution Multithreading task 2805

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -1,13 +1,38 @@
 package com.javarush.task.task28.task2805;
 
-public class MyThread {
+public class MyThread  extends Thread {
     public MyThread() {
     }
 
-    public MyThread(ThreadGroup group, String s) {
+    public MyThread(Runnable target) {
+        super(target);
     }
 
-    public boolean getPriority() {
+    public MyThread(ThreadGroup group, String s) {
+        super(group, s);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize, boolean inheritThreadLocals) {
+        super(group, target, name, stackSize, inheritThreadLocals);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public boolean getPriority(int min, int max) {
         return false;
     }
 }
