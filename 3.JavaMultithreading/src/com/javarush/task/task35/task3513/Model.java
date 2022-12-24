d294ff5b1ecf298b6735db31e4d697850e2e0b34
oldSorcerer <Echorupta0habibi0viki>
2022-12-24 13:42:20 +0300

    add solution Multithreading task 3513 2048 3/17

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.Arrays;
+
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for (Tile[] gameTile : gameTiles) {
+            Arrays.fill(gameTile, new Tile());
+        }
+    }
 }
