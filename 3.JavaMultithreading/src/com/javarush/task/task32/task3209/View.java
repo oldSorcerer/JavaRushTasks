46f492561366c2b9452487c23e8e4edd66278c27
oldSorcerer <Echorupta0habibi0viki>
2022-12-26 01:58:42 +0300

    add solution Multithreading task 3209 HTML 5/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -52,4 +52,8 @@ public class View extends JFrame implements ActionListener {
         initEditor();
         pack();
     }
+
+    public void selectedTabChanged() {
+
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TabbedPaneChangeListener.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import com.javarush.task.task32.task3209.View;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class TabbedPaneChangeListener implements ChangeListener {
+
+    private View view;
+
+    public TabbedPaneChangeListener(View view) {
+        this.view = view;
+    }
+
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        view.selectedTabChanged();
+    }
+
+}
