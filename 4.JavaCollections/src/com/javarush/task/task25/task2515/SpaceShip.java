6d7d3b1f3442e62fc0406bf902d2e013487a45b2
oldSorcerer <Echorupta0habibi0viki>
2022-12-27 02:52:14 +0300

    add solution Collections task 2515 Space 13/18

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task25.task2515;
 
-public class SpaceShip extends BaseObject{
+import java.util.List;
+
+public class SpaceShip extends BaseObject {
 
     private double dx;
 
@@ -14,6 +16,23 @@ public class SpaceShip extends BaseObject{
 
     public void moveRight() {
         dx = 1;
+    }
+
+    public void move() {
+        x = x + dx;
+        checkBorders(0, Space.game.getWidth(), 0, Space.game.getHeight());
+    }
+
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+        Rocket rocketOne = new Rocket(x + 2, y);
+        Rocket rocketTwo = new Rocket(x - 2, y);
+        List<Rocket> rockets = Space.game.getRockets();
+        rockets.add(rocketOne);
+        rockets.add(rocketTwo);
     }
 
 }
