01637ecb7b26ccc4cabcfc78f17dabdbd5634e06
oldSorcerer <Echorupta0habibi0viki>
2022-12-27 01:46:50 +0300

    add solution Multithreading task 3209 HTML 17/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -6,6 +6,7 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
 
@@ -41,15 +42,24 @@ public class Controller {
 
     public void setPlainText(String text) {
         resetDocument();
-        StringReader stringReader = new StringReader(text);
-        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
-        try {
+        try (StringReader stringReader = new StringReader(text)) {
+            HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
             htmlEditorKit.read(stringReader, document, 0);
         } catch (IOException | BadLocationException e) {
             ExceptionHandler.log(e);
         }
     }
 
+    public String getPlainText() {
+        StringWriter stringWriter = new StringWriter();
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        try {
+            htmlEditorKit.write(stringWriter, document, 0, document.getLength());
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
+    }
 
 
     public static void main(String[] args) {
