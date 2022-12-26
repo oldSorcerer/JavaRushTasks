2cce3e502e380f93878c475e6512418f4a150e5c
oldSorcerer <Echorupta0habibi0viki>
2022-12-27 01:36:54 +0300

    add solution Multithreading task 3209 HTML 16/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class Controller {
 
@@ -35,6 +38,19 @@ public class Controller {
         document.addUndoableEditListener(view.getUndoListener());
         view.update();
     }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader stringReader = new StringReader(text);
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        try {
+            htmlEditorKit.read(stringReader, document, 0);
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         View view = new View();
