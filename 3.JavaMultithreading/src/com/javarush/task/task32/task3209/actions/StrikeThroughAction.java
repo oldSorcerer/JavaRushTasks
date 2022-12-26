5d7d8859488dc557b31effce3f93f9d09f6246a7
oldSorcerer <Echorupta0habibi0viki>
2022-12-26 23:53:59 +0300

    add solution Multithreading task 3209 HTML 12/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
@@ -1,16 +1,25 @@
 package com.javarush.task.task32.task3209.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
-public class StrikeThroughAction extends StyledEditorKit.StyledTextAction  {
+public class StrikeThroughAction extends StyledEditorKit.StyledTextAction {
 
     public StrikeThroughAction() {
-        super("Зачеркнутый");
+        super(StyleConstants.StrikeThrough.toString());
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        JEditorPane editor = getEditor(actionEvent);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setStrikeThrough(simpleAttributeSet, !StyleConstants.isStrikeThrough(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SuperscriptAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SuperscriptAction.java
@@ -1,16 +1,26 @@
 package com.javarush.task.task32.task3209.actions;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
 public class SuperscriptAction extends StyledEditorKit.StyledTextAction {
 
     public SuperscriptAction() {
-        super("Надстрочный знак");
+        super(StyleConstants.Superscript.toString());
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        JEditorPane editor = getEditor(actionEvent);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setSuperscript(simpleAttributeSet, !StyleConstants.isSuperscript(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
