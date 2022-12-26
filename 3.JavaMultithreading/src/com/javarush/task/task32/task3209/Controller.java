05c5e7b4ce231dac6667813532c69d30867854f0
oldSorcerer <Echorupta0habibi0viki>
2022-12-27 01:28:18 +0300

    add solution Multithreading task 3209 HTML 15/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -23,6 +25,17 @@ public class Controller {
 
     public HTMLDocument getDocument() {
         return document;
+    }
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        document = (HTMLDocument) htmlEditorKit.createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+
     }
 
     public static void main(String[] args) {
