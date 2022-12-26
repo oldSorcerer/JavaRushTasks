15f8b597f83451ba6a632be1bb7c2c45c590c5b1
oldSorcerer <Echorupta0habibi0viki>
2022-12-26 15:20:20 +0300

    add solution Multithreading task 3209 HTML 7/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
@@ -1,4 +1,56 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
 public class MenuHelper {
+
+
+    public static JMenuItem addMenuItem(JMenu parent, String text, ActionListener actionListener) {
+        JMenuItem jMenuItem = new JMenuItem(text);
+        jMenuItem.addActionListener(actionListener);
+        parent.add(jMenuItem);
+        return jMenuItem;
+    }
+
+    public static JMenuItem addMenuItem(JMenu parent, Action action) {
+        JMenuItem jMenuItem = new JMenuItem(action);
+        parent.add(jMenuItem);
+        return jMenuItem;
+    }
+
+    public static JMenuItem addMenuItem(JMenu parent, String text, Action action) {
+        JMenuItem jMenuItem = addMenuItem(parent, action);
+        jMenuItem.setText(text);
+        return jMenuItem;
+    }
+
+    public static void initHelpMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initFontMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initColorMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initAlignMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initStyleMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initEditMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initFileMenu(View view, JMenuBar menuBar) {
+
+    }
+
 }
