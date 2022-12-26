cb9c9ba4d956fe3c5291cc2db7c4cc5261b2f514
oldSorcerer <Echorupta0habibi0viki>
2022-12-26 23:16:34 +0300

    add solution Multithreading task 3209 HTML 10/24

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
@@ -8,22 +8,29 @@ import javax.swing.event.MenuListener;
 
 public class UndoMenuListener implements MenuListener {
 
+    private View view;
+    private JMenuItem undoMenuItem = new JMenuItem("Отменить");
+    private JMenuItem redoMenuItem = new JMenuItem("Вернуть");
+
     public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+        this.view = view;
+        this.undoMenuItem = undoMenuItem;
+        this.redoMenuItem = redoMenuItem;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent MenuEvent) {
+        undoMenuItem.setEnabled(view.canUndo());
+        redoMenuItem.setEnabled(view.canRedo());
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent MenuEvent) {
 
     }
 
     @Override
-    public void menuSelected(MenuEvent e) {
-
-    }
-
-    @Override
-    public void menuDeselected(MenuEvent e) {
-
-    }
-
-    @Override
-    public void menuCanceled(MenuEvent e) {
+    public void menuCanceled(MenuEvent MenuEvent) {
 
     }
 }
