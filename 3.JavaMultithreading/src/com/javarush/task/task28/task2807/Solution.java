319b0a2a2f5fae22ec846027095f40cbd7ffecf3
oldSorcerer <Echorupta0habibi0viki>
2022-12-29 20:43:00 +0300

    add solution Multithreading task 2807

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
@@ -10,9 +10,26 @@ import java.util.concurrent.TimeUnit;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        // Add your code here
 
-        /* Example output
+        LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            blockingQueue.add(() -> doExpensiveOperation(localId));
+        }
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 5, 1000, TimeUnit.MILLISECONDS, blockingQueue);
+        executor.prestartAllCoreThreads();
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
+/* Example output
 pool-1-thread-2, localId=2
 pool-1-thread-3, localId=3
 pool-1-thread-1, localId=1
@@ -24,9 +41,3 @@ pool-1-thread-3, localId=9
 pool-1-thread-2, localId=8
 pool-1-thread-1, localId=10
          */
-    }
-
-    private static void doExpensiveOperation(int localId) {
-        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
-    }
-}
