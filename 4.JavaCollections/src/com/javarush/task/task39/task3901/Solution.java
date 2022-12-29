51b4cd7b79d78d2609c92a831cc317a6286ee8e2
oldSorcerer <Echorupta0habibi0viki>
2022-12-29 10:01:46 +0300

    add solution Collections task 3901

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task39.task3901;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Уникальные подстроки
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter your string: ");
+        String s = bufferedReader.readLine();
+
+        System.out.println("The longest unique substring length is: \n" + lengthOfLongestUniqueSubstring(s));
+    }
+
+    public static int lengthOfLongestUniqueSubstring(String s) {
+        return -1;
+    }
+}
