b6341e4228b27b2b5c62cc3fe358cdac508a3de8
oldSorcerer <Echorupta0habibi0viki>
2022-12-30 15:23:36 +0300

    add solution Collections task 3201

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task32.task3201;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/* 
+Запись в существующий файл
+*/
+
+public class Solution {
+    public static void main(String... args) {
+
+    }
+}
