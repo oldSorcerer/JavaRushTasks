1271f2c21e5b99fb7b946b30da7089d6f4abbd60
oldSorcerer <Echorupta0habibi0viki>
2022-12-31 13:01:13 +0300

    add solution Collections task 3811

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3811/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task38.task3811;
+
+/* 
+Тикеты
+*/
+//
+//@Ticket(
+//        priority = Ticket.Priority.HIGH,
+//        createdBy = "Noodles",
+//        tags = {"bug", "fix asap"}
+//)
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
