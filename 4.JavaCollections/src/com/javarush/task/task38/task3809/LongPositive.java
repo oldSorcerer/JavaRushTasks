b3d0346e3c01509714a4b13ed5b46a2be610187e
oldSorcerer <Echorupta0habibi0viki>
2022-12-31 11:13:19 +0300

    add solution Collections task 3809

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3809/LongPositive.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task38.task3809;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+public @interface LongPositive {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3809/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task38.task3809;
+
+/* 
+Annotation + Reflection
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IllegalAccessException {
+        JavaRushBankAccount account = new JavaRushBankAccount("Mr.Smith");
+        System.out.println("Проверка №1:");
+        ReflectionAnnotationUtil.check(account);
+
+        System.out.println("Проверка №2:");
+        account.setAmount(100);
+        ReflectionAnnotationUtil.check(account);
+
+        System.out.println("Проверка №3:");
+        ReflectionAnnotationUtil.check(new IncorrectAccount());
+/* Ожидаемый вывод:
+
+Проверка №1:
+Поле amount в классе JavaRushBankAccount имеет аннотацию LongPositive, но его значение не положительное.
+Проверка №2:
+Проверка №3:
+Поле amountString в классе IncorrectAccount имеет аннотацию LongPositive, но его тип String.
+
+*/
+    }
+}
