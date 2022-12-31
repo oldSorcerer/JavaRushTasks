9fa072dc04b50241b78bf75a6b2e248ab34a2e9b
oldSorcerer <Echorupta0habibi0viki>
2022-12-31 09:38:23 +0300

    add solution Collections task 3812

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3812/PrepareMyTest.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task38.task3812;
+
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+//@PrepareForTest
+public @interface PrepareMyTest {
+    Class<?>[] value() default Solution.class;
+
+    String[] fullyQualifiedNames() default "";
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3812/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task38.task3812;
+
+/* 
+Обработка аннотаций
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printFullyQualifiedNames(Solution.class);
+        printFullyQualifiedNames(SomeTest.class);
+
+        printValues(Solution.class);
+        printValues(SomeTest.class);
+    }
+
+    public static boolean printFullyQualifiedNames(Class c) {
+        return true;
+    }
+
+    public static boolean printValues(Class c) {
+        return true;
+    }
+}
