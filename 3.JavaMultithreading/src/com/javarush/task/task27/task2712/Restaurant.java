8c854ed68df2c3b9750f029d591da38f90e52680
oldSorcerer <Echorupta0habibi0viki>
2023-01-05 22:44:50 +0300

    add solution Multithreading task 2712 Restaurant 4/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
@@ -12,5 +13,7 @@ public class Restaurant {
 
         Cook cook = new Cook("Amigo");
         tablet.addObserver(cook);
+        Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
     }
 }
