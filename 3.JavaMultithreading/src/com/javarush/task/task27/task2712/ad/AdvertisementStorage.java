be9874bad70923f11cca0a8a47f4996fcbce3cd4
oldSorcerer <Echorupta0habibi0viki>
2023-01-06 23:48:08 +0300

    add solution Multithreading task 2712 Restaurant 7/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,13 +1,29 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
 
+    private final List<Advertisement> videos = new ArrayList<>();
     private static final AdvertisementStorage instance = new AdvertisementStorage();
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
     }
 
     public static AdvertisementStorage getInstance() {
         return instance;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
