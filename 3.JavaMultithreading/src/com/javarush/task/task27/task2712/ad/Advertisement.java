7d0515f6087b2a15d88050f666134abb9d036e6f
oldSorcerer <Echorupta0habibi0viki>
2023-01-06 23:27:34 +0300

    add solution Multithreading task 2712 Restaurant 6/23

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class Advertisement {
+
+    private Object content;
+    private String name;
+    private long initialAmount;
+    private int hits;
+    private int duration;
+
+    public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
+        this.content = content;
+        this.name = name;
+        this.initialAmount = initialAmount;
+        this.hits = hits;
+        this.duration = duration;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class AdvertisementManager {
+}
