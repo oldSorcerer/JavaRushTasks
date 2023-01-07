3d239ff73b1c29a2accc6cc7f5db539c3f544fc3
oldSorcerer <Echorupta0habibi0viki>
2023-01-07 03:10:39 +0300

    add solution Multithreading task 2913

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2913;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /* 
 Замена рекурсии
@@ -10,22 +11,27 @@ public class Solution {
     private static int numberA;
     private static int numberB;
 
-    public static String recursion(int a, int b) {
+    public static String getAllNumbersBetween(int a, int b) {
+
+        StringBuilder builder = new StringBuilder();
         if (a > b) {
-            return a + " " + recursion(a - 1, b);
+            for (int i = a; i >= b; i--) {
+                builder.append(i).append(" ");
+            }
         } else {
             if (a == b) {
                 return Integer.toString(a);
             }
-            return a + " " + recursion(a + 1, b);
+            IntStream.rangeClosed(a, b).forEach(i -> builder.append(i).append(" "));
         }
+        return builder.toString().trim();
     }
 
     public static void main(String[] args) {
         Random random = new Random();
         numberA = random.nextInt(1000);
         numberB = random.nextInt(1000);
-//        System.out.println(getAllNumbersBetween(numberA, numberB));
-//        System.out.println(getAllNumbersBetween(numberB, numberA));
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
     }
 }
