ab5cf64838118ad38878d21e3a7c1be2490132cf
oldSorcerer <Echorupta0habibi0viki>
2023-01-10 09:58:11 +0300

    add solution Collections task 3205

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.Proxy;
+
+/* 
+Создание прокси-объекта
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        SomeInterfaceWithMethods obj = getProxy();
+        obj.stringMethodWithoutArgs();
+        obj.voidMethodWithIntArg(1);
+
+        /* expected output
+        stringMethodWithoutArgs in
+        inside stringMethodWithoutArgs
+        stringMethodWithoutArgs out
+        voidMethodWithIntArg in
+        inside voidMethodWithIntArg
+        inside voidMethodWithoutArgs
+        voidMethodWithIntArg out
+        */
+    }
+
+    public static SomeInterfaceWithMethods getProxy() {
+        return null;
+    }
+}
