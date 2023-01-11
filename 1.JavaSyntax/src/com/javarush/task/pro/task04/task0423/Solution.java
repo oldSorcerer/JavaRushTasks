e21bdafe2020a7d1489549e9c8a9bb4aeb879d5a
oldSorcerer <Echorupta0habibi0viki>
2023-01-11 16:54:20 +0300

    add solution Syntax task 0423

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0423/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0423/Solution.java
@@ -10,13 +10,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int number = 0;
         int sum = 0;
         for (int i = 0; i < 5; i++) {
-            number = scanner.nextInt();
-            sum = sum + number;
+            sum += scanner.nextInt();
         }
-        sum = sum/5;
-        System.out.println(sum);
+        System.out.println(sum / 5);
     }
 }
