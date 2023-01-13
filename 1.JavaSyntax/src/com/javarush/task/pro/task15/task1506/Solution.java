52e57e9e956618dc15c31a76154a0a22f281fbbf
oldSorcerer <Echorupta0habibi0viki>
2023-01-13 19:11:52 +0300

    add solution Syntax task 1506

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -3,8 +3,6 @@ package com.javarush.task.pro.task15.task1506;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Scanner;
 
 /* 
@@ -14,13 +12,8 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
 
-        Path path = Paths.get(str);
-
-        List<String> strings = Files.readAllLines(path);
-
-        for (String string : strings) {
+        for (String string : Files.readAllLines(Path.of(scanner.nextLine()))) {
             System.out.println(string.replaceAll("[.,\\s]", ""));
         }
     }
