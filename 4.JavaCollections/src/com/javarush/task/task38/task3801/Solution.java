a532971d366651c58312579258a6a81bb44be7b1
oldSorcerer <Echorupta0habibi0viki>
2023-01-14 21:46:00 +0300

    add solution Collections task 3801

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3801/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task38.task3801;
+
+/* 
+Исправь ошибки в коде
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                System.out.println("Имя содержит " + NameChecker.getNumberOfCharacters(args[0]) + " символов");
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            } catch (NameIsNullException e) {
+                System.out.println("Ошибка: Имя не задано");
+            } catch (NameIsEmptyException e) {
+                System.out.println("Ошибка: Имя пустое");
+            }
+        }
+    }
+}
