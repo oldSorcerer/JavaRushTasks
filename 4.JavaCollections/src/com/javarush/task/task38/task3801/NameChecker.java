5ae54478c2b1761bb8e0429422ba1c2ee43d2932
oldSorcerer <Echorupta0habibi0viki>
2023-01-14 21:47:48 +0300

    add solution Collections task 3801

--- 4.JavaCollections/src/com/javarush/task/task38/task3801/NameChecker.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3801/NameChecker.java
@@ -3,15 +3,15 @@ package com.javarush.task.task38.task3801;
 public class NameChecker {
     private static final int MAX_NUMBER_OF_CHARACTERS = 100500;
 
-    public static int getNumberOfCharacters(String name) throws Exception {
-        if (name == null)
-            throw new NameIsNullException();
-        else if (name.isEmpty())
-            throw new NameIsEmptyException();
+//    public static int getNumberOfCharacters(String name) throws Exception {
+//        if (name == null)
+//            throw new NameIsNullException();
+//        else if (name.isEmpty())
+//            throw new NameIsEmptyException();
 
-        int length = name.length();
-        if (length > MAX_NUMBER_OF_CHARACTERS)
-            throw new Exception("Слишком длинное имя");
-        return length;
-    }
+//        int length = name.length();
+//        if (length > MAX_NUMBER_OF_CHARACTERS)
+//            throw new Exception("Слишком длинное имя");
+//        return length;
+//    }
 }
--- 4.JavaCollections/src/com/javarush/task/task38/task3801/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3801/Solution.java
@@ -7,15 +7,15 @@ package com.javarush.task.task38.task3801;
 public class Solution {
     public static void main(String[] args) {
         if (args.length > 0) {
-            try {
-                System.out.println("Имя содержит " + NameChecker.getNumberOfCharacters(args[0]) + " символов");
-            } catch (Exception e) {
-                System.out.println(e.toString());
-            } catch (NameIsNullException e) {
-                System.out.println("Ошибка: Имя не задано");
-            } catch (NameIsEmptyException e) {
-                System.out.println("Ошибка: Имя пустое");
-            }
+//            try {
+//                System.out.println("Имя содержит " + NameChecker.getNumberOfCharacters(args[0]) + " символов");
+//            } catch (Exception e) {
+//                System.out.println(e.toString());
+//            } catch (NameIsNullException e) {
+//                System.out.println("Ошибка: Имя не задано");
+//            } catch (NameIsEmptyException e) {
+//                System.out.println("Ошибка: Имя пустое");
+//            }
         }
     }
 }
