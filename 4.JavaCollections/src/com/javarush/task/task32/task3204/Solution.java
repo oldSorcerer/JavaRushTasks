1f92a744e277eb5b5d46508a0fd6dc52c1728db5
oldSorcerer <Echorupta0habibi0viki>
2023-01-14 21:44:42 +0300

    add solution Collections task 3204

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task32.task3204;
+
+import java.io.ByteArrayOutputStream;
+
+/* 
+Генератор паролей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ByteArrayOutputStream password = getPassword();
+        System.out.println(password.toString());
+    }
+
+    public static ByteArrayOutputStream getPassword() {
+        return null;
+    }
+}
