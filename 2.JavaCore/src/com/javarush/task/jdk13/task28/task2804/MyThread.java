2ea7c1ed137108ba28af8a0e641a45dba7d5588f
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 00:09:29 +0300

    add solution University Core task 2804

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2804/MyThread.java
@@ -0,0 +1,35 @@
+package com.javarush.task.jdk13.task28.task2804;
+
+public class MyThread  extends Thread{
+
+    public MyThread() {
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+}
