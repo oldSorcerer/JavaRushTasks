e18444567d2b32f33aeb3025b6a9772b1e2a4185
oldSorcerer <Echorupta0habibi0viki>
2023-01-16 23:53:20 +0300

    add solution University Core task 2811

--- 2.JavaCore/src/com/javarush/task/jdk13/task28/task2811/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2811/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.jdk13.task28.task2811;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /* 
 Знакомство с Executors
@@ -10,8 +11,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        //напишите тут ваш код
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
+        IntStream.range(1, 21).mapToObj(i -> (Runnable)() -> doExpensiveOperation(i))
+                .forEach(executorService::submit);
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
     }
 
     private static void doExpensiveOperation(int localID) {
