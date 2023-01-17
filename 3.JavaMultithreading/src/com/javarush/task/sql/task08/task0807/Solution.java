3d26d8b4fa305bac1d7855486011d0bd9682d188
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 20:57:54 +0300

    add solution SQL and Hibernate task 0807

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0807/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0807/Solution.java
@@ -14,17 +14,17 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         List<Employee> employees = new ArrayList<>();
-        try (Connection con = HikariCPDataSource.getConnection();
-             PreparedStatement pst = con.prepareStatement("select * from employee");
-             ResultSet rs = pst.executeQuery()) {
-            while (rs.next()) {
-                employees.add(new Employee(
-                        rs.getInt("id"),
-                        rs.getString("name"),
-                        rs.getInt("age"),
-                        rs.getString("smth")));
-            }
-        }
+//        try (Connection con = HikariCPDataSource.getConnection();
+//             PreparedStatement pst = con.prepareStatement("select * from employee");
+//             ResultSet rs = pst.executeQuery()) {
+//            while (rs.next()) {
+//                employees.add(new Employee(
+//                        rs.getInt("id"),
+//                        rs.getString("name"),
+//                        rs.getInt("age"),
+//                        rs.getString("smth")));
+//            }
+//        }
 
         employees.forEach(System.out::println);
     }
