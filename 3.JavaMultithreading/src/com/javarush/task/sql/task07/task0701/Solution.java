28bddcd9b280e8725666675e38bb8ccf216ec6b3
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 22:36:24 +0300

    add solution SQL and Hibernate task 0701

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0701/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0701/Solution.java
@@ -11,9 +11,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/test",
-                "root", "root");) {
-            Statement statement = connection.createStatement();
+                "root", "root")) {
         }
-
     }
 }
