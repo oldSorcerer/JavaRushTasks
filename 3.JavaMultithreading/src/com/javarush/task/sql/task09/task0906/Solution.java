9709620101f05f492be896cdf2502c56e536a01b
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 13:50:19 +0300

    add solution SQL and Hibernate task 0906

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0906/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.sql.task09.task0906;
+
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+/* 
+task0906
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+//        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+//        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+//        properties.put(Environment.USER, "root");
+//        properties.put(Environment.PASS, "root");
+
+        //напишите тут ваш код
+
+//        SessionFactory sessionFactory = new Configuration()
+//                .setProperties(properties)
+//                .buildSessionFactory();
+    }
+}
