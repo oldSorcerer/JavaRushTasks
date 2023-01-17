1d310a7b48917fc50a032ab409aa2edf039dcea5
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 13:38:20 +0300

    add solution SQL and Hibernate task 0802

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0802/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.sql.task08.task0802;
+
+import java.sql.*;
+
+/* 
+Откат транзакции
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
