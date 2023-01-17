9dda362b8af47c1d9ba6924a2d95a3e9bca8579c
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 13:38:53 +0300

    add solution SQL and Hibernate task 0804

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0804/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.sql.task08.task0804;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/* 
+task0804
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        String sql = "insert into employee (name, age, smth) values (?, ?, ?)";
+        //напишите тут ваш код
+
+    }
+}
