1c6f7d09440b18f145b8f3b6719fd6f9591e129c
oldSorcerer <Echorupta0habibi0viki>
2023-01-17 13:45:41 +0300

    add solution SQL and Hibernate task 0904

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0904/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.sql.task09.task0904;
+
+import java.util.List;
+
+/* 
+Все имена
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        AnimalManager animalManager = new AnimalManager();
+        List<Animal> animals = animalManager.getAllAnimals();
+
+        for (Animal animal : animals) {
+            System.out.println(animal.getName());
+        }
+    }
+}
