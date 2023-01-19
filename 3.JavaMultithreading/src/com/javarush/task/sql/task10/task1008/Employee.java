d008d706b6fb27282486c8431b9af8e328c1f4da
oldSorcerer <Echorupta0habibi0viki>
2023-01-19 14:35:43 +0300

    add solution SQL and Hibernate task 1008

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1008/Employee.java
@@ -0,0 +1,59 @@
+package com.javarush.task.sql.task10.task1008;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "smth")
+    private String smth;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSmth() {
+        return smth;
+    }
+
+    public void setSmth(String smth) {
+        this.smth = smth;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", smth='" + smth + '\'' +
+                '}';
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1008/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.sql.task10.task1008;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.Collections;
+import java.util.List;
+
+/* 
+task1008
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        getSorted(3, 5).forEach(System.out::println);
+    }
+
+    public static List<Employee> getSorted(int offset, int limit) {
+        //напишите тут ваш код
+        return Collections.emptyList();
+    }
+}
