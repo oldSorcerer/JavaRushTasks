c6dbc274b64413bb74a368bfbb8a41f9d171b2a0
oldSorcerer <Echorupta0habibi0viki>
2023-01-19 13:53:01 +0300

    add solution SQL and Hibernate task 0706

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
@@ -12,15 +12,24 @@ public class Solution {
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/test", "root", "root");
              Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM employe limit 1");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee limit 1");
 
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                int age = resultSet.getInt("age");
-                String smth = resultSet.getString("smth");
-                System.out.printf("%d %s %d %s" + System.lineSeparator(), id, name, age, smth);
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                int age = resultSet.getInt(3);
+                String smth = resultSet.getString(4);
+                System.out.printf("id = %d\t\t name = %s\t\t age = %d\t\t smth = %s" + System.lineSeparator(), id, name, age, smth);
             }
         }
     }
 }
+/*
+* CREATE TABLE employee
+(
+    id   INT,
+    name VARCHAR(100) NULL,
+    age INT NULL,
+    smth VARCHAR(100) NULL,
+    PRIMARY KEY (id)
+);*/
