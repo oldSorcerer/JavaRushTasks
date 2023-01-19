94b684c8b1fd472c5bd480737c0502051db42475
oldSorcerer <Echorupta0habibi0viki>
2023-01-19 14:21:51 +0300

    add solution SQL and Hibernate task 1003

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1003/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.sql.task10.task1003;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+/* 
+task1003
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        promoteAll();
+    }
+
+    public static void promoteAll() {
+        //напишите тут ваш код
+
+    }
+}
