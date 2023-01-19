a0bfdc6e02509c0604249598d1f9639ea77097fb
oldSorcerer <Echorupta0habibi0viki>
2023-01-19 14:45:48 +0300

    add solution SQL and Hibernate task 1101

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task11/task1101/MySessionFactory.java
@@ -0,0 +1,29 @@
+package com.javarush.task.sql.task11.task1101;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import java.util.Properties;
+
+public class MySessionFactory {
+    private static MySessionFactory instance;
+    private final SessionFactory sessionFactory;
+    private MySessionFactory() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Animal.class)
+                .buildSessionFactory();
+    }
+    public static SessionFactory getSessionFactory() {
+        if (instance == null) {
+            instance = new MySessionFactory();
+        }
+        return instance.sessionFactory;
+    }
+}
