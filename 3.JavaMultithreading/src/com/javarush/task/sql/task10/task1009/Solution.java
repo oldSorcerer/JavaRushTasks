4b62a372f306b9dba410f61d791c6fe315c5d3b6
oldSorcerer <Echorupta0habibi0viki>
2023-01-19 14:36:14 +0300

    add solution SQL and Hibernate task 1008

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1009/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.sql.task10.task1009;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+/* 
+task1009
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Salary fund: $" + getSalaryFund());
+        System.out.println("Agerage age: " + getAverageAge());
+    }
+
+    public static Long getSalaryFund() {
+        //напишите тут ваш код
+        return 0L;
+    }
+
+    public static Double getAverageAge() {
+        //напишите тут ваш код
+        return 0.0;
+    }
+}
