cf2859547d2e9de52e7464807c71d2a07a35c74e
oldSorcerer <Echorupta0habibi0viki>
2023-01-20 18:30:01 +0300

    add solution SQL and Hibernate task 1202

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task12/task1202/Salaries.java
@@ -0,0 +1,18 @@
+package com.javarush.task.sql.task12.task1202;
+
+/* 
+Entity с вычислением
+*/
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.Type;
+
+//напишите тут ваш код
+public class Salaries {
+    //напишите тут ваш код
+
+}
