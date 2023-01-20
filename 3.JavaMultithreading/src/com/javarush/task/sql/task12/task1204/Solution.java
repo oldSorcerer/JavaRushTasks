8679001225eb9bf0d318d179f03a861ef8b67874
oldSorcerer <Echorupta0habibi0viki>
2023-01-20 18:31:41 +0300

    add solution SQL and Hibernate task 1204

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task12/task1204/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.sql.task12.task1204;
+
+/* 
+Время создания и изменения данных
+*/
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Properties;
+
+//напишите тут ваш код
+public class Solution {
+        //напишите тут ваш код
+
+}
