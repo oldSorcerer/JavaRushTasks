dd02ab805f6fdae66e7da097aa3706e27d2c04d3
oldSorcerer <Echorupta0habibi0viki>
2023-01-22 13:40:56 +0300

    add solution SQL and Hibernate task 1404

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task14/task1404/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.sql.task14.task1404;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+/* 
+task1404
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        List<Task> tasks = getTasks();
+        tasks.forEach(System.out::println);
+    }
+
+    public static List<Task> getTasks() {
+        //напишите тут ваш код
+
+    }
+}
