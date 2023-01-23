6493ac3f193054c8f22b6bab75775b1dd21e8227
oldSorcerer <Echorupta0habibi0viki>
2023-01-23 16:19:44 +0300

    add solution SQL and Hibernate task 1601

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task16/task1601/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.sql.task16.task1601;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Знакомство с Criteria API
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Animal> results = new ArrayList<>();
+        try {
+            SessionFactory sessionFactory = MySessionFactory.getSessionFactory();
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(new Animal("Tom", 5, "Cat"));
+            session.saveOrUpdate(new Animal("Jerry", 3, "Mouse"));
+            session.saveOrUpdate(new Animal("Spike", 7, null));
+            transaction.commit();
+            //напишите тут ваш код
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        results.forEach(System.out::println);
+    }
+}
