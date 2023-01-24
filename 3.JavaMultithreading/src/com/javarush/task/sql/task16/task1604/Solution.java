a36805e0d49b2dbbabc7145568a27b0a4e7ed088
oldSorcerer <Echorupta0habibi0viki>
2023-01-24 15:01:03 +0300

    add solution SQL and Hibernate task 1604

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task16/task1604/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.sql.task16.task1604;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+/* 
+Метод list
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Session session = MySessionFactory.getSessionFactory().openSession()) {
+            //напишите тут ваш код
+
+        }
+    }
+}
