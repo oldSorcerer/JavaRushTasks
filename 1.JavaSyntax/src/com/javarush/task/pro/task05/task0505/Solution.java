803b84733e927fbe81ebd3881eb4ce659770143d
oldSorcerer <Echorupta0habibi0viki>
2023-01-25 16:20:08 +0300

    add solution Syntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -23,11 +23,11 @@ public class Solution {
 
         if (number % 2 != 0) {
             for (int i = 0; i < array.length ; i++) {
-                System.out.println(array[i] + " ");
+                System.out.println(array[i]);
             }
         } else {
             for (int i = array.length - 1; i >= 0 ; i--) {
-                System.out.println(array[i] + " ");
+                System.out.println(array[i]);
             }
         }
     }
