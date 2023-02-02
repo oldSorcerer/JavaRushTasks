e5ee9a30bc24591049489667cfc994a3e0a6aa18
oldSorcerer <Echorupta0habibi0viki>
2023-02-02 22:53:25 +0300

    add solution Collections task 3713

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3713/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task37.task3713;
+
+import com.javarush.task.task37.task3713.space.SpaceShip;
+import com.javarush.task.task37.task3713.space.crew.AbstractCrewMember;
+
+/* 
+Chain of Responsibility
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        SpaceShip spaceShip = new SpaceShip();
+        AbstractCrewMember crewMember = spaceShip.getCrewChain();
+
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.EXPERT, "ATTACK");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.NOVICE, "WASH THE FLOOR");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.INTERMEDIATE, "CHECK ENGINE");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.ADVANCED, "SET ROUTE");
+    }
+}
