289cc409e1bfc5b3a9d24879e65c501ed952708f
oldSorcerer <Echorupta0habibi0viki>
2023-02-07 17:05:52 +0300

    add solution SQL and Hibernate task 0802

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0802/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0802/Solution.java
@@ -9,7 +9,17 @@ import java.sql.*;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test", "root", "root");
+        connection.setAutoCommit(false);
+        try (Statement statement = connection.createStatement();) {
+            statement.executeUpdate("UPDATE employee SET salary = salary + 2000 WHERE name = 'Diego'");
+            statement.executeUpdate("UPDATE employee SET salary = salary + 500 WHERE name = 'Amigo'");
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) connection.rollback();
+        } finally {
+            if (connection != null) connection.close();
+        }
     }
 }
