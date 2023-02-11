82ef0e436e17cb14464451f02459d0247b181114
oldSorcerer <48804793+oldSorcerer@users.noreply.github.com>
2023-02-11 11:46:20 +0300

    add solution SQL and Hibernate task 0807

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0807/HikariCPDataSource.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0807/HikariCPDataSource.java
@@ -1,25 +1,25 @@
 package com.javarush.task.sql.task08.task0807;
 
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class HikariCPDataSource {
     private static HikariCPDataSource instance;
-//    private HikariConfig config;
-//    private HikariDataSource ds;
+    private HikariConfig config;
+    private HikariDataSource ds;
 
     private HikariCPDataSource() {
         //напишите тут ваш код
 
     }
 
-//    public static Connection getConnection() throws SQLException {
-//        if (instance == null) {
-//            instance = new HikariCPDataSource();
-//        }
-//        return instance.ds.getConnection();
-//    }
+    public static Connection getConnection() throws SQLException {
+        if (instance == null) {
+            instance = new HikariCPDataSource();
+        }
+        return instance.ds.getConnection();
+    }
 }
