97b70e1ef83a218423ad3c0c55bdfb54939380f8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-02-15 10:45:59 +0300

    add solution SQL and Hibernate task 0903

--- 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0903/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task09/task0903/Solution.java
@@ -13,6 +13,16 @@ task0903
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+
+        Configuration configuration = new Configuration();
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(Animal.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
     }
 }
