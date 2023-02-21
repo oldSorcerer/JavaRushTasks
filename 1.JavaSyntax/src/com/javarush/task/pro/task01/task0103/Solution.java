8c31fa1c2e9521089d1bbcf91854b4751c23808d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-02-21 13:31:09 +0300

    add solution Syntax task 0103

--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
@@ -6,6 +6,16 @@ package com.javarush.task.pro.task01.task0103;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(3126 - 8);
+        String drop = "Сбрасываем";
+        String diego = "Диего";
+        String secret = "сверхсекретный";
+        String cargo = "груз";
+
+        System.out.print(drop);
+        System.out.print(" Диего");
+        System.out.println();
+        System.out.print(drop);
+        System.out.print(" сверхсекретный ");
+        System.out.print(cargo);
     }
 }
