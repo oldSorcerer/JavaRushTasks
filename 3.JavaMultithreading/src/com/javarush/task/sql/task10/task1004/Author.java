bd6bd4ad5d1af89934dfba5b9ead4d35b0cc47e2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-02-24 14:23:54 +0300

    add solution SQL and Hibernate task 1004

--- 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1004/Author.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1004/Author.java
@@ -1,14 +1,20 @@
 package com.javarush.task.sql.task10.task1004;
 
 import jakarta.persistence.*;
-
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue
     private Integer id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "full_name")
     private String fullName;
 
     public Integer getId() {
