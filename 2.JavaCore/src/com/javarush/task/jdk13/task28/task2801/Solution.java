c7126a0c52ad4fd1241d9e52b03d52969e2c708e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-02 17:15:35 +0300

    add solution University Core task 2801

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2801/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.jdk13.task28.task2801;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+}
