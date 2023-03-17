c5baa18b5a88bc1315e2d60c47c348ca53339bdb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-17 17:47:51 +0300

    add solution SQL and Hibernate task 0216

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0216/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0216/query.sql
@@ -1,1 +1,8 @@
--- Write your code here:
+select
+case
+    when position = 'backend developer' then 'yes'
+    when position = 'frontend developer' then 'yes'
+    else 'no'
+end
+from employee
+where department = 'cool devs'
