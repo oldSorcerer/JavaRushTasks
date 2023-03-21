f79aa496e7ce0de7f36d28ae268b0594ff802114
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-22 01:23:48 +0300

    add solution SQL and Hibernate task 0322

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
@@ -1,1 +1,12 @@
--- Write your code here:
+select
+c.customer_id,
+c.email,
+o.order_id,
+o.order_status
+from customers as c
+join orders as o
+on c.customer_id = o.customer_id
+where
+o.order_status = 'PAID'
+or
+o.order_status = 'PROCESSING'
