079eb13e7e406bb29deed0a5fdc18fe55b72e437
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-22 01:06:52 +0300

    add solution SQL and Hibernate task 0318

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0318/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0318/query.sql
@@ -1,1 +1,8 @@
--- Write your code here:
+select
+top_retailers.ret_name,
+top_retailers.ret_revenue,
+suppliers.sup_name,
+suppliers.sup_revenue
+from top_retailers
+join suppliers
+on top_retailers.ret_revenue = suppliers.sup_revenue
