f5fc7f8713c7a2eb30d55453e49f5ae64f69513d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-23 20:21:51 +0300

    add solution SQL and Hibernate task 0332

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0332/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0332/query.sql
@@ -2,8 +2,8 @@ select
 auth.last_name as author,
 book.genre as book_genre,
 book.date_released
-from authors as аuth
+from authors as auth
 right join books as book
 on auth.author_id = book.author_id
 where book.date_released < 1900
-group by book_genre, book.date_released
+group by book.genre, book.date_released
