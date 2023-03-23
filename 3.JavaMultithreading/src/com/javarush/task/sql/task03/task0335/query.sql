8b0bad60953b9e649006e2ace38e19861247d403
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-23 21:15:51 +0300

    add solution SQL and Hibernate task 0335

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0335/query.sql
@@ -1,1 +1,9 @@
--- Write your code here:
+select
+author.country as author_country,
+count(books.book_id) as book_count
+from authors as author
+join books as book
+on author.id = book.author_id
+where book.genre = 'novel'
+group by author.country
+having count(books.book_id) > 2
