d7cd8f3dc0f659e9ddedaddf4b5b870851140f43
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-23 08:53:04 +0300

    add solution SQL and Hibernate task 0327

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0327/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0327/query.sql
@@ -1,1 +1,7 @@
--- Write your code here:
+select
+customers.city,
+orders.store_id
+from customers
+right join orders
+on customers.customer_id = orders.customer_id
+where orders.order_status = 'SHIPPED' and orders.total_cost > 100
