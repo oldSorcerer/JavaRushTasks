7737eb6062d236223b21a39f0ce61d1b5181f3a6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-23 18:23:38 +0300

    add solution SQL and Hibernate task 0331

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0331/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0331/query.sql
@@ -1,1 +1,8 @@
--- Write your code here:
+select
+a.last_name as author,
+b.title as book_title,
+b.copies_sold_millions as books_sold
+from authors as a
+right join books as b
+on a.author_id = b.author_id
+where b.copies_sold_millions > 50
