ce67cc4694898a73c9846d92d9fdf45b6e67e426
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-23 21:06:45 +0300

    add solution SQL and Hibernate task 0334

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
@@ -1,1 +1,12 @@
--- Write your code here:
+select
+author.last_name as author,
+author.country as author_country,
+book.genre as book_genre
+from authors as author
+left join books as book
+on authors.id = books.id
+where books.genre = 'fantasy'
+group by authors.last_name, authors.country, books.genre
+order by authors.country asc
+limit 5
+
