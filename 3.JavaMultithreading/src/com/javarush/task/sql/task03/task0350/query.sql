f85b4ef4c6ca62aaacf746ea610b630596c5b746
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-03-27 23:39:45 +0300

    add solution SQL and Hibernate task 0350

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0350/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0350/query.sql
@@ -1,2 +1,7 @@
-with tempTable(averageGrossed) as
-select
+with tempTable(averageGrossed) as (select avg(grossed) from films ),
+tempTable2(averageYearReleased) as (select avg(year_released) from films)
+select title, genre, year_released, grossed
+from films, tempTable, tempTable2
+where films.grossed > tempTable.averageGrossed and
+ films.year_released > tempTable2.averageYearReleased
+ group by films.year_released
