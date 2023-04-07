dae2dd6cff2623927c08fcfd55a772a37f2e5c9a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-07 22:35:04 +0300

    add solution SQL and Hibernate task 0503

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0503/query.sql
@@ -0,0 +1,4 @@
+insert into lego_set (number, name, released, inventory) values (42110, 'Land Rover Defender', 2019, 2573);
+insert into lego_set (number, name, released, inventory) values (75192, 'Millennium Falcon', 2017, 7541);
+insert into lego_set (number, name, released, inventory) values (70676, 'Lloyd''s Titan Mech', 2019, 878);
+insert into lego_set (number, name, released, inventory) values (71043, 'Hogwarts Castle', 2018, 6020);
