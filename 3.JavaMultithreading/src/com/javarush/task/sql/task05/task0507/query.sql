10d18417560f4f324e1085a9b66606088dface52
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-08 00:55:25 +0300

    add solution SQL and Hibernate task 0507

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -1,1 +1,2 @@
--- Write your code here:
+update part set name = 'Technic, Steering Wheel Hub 3 Pin Round' where number = '92909';
+update part set name = 'Minifigure, Headgear Helmet SW Darth Vader Type 2 Top' where number = '19916';
