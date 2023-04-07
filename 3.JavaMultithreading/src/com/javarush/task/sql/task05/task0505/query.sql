071caf0ba9b9a22e7c5180e33c181cc27385c51a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-07 23:26:03 +0300

    add solution SQL and Hibernate task 0505

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0505/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0505/query.sql
@@ -1,4 +1,5 @@
-insert into object (name, type, discoverer, discovery_date) values ('2020 XL5', 'ASTEROID', 'Pan-STARRS 1', '2020-12-12' );
-insert into object (name, type, discoverer, discovery_date) values ('Moon', 'SATELLITE', null, null);
-insert into object (name, type, discoverer, discovery_date) values ('Jupiter', 'PLANET', null, null);
-insert into object (name, type, discoverer, discovery_date) values ('C/1910 A1', 'COMET', 'Robert T. A. Innes', '1910-01-17');
+insert into object (name, type, discoverer, discovery_date)
+values ('2020 XL5', 'ASTEROID', 'Pan-STARRS 1', '2020-12-12' ),
+       ('Moon', 'SATELLITE', null, null),
+       ('Jupiter', 'PLANET', null, null),
+       ('C/1910 A1', 'COMET', 'Robert T. A. Innes', '1910-01-17');
