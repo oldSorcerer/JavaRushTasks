e21e40edd0a73558cb8c7ef857dc5bbefd1ed0c7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-07 22:20:39 +0300

    add solution SQL and Hibernate task 0502

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0502/query.sql
@@ -0,0 +1,3 @@
+INSERT INTO part (number, name, weight) VALUES (3004, 'Brick 1 x 2', 0.83);
+insert into part (number, name, weight) values (92909, 'Technic Steering Hub for Portal Axle', 2.5);
+insert into part (number, name, weight) values (19916, 'Helmet Darth Vader Type 2 Top', 0.5);
