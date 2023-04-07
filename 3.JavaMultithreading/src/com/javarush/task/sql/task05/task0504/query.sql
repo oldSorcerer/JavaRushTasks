da1e82b24e1b0fea85e2b62473581e2bce962c55
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-07 22:49:24 +0300

    add solution SQL and Hibernate task 0504

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
@@ -1,1 +1,11 @@
--- Write your code here:
+insert into author (first_name, last_name, full_name) values ('Joanne', 'Rowling', 'J. K. Rowling');
+insert into author (first_name, last_name, full_name) values ('Stephen', 'King', 'Stephen King');
+insert into author (first_name, last_name, full_name) values ('Clive', 'Lewis', 'C. S. Lewis');
+insert into author (first_name, last_name, full_name) values ('Hajime', 'Isayama', 'Hajime Isayama');
+insert into author (first_name, last_name, full_name) values ('Edgar', 'Burroughs', 'Edgar Rice Burroughs');
+insert into author (first_name, last_name, full_name) values ('Lewis', 'Carroll', 'Lewis Carroll');
+insert into author (first_name, last_name, full_name) values ('Astrid', 'Lindgren', 'Astrid Lindgren');
+insert into author (first_name, last_name, full_name) values ('Richard', 'Scarry', 'Richard Scarry');
+
+
+
