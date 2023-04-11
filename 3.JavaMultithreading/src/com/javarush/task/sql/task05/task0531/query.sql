92b240d2e5a13619d29ab8616b2d0a9ae7cc4371
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-11 17:21:29 +0300

    add solution SQL and Hibernate task 0531

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
@@ -1,1 +1,6 @@
--- Write your code here:
+select *
+from object
+where year(discovery_date) >= 2010
+  and month(discovery_date) >= 1
+  and year(discovery_date) <= 2020
+  and month(discovery_date) <= 12
