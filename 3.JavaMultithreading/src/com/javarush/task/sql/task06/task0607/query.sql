6d0dd01c69095e496ca752fb4aeb251b8842bab8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-13 01:02:12 +0300

    add solution SQL and Hibernate task 0607

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0607/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0607/query.sql
@@ -1,7 +1,7 @@
 insert into cars (brand, model, model_year, engine, consumption)
-values ('Lexus' 'IS' 2017 'petrol' 7.5),
-        ('Volvo' 'XC90' 2019 'diesel' 8.5),
-        ('Volkswagen' 'Golf' 2020 'petrol' 8),
-        ('Toyota' 'Corolla' 2015 'petrol' 10),
-        ('BMW' '5' 2005 'petrol' 15.5),
-        ('Ford' 'Transit' 2010 'diesel' 9);
+values ('Lexus', 'IS', 2017, 'petrol', 7.5),
+        ('Volvo', 'XC90', 2019, 'diesel', 8.5),
+        ('Volkswagen', 'Golf', 2020, 'petrol', 8),
+        ('Toyota', 'Corolla', 2015, 'petrol', 10),
+        ('BMW', '5', 2005, 'petrol', 15.5),
+        ('Ford', 'Transit', 2010, 'diesel', 9);
