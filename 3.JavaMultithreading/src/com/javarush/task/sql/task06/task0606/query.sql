2c0ec04e9545232b90a7ba4f065b891a6b5e358d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-13 00:54:27 +0300

    add solution SQL and Hibernate task 0606

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0606/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0606/query.sql
@@ -1,9 +1,9 @@
 insert into team (position, full_name, number)
 values ('Goalkeeper', 'Lev Yashin', 1),
-       ('Right - back', 'Cafu', 2),
-       ('Centre - back', 'Franz Beckenbauer', 3),
-       ('Centre - back', 'Bobby Moore', 4),
-       ('Left - back', 'Paolo Maldini', 5),
+       ('Right-back', 'Cafu', 2),
+       ('Centre-back', 'Franz Beckenbauer', 3),
+       ('Centre-back', 'Bobby Moore', 4),
+       ('Left-back', 'Paolo Maldini', 5),
        ('Midfield', 'Johan Cruyff', 6),
        ('Midfield', 'Zinedine Zidane', 7),
        ('Midfield', 'Andrea Pirlo', 8),
