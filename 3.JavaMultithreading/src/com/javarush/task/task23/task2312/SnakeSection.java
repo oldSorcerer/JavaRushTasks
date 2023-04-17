618cb4a885686ac719a4d44f28cc00c2831cea72
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-17 10:53:26 +0300

    add solution Multithreading task 2312 Snake 6/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,4 +1,19 @@
 package com.javarush.task.task23.task2312;
 
 public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
