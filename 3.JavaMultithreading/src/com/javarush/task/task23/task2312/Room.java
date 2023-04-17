f5e95f2b5b786cf42e714d7a46b0eb8008c6b110
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-17 18:15:47 +0300

    add solution Multithreading task 2312 Snake 13/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task23.task2312;
 
-public class Room {
+import java.awt.event.KeyEvent;
 
+public class Room {
     private int width;
     private int height;
     private Snake snake;
@@ -13,50 +14,89 @@ public class Room {
         this.width = width;
         this.height = height;
         this.snake = snake;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
+        game = this;
     }
 
     public Snake getSnake() {
         return snake;
     }
 
-    public void setSnake(Snake snake) {
-        this.snake = snake;
-    }
-
     public Mouse getMouse() {
         return mouse;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
     }
 
     public void setMouse(Mouse mouse) {
         this.mouse = mouse;
     }
 
+    /**
+     * Основной цикл программы.
+     * Тут происходят все важные действия
+     */
     public void run() {
+        //Создаем объект "наблюдатель за клавиатурой" и стартуем его.
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
 
-    }
+        //пока змея жива
+        while (snake.isAlive()) {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents()) {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если равно символу 'q' - выйти из игры.
+                if (event.getKeyChar() == 'q') return;
 
-    public void sleep() {
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    snake.setDirection(SnakeDirection.LEFT);
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    snake.setDirection(SnakeDirection.RIGHT);
+                    //Если "стрелка вверх" - сдвинуть фигурку вверх
+                else if (event.getKeyCode() == KeyEvent.VK_UP)
+                    snake.setDirection(SnakeDirection.UP);
+                    //Если "стрелка вниз" - сдвинуть фигурку вниз
+                else if (event.getKeyCode() == KeyEvent.VK_DOWN)
+                    snake.setDirection(SnakeDirection.DOWN);
+            }
 
+            snake.move();   //двигаем змею
+            print();        //отображаем текущее состояние игры
+            sleep();        //пауза между ходами
+        }
+
+        System.out.println("Game Over!");
     }
 
     public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        //Рисуем все кусочки змеи
+        //Рисуем мышь
+        //Выводим все это на экран
+    }
 
+    public void eatMouse() {
+        createMouse();
     }
 
     public void createMouse() {
@@ -65,17 +105,19 @@ public class Room {
         mouse = new Mouse(x, y);
     }
 
-    public void eatMouse(){
-        createMouse();
-    }
-
     public static void main(String[] args) {
-        Snake snake = new Snake(10,10);
-        game = new Room(20,20, snake);
-        snake.setDirection(SnakeDirection.DOWN);
+        game = new Room(20, 20, new Snake(10, 10));
+        game.snake.setDirection(SnakeDirection.DOWN);
         game.createMouse();
         game.run();
+    }
 
-
+    public void sleep() {
+        try {
+            int level  = snake.getSections().size();
+            Thread.sleep(level < 15 ? (520 - 20 * level) : 200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
