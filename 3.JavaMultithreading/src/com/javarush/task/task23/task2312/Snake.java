1762ae397373f652fa59e56b926c44d3f6a054a5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-17 12:46:59 +0300

    add solution Multithreading task 2312 Snake 8/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
@@ -7,6 +8,12 @@ public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
 
     public List<SnakeSection> getSections() {
         return sections;
@@ -22,5 +29,17 @@ public class Snake {
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {
+
     }
 }
