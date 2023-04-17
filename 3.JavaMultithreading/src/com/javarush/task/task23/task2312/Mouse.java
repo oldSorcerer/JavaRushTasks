3fb94ea29773c434bc084ecfbc1003b7c6906acb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-17 10:46:48 +0300

    add solution Multithreading task 2312 Snake 3/18

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -1,19 +1,6 @@
 package com.javarush.task.task23.task2312;
 
 public class Mouse {
-    private final int x;
-    private final int y;
-
-    public Mouse(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+    private int x;
+    private int y;
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,34 +1,4 @@
 package com.javarush.task.task23.task2312;
 
-import java.util.Objects;
-
 public class SnakeSection {
-    private final int x;
-    private final int y;
-
-    public SnakeSection(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SnakeSection that = (SnakeSection) o;
-        return x == that.x && y == that.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
