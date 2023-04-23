20dab4383b514e03e79c1ceef27f93f465d7d3cb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-23 11:47:59 +0300

    add solution Collections task 2213 Tetris 16/17

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -2,8 +2,8 @@ package com.javarush.task.task22.task2213;
 
 public class Figure {
 
-    private final int x;
-    private final int y;
+    private int x;
+    private int y;
     private final int[][] matrix;
 
     public Figure(int x, int y, int[][] matrix) {
@@ -25,19 +25,27 @@ public class Figure {
     }
 
     public void left() {
-
+        if (isCurrentPositionAvailable()) {
+            x--;
+        }
     }
 
     public void right() {
-
+        if (isCurrentPositionAvailable()) {
+            x++;
+        }
     }
 
     public void down() {
-
+        if (isCurrentPositionAvailable()) {
+            y++;
+        }
     }
 
     public void up() {
-
+        if (isCurrentPositionAvailable()) {
+            y--;
+        }
     }
 
     public void rotate() {
--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyEvent;
  * Класс Tetris - содержит основной функционал игры.
  */
 public class Tetris {
+
     private Field field;                //Поле с клетками
     private Figure figure;              //Фигурка
 
@@ -16,10 +17,16 @@ public class Tetris {
         figure = null;
     }
 
+    /**
+     * Геттер переменной field.
+     */
     public Field getField() {
         return field;
     }
 
+    /**
+     * Геттер переменной figure.
+     */
     public Figure getFigure() {
         return figure;
     }
@@ -69,33 +76,33 @@ public class Tetris {
         System.out.println("Game Over");
     }
 
-    /**
-     * Один шаг игры
-     */
     public void step() {
         //опускам фигурку вниз
-
-        //если разместить фигурку на текущем месте невозможно:
-        //поднимаем обратно
-        //приземляем
-        //удаляем заполненные линии
-        //создаем новую фигурку
-
         figure.down();
 
+        //если разместить фигурку на текущем месте невозможно
         if (!figure.isCurrentPositionAvailable()) {
-            figure.up();
-            figure.landed();
-            field.removeFullLines();
-            figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0);
-        }
+            figure.up();                    //поднимаем обратно
+            figure.landed();                //приземляем
 
+            isGameOver = figure.getY() <= 1;//если фигурка приземлилась на самом верху - игра окончена
+
+            field.removeFullLines();        //удаляем заполненные линии
+
+            figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0); //создаем новую фигурку
+        }
     }
 
+    /**
+     * Сеттер для figure
+     */
     public void setFigure(Figure figure) {
         this.figure = figure;
     }
 
+    /**
+     * Сеттер для field
+     */
     public void setField(Field field) {
         this.field = field;
     }
