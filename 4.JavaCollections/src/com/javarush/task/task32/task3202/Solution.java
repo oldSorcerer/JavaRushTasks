2d849231653d32b1257c1aabe46d515b1d9f13c1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-23 15:15:33 +0300

    add solution Collections task 3202

--- 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -10,9 +10,22 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
         System.out.println(writer.toString());
+        StringWriter writer2 = getAllDataFromInputStream(null);
+        System.out.println(writer2.toString());
     }
 
     public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
-        return null;
+        StringWriter writer = new StringWriter();
+        if (is != null) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                while (reader.ready()) {
+                    writer.write(reader.read());
+                }
+            }
+        }
+//        if (is != null)
+//            new BufferedReader(new InputStreamReader(is)).lines().forEach(writer::write);
+
+        return writer;
     }
 }
