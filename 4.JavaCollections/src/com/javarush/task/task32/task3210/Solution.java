26fb0d433a57fdeba505529e7e77ddbc134164b7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-23 13:23:26 +0300

    add solution Collections task 3210

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -8,7 +8,17 @@ import java.io.RandomAccessFile;
 */
 
 public class Solution {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(args[0], "rw")) {
+            int number = Integer.parseInt(args[1]);
+            String text = args[2];
 
+            randomAccessFile.seek(number);
+
+            byte[] buff = new byte[text.length()];
+            randomAccessFile.read(buff, 0, buff.length);
+
+            randomAccessFile.write(text.equals(new String(buff)) ? "true".getBytes() : "false".getBytes());
+        }
     }
 }
