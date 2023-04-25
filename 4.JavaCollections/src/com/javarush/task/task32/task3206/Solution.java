fb868c5660b1e74b00721c83ac8fa86cfbaefebc
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-25 21:54:57 +0300

    add solution Collections task 3206

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3206/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task32.task3206;
+
+import java.lang.reflect.Proxy;
+
+/* 
+Дженерики для создания прокси-объекта
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        test(solution.getProxy(Item.class));                        //true false false
+        test(solution.getProxy(Item.class, Small.class));           //true false true
+        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+    }
+
+
+    private static void test(Object proxy) {
+        boolean isItem = proxy instanceof Item;
+        boolean isBig = proxy instanceof Big;
+        boolean isSmall = proxy instanceof Small;
+
+        System.out.format("%b %b %b\n", isItem, isBig, isSmall);
+    }
+}
