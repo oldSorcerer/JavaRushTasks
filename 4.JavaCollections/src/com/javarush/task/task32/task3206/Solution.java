f91767dbefef5aaf328e395b5ed019364098de70
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-25 21:55:28 +0300

    add solution Collections task 3206

--- 4.JavaCollections/src/com/javarush/task/task32/task3206/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3206/Solution.java
@@ -10,11 +10,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        test(solution.getProxy(Item.class));                        //true false false
-        test(solution.getProxy(Item.class, Small.class));           //true false true
-        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
-        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
-        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+//        test(solution.getProxy(Item.class));                        //true false false
+//        test(solution.getProxy(Item.class, Small.class));           //true false true
+//        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+//        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+//        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
     }
 
 
