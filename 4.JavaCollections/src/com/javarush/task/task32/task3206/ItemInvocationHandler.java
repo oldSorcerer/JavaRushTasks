d3d739050cd4b5b0e9e83f2e35741b7cf8e13a2a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-26 23:52:36 +0300

    add solution Collections task 3206

--- 4.JavaCollections/src/com/javarush/task/task32/task3206/ItemInvocationHandler.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3206/ItemInvocationHandler.java
@@ -4,6 +4,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class ItemInvocationHandler implements InvocationHandler {
+
+    private Class<?> item;
+
+    public ItemInvocationHandler(Class<?> item) {
+        this.item = item;
+    }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return null;
