067696bd4940cce820cb6234696c85372af57078
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-30 13:45:42 +0300

    add solution Multithreading task 3608 MVC 1/9

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -2,6 +2,9 @@ package com.javarush.task.task36.task3608.model;
 
 import com.javarush.task.task36.task3608.bean.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FakeModel implements Model{
 
     private ModelData modelData = new ModelData();
@@ -13,7 +16,9 @@ public class FakeModel implements Model{
 
     @Override
     public void loadUsers() {
-        modelData.users.add(new User("A", 1, 1));
-        modelData.users.add(new User("B", 2, 1));
+        List<User> users = new ArrayList<>();
+        users.add(new User("A", 1, 1));
+        users.add(new User("B", 2, 1));
+        modelData.setUsers(users);
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class ModelData {
 
-    List<User> users;
+    private List<User> users;
 
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
