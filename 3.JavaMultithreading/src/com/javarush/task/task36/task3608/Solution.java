6a26e65268dc039b6e7fbc32da669ab2613e2554
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-30 16:21:49 +0300

    add solution Multithreading task 3608 MVC 3/9

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.FakeModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new FakeModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
 
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -12,5 +14,10 @@ public class Controller {
 
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
