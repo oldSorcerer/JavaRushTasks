14ef4313887ace0be492abf59621d4e73ad8baaa
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-04-30 11:55:51 +0300

    add solution Multithreading task 3608

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task36.task3608.model;
+
+public interface Model {
+    ModelData getModelData();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.List;
+
+public class ModelData {
+
+    List<User> users;
+
+}
