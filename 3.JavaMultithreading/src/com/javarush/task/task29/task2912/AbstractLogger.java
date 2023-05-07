d4e3352164b6899fc27c59572f2b588e633cd223
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-07 20:01:39 +0300

    add solution Multithreading task 2912

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2912/AbstractLogger.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task29.task2912;
+
+public abstract class AbstractLogger implements Logger {
+
+    int level;
+    Logger next;
+
+    public AbstractLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public void inform(String message, int level) {
+        if (this.level <= level) {
+            info(message);
+        }
+        if (next != null) {
+            next.inform(message, level);
+        }
+    }
+
+    @Override
+    public void setNext(Logger next) {
+        this.next = next;
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2912/ConsoleLogger.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2912/ConsoleLogger.java
@@ -1,26 +1,9 @@
 package com.javarush.task.task29.task2912;
 
-public class ConsoleLogger implements Logger {
-    int level;
-    Logger next;
+public class ConsoleLogger extends AbstractLogger{
 
     public ConsoleLogger(int level) {
-        this.level = level;
-    }
-
-    @Override
-    public void inform(String message, int level) {
-        if (this.level <= level) {
-            info(message);
-        }
-        if (next != null) {
-            next.inform(message, level);
-        }
-    }
-
-    @Override
-    public void setNext(Logger next) {
-        this.next = next;
+        super(level);
     }
 
     @Override
