a3c492d07c0eda96cac4d9180f6a44c8065cca25
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-09 00:06:12 +0300

    add solution Collections task 3212

--- 4.JavaCollections/src/com/javarush/task/task32/task3212/Cache.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3212/Cache.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cache {
-    private List<Service> services;
+    private final List<Service> services;
 
     public Cache() {
         services = new ArrayList<>();
--- 4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
@@ -4,7 +4,7 @@ import com.javarush.task.task32.task3212.contex.InitialContext;
 import com.javarush.task.task32.task3212.service.Service;
 
 public class ServiceLocator {
-    private static Cache cache;
+    private static final Cache cache;
 
     static {
         cache = new Cache();
@@ -20,6 +20,13 @@ public class ServiceLocator {
      * @return Object mapped to the name in context
      */
     public static Service getService(String jndiName) {
-        return null;
+        Service service = cache.getService(jndiName);
+        if (service != null) {
+            return service;
+        }
+        InitialContext context = new InitialContext();
+        Service service1 = (Service)context.lookup(jndiName);
+        cache.addService(service1);
+        return service1;
     }
 }
