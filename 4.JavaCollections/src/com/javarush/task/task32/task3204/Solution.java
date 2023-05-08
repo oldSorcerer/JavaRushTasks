f22fa5379a4de40860f8b374d5bbaed4d1f68da4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-08 23:41:00 +0300

    add solution Collections task 3204

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task32.task3204;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 /* 
 Генератор паролей
@@ -9,10 +11,28 @@ import java.io.ByteArrayOutputStream;
 public class Solution {
     public static void main(String[] args) {
         ByteArrayOutputStream password = getPassword();
-        System.out.println(password.toString());
+        System.out.println(password);
     }
 
     public static ByteArrayOutputStream getPassword() {
-        return null;
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String alphanumeric = "0123456789" + alphabet + alphabet.toUpperCase();
+
+        char[] password = new char[8];
+        String string = "";
+        while (!string.matches(".*\\d.*") || !string.matches(".*[a-z].*") || !string.matches(".*[A-Z].*")) {
+            for (int i = 0; i < password.length; i++) {
+                password[i] = alphanumeric.charAt(new Random().nextInt(alphanumeric.length()));
+            }
+            string = new String(password);
+        }
+        
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            byteArrayOutputStream.write(string.getBytes());
+            return byteArrayOutputStream;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
