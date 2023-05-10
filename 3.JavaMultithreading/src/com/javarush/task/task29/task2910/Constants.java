dad36eb40df6db971380c3faa564b4114535414a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-10 15:52:57 +0300

    add solution Multithreading task 2910

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Constants.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Constants.java
@@ -3,7 +3,7 @@ package com.javarush.task.task29.task2910;
 import java.math.BigDecimal;
 
 public class Constants {
-    public static final Double RACE_LENGTH = new Double(20.0);
+    public static final Double RACE_LENGTH = 20.0;
     public static final int NUMBER_OF_FIRE_LINES = 4;
     public static final int PENALTY_TIME_PER_ONE_MISS = 20;
     public static final BigDecimal MIN_CHANCE_TO_HIT_IN_TARGET = new BigDecimal("0.815");
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Game.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Game.java
@@ -1,45 +1,46 @@
 package com.javarush.task.task29.task2910;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.javarush.task.task29.task2910.Constants.*;
 
 public class Game {
-    private List<Sportsman> members;
+    private final List<Sportsman> sportsmenList;
     Sportsman controlSportsman;
 
     public Game() {
         initializeControlSportsman();
-        members = new ArrayList<>();
+        sportsmenList = new ArrayList<>();
         for (int i = 0; i < SPORTSMAN_COUNT; i++) {
-            members.add(new Sportsman(i + 1));
+            sportsmenList.add(new Sportsman(i + 1));
         }
     }
 
     private void initializeControlSportsman() {
         controlSportsman = new Sportsman(0);
-        controlSportsman.setAverageSpeed(new BigDecimal(20.));
+        controlSportsman.setAverageSpeed(new BigDecimal("20."));
         controlSportsman.setChanceToHit(MAX_CHANCE_TO_HIT_IN_TARGET);
         controlSportsman.setCountHitOutTarget(2);
-        controlSportsman.setResultTime(new BigDecimal("3640.00"));
+        controlSportsman.setResultTime(new BigDecimal("3640.00")); // fucking BigDecimal
         System.out.println("РЕЗУЛЬТАТ КОНТРОЛЬНОГО СПОРТСМЕНА:");
         controlSportsman.printResultInfo();
     }
 
     public void startCompetition() {
-        for (int i = 0; i < members.size(); i++) {
-            members.get(i).calculateNumberOfMiss();
-            BigDecimal timeToRace = controlSportsman.getResultTime().subtract(BigDecimal.valueOf(members.get(i).getCountHitOutTarget() * PENALTY_TIME_PER_ONE_MISS));
-            members.get(i).setAverageSpeed(new BigDecimal(RACE_LENGTH / (timeToRace.doubleValue() / 3600)).setScale(4, BigDecimal.ROUND_HALF_UP));
-            members.get(i).calculateFullTime();
+        for (Sportsman sportsman : sportsmenList) {
+            sportsman.calculateNumberOfMiss();
+            BigDecimal timeToRace = controlSportsman.getResultTime().subtract(BigDecimal.valueOf(sportsman.getCountHitOutTarget() * PENALTY_TIME_PER_ONE_MISS));
+            sportsman.setAverageSpeed(new BigDecimal(RACE_LENGTH / (timeToRace.doubleValue() / 3600)).setScale(4, RoundingMode.HALF_UP ));
+            sportsman.calculateFullTime();
         }
     }
 
     public void printAllResults() {
-        for (int i = 0; i < members.size(); i++) {
-            members.get(i).printResultInfo();
+        for (Sportsman member : sportsmenList) {
+            member.printResultInfo();
         }
     }
 
@@ -51,9 +52,9 @@ public class Game {
 
     private List<Sportsman> getSportsmanWithEqualTime() {
         List<Sportsman> result = new ArrayList<>();
-        for (int i = 0; i < members.size(); i++) {
-            if (members.get(i).getResultTime().equals(controlSportsman.getResultTime())) {
-                result.add((members.get(i)));
+        for (Sportsman member : sportsmenList) {
+            if (member.getResultTime().equals(controlSportsman.getResultTime())) {
+                result.add(member);
             }
         }
         return result;
