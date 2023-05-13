6ecf9231a8cc654a62c045ca1b1b3634797d3935
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-13 11:40:00 +0300

    add solution Multithreading task 3009

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -9,9 +9,13 @@ import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-//        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
-//        System.out.println(getRadix("123"));        //expected output: [6]
-//        System.out.println(getRadix("5321"));       //expected output: []
-//        System.out.println(getRadix("1A"));         //expected output: []
+        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
+        System.out.println(getRadix("123"));        //expected output: [6]
+        System.out.println(getRadix("5321"));       //expected output: []
+        System.out.println(getRadix("1A"));         //expected output: []
+    }
+
+    private static Set<Integer> getRadix(String number) {
+        return null;
     }
 }
