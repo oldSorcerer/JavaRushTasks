19d6269edc4062d6472f15be2423c0bce8a46bb4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-14 13:47:08 +0300

    add solution Collections task 3909

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3909/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task39.task3909;
+
+/* 
+Одно изменение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isOneEditAway(String first, String second) {
+
+    }
+}
