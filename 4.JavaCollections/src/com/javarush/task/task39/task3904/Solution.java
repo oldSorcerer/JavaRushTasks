2b1ffcab0a956bfb8da843b67e2267ad1dbd3fe1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-14 13:47:57 +0300

    add solution Collections task 3904

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3904/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task39.task3904;
+
+import java.util.Arrays;
+
+/* 
+Лестница
+*/
+
+public class Solution {
+    private static int n = 70;
+
+    public static void main(String[] args) {
+        System.out.println("The number of possible ascents for " + n + " steps is: " + numberOfPossibleAscents(n));
+    }
+
+    public static long numberOfPossibleAscents(int n) {
+
+    }
+}
+
