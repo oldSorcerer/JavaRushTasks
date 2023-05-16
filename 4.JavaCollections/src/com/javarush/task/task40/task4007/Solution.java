719f8b7fe7f37c5a530643c65539cc3f8f71f16c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-16 22:29:12 +0300

    add solution from JavaCollections task 4007

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4007/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task40.task4007;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/* 
+Работа с датами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printDate("21.4.2014 15:56:45");
+        System.out.println();
+        printDate("21.4.2014");
+        System.out.println();
+        printDate("17:33:40");
+    }
+
+    public static void printDate(String date) {
+        //напишите тут ваш код
+    }
+}
