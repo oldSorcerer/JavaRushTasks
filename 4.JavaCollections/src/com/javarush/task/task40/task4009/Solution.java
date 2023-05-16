dfa4ff6e6559abeccce3ab645444e6b144552e07
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-16 22:26:48 +0300

    add solution from JavaCollections task 4009

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4009/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task40.task4009;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/* 
+Buon Compleanno!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getWeekdayOfBirthday("30.05.1984", "2015"));
+    }
+
+    public static String getWeekdayOfBirthday(String birthday, String year) {
+        //напишите тут ваш код
+        return "";
+    }
+}
