6acf94f0611cec391e75da5f5a77989831337343
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-16 22:32:55 +0300

    add solution from JavaCollections task 4010

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4010/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task40.task4010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* 
+Коды ошибок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://jsonplaceholder.typicode.com/posts/1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            if (conn.getResponseCode() != 100) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Server output .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
