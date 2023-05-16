839ebf1717163c83f29316d243468c478ba62940
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-16 22:28:31 +0300

    add solution from JavaCollections task 4008

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4008/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task40.task4008;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/* 
+Работа с Java 8 DateTime API
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printDate("21.4.2014 15:56:45");
+        System.out.println();
+        printDate("21.4.2014");
+        System.out.println();
+        printDate("17:33:40");
+    }
+
+    public static void printDate(String date) {
+        //напишите тут ваш код
+    }
+}
