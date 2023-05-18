5c6e69d5eccac8ab6ae80362700ea38bbee51ea4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-18 20:24:44 +0300

    add solution from JavaCollections task 3905

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3905/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task39.task3905;
+
+import java.util.Random;
+
+/* 
+Залей меня полностью
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
