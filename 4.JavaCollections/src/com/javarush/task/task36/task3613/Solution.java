c58fe2edbbaf8da2dd997bc67b813ba7ecb25bd8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-23 23:57:21 +0300

    add solution Collections task 3613

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task36.task3613;
+
+/* 
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 10 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return null;
+    }
+}
