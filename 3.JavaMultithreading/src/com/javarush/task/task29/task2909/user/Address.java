c7cca324a594419f64cab81febdbaba33faa010d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-01 23:28:35 +0300

    add solution Multithreading task 2909 add lombok

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/Address.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/Address.java
@@ -11,28 +11,4 @@ public class Address {
     private String city;
     private String house;
 
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-
-    public void setHouse(String house) {
-        this.house = house;
-    }
 }
