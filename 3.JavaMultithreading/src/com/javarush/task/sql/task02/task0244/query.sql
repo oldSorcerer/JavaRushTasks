9909a8d5384c666618e6d0fa61d7b0e13fedf54a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-04 18:44:37 +0300

    add solution SQL and Hibernate task 0244

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0244/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0244/query.sql
@@ -1,4 +1,7 @@
-select year(prod_date) as prod_year, month(prod_date) as prod_month, count(*)
+select
+    year(prod_date) as prod_year,
+    month(prod_date) as prod_month,
+    count(*)
 from cars
 where name = 'Black Car' and price > 99000
 group by  prod_year, prod_date
