140fefda74651bc2344f3c83323afe45448ab32d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-07 15:29:08 +0300

    add solution University Professional task 4114

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4114/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4114/Solution.java
@@ -7,20 +7,20 @@ package com.javarush.task.jdk13.task41.task4114;
 public class Solution {
 
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        processor.plus(3, 9);
-        processor.minus(3, 9);
-        processor.multiply(3, 9);
-        processor.divide(3, 9);
-        processor.divide(-234, 12);
-        processor.plus(123, -631);
+//        Processor processor = new Processor();
+//        processor.plus(3, 9);
+//        processor.minus(3, 9);
+//        processor.multiply(3, 9);
+//        processor.divide(3, 9);
+//        processor.divide(-234, 12);
+//        processor.plus(123, -631);
 
-//        Calculator calculator = new Calculator();
-//        calculator.calculate(3, "+", 9);
-//        calculator.calculate(3, "-", 9);
-//        calculator.calculate(3, "*", 9);
-//        calculator.calculate(3, "/", 9);
-//        calculator.calculate(-234, "/", 12);
-//        calculator.calculate(123, "+", -631);
+        Calculator calculator = new Calculator();
+        calculator.calculate(3, "+", 9);
+        calculator.calculate(3, "-", 9);
+        calculator.calculate(3, "*", 9);
+        calculator.calculate(3, "/", 9);
+        calculator.calculate(-234, "/", 12);
+        calculator.calculate(123, "+", -631);
     }
 }
