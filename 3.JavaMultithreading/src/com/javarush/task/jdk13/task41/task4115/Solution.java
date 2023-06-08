631d61293c1f74e366990f92847d7cc5d68f2828
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-08 22:04:13 +0300

    add solution University Professional task 4112

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4115/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4115/Solution.java
@@ -4,6 +4,10 @@ package com.javarush.task.jdk13.task41.task4115;
 У вас скидка!
 */
 
+import com.javarush.task.jdk13.task41.task4115.listeners.*;
+
+import static com.javarush.task.jdk13.task41.task4115.listeners.NotificationType.*;
+
 public class Solution {
 
     public static ChainStore atb = new ChainStore("АТБ");
@@ -19,17 +23,17 @@ public class Solution {
     }
 
     public static void subscribeAll() {
-//        atb.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
-//        atb.notifications.subscribe(EVENT, new EmailNotificationListener("imyarek@javarush.co"));
-//        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("123456789"));
-//        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("562346245626"));
-//        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("234534565636787689"));
-//        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("2345245763537"));
-//
-//        lidl.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
-//
-//        walmart.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
-//        walmart.notifications.subscribe(PROMO, new PigeonMailNotificationListener("Лондон, Бейкер-стрит, 221b"));
+        atb.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
+        atb.notifications.subscribe(EVENT, new EmailNotificationListener("imyarek@javarush.co"));
+        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("123456789"));
+        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("562346245626"));
+        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("234534565636787689"));
+        atb.notifications.subscribe(SALE, new FirebasePushNotificationListener("2345245763537"));
+
+        lidl.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
+
+        walmart.notifications.subscribe(EVENT, new EmailNotificationListener("diego@javarush.co"));
+        walmart.notifications.subscribe(PROMO, new PigeonMailNotificationListener("Лондон, Бейкер-стрит, 221b"));
     }
 
     public static void lunchAll(ChainStore store) {
