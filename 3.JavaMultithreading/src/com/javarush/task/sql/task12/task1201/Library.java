7edc4825fa656f5be5476d28b72b38f6ccab0b38
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-20 21:12:34 +0300

    add solution SQL and Hibernate task 1201

--- 3.JavaMultithreading/src/com/javarush/task/sql/task12/task1201/Library.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task12/task1201/Library.java
@@ -6,8 +6,46 @@ import jakarta.persistence.*;
 Создаём Entity из класса
 */
 
-//напишите тут ваш код
+@Entity
+@Table(name = "library_table")
 public class Library {
-    //напишите тут ваш код
+    @Id
+    private long id;
+//    @Enumerated(value = "")
+    PublicationStatus status;
+    String publicationName;
+    String isdn;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PublicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PublicationStatus status) {
+        this.status = status;
+    }
+
+    public String getPublicationName() {
+        return publicationName;
+    }
+
+    public void setPublicationName(String publicationName) {
+        this.publicationName = publicationName;
+    }
+
+    public String getIsdn() {
+        return isdn;
+    }
+
+    public void setIsdn(String isdn) {
+        this.isdn = isdn;
+    }
 }
