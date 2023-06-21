6876078cb56429417120edabd3fe8ec3faac1db3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-21 09:59:33 +0300

    add solution Multithreading task 2909 add lombok

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/Work.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/Work.java
@@ -1,11 +1,15 @@
 package com.javarush.task.task29.task2909.user;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
-@Getter @Setter
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Work {
 
-    private String boss;
+    String boss;
 
 }
