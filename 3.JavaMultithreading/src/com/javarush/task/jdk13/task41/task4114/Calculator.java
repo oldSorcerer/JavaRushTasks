86b75982041eb2f4df5cb328080e04a89fcc203c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-06-27 18:59:27 +0300

    add solution University Professional task 4114

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4114/Calculator.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4114/Calculator.java
@@ -22,17 +22,16 @@ public class Calculator {
         operation.execute(operand1, operand2);
     }
 
-    public void calculate1(int operand1, String operationSign, int operand2) {
-        if (operationMap.containsKey(operationSign)) {
-            for (Map.Entry<String, Operation> entry : operationMap.entrySet()) {
-                if (entry.getKey().equals(operationSign)) {
-                    Operation operation = entry.getValue();
-                    operation.execute(operand1, operand2);
-                }
-            }
-        } else {
-            throw new IllegalStateException("no operation registered for '%s'".formatted(operationSign));
-        }
-    }
-
+//    public void calculate1(int operand1, String operationSign, int operand2) {
+//        if (operationMap.containsKey(operationSign)) {
+//            for (Map.Entry<String, Operation> entry : operationMap.entrySet()) {
+//                if (entry.getKey().equals(operationSign)) {
+//                    Operation operation = entry.getValue();
+//                    operation.execute(operand1, operand2);
+//                }
+//            }
+//        } else {
+//            throw new IllegalStateException("no operation registered for '%s'".formatted(operationSign));
+//        }
+//    }
 }
