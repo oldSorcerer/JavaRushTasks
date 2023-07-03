a86e7950b9eef1cacf54b0fe4231fa7eaaaea3a7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-03 18:00:42 +0300

    add solution Collections task 3913

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task39.task3913;
+
+import com.javarush.task.task39.task3913.query.IPQuery;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class LogParser {
+}
