f703924fe8234a52651b47d70a023bb1a45d46f1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-07 19:26:04 +0300

    add solution Collections task 3702 Factory 1/4

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task37.task3702;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/Man.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class Man implements Human {
+
+
+    @Override
+    public String toString() {
+        return "Man{}";
+    }
+}
