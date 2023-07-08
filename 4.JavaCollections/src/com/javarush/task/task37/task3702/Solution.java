851bb07b65c6d967e6d657c07da09615597bec3d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-08 03:49:41 +0300

    add solution Collections task 3702 Factory 2/4

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,8 +1,14 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
 
     public static void main(String[] args) {
+        MaleFactory maleFactory = new MaleFactory();
+        System.out.println(maleFactory.getPerson(99));
+        System.out.println(maleFactory.getPerson(4));
+        System.out.println(maleFactory.getPerson(15));
 
     }
 
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory {
+
+    public Human getPerson(int age) {
+        if (age <= KidBoy.MAX_AGE) {
+            return new KidBoy();
+        } else if (age <= TeenBoy.MAX_AGE) {
+            return new TeenBoy();
+        } else {
+            return new Man();
+        }
+    }
+}
