c8c1cebdf72bcf94294056529d797a7640ccc950
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-09 17:26:24 +0300

    add solution Collections task 2613 CashMachine 2/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,13 +1,23 @@
 package com.javarush.task.task26.task2613;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
     public static void writeMessage(String message) {
         System.out.println(message);
     }
 
     public static String readString() {
-        return "";
+        try {
+            return bis.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/NotEnoughMoneyException.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/NotEnoughMoneyException.java
@@ -1,4 +1,4 @@
 package com.javarush.task.task26.task2613.exception;
 
-public class NotEnoughMoneyException {
+public class NotEnoughMoneyException extends Exception {
 }
