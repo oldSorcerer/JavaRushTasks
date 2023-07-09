222c254030132a01fc99c0ca5361726601ce2b23
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-09 17:44:46 +0300

    add solution Collections task 2613 CashMachine 3/15

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.Map;
+
+public class CurrencyManipulator {
+
+    private String currencyCode;
+    private Map<Integer, Integer> denominations;
+
+    public CurrencyManipulator(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+}
