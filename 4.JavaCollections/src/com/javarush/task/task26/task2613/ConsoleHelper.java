ab5844e791aaf9b87f07226ab9ff1d2c7d0f11e1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-09 09:49:28 +0300

    add solution Collections task 2613 CashMachine 2/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,4 +1,13 @@
 package com.javarush.task.task26.task2613;
 
 public class ConsoleHelper {
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        return "";
+    }
+
 }
