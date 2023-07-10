361f221f0f600833995324eaca76c28dc0fae038
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-10 19:30:23 +0300

    add solution Collections task 3410 Sokoban 1/17

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task34.task3410.model;
+
+public class Model {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task34.task3410.view;
+
+import com.javarush.task.task34.task3410.controller.Controller;
+
+import javax.swing.*;
+
+public class View extends JFrame {
+
+    private Controller controller;
+
+    public View(Controller controller) {
+        this.controller = controller;
+    }
+}
