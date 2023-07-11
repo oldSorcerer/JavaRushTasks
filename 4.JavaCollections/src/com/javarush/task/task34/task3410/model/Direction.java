34656381a7dee526bb3a0f964244e8ea72d1eb8d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-11 12:55:16 +0300

    add solution Collections task 3410 Sokoban 4/17

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Direction.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task34.task3410.model;
+
+public enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Movable.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Movable.java
@@ -1,4 +1,7 @@
 package com.javarush.task.task34.task3410.model;
 
 public interface Movable {
+
+    void move(int x, int y);
+
 }
