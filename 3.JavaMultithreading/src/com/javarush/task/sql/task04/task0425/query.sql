67a689723730702a20294d36e5af0d2f3cb704d9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-13 00:01:03 +0300

    add solution SQL and Hibernate task 0425

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0425/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0425/query.sql
@@ -1,2 +1,3 @@
-select year(date_of_birth), month(date_of_birth), count(*) from employee
+select year(date_of_birth), month(date_of_birth), count(*)
+from employee
 group by year(date_of_birth), month(date_of_birth)
