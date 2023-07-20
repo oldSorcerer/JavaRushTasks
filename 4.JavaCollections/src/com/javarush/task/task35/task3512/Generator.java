863471bcaa8204006067cfebff3292d82d55f4b0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-20 17:42:49 +0300

    add solution Collections task 3512

--- 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -1,7 +1,18 @@
 package com.javarush.task.task35.task3512;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Generator<T> {
-//    T newInstance() {
-//        return new T();
-//    }
+
+    private final Class<T> clazz;
+
+    public Generator(Class<T> instance) {
+        this.clazz = instance;
+    }
+
+    T newInstance() throws InstantiationException, IllegalAccessException {
+        return clazz.newInstance();
+    }
+
 }
+
