ed6a91943a9bac8180fb445a3b729163818a56ab
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-24 21:08:05 +0300

    add solution Multithreading task 3010

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task30.task3010;
 
-import java.util.regex.Pattern;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /* 
 Минимальное допустимое основание системы счисления
@@ -8,6 +9,16 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String result = "";
+        for (int i = 2; i < 37; i++) {
+            try {
+                new BigDecimal(new BigInteger(args[0], i));
+                result = String.valueOf(i);
+                break;
+            } catch (Exception e) {
+                result = "incorrect";
+            }
+        }
+        System.out.println(result);
     }
 }
