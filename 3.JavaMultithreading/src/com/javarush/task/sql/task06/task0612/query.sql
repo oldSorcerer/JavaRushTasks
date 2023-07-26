d4031b161e81f05a59fd8385964339cdcc69370c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-26 19:12:08 +0300

    add solution SQL and Hibernate task 0612

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0612/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0612/query.sql
@@ -1,4 +1,4 @@
-update employee set
-salary = 5000,
-position = 'fullstack developer'
+update employee
+set salary   = 5000,
+    position = 'fullstack developer'
 where position like '%developer%';
