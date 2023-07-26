c95bc59f1c17b887ee4e944055bcda449fd77280
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-26 19:00:30 +0300

    add solution SQL and Hibernate task 0501

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0501/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0501/query.sql
@@ -1,4 +1,4 @@
 INSERT INTO publisher (id, name)
-VALUES  (1, 'Азбука'),
-        (2, 'Фолио'),
-        (3, 'Wordsworth Editions');
+VALUES (1, 'Азбука'),
+       (2, 'Фолио'),
+       (3, 'Wordsworth Editions');
