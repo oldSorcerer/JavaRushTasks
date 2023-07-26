0bdb890b927689e0630719bad6aa1302064e3834
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-26 19:00:56 +0300

    add solution SQL and Hibernate task 0504

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0504/query.sql
@@ -1,12 +1,12 @@
 insert into author (first_name, last_name, full_name)
-values  ('Joanne', 'Rowling', 'J. K. Rowling'),
-        ('Stephen', 'King', 'Stephen King'),
-        ('Clive', 'Lewis', 'C. S. Lewis'),
-        ('Hajime', 'Isayama', 'Hajime Isayama'),
-        ('Edgar', 'Burroughs', 'Edgar Rice Burroughs'),
-        ('Lewis', 'Carroll', 'Lewis Carroll'),
-        ('Astrid', 'Lindgren', 'Astrid Lindgren'),
-        ('Richard', 'Scarry', 'Richard Scarry');
+values ('Joanne', 'Rowling', 'J. K. Rowling'),
+       ('Stephen', 'King', 'Stephen King'),
+       ('Clive', 'Lewis', 'C. S. Lewis'),
+       ('Hajime', 'Isayama', 'Hajime Isayama'),
+       ('Edgar', 'Burroughs', 'Edgar Rice Burroughs'),
+       ('Lewis', 'Carroll', 'Lewis Carroll'),
+       ('Astrid', 'Lindgren', 'Astrid Lindgren'),
+       ('Richard', 'Scarry', 'Richard Scarry');
 
 
 
