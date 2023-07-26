4b6cf1f505bd4628a2b0c796f726f25739547ad4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-26 23:21:07 +0300

    add solution SQL and Hibernate task 0629

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0629/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0629/query.sql
@@ -1,14 +1,13 @@
 CREATE TABLE `event`
 (
-    `id`        INT         not null    auto_increment,
-    `date`      DATE        not null,
-    `user_id`   INT         not null,
-    `type`      VARCHAR(20) not null,
-    `status`    VARCHAR(10) not null,
-    PRIMARY KEY(id),
+    `id`      INT         not null auto_increment,
+    `date`    DATE        not null,
+    `user_id` INT         not null,
+    `type`    VARCHAR(20) not null,
+    `status`  VARCHAR(10) not null,
+    PRIMARY KEY (id),
     foreign key (user_id)
-        references users(id)
-
+        references users (id)
 );
 
 insert into event (id, date, user_id, type, status)
