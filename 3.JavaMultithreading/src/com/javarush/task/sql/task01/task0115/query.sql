75a8133750c1d22d1c78b7fd4f3ad2391645c907
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-28 10:30:46 +0300

    add solution SQL and Hibernate task 0115

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0115/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0115/query.sql
@@ -1,2 +1,4 @@
-SELECT id, salary, department, name FROM employee
-  WHERE salary < 5000 AND department = 'dev';
+SELECT id, salary, department, name
+FROM employee
+WHERE salary < 5000
+  AND department = 'dev';
