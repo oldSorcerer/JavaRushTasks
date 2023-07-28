4da416b21dd0e423bd4aeff858b8cc092dfd145a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-28 10:34:01 +0300

    add solution SQL and Hibernate task 0143

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0143/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0143/query.sql
@@ -1,2 +1,4 @@
-SELECT id, identifier, description FROM parts
-ORDER BY identifier ASC, required DESC, description DESC LIMIT 7;
+SELECT id, identifier, description
+FROM parts
+ORDER BY identifier ASC, required DESC, description DESC
+LIMIT 7;
