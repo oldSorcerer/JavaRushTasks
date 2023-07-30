1a4c1b1dc06107a54a17bd981bf3cac80bcecf37
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-31 01:04:34 +0300

    add solution SQL and Hibernate task 0215

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0215/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0215/query.sql
@@ -1,9 +1,9 @@
 select
-case number_of_season
-    when 1 then 'winter'
-    when 2 then 'spring'
-    when 3 then 'summer'
-    when 4 then 'autumn'
-    else 'unknown'
-end
+    case number_of_season
+        when 1 then 'winter'
+        when 2 then 'spring'
+        when 3 then 'summer'
+        when 4 then 'autumn'
+        else 'unknown'
+    end
 from months
