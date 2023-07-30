fe10e05edbb8e4b5eed0386153a274db1ab64820
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-31 01:04:48 +0300

    add solution SQL and Hibernate task 0217

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0217/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0217/query.sql
@@ -1,9 +1,9 @@
 select
-case
-    when euro > 5 then 'best'
-    when euro = 5 then 'good'
-    else 'bad'
-end
+    case
+           when euro > 5 then 'best'
+           when euro = 5 then 'good'
+           else 'bad'
+    end
 from cars
 where prod_year > 2020
 order by price
