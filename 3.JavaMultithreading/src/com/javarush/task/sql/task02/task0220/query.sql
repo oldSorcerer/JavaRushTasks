1ed9a8cb35e49d69e97b60ed11084d5dc097fc9d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-31 01:05:11 +0300

    add solution SQL and Hibernate task 0220

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0220/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0220/query.sql
@@ -1,7 +1,7 @@
 select distinct name,
-    case prod_year
-        when 2020 then 'new'
-        when 2021 then 'newer'
-        when 2022 then 'even newer'
-    end
+                case prod_year
+                    when 2020 then 'new'
+                    when 2021 then 'newer'
+                    when 2022 then 'even newer'
+                    end
 from cars
