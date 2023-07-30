b1072eedd9bbcb2b8f2146608bd0f340c1bee872
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-07-31 01:04:27 +0300

    add solution SQL and Hibernate task 0214

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0214/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0214/query.sql
@@ -1,7 +1,7 @@
 select
-case
-    when experience < 1 then 'junior'
-    when experience < 3 then 'middle'
-    when experience < 5 then 'senior'
-end
+    case
+        when experience < 1 then 'junior'
+        when experience < 3 then 'middle'
+        when experience < 5 then 'senior'
+    end
 from developers
