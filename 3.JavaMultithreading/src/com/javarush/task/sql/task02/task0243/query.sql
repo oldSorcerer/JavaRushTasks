d2dd944e62dc2dc292e38741c654502b26a5ca6a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-01 10:22:19 +0300

    add solution SQL and Hibernate task 0243

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0243/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0243/query.sql
@@ -1,6 +1,5 @@
-select
-    year(prod_date) as prod_year,
-    month(prod_date) as prod_month,
-    count(*)
+select year(prod_date)  as prod_year,
+       month(prod_date) as prod_month,
+       count(*)
 from cars
 group by prod_year, prod_month
