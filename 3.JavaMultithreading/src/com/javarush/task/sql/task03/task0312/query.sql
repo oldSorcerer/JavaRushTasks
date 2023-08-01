871215523a53c37225676ff3e50eced59b5dc8bf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-01 10:25:08 +0300

    add solution SQL and Hibernate task 0312

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0312/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0312/query.sql
@@ -1,6 +1,6 @@
-select
-top_retailers.rank as company_rank,
-top_retailers.name as company_name,
-suppliers.annual_revenue_billions as supplier_revenue
-from top_retailers, suppliers
+select top_retailers.rank                as company_rank,
+       top_retailers.name                as company_name,
+       suppliers.annual_revenue_billions as supplier_revenue
+from top_retailers,
+     suppliers
 where suppliers.annual_revenue_billions > 25
