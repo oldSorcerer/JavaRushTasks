b948a1a7be80d4d85431dec60da1c905360c33e2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-01 10:25:00 +0300

    add solution SQL and Hibernate task 0311

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0311/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0311/query.sql
@@ -1,5 +1,5 @@
-select
-top_retailers.name as retailer,
-top_retailers.hq_location as retailer_hq,
-suppliers.country as supplier_country
-from top_retailers, suppliers
+select top_retailers.name        as retailer,
+       top_retailers.hq_location as retailer_hq,
+       suppliers.country         as supplier_country
+from top_retailers,
+     suppliers
