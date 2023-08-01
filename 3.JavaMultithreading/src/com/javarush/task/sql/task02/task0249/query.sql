7dea0a1740d316dc8f70789beabab912338d11c8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-01 10:22:41 +0300

    add solution SQL and Hibernate task 0249

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0249/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0249/query.sql
@@ -1,4 +1,5 @@
 select department, position, count(*) as total
 from employee
 group by department, position
-having  position = 'backend developer' and total = 1
+having position = 'backend developer'
+   and total = 1
