7c6bb43aa604b5a63426e8e5a411fdcb18c715bb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-01 10:25:54 +0300

    add solution SQL and Hibernate task 0316

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0316/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0316/query.sql
@@ -1,6 +1,6 @@
-select
-retailer.name as ret_name,
-retailer.hq_location as ret_location,
-supplier.name as sup_name
-from top_retailers as retailer, suppliers as supplier
+select retailer.name        as ret_name,
+       retailer.hq_location as ret_location,
+       supplier.name        as sup_name
+from top_retailers as retailer,
+     suppliers as supplier
 where retailer.annual_revenue_billions > 100
