fa64cf59e69e6ad3c3852e27ef546dbf0d491375
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:08:58 +0300

    add solution SQL and Hibernate task 0320

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0320/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0320/query.sql
@@ -1,9 +1,8 @@
-select
-retailer.ret_name,
-retailer.ret_revenue,
-supplier.sup_name,
-supplier.sup_revenue
+select retailer.ret_name,
+       retailer.ret_revenue,
+       supplier.sup_name,
+       supplier.sup_revenue
 from top_retailers as retailer
-join suppliers as supplier
-on retailer.ret_revenue = supplier.sup_revenue
+         join suppliers as supplier
+              on retailer.ret_revenue = supplier.sup_revenue
 where supplier.sup_revenue > 50
