6e49ff9b15994f4c811fd333cf3274f4edd23377
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:12:03 +0300

    add solution SQL and Hibernate task 0345

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0345/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0345/query.sql
@@ -1,5 +1,7 @@
-select last_name from film_directors
-left join (select grossed, director_id from films
-where grossed > 100 ) as film
-on film_directors.id = film.director_id
+select last_name
+from film_directors
+         left join (select grossed, director_id
+                    from films
+                    where grossed > 100) as film
+                   on film_directors.id = film.director_id
 where film_directors.country = 'UK'
