814b6c64ed406c348d87ed9a08fa6ac4dbd852c7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:09:27 +0300

    add solution SQL and Hibernate task 0324

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0324/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0324/query.sql
@@ -1,7 +1,6 @@
-select
-c.customer_id,
-o.order_id
+select c.customer_id,
+       o.order_id
 from customers as c
-left join orders as o
-on c.customer_id = o.customer_id
+         left join orders as o
+                   on c.customer_id = o.customer_id
 where o.shipped_date is null
