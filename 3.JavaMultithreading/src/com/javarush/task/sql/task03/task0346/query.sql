292ed96816fb5e8b8484a43d6eeb6510f7228f38
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:12:10 +0300

    add solution SQL and Hibernate task 0346

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
@@ -1,5 +1,6 @@
-select * from films
-join
-(select year_born, id from film_directors where film_directors.year_born < 1940) as director
-on director.id = films.director_id
+select *
+from films
+         join
+     (select year_born, id from film_directors where film_directors.year_born < 1940) as director
+     on director.id = films.director_id
 where films.genre = 'sci-fi'
