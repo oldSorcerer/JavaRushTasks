aa7af90ae78f58f8c6484c307c3dda722c903367
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:10:07 +0300

    add solution SQL and Hibernate task 0329

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0329/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0329/query.sql
@@ -1,6 +1,5 @@
-select
-c.customer_id,
-o.order_status
+select c.customer_id,
+       o.order_status
 from customers as c
-join orders as o
-on c.customer_id = o.customer_id
+         join orders as o
+              on c.customer_id = o.customer_id
