c5f99150e64afaf118b921b6de630a4b9ece214c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:10:35 +0300

    add solution SQL and Hibernate task 0333

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0333/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0333/query.sql
@@ -1,10 +1,9 @@
-select
-a.sex as author_sex,
-a.country as author_country,
-b.copies_sold_millions as books_sold
+select a.sex                  as author_sex,
+       a.country              as author_country,
+       b.copies_sold_millions as books_sold
 from authors as a
-join books as b
-on a.author_id = b.author_id
+         join books as b
+              on a.author_id = b.author_id
 where b.copies_sold_millions > 10
 group by a.sex, a.country, b.copies_sold_millions
 order by b.copies_sold_millions desc
