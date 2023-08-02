ca301b9d7dfb41ea5f3d799376d34ae8e829c0c3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:10:42 +0300

    add solution SQL and Hibernate task 0334

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0334/query.sql
@@ -1,10 +1,9 @@
-select
-author.last_name as author,
-author.country as author_country,
-book.genre as book_genre
+select author.last_name as author,
+       author.country   as author_country,
+       book.genre       as book_genre
 from authors as author
-left join books as book
-on author.id = book.author_id
+         left join books as book
+                   on author.id = book.author_id
 where book.genre = 'fantasy'
 group by author.last_name, author.country, book.genre
 order by author.country asc
