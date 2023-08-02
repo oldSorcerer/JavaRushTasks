adeb82ee95981685672f34ab01ca706f24656178
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-03 01:12:16 +0300

    add solution SQL and Hibernate task 0347

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0347/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0347/query.sql
@@ -1,6 +1,7 @@
-select * from film_directors
-right join
-(select title, director_id from films where films.year_released > 1990) as film
-on film_directors.id = film.director_id
+select *
+from film_directors
+         right join
+         (select title, director_id from films where films.year_released > 1990) as film
+         on film_directors.id = film.director_id
 where film_directors.country = 'USA'
 limit 5
