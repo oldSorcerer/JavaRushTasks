2c7feaf9fff9f63f77173f82e19976f7987d25af
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-04 11:30:26 +0300

    add solution SQL and Hibernate task 0432

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0432/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0432/query.sql
@@ -1,5 +1,7 @@
-select monthname(date), count(*) from event
-where status = 'ERROR' or status = 'FAILED'
+select monthname(date), count(*)
+from event
+where status = 'ERROR'
+   or status = 'FAILED'
 group by monthname(date)
 order by count(*) desc
 limit 1
