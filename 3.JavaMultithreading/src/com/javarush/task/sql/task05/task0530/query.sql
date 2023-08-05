333452b34a4c6f0a4f46073c6124d7f1f52081a0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-05 10:10:42 +0300

    add solution SQL and Hibernate task 0530

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
@@ -1,5 +1,5 @@
 select name,
-       year(discovery_date) as d_year,
+       year(discovery_date)      as d_year,
        monthname(discovery_date) as d_month,
-       dayname(discovery_date) as d_day
+       dayname(discovery_date)   as d_day
 from object
