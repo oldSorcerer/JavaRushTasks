eef1067f3b71c2528d2d989c79c362756d00a39e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-10 22:42:13 +0300

    add solution Collections task 3310 Shortener 1/16

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+
+    private Long id;
+    private String string;
+
+    public Long getId(String string) {
+        return 0L;
+    }
+
+    public String getString(Long id) {
+        return "";
+    }
+}
