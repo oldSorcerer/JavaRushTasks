db5fb83f505a063d9c00b689fae1cb8080575d37
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-11 18:18:23 +0300

    add solution Collections task 3310 Shortener 4/16

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
@@ -1,8 +1,14 @@
 package com.javarush.task.task33.task3310;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class Helper {
     public static String generateRandomString() {
-        return "";
+        SecureRandom random = new SecureRandom();
+        BigInteger integer = new BigInteger(130, random);
+
+        return integer.toString(32);
     }
 
     public static void printMessage(String message) {
