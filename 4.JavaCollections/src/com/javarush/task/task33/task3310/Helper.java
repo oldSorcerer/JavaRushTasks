5048afa07f1da3b765607581969c8d21864c4a31
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-11 12:11:22 +0300

    add solution Collections task 3310 Shortener 4/16

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+public class Helper {
+    public static String generateRandomString() {
+        return "";
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
