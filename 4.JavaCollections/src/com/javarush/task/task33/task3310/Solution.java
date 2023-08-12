cb2ff1d377cdf73cb729de51a46811894276967f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-12 10:59:02 +0300

    add solution Collections task 3310 Shortener 6/16

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,7 +1,24 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Solution {
     public static void main(String[] args) {
+
+    }
+
+    public Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        return strings.stream().map(shortener::getId).collect(Collectors.toSet());
+    }
+
+    public Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+       return null;
+    }
+
+    public void testStrategy(StorageStrategy strategy, long elementsNumber) {
 
     }
 }
