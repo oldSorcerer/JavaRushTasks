3fe857db1c24e97ae74babe6f24b249cc10fc7e8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-19 22:40:48 +0300

    add solution Collections task 3714

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task37.task3714;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Древний Рим
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input a roman number to be converted to decimal: ");
+        String romanString = bufferedReader.readLine();
+        System.out.println("Conversion result equals " + romanToInteger(romanString));
+    }
+
+    public static int romanToInteger(String s) {
+        return 0;
+    }
+}
