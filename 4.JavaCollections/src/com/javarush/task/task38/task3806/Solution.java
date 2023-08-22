6642c942106381807a74dc7e3edc8a0c021b366e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-22 08:23:00 +0300

    add solution Collections task 3806

--- 4.JavaCollections/src/com/javarush/task/task38/task3806/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3806/Solution.java
@@ -10,43 +10,24 @@ import java.io.IOException;
 
 public class Solution {
     public void printFile1() throws IOException {
-        FileInputStream fileInputStream = null;
 
-        try {
-            fileInputStream = new FileInputStream("file.txt");
-
+        try (FileInputStream fileInputStream = new FileInputStream("file.txt")) {
             int data = fileInputStream.read();
             while (data != -1) {
                 System.out.println(data);
                 data = fileInputStream.read();
             }
-        } finally {
-            if (fileInputStream != null) {
-                fileInputStream.close();
-            }
         }
     }
 
     public void printFile2() throws IOException {
-        FileInputStream fileInputStream = null;
-        BufferedInputStream bufferedInputStream = null;
-        try {
-            fileInputStream = new FileInputStream("file.txt");
-            bufferedInputStream = new BufferedInputStream(fileInputStream);
-
+        try (FileInputStream fileInputStream = new FileInputStream("file.txt");
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
 
             int data = bufferedInputStream.read();
             while (data != -1) {
                 System.out.println(data);
                 data = bufferedInputStream.read();
-            }
-        } finally {
-            if (fileInputStream != null) {
-                fileInputStream.close();
-            }
-
-            if (bufferedInputStream != null) {
-                bufferedInputStream.close();
             }
         }
     }
