efaa6d209d7c81c9636997114bd3bb0b01a0b94a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-24 09:59:20 +0300

    add solution University Professional task 4204

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4204/Key.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4204/Key.java
@@ -3,8 +3,8 @@ package com.javarush.task.jdk13.task42.task4204;
 import java.util.Random;
 
 public class Key {
-    private static Random random = new Random();
-    private int intField = random.nextInt(5) + 1;
+    private static final Random random = new Random();
+    private final int intField = random.nextInt(5) + 1;
 
     public int getIntField() {
         return intField;
