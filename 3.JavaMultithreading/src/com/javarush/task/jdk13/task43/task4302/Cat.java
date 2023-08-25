61d60db8b434a481efd3624cb318a98f76cd5336
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-25 12:36:55 +0300

    add solution University Professional task 4302

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4302/Cat.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4302/Cat.java
@@ -4,7 +4,7 @@ package com.javarush.task.jdk13.task43.task4302;
 Сравниваем котов
 */
 
-//import org.apache.commons.lang3.builder.*;
+import org.apache.commons.lang3.builder.*;
 
 public class Cat {
 
@@ -20,18 +20,18 @@ public class Cat {
         this.breed = breed;
     }
 
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (this == obj) return true;
-//        if (obj == null || getClass() != obj.getClass()) return false;
-//
-//        return EqualsBuilder.reflectionEquals(this, obj, "name", "weight");
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        напишите тут ваш код
-//
-//        return null;
-//    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        return EqualsBuilder.reflectionEquals(this, obj, "name", "weight");
+    }
+
+    @Override
+    public int hashCode() {
+        //напишите тут ваш код
+
+        return 0;
+    }
 }
