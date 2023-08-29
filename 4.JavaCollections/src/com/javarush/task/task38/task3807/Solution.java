ddf8dab127150c8605671389a772a8e6cb1c71cf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-08-30 01:43:00 +0300

    add solution Collections task 3807

--- 4.JavaCollections/src/com/javarush/task/task38/task3807/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3807/Solution.java
@@ -3,24 +3,32 @@ package com.javarush.task.task38.task3807;
 /* 
 Предопределенные типы аннотаций (Predefined Annotation Types)
 */
-
+@Deprecated
 public class Solution {
 
+    @Deprecated
     private String[] arguments;
 
+    @Deprecated
+    @SafeVarargs
     public Solution(String... arguments) {
         this.arguments = arguments;
     }
 
+    @Deprecated
     public void voidMethod() throws Exception {
     }
 
+    @Deprecated
     public static void main(String[] args) throws Exception {
         new Solution().new SubSolution().voidMethod();
     }
 
+    @Deprecated
     class SubSolution extends Solution {
 
+        @Deprecated
+        @Override
         public void voidMethod() throws Exception {
             super.voidMethod();
         }
