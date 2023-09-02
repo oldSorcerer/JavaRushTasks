e0128acef156fd62dbf41847e87e7204b6095a82
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-09-02 10:02:25 +0300

    add solution from JavaCollections task 3710

--- 4.JavaCollections/src/com/javarush/task/task37/task3710/decorators/ShapeDecorator.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3710/decorators/ShapeDecorator.java
@@ -3,6 +3,7 @@ package com.javarush.task.task37.task3710.decorators;
 import com.javarush.task.task37.task3710.shapes.Shape;
 
 public abstract class ShapeDecorator implements Shape {
+
     protected Shape decoratedShape;
 
     public ShapeDecorator(Shape decoratedShape) {
