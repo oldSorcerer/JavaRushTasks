dbc8b5c81c660bb5d6a8e6b468d8b7363c20bc84
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-09-05 18:15:29 +0300

    add solution University Core task 3803

--- 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.jdk13.task38.task3803;
 Обработка аннотаций
 */
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         printFullyQualifiedNames(Solution.class);
@@ -14,10 +16,25 @@ public class Solution {
     }
 
     public static <T> boolean printFullyQualifiedNames(Class<T> c) {
-        return true;
+
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            Arrays.stream(annotation.fullyQualifiedNames()).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public static <T> boolean printValues(Class<T> c) {
-        return true;
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            Arrays.stream(annotation.value())
+                    .map(Class::getSimpleName).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
