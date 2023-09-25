f2e03c1bf71ea97557c1b5fb20c31c83578dc350
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-09-25 18:36:46 +0300

    add solution University Algorithm task 5404

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task54/task5404/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.jdk13.task54.task5404;
+
+/* 
+Стек или очередь?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(isNestedProperly("<{[(<>)(<>)]}>"));
+        System.out.println(isNestedProperly("{[()<>]})"));
+        System.out.println(isNestedProperly("){<()()>}"));
+        System.out.println(isNestedProperly("<[()()]"));
+    }
+
+    public static boolean isNestedProperly(String nestedBrackets) {
+        //напишите тут ваш код
+
+        return false;
+    }
+}
