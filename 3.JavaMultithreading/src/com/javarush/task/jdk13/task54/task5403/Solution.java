e381bc52d618d9bad8d239daf657644289bc0f6f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-09-25 18:36:38 +0300

    add solution University Algorithm task 5403

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task54/task5403/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.jdk13.task54.task5403;
+
+/* 
+Сортировка рулит
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getMinPairProduct(new int[]{-3, 1, 2, -2, 5, 6}));
+    }
+
+    public static int getMinPairProduct(int[] array) {
+        //напишите тут ваш код
+
+        return 0;
+    }
+}
