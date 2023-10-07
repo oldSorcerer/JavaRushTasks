8f48aaf887a2739253dfadd5b549c29e9326f2df
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-07 13:52:33 +0300

    add solution SQL and Hibernate task 0212

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0212/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0212/query.sql
@@ -1,5 +1,5 @@
 select
 -- if((position = 'manager' and salary > 10000) or (position != 'manager' and salary > 5000), 'good', 'bad')
-if(position = 'manager', if(salary > 10000, 'good', 'bad'), if(salary > 5000, 'good', 'bad')
+if(position = 'manager', if(salary > 10000, 'good', 'bad'), if(salary > 5000, 'good', 'bad'))
    from employee
    where city = 'London';
