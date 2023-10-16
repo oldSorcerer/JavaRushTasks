804fcbff9c8a8654f93bc824951b1b0e3c3d15b0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-16 12:32:47 +0300

    add solution SQL and Hibernate task 0315

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
@@ -1,5 +1,5 @@
 select ret.hq_location             as ret_location,
-       sup.annual_revenue_billions as sup_revenue,
+       sup.annual_revenue_billions as sup_revenue
 from top_retailers as ret,
      suppliers as sup
-where ret.hq_location != 'USA'
+where ret.hq_location != 'USA';
