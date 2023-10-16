a6813eac3a2b2d7d30835350df87ed42c345c12f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-16 12:32:42 +0300

    add solution SQL and Hibernate task 0314

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0314/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0314/query.sql
@@ -2,4 +2,4 @@ select ret.name                    as ret_name,
        ret.annual_revenue_billions as ret_revenue,
        supplier.name               as sup_name
 from top_retailers as ret,
-     suppliers as supplier
+     suppliers as supplier;
