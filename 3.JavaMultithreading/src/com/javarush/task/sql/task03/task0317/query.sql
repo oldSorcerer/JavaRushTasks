a1fc2d894a94ebfa1c332a158bfd84be1b1a60d3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-16 12:32:57 +0300

    add solution SQL and Hibernate task 0317

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0317/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0317/query.sql
@@ -1,4 +1,4 @@
 select *
 from top_retailers
          join suppliers
-              on top_retailers.ret_location = suppliers.sup_country
+              on top_retailers.ret_location = suppliers.sup_country;
