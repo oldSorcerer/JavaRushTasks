bc11fc646bdfd57b9e390c73663343db839e30a0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-18 00:40:36 +0300

    add solution SQL and Hibernate task 0343

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0343/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0343/query.sql
@@ -1,4 +1,4 @@
 select full_name
 from film_directors
          left join (select title, director_id from films where films.genre = 'comedy') as f
-                   on film_directors.id = f.director_id
+                   on film_directors.id = f.director_id;
