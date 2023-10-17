6785b7d16d73980eea4211386d74517da2943e1b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-17 12:36:00 +0300

    add solution SQL and Hibernate task 0323

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0323/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0323/query.sql
@@ -2,4 +2,4 @@ select customers.last_name as surname,
        orders.order_id
 from customers
          left join orders
-                   on customers.customer_id = orders.customer_id
+                   on customers.customer_id = orders.customer_id;
