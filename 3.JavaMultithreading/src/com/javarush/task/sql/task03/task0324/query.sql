f78a1299a4a94672dd06a796e1ccc5070895315d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-17 12:36:05 +0300

    add solution SQL and Hibernate task 0324

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0324/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0324/query.sql
@@ -3,4 +3,4 @@ select c.customer_id,
 from customers as c
          left join orders as o
                    on c.customer_id = o.customer_id
-where o.shipped_date is null
+where o.shipped_date is null;
