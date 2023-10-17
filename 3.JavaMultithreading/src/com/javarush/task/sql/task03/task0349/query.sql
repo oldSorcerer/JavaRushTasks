877645f4dcc05109a18417639a8cd4ab697d2ccf
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-18 00:41:07 +0300

    add solution SQL and Hibernate task 0349

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0349/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0349/query.sql
@@ -2,4 +2,4 @@ with tempTable (averageDOB) as (select avg(year_born) from film_directors)
 select id, full_name, year_born
 from film_directors,
      tempTable
-where film_directors.year_born < tempTable.averageDOB
+where film_directors.year_born < tempTable.averageDOB;
