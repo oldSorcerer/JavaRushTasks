3e727ccdcab4abc206193d371b8c30a09fc01559
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-17 12:36:36 +0300

    add solution SQL and Hibernate task 0330

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0330/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0330/query.sql
@@ -2,4 +2,4 @@ select customers.zip_code  as cust_zip_code,
        orders.order_status as status
 from customers
          left join orders
-                   on customers.customer_id = orders.customer_id
+                   on customers.customer_id = orders.customer_id;
