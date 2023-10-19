2b343bfabdc89eca18c758e425c023552710e5b0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-19 10:23:12 +0300

    add solution SQL and Hibernate task 0112

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0112/init_data.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS 'employee';
+
+CREATE TABLE employee
+(
+    `id`         INT          not null auto_increment,
+    `name`       VARCHAR(100) not null,
+    `salary`     INT          not null,
+    `department` VARCHAR(200) null
+);
+
+insert into employee (id, name, salary, department)
+values (1, 'Ravshan', 199, 'support'),
+       (2, 'Jackie', 779, 'hr'),
+       (3, 'Mohamed', 2333, 'development'),
+       (4, 'Alice', 10000, 'management'),
+       (5, 'Jack', 3500, 'development'),
+       (7, 'Mary', 1595, 'qa'),
+       (8, 'Anna', 1593, 'hr'),
+       (9, 'Kostik', 8765, 'development'),
+       (10, 'Dzhumshut', 201, 'support'),
+       (11, 'John', 1999, 'management'),
+       (12, 'Sarah', 15000, 'management')
+;
