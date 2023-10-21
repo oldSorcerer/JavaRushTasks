97b4e244a69c1c06b2c3d822ead32a097297e24c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-21 17:04:24 +0300

    add solution Syntax task 1107

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -9,16 +9,16 @@ public class Car {
     public Engine engine;
 
     class Engine {
+
         private boolean isRunning;
 
         public void start() {
             isRunning = true;
         }
 
-        public  void stop() {
+        public void stop() {
             isRunning = false;
         }
-
     }
 }
 
