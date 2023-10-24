bf06fbede1d7e936fd71eed4859645f67fc54151
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-10-24 22:56:07 +0300

    add solution SQL and Hibernate task 0416

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0416/init_data.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS 'math';
+
+CREATE TABLE `math`
+(
+       `a` INT not null
+);
+
+insert into `math` (a)
+values (2),
+       (9),
+       (17),
+       (33),
+       (101),
+       (123),
+       (72)
+;
