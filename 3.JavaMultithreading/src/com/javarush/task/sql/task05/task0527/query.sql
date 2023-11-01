c6250032be7460dc99bbc2e8289613b2d9a5517d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-01 11:12:07 +0300

    add solution SQL and Hibernate task 0527

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0527/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0527/query.sql
@@ -1,4 +1,4 @@
 select distinct book.title
 from book
          left join author on author.id = book.author_id
-where author.full_name = 'Edgar Allan Poe'
+where author.full_name = 'Edgar Allan Poe';
