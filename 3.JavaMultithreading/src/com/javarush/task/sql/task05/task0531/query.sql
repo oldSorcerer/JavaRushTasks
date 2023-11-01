58fa94bb8585599bfc28107647622c03d863e4a0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-01 11:13:09 +0300

    add solution SQL and Hibernate task 0531

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0531/query.sql
@@ -5,7 +5,7 @@ where year(discovery_date) >= 2010
   and year(discovery_date) <= 2020
   and month(discovery_date) <= 12
 
-/*
-select *
-from object
-where discovery_date between '2010-01-01' and '2020-12-31'; */
+
+--select *
+--from object
+--where discovery_date between '2010-01-01' and '2020-12-31';
