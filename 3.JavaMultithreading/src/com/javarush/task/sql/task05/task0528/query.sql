69ea83646b484c7fb328a1cfd39997ea180562ec
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-01 11:12:22 +0300

    add solution SQL and Hibernate task 0528

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
@@ -4,11 +4,10 @@ from publisher
          left join author on author.id = book.author_id
 where author.full_name = 'Mark Twain'
 
-/*
-select publisher.name
-from book
-         left join author on author.id = book.author_id
-         left join publisher on publisher.id = book.publisher_id
-where author.full_name = 'Mark Twain'
-group by publisher.name;
- */
+
+--select publisher.name
+--from book
+--         left join author on author.id = book.author_id
+--         left join publisher on publisher.id = book.publisher_id
+--where author.full_name = 'Mark Twain'
+--group by publisher.name;
