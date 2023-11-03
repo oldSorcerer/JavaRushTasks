61750c6518a1b1c58a21d176a0b78dd303960974
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-03 13:48:30 +0300

    add solution SQL and Hibernate task 0609

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
@@ -1,4 +1,4 @@
 insert into sale_addresses (street, city, state, zip_code, country)
 select street, city, state, zip_code, country
 from customers
-where country not like '%Testostan%'
+where country not like '%Testostan%';
