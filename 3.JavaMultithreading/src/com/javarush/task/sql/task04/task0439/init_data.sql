05185e2b5d0523b8600a0aada72adc903c1a34e8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-18 10:24:43 +0300

    add solution SQL and Hibernate task 0439

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0439/init_data.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0439/init_data.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS 'employee';
+DROP TABLE IF EXISTS employee;
 
-CREATE TABLE `employee`
+CREATE TABLE employee
 (
-       `id`            INT          not null auto_increment,
+       `id`            INT          not null auto_increment primary key ,
        `name`          VARCHAR(100) not null,
        `date_of_birth` DATE         not null,
        `position`      VARCHAR(100) not null,
