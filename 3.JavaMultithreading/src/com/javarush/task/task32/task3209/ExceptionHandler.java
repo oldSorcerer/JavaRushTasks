7dd99858840f52e0af9e1727ec6833ff056bedda
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-22 22:27:02 +0300

    add solution University Core task 3509

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/ExceptionHandler.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/ExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task32.task3209;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionHandler {
 
     public static void log(Exception e) {
