75adb7e6e69b9d85b092db9e1b9836bba6d644f0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-22 22:30:39 +0300

    add solution University Core task 3509

--- 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
@@ -15,38 +15,38 @@ public class Solution {
     }
 
     public static Double sum(List<? extends Number> list) {
-        double result = 0.0;
-        for (Number numb : list) {
-            result += numb.doubleValue();
-        }
-        return result;
-//        return list.stream().mapToDouble(Number::doubleValue).sum();
+//        double result = 0.0;
+//        for (Number numb : list) {
+//            result += numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).sum();
     }
 
     public static Double multiply(List<? extends Number> list) {
-        double result = 1.0;
-        for (Number numb : list) {
-            result *= numb.doubleValue();
-        }
-        return result;
-//        return list.stream().mapToDouble(Number::doubleValue).reduce(1.0, (a, b) -> a * b);
+//        double result = 1.0;
+//        for (Number numb : list) {
+//            result *= numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).reduce(1.0, (a, b) -> a * b);
     }
 
     public static String concat(List<?> list) {
-        StringBuilder builder = new StringBuilder();
-        for (Object obj : list) {
-            builder.append(obj);
-        }
-        return builder.toString();
-//        return list.stream().map(String::valueOf).collect(Collectors.joining());
+//        StringBuilder builder = new StringBuilder();
+//        for (Object obj : list) {
+//            builder.append(obj);
+//        }
+//        return builder.toString();
+        return list.stream().map(String::valueOf).collect(Collectors.joining());
     }
 
     public static <T> List<T> combine(List<? extends Collection<T>> list) {
-        List<T> result = new ArrayList<>();
-        for (Collection<T> collection : list) {
-            result.addAll(collection);
-        }
-        return result;
-//        return list.stream().flatMap(Collection::stream).collect(Collectors.toList());
+//        List<T> result = new ArrayList<>();
+//        for (Collection<T> collection : list) {
+//            result.addAll(collection);
+//        }
+//        return result;
+        return list.stream().flatMap(Collection::stream).collect(Collectors.toList());
     }
 }
