1de8bf74fd78fdd844b01ab8ff08ac17217e1d0e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-11-26 13:18:01 +0300

    add solution SQL and Hibernate task 0108

--- 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0108/init_data.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0108/init_data.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS owner;
 
 CREATE TABLE owner
 (
-    `id`         INT          not null auto_increment primary key,
+    `id`         INT          not null auto_increment primary key ,
     `first_name` VARCHAR(100) not null,
     `last_name`  VARCHAR(100) not null,
     `nick_name`  VARCHAR(100) not null
@@ -12,7 +12,7 @@ insert into owner (id, first_name, last_name, nick_name)
 values (5, 'Jack', 'Sparrow', 'pirate'),
        (11, 'Jeffrey', 'Lebowski', 'dude'),
        (7, 'James', 'Bond', 'agent'),
-       (1, 'Vito', 'Corleone', 'godfather')
-       (100, 'Arnold', 'Schwarzenegger', 'terminator')
-       (1, 'Hannibal', 'Lecter', 'cannibal')
+       (1, 'Vito', 'Corleone', 'godfather'),
+       (100, 'Arnold', 'Schwarzenegger', 'terminator'),
+       (14, 'Hannibal', 'Lecter', 'cannibal')
 ;
