693e95db125ede0184eaf2c57949fcc18ec0d667
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-12-27 10:00:41 +0300

    add solution University Core task 2703

--- 2.JavaCore/src/com/javarush/task/jdk13/task27/task2703/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task27/task2703/Solution.java
@@ -14,7 +14,11 @@ public class Solution {
     }
 
     public void secondMethod() {
-        doSomething();
+        synchronized (lock) {
+            synchronized (this) {
+                doSomething();
+            }
+        }
     }
 
     private void doSomething() {
