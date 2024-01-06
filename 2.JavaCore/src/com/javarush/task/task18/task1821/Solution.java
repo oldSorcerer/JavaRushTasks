c121470075fd2a9e6eca9f136c0455faa0cd38c3
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-01-06 20:12:00 +0300

    add solution Core task 1821

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task18.task1821;
 
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /* 
 Встречаемость символов
@@ -13,9 +16,9 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         Map<Character, Integer> map = new TreeMap<>();
 
-        try (FileInputStream inputStream = new FileInputStream(args[0])) {
-            while (inputStream.available() > 0) {
-                char aChar = (char) inputStream.read();
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                char aChar = (char) reader.read();
                 if (!map.containsKey(aChar)) {
                     map.put(aChar, 1);
                 } else {
@@ -26,5 +29,9 @@ public class Solution {
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
+
+        Files.readString(Path.of(args[0])).chars().boxed()
+                .collect(Collectors.toMap(aChar -> (char) (aChar.intValue()), aChar -> 1, Integer::sum, TreeMap::new))
+                .forEach((key, value) -> System.out.println(key + " " + value));
     }
 }
