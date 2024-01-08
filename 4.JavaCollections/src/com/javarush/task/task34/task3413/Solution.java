a58897f864607babe700cccff39434fd08458d0b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-01-08 14:40:01 +0300

    add solution Collections task 3413

--- 4.JavaCollections/src/com/javarush/task/task34/task3413/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3413/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.task34.task3413;
 Кеш на основании SoftReference
 */
 
+import java.util.stream.LongStream;
+
 public class Solution {
     public static void main(String[] args) {
         SoftCache cache = new SoftCache();
@@ -11,5 +13,6 @@ public class Solution {
         for (long i = 0; i < 2_500_000; i++) {
             cache.put(i, new AnyObject(i, null, null));
         }
+        LongStream.range(0, 2_500_000).mapToObj(cache::get).forEach(System.out::println);
     }
 }
