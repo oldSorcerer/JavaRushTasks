18dd703d783651b71589e7c8cf82e8b2c0b2b001
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-01-17 13:49:02 +0300

    add solution University Core task 2804

--- 2.JavaCore/src/com/javarush/task/jdk13/task28/task2804/MyThread.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2804/MyThread.java
@@ -1,8 +1,30 @@
 package com.javarush.task.jdk13.task28.task2804;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MyThread extends Thread {
 
+    private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(9); //1
+
+    {
+        setPriority(ATOMIC_INTEGER.incrementAndGet() % 10 + 1);
+    }
+
+//    {
+//        int priority = ATOMIC_INTEGER.getAndIncrement();
+//        if (priority > MAX_PRIORITY) {
+//            priority = MIN_PRIORITY;
+//        }
+//        ATOMIC_INTEGER.set(priority);
+//        setPriority(ATOMIC_INTEGER.getAndIncrement());
+//
+//        if (getThreadGroup() != null && getThreadGroup().getMaxPriority() < priority) {
+//            setPriority(getThreadGroup().getMaxPriority());
+//        }
+//    }
+
     public MyThread() {
+        super();
     }
 
     public MyThread(Runnable target) {
@@ -31,5 +53,8 @@ public class MyThread extends Thread {
 
     public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
         super(group, target, name, stackSize);
+    }
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize, boolean inheritThreadLocals) {
+        super(group, target, name, stackSize, inheritThreadLocals);
     }
 }
