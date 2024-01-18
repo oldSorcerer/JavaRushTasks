9f4e2cb357efab420b368678a4f89f476585c614
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-01-18 16:32:08 +0300

    add solution University Professional task 4201

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4201/CompanyAccount.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4201/CompanyAccount.java
@@ -1,17 +1,19 @@
 package com.javarush.task.jdk13.task42.task4201;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CompanyAccount {
-    private Integer totalMoney;
+    private final AtomicInteger totalMoney;
 
     public CompanyAccount(int totalMoney) {
-        this.totalMoney = totalMoney;
+        this.totalMoney = new AtomicInteger(totalMoney);
     }
 
     public int getTotalMoney() {
-        return totalMoney;
+        return totalMoney.get();
     }
 
     public void addMoney(int money) {
-        totalMoney += money;
+        totalMoney.getAndAdd(money);
     }
 }
