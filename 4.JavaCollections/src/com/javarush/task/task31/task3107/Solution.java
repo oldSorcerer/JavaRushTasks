3130d7a232dcb0bd190f8b3e21373876cf8325d6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-01-24 19:06:47 +0300

    add solution Collections task 3107

--- 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -12,6 +12,15 @@ public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
+        try {
+
+//            Files.
+
+            fileData = new ConcreteFileData(true, true, true, true);
+        } catch (Exception e ) {
+            fileData = new NullFileData(e);
+        }
+
 
     }
 
