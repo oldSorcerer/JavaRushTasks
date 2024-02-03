ca3500e2b12e72520b435d12d33325255ad8d015
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-02-03 15:48:36 +0300

    add solution University Professional task 4208

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4208/MyQueue.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4208/MyQueue.java
@@ -4,6 +4,9 @@ import java.util.AbstractQueue;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /* 
 Потоки в очередь!
@@ -12,30 +15,53 @@ import java.util.List;
 public class MyQueue extends AbstractQueue<String> {
 
     private final List<String> values = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock writeLock = lock.writeLock();
+    private final Lock readLock = lock.readLock();
 
     @Override
     public Iterator<String> iterator() {
-        return values.listIterator();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
-        return values.size();
+        try {
+            readLock.lock();
+            return values.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public boolean offer(String o) {
-        values.add(o);
-        return true;
+        try {
+            writeLock.lock();
+            values.add(o);
+            return true;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     @Override
     public String poll() {
-        return values.size() > 0 ? values.remove(0) : null;
+        try {
+            writeLock.lock();
+            return !values.isEmpty() ? values.remove(0) : null;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     @Override
     public String peek() {
-        return values.size() > 0 ? values.get(0) : null;
+        try {
+            readLock.lock();
+            return !values.isEmpty() ? values.get(0) : null;
+        } finally {
+            readLock.unlock();
+        }
     }
 }
