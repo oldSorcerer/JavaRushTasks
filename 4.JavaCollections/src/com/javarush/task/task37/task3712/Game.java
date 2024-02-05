1608d34e54d6336238397a08d2247801bdb4a9c1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-02-05 10:42:33 +0300

    add solution Collections task 3712

--- 4.JavaCollections/src/com/javarush/task/task37/task3712/Game.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3712/Game.java
@@ -3,6 +3,15 @@ package com.javarush.task.task37.task3712;
 public abstract class Game {
 
     public void run() {
-
+        prepareForTheGame();
+        playGame();
+        congratulateWinner();
     }
+
+    public abstract void prepareForTheGame();
+
+    public abstract void playGame();
+
+    public abstract  void congratulateWinner();
+
 }
