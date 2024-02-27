3c41b0f85deed9d57b19a42d0729422ebe1e369e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-02-27 14:33:03 +0300

    add solution from JavaCollections task 3711

--- 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -11,10 +11,51 @@ import java.util.List;
 
 public class SearchFileVisitor extends SimpleFileVisitor<Path> {
 
+    private String partOfName;
+    private String partOfContent;
+    private int minSize;
+    private int maxSize;
+
+    private List<Path> foundFiles = new ArrayList<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+        if (partOfName == null && !file.getFileName().toString().contains(partOfContent)) {
+            return FileVisitResult.CONTINUE;
+        }
+
         byte[] content = Files.readAllBytes(file); // размер файла: content.length
 
+        if (partOfContent != null && !partOfContent.isEmpty()) {
+            String string = new String(content);
+            if (!string.contains(partOfContent)) {
+                return FileVisitResult.CONTINUE;
+            }
+        }
+
+
+
         return super.visitFile(file, attrs);
+    }
+
+    public void setPartOfName(String partOfName) {
+        this.partOfName = partOfName;
+    }
+
+    public void setPartOfContent(String partOfContent) {
+        this.partOfContent = partOfContent;
+    }
+
+    public void setMinSize(int minSize) {
+        this.minSize = minSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public List<Path> getFoundFiles() {
+        return foundFiles;
     }
 }
