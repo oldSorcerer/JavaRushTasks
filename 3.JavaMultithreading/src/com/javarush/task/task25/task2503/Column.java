d7846b251f39f190c621f36aa6c332ca711a3c59
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-03-12 14:10:54 +0300

    add solution Multithreading task 2503

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Column.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Column.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public enum Column implements Columnable {
+
     Customer("Customer"),
     BankName("Bank Name"),
     AccountNumber("Account Number"),
