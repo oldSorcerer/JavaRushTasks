b3e13cbac1943fd6b5f467201285daf9cd3726ce
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-04-05 08:48:24 +0300

    add solution Multithreading task 3513 2048 10/17

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -1,7 +1,17 @@
 package com.javarush.task.task35.task3513;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        Model model = new Model();
+        Controller controller = new Controller(model);
+        JFrame game = new JFrame();
+        game.setTitle("2048");
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game.setSize(450, 500);
+        game.setResizable(false);
+
 
     }
 }
