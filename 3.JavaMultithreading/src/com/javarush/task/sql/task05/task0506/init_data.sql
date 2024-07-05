5f45eedb62b16f5242156814559d8aa010ce84a2
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-06 01:05:22 +0300

    add solution SQL and Hibernate task 0506

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0506/init_data.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS publisher;
+
+CREATE TABLE publisher
+(
+    `id`   int NOT NULL PRIMARY KEY,
+    `name` varchar(100) DEFAULT NULL
+);
+
+INSERT INTO publisher
+VALUES (0, 'Фолио'),
+       (1, 'Азбука'),
+       (2, 'Фолио'),
+       (3, 'Wordsworth Editions');
