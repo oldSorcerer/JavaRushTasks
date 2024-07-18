be12b4ef6ca405c6f68afe34757a6009bc3bae90
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-19 00:56:58 +0300

    add solution Core task 1904

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task19.task1904;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -41,6 +42,15 @@ public class Solution {
 
             return new Person(split[1], split[2], split[0], date) ;
         }
+
+        public Person read1() throws IOException {
+            String string = fileScanner.nextLine();
+            String[] date = string.replaceAll("\\D", " ").trim().split(" ");
+            Calendar calendar = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]) - 1, Integer.parseInt(date[0]));
+            String[] strings = string.replaceAll("\\d", " ").trim().split(" ");
+            return new Person(strings[1], strings[2], strings[0], calendar.getTime());
+        }
+
 
         @Override
         public void close() throws IOException {
