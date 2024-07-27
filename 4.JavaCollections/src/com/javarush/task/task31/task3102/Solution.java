bce5f0c6bde48c0647b9b61aa16de9dafeb601f9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-07-27 23:16:41 +0300

    add solution Collections task 3102

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,28 +1,29 @@
 package com.javarush.task.task31.task3102;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
 
 /* 
 Находим все файлы
 */
 
 public class Solution {
-    private static final List<String> list = new ArrayList<>();
 
     public static List<String> getFileTree(String root) throws IOException {
-        DirectoryStream<Path> paths = Files.newDirectoryStream(Path.of(root));
-        for (Path path : paths) {
-            if (Files.isRegularFile(path)) {
-                list.add(path.toString());
-            } else if (Files.isDirectory(path)) {
-                getFileTree(path.toString());
+        List<String> list = new ArrayList<>();
+        Queue<Path> queue = new ArrayDeque<>();
+        queue.add(Paths.get(root));
+        while (!queue.isEmpty()) {
+            Path path = queue.poll();
+            try (DirectoryStream<Path> paths = Files.newDirectoryStream(path)) {
+                for (Path entry : paths) {
+                    if (Files.isRegularFile(entry)) {
+                        list.add(entry.toString());
+                    } else if (Files.isDirectory(entry)) {
+                        queue.add(entry);
+                    }
+                }
             }
         }
         return list;
@@ -31,5 +32,23 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         String path = "D:\\TheProjects\\IdeaProjects\\JavaRushTasks";
         getFileTree(path).forEach(System.out::println);
+        System.out.println(getFileTree(path).size());
+    }
+
+    public static List<String> getFileTreeRec(String root) {
+        List<String> list = new ArrayList<>();
+
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(root))) {
+            for (Path path : paths) {
+                if (Files.isRegularFile(path)) {
+                    list.add(path.toString());
+                } else if (Files.isDirectory(path)) {
+                    list.addAll(getFileTree(path.toString()));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 }
