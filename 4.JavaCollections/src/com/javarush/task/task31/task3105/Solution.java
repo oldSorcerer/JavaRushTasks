3813fab421042b87903f2ed6a4be6174e258ffd6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-08-05 17:22:06 +0300

    add solution Multithreading task 3105

--- 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -14,5 +14,20 @@ import java.util.zip.ZipOutputStream;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+
+        String fileName = args[0];
+        String zipFilePath = args[1];
+
+        ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath));
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ZipEntry zipEntry;
+        while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+            byte[] buffer = new byte[1024];
+            for (int length; (length = zipInputStream.read(buffer)) > 0; ) {
+                byteArrayOutputStream.write(buffer, 0, length);
+            }
+
+        }
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(fileName));
     }
 }
