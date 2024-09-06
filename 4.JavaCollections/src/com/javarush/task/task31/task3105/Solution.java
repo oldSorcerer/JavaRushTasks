95f522d4b7440df38115e7dcbc8c479e4958e0cd
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-06 15:59:26 +0300

    add solution Collections task 3105

--- 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -2,8 +2,10 @@ package com.javarush.task.task31.task3105;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -15,19 +17,39 @@ import java.util.zip.ZipOutputStream;
 public class Solution {
     public static void main(String[] args) throws IOException {
 
-        String fileName = args[0];
-        String zipFilePath = args[1];
+        File file = new File(args[0]);
 
-        ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath));
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ZipEntry zipEntry;
-        while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-            byte[] buffer = new byte[1024];
-            for (int length; (length = zipInputStream.read(buffer)) > 0; ) {
-                byteArrayOutputStream.write(buffer, 0, length);
+        Map<String, ByteArrayOutputStream> map = getStringByteArrayOutputStreamMap(args[1]);
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(args[1]))) {
+            zipOutputStream.putNextEntry(new ZipEntry("new/" + file.getName()));
+            Files.copy(file.toPath(), zipOutputStream);
+
+            for (Map.Entry<String, ByteArrayOutputStream> entry : map.entrySet()) {
+                if (!entry.getKey().equals("new/" + file.getName())) {
+                    ZipEntry zipEntry = new ZipEntry(entry.getKey());
+                    zipOutputStream.putNextEntry(zipEntry);
+                    zipOutputStream.write(entry.getValue().toByteArray());
+                    zipOutputStream.closeEntry();
+                }
             }
-
         }
-        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(fileName));
+    }
+
+    private static Map<String, ByteArrayOutputStream> getStringByteArrayOutputStreamMap(String zipFilePath) throws IOException {
+        Map<String, ByteArrayOutputStream> map = new HashMap<>();
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                for (int length; (length = zipInputStream.read(buffer)) > 0; ) {
+                    byteArrayOutputStream.write(buffer, 0, length);
+                }
+                map.put(zipEntry.getName(), byteArrayOutputStream);
+            }
+        }
+        return map;
     }
 }
