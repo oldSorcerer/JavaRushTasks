10a3c3a0654eba7a7a75011922bd4d2186901e0c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-10-27 18:51:24 +0300

    add solution Syntax task 1226

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1226/Core.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1226/Core.java
@@ -3,12 +3,12 @@ package com.javarush.task.pro.task12.task1226;
 public class Core {
     private final int coreNumber;
 
-    public int getCoreNumber() {
-        return coreNumber;
-    }
-
     public Core(int coreNumber) {
         this.coreNumber = coreNumber;
+    }
+
+    public int getCoreNumber() {
+        return coreNumber;
     }
 
     @Override
