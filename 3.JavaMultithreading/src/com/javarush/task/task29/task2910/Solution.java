105ca5da88d8d5351ad0276e718e8336718d8184
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-12-04 14:12:55 +0300

    add solution Multithreading task 2910

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Solution.java
@@ -1,5 +1,16 @@
+package com.javarush.task.task29.task2910;
 
-### ВСЕ РЕЗУЛЬТАТЫ:");
+/*
+Это архаичное слово "Биатлон"
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        printDelimiter();
+        game.startCompetition();
+        System.out.println("\n\nВСЕ РЕЗУЛЬТАТЫ:");
         game.printAllResults();
         printDelimiter();
         System.out.println("\n\nСПОРТСМЕНЫ С ТАКИМ ЖЕ ИТОГОВЫМ ВРЕМЕНЕМ, КАК У КОНТРОЛЬНОГО СПОРТСМЕНА:");
