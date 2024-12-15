27e7f88d50d792a92b179a30148d0214cd631855
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-12-15 16:22:50 +0300

    add solution Multithreading task 2712 - 07/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,4 +1,5 @@
 package com.javarush.task.task27.task2712.ad;
 
 public class AdvertisementManager {
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,10 +1,26 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
 
-    private static AdvertisementStorage instance = new AdvertisementStorage();
+    private final List<Advertisement> videos = new ArrayList<>();
+    private static final AdvertisementStorage instance = new AdvertisementStorage();
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); //3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); // 15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); // 10 min
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 
     public static AdvertisementStorage getInstance() {
