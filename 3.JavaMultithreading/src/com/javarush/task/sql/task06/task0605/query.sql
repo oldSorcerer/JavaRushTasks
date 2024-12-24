0e541d34d5c8e4dd9665822f8439128e960221da
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-12-24 16:58:58 +0300

    add solution SQL and Hibernate task 0605

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0605/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0605/query.sql
@@ -3,7 +3,7 @@ create table users
     user_id    int          not null auto_increment,
     first_name varchar(255) not null,
     last_name  varchar(255) not null,
-    date       date         not null default GETDATE(),
+    date       date         not null default CURRENT_DATE(),
     weight     float(10) null,
     primary key (user_id)
 );
