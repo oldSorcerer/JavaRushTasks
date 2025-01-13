f2d812c30c5e5083457326ae084c311be128bb4d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-01-13 15:32:11 +0300

    add solution Collections task 3708

--- 4.JavaCollections/src/com/javarush/task/task37/task3708/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3708/Solution.java
@@ -1,27 +1,5 @@
-package com.javarush.task.task37.task3708;
 
-import com.javarush.task.task37.task3708.retrievers.CachingProxyRetriever;
-import com.javarush.task.task37.task3708.retrievers.OriginalRetriever;
-import com.javarush.task.task37.task3708.retrievers.Retriever;
-import com.javarush.task.task37.task3708.storage.RemoteStorage;
-import com.javarush.task.task37.task3708.storage.Storage;
-
-/* 
-Кеширующий Proxy
-*/
-
-public class Solution {
-    private static final int n = 10;
-
-    public static void main(String[] args) {
-        Storage storage = new RemoteStorage();
-
-        fillStorage(storage);
-
-        System.out.println("Testing OriginalRetriever: ");
-        testRetriever(new OriginalRetriever(storage));
-
-        System.out.println("\n\n\n\n\n\n\n\n\n\nTesting CachingProxyRetriever: ");
+### \n\n\n\n\n\n\n\nTesting CachingProxyRetriever: ");
         testRetriever(new CachingProxyRetriever(storage));
     }
 
