f84d08307f2bd5bbe7e71d4ed76e0693c37304e7
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-01 20:15:05 +0300

    add solution Multithreading task 2712 - 10/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task27.task2712.ad;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
+
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +12,11 @@ public class AdvertisementManager {
     private final int timeSeconds;
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 
+    private List<Advertisement> bestAdvertisements;
+    private int bestDuration = Integer.MAX_VALUE;
+    private long bestAmount;
+
+
     public AdvertisementManager(int timeSeconds) {
         this.timeSeconds = timeSeconds;
     }
@@ -17,14 +25,78 @@ public class AdvertisementManager {
         if (storage.list().isEmpty()) {
             throw new NoVideoAvailableException("");
         }
-//        List<Advertisement> advertisements = storage.list().stream()
-//                .filter(adv -> adv.getDuration() <= timeSeconds)
-//                .filter(adv -> adv.getAmountPerOneDisplaying() > 0)
-//                .collect(Collectors.toList());
-//
-//        advertisements.stream()
-//                .sorted(Comparator.comparing(Advertisement::getAmountPerOneDisplaying).reversed())
-//                .sorted(null);
+        List<Advertisement> advertisements = storage.list().stream()
+                .filter(adv -> adv.getDuration() <= timeSeconds)
+                .filter(adv -> adv.getAmountPerOneDisplaying() > 0)
+//                .filter(advertisement -> advertisement.getHits() >0)
+                .collect(Collectors.toList());
+
+        makeAllAdvertisements(advertisements);
+
+        bestAdvertisements.sort(
+                Comparator.comparingLong(Advertisement::getAmountPerOneDisplaying)
+                        .thenComparingInt(Advertisement::getDuration)
+                        .reversed());
+
+        for (Advertisement advertisement : bestAdvertisements) {
+            ConsoleHelper.writeMessage(advertisement.toString());
+            advertisement.revalidate();
+        }
 
     }
+
+    private int calcDuration(List<Advertisement> advertisements) {
+        return advertisements.stream().mapToInt(Advertisement::getDuration).sum();
+    }
+
+    private long calcAmount(List<Advertisement> advertisements) {
+        return advertisements.stream().mapToLong(Advertisement::getAmountPerOneDisplaying).sum();
+    }
+
+    private void checkSet(List<Advertisement> advertisements) {
+        int newDuration = calcDuration(advertisements);
+        long newAmount = calcAmount(advertisements);
+
+        if (bestAdvertisements == null && newDuration <= timeSeconds) {
+            bestAdvertisements = advertisements;
+            bestAmount = newAmount;
+            bestDuration = newDuration;
+
+        } else {
+            if (newDuration <= timeSeconds) {
+
+                if (newAmount > bestAmount) {
+                    extracted(advertisements, newAmount, newDuration);
+                }
+
+                if (newAmount == bestAmount) {
+                    if (newDuration > bestDuration) {
+                        extracted(advertisements, newAmount, newDuration);
+                    }
+
+                    if (newDuration == bestDuration && advertisements.size() < bestAdvertisements.size()) {
+                        extracted(advertisements, newAmount, newDuration);
+                    }
+                }
+            }
+        }
+    }
+
+    private void extracted(List<Advertisement> advertisements, long newAmount, int newDuration) {
+        bestAdvertisements = advertisements;
+        bestAmount = newAmount;
+        bestDuration = newDuration;
+    }
+
+    private void makeAllAdvertisements(List<Advertisement> advertisements) {
+        if (!advertisements.isEmpty()) {
+            checkSet(advertisements);
+        }
+        for (int i = 0; i < advertisements.size(); i++) {
+            List<Advertisement> newAdvertisements = new ArrayList<>(advertisements);
+            newAdvertisements.remove(i);
+            makeAllAdvertisements(newAdvertisements);
+        }
+    }
+
 }
