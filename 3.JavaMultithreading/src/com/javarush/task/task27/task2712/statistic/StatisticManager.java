27f9392bf80e1651bf5818b226d6b84f3dc863c4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-01 21:40:21 +0300

    add solution Multithreading task 2712 - 13/23

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,20 +1,34 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.*;
 
 public class StatisticManager {
 
     private static final StatisticManager instance = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
 
     private StatisticManager() {
-
     }
 
-    public void  register(EventDataRow data) {
+    public void register(EventDataRow data) {
 
     }
 
     public static StatisticManager getInstance() {
         return instance;
+    }
+
+    private class StatisticStorage {
+
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        private StatisticStorage() {
+            Arrays.stream(EventType.values())
+                    .forEach(type -> storage.put(type, new ArrayList<>()));
+        }
     }
 }
