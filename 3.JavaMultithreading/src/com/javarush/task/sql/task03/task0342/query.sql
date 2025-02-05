1c1a88cb11af727112905cd35ef8b484b3af73b4
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-05 13:39:58 +0300

    add solution SQL and Hibernate task 0342

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0342/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0342/query.sql
@@ -1,6 +1,4 @@
 select *
 from film_directors
 where full_name like
-      (select concat(first_name, ' ', last_name)
-       from film_directors
-       limit 1);
+      (select concat(first_name, ' ', last_name) from film_directors limit 1);
