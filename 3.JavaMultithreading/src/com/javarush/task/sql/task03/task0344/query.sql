fc13e92f7f14d628bf1f5813819916e06312354e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-05 13:40:04 +0300

    add solution SQL and Hibernate task 0344

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0344/query.sql
@@ -1,7 +1,4 @@
 select title
 from films
-         left join
-     (select last_name, id
-      from film_directors
-      where last_name = 'Spielberg') as director
-     on director.id = films.director_id;
+         left join (select last_name, id from film_directors where last_name = 'Spielberg') as director
+                    on director.id = films.director_id;
