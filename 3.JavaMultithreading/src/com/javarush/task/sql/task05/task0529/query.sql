57220a63f924ebee8784d39695a3693144a19793
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-10 13:44:03 +0300

    add solution SQL and Hibernate task 0529

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
@@ -5,10 +5,10 @@ from author a
 group by a.id;
 
 
---select author.full_name, count(distinct publisher.name) as publisher
---from author
---         left join book on author.id = book.author_id
---         left join publisher on book.publisher_id = publisher.id
---group by author.id;
+select author.full_name, count(distinct publisher.name) as publisher
+from author
+         left join book on author.id = book.author_id
+         left join publisher on book.publisher_id = publisher.id
+group by author.id;
 
 
