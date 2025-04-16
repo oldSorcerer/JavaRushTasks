14146fbf88e49b7cf0bf9edf8b617b4ad27e7d56
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-16 16:57:01 +0300

    add solution Collections task 2613 CashMachine 2/15

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/InterruptOperationException.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task26.task2613.exception;
+
+public class InterruptOperationException extends Exception {
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/NotEnoughMoneyException.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task26.task2613.exception;
+
+public class NotEnoughMoneyException extends Exception {
+}
