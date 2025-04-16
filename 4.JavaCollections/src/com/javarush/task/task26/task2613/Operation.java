82d317a4b0ddb4be1869c7573d5533b51839e062
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-16 16:50:22 +0300

    add solution Collections task 2613 CashMachine 1/15

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/Operation.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/Operation.java
@@ -1,8 +1,5 @@
 package com.javarush.task.task26.task2613;
 
 public enum Operation {
-    INFO,
-    DEPOSIT,
-    WITHDRAW,
-    EXIT
+    INFO, DEPOSIT, WITHDRAW, EXIT
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/InterruptOperationException.java
+++
@@ -1,4 +0,0 @@
-package com.javarush.task.task26.task2613.exception;
-
-public class InterruptOperationException extends Exception {
-}
