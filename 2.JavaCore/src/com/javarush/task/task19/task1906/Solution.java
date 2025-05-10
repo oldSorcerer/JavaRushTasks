9a91d38229a0c63b9f54a3c3a47ae2241eaf1588
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-10 10:40:32 +0300

    add solution Core task 1906

--- 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1906/Solution.java
@@ -1,15 +1,21 @@
 package com.javarush.task.task19.task1906;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /* 
 Четные символы
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main1(String[] args) throws IOException {
         List<Integer> list = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              FileReader fileReader = new FileReader(reader.readLine());
@@ -22,5 +28,18 @@ public class Solution {
                 fileWriter.write(list.get(i));
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String string = Files.readString(Path.of(new Scanner(System.in).nextLine()));
+
+        String collect = IntStream.range(0, string.length())
+                .filter(index -> index % 2 != 0)
+                .mapToObj(intdex -> String.valueOf(string.charAt(intdex)))
+                .collect(Collectors.joining());
+
+
+        Files.writeString(Path.of(new Scanner(System.in).nextLine()), collect);
+
     }
 }
