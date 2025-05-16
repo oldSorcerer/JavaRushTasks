cd13182233d5813936969b61b5f6f0f5d41d52e6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-16 16:39:47 +0300

    add solution SQL and Hibernate task 1003

--- 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1003/init_data.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1003/init_data.sql
@@ -9,9 +9,20 @@ CREATE TABLE employee
 );
 
 insert into employee (id, name, age, smth)
-values (1, 'Hannah', 50, 'director'),
-       (2, 'Mary', 17, 'hr'),
-       (3, 'Joseph', 45, 'architect'),
-       (4, 'Jose', 33, 'engineer'),
-       (5, 'Dave', 26, 'sales'),
-       (6, 'Maria', 31, 'hr');
+values (1, 'Ravshan', 19, 'support'),
+       (2, 'Jackie', 19, 'hr'),
+       (3, 'Mohamed', 23, 'dev'),
+       (4, 'Alice', 18, 'management'),
+       (5, 'Jack', 35, 'development'),
+       (7, 'Mary', 20, 'qa'),
+       (8, 'Anna', 30, 'hr'),
+       (9, 'Kostik', 65, 'dev'),
+       (10, 'Dzhumshut', 21, 'support'),
+       (11, 'John', 19, 'management'),
+       (12, 'Sarah', 25, 'management'),
+       (13, 'Hannah', 50, 'director'),
+       (14, 'Mary', 17, 'hr'),
+       (15, 'Joseph', 45, 'architect'),
+       (16, 'Jose', 33, 'engineer'),
+       (17, 'Dave', 45, 'sales'),
+       (18, 'Maria', 31, 'hr');
