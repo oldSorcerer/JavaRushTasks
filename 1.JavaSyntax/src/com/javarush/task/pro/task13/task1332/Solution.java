ed9e248aad796ef857ad45e4f96e0e9a2e46c37c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-22 16:20:44 +0300

    add solution Syntax task 1332

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1332/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1332/Solution.java
@@ -13,15 +13,9 @@ public class Solution {
 
     public static void main(String[] args) {
         fillMatrixLog();
+        System.out.println(matrixLogs);
         removeBugs();
-    }
-
-    public static void removeBugs() {
-        for (String string : new HashSet<>(matrixLogs)){
-            if (string.startsWith("buahaha")){
-                matrixLogs.remove(string);
-            }
-        }
+        System.out.println(matrixLogs);
     }
 
     public static void fillMatrixLog() {
@@ -29,6 +23,14 @@ public class Solution {
             matrixLogs.add("interestingLog" + i + ".log");
             if ((i > 0) && (i % 5 == 0) && (i < 295)) {
                 matrixLogs.add("buahaha" + i + ".bug");
+            }
+        }
+    }
+
+    public static void removeBugs() {
+        for (String string : new HashSet<>(matrixLogs)){
+            if (string.startsWith("buahaha")){
+                matrixLogs.remove(string);
             }
         }
     }
