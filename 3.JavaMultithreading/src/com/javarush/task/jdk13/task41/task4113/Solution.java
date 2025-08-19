f5cfa39e21e82a6d8131f0e031643fed0b164402
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-08-19 09:02:58 +0300

    add solution University Professional task 4113

--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4113/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4113/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
     public static void main(String[] args) {
         getIterators().stream()
                 .map(i -> i.getClass().getCanonicalName())
-                .sorted()
+//                .sorted()
                 .forEach(System.out::println);
     }
 
@@ -22,6 +22,9 @@ public class Solution {
         list.add(new ArrayList<>(List.of()).iterator());
         list.add(new LinkedList<>(List.of()).iterator());
         list.add(new Vector<>(List.of()).iterator());
+        Stack<Object> stack = new Stack<>();
+        stack.add(new Object());
+        list.add(stack.iterator());
 
         list.add(new PriorityQueue<>(List.of()).iterator());
         list.add(new ArrayDeque<>(List.of()).iterator());
@@ -39,19 +42,25 @@ public class Solution {
         list.add(new TreeMap<>(Map.of()).keySet().iterator());
         list.add(new TreeMap<>(Map.of()).values().iterator());
 
+        list.add(new CopyOnWriteArrayList<>(List.of()).iterator());
+
         list.add(new DelayQueue<>(List.of()).iterator());
-        list.add(new LinkedBlockingQueue<>(List.of()).iterator());
-        list.add(new LinkedBlockingDeque<>(List.of()).iterator());
         list.add(new ConcurrentLinkedQueue<>(List.of()).iterator());
         list.add(new ConcurrentLinkedDeque<>(List.of()).iterator());
-        list.add(new ConcurrentSkipListSet<>(List.of()).iterator());
-        list.add(new CopyOnWriteArrayList<>(List.of()).iterator());
-        list.add(new CopyOnWriteArraySet<>(List.of()).iterator());
+        list.add(new LinkedBlockingQueue<>(List.of()).iterator());
+        list.add(new LinkedBlockingDeque<>(List.of()).iterator());
         list.add(new PriorityBlockingQueue<>(List.of()).iterator());
+
+        list.add(new ConcurrentSkipListSet<>(List.of()).iterator());
+        list.add(new CopyOnWriteArraySet<>(List.of()).iterator());
+
         list.add(new ConcurrentHashMap<>(Map.of()).keySet().iterator());
         list.add(new ConcurrentHashMap<>(Map.of()).values().iterator());
         list.add(new ConcurrentSkipListMap<>(Map.of()).keySet().iterator());
         list.add(new ConcurrentSkipListMap<>(Map.of()).values().iterator());
+
+        list.add(new WeakHashMap<>(Map.of()).keySet().iterator());
+        list.add(new WeakHashMap<>(Map.of()).values().iterator());
 
         return list;
     }
